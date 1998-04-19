a9146ce46f1879d648f2700651b669ffa0405bc4
unknown <freeciv@freeciv.org>
1998-04-19 08:15:48 +0000

    Fixed bug in timer.  The turn wasn't ending when it was supposed to.
    [[originally from CVS; cvs2svn created svn r54]]

--- server/sernet.c
+++ server/sernet.c
@@ -78,29 +78,22 @@ int sniff_packets(void)
   int i;
   int max_desc;
   fd_set readfs;
-  time_t time_at_tick_start, time_diff;
   struct timeval tv;
-  static int sec_counter;
+  static time_t time_at_turn_end;
   static int year;
   
   if(year!=game.year) {
+    time_at_turn_end = time(NULL) + game.timeout;
     year=game.year;
-    sec_counter=0;
   }
   
-  time_at_tick_start=time(NULL);
-  time_diff=0;
-  
-  /* sec_counter=0;*/
-    
   while(1) {
     if(force_end_of_sniff) {
       force_end_of_sniff=0;
       return 2;
     }
     
-    tv.tv_sec=1;
-    tv.tv_usec= 0;
+    tv.tv_sec=1; tv.tv_usec=0;
     
     MY_FD_ZERO(&readfs);
     FD_SET(0, &readfs);	
@@ -115,9 +108,8 @@ int sniff_packets(void)
     }
     
     if(select(max_desc+1, &readfs, NULL, NULL, &tv)==0) { /* timeout */
-      sec_counter++;
       send_server_info_to_metaserver(0);
-      if(game.timeout!=0 && sec_counter>game.timeout) {
+      if(game.timeout && time(NULL)>time_at_turn_end) {
 	return 0;
       }
       continue;
@@ -159,6 +151,7 @@ int sniff_packets(void)
     }
     break;
   }
+  if(game.timeout && time(NULL)>time_at_turn_end) return 0;
   return 1;
 }
   
