137ddf2159d261e872ec44327a141afe79972aca
James Blackwell <innocent@mercury.merconline.com>
1998-05-25 19:32:52 +0000

    Autoconf added, thanks to Faulk Hauffner. Imake is still there for now.                                  -James
    [[originally from CVS; cvs2svn created svn r151]]

---
+++ acconfig.h
@@ -0,0 +1,10 @@
+/*
+ *   Defaults for configuration settings
+ *   by Falk Hüffner <falk.hueffner@student.uni-tuebingen.de>
+ */
+@TOP@
+/* Define to the name of the distribution.  */
+#undef PACKAGE
+
+/* Define to the version of the distribution.  */
+#undef VERSION
