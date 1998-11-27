e3d3918afe70451ca8896dfac03707fd9a19d500
David Pfitzner <dwp@mso.anu.edu.au>
1998-11-27 12:02:00 +0000

    Set default diplcost to 0.  Patch submitted by Mirar <mirar@idonex.se>.
    [[originally from CVS; cvs2svn created svn r846]]

--- common/map.h
+++ common/map.h
@@ -258,7 +258,7 @@ int map_build_mine_time(int x, int y);
 #define GAME_MIN_RESEARCHLEVEL       4
 #define GAME_MAX_RESEARCHLEVEL       100
 
-#define GAME_DEFAULT_DIPLCOST        100
+#define GAME_DEFAULT_DIPLCOST        0
 #define GAME_MIN_DIPLCOST            0
 #define GAME_MAX_DIPLCOST            100
 
