97c0aec2010fe37e3820fdff0fc24871cc19136e
David Pfitzner <dwp@mso.anu.edu.au>
1998-12-16 11:15:54 +0000

    Attempt to allow ':' character in chat line messages more flexibly, while still allowing messages directed to one player, and noticing some player name typos. Based in part on a patch by Reinier Post <rp@win.tue.nl>
    [[originally from CVS; cvs2svn created svn r883]]

--- server/handchat.c
+++ server/handchat.c
@@ -40,13 +40,31 @@ void handle_chat_msg(struct player *pplayer,
       *cp='\0';
       break;
     }
+
+  /* Want to allow private messages with "player_name: message",
+     including unambiguously abbreviated player name, but also want
+     to allow sensible use of ':' within messages, and _also_ want to
+     notice intended private messages with (eg) mis-spelt player name.
+
+     Approach:
+     
+     If there is no ':', or ':' is first on line,
+          message is global (send to all players)
+     else if the part before ':' matches one player unambiguously,
+          send to that one player
+     else if it matches several players ambiguously, complain,
+     else if some heuristics apply (a space anywhere before first ':')
+          then treat as global message,
+     else complain (might be a typo-ed intended private message)
+  */
   
   cp=strchr(packet->message, ':');
-  
-  if (cp && *(cp-1)!=' ') {
+
+  if (cp != NULL && (cp != &packet->message[0])) {
     struct player *pdest=0;
     int nlen, nmatches=0;
     char name[MAX_LENGTH_NAME];
+    char *cpblank;
 
     nlen=MIN(MAX_LENGTH_NAME-1, cp-packet->message);
     strncpy(name, packet->message, nlen);
@@ -72,20 +90,26 @@ void handle_chat_msg(struct player *pplayer,
       sprintf(genmsg.message, "*%s* %s",
 	      pplayer->name, cp+1+(*(cp+1)==' '));
       send_packet_generic_message(pdest->conn, PACKET_CHAT_MSG, &genmsg);
+      return;
     }
-    else if(nmatches>=2) {
+    if(nmatches>=2) {
       sprintf(genmsg.message, "Game: %s is an ambiguous name-prefix", name);
       send_packet_generic_message(pplayer->conn, PACKET_CHAT_MSG, &genmsg);
+      return;
     }
-    else {
+    /* Didn't match; check heuristics to see if this is likely
+     * to be a global message
+     */
+    cpblank=strchr(packet->message, ' ');
+    if (!cpblank || (cp < cpblank)) {
       sprintf(genmsg.message, "Game: there's no player by the name %s", name);
       send_packet_generic_message(pplayer->conn, PACKET_CHAT_MSG, &genmsg);
+      return;
     }
   }
-  else {
-    sprintf(genmsg.message, "<%s> %s", pplayer->name, packet->message);
-    for(i=0; i<game.nplayers; i++)
-      send_packet_generic_message(game.players[i].conn, PACKET_CHAT_MSG, 
-				  &genmsg);
-  }
+  /* global message: */
+  sprintf(genmsg.message, "<%s> %s", pplayer->name, packet->message);
+  for(i=0; i<game.nplayers; i++)
+    send_packet_generic_message(game.players[i].conn, PACKET_CHAT_MSG, 
+				&genmsg);
 }
