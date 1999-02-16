fadf97eb671877bece5bb137136e401a574904f1
Trent Piepho <xyzzy@u.washington.edu>
1999-02-16 00:28:02 +0000

    Clean up a gcc warning about an assignment to a const string.
    [[originally from CVS; cvs2svn created svn r1039]]

--- common/shared.c
+++ common/shared.c
@@ -359,12 +359,14 @@ char *datafilename(char *filename)
   static const char* datadir=0;
   static char  realfile[512];
   if(!datadir) {
-    if((datadir = getenv("FREECIV_DATADIR"))) {
+    char *tmp;
+    if((tmp = getenv("FREECIV_DATADIR"))) {
       int i;
       for(i=strlen(datadir)-1; i>=0 && isspace((int)datadir[i]); i--)
-	datadir[i] = '\0';
-      if(datadir[i] == '/')
-	datadir[i] = '\0';
+	tmp[i] = '\0';
+      if(tmp[i] == '/')
+	tmp[i] = '\0';
+      datadir = tmp;
     } else {
       datadir = FREECIV_DATADIR; /* correct if not 'data' is the default */
     };
