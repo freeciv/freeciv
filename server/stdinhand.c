00b36d35424433384cef7c5d569c308f3c71cd29
David Pfitzner <dwp@mso.anu.edu.au>
1999-04-11 02:38:18 +0000

    Bounds checking in handle_stdin_input() wasn't quite militant enough, causing stack-clobbering segfaults with too long commands (parameters were immune, though). Patch submitted by Juho Snellman <jsnell@iki.fi>.
    [[originally from CVS; cvs2svn created svn r1184]]

--- server/stdinhand.c
+++ server/stdinhand.c
@@ -1006,7 +1006,8 @@ void handle_stdin_input(char *str)
   
   for(cptr_s=str; *cptr_s && !isalnum(*cptr_s); cptr_s++);
 
-  for(cptr_d=command; *cptr_s && isalnum(*cptr_s); cptr_s++, cptr_d++)
+  for(cptr_d=command; *cptr_s && isalnum(*cptr_s) &&
+      cptr_d < command+sizeof(command)-1; cptr_s++, cptr_d++)
     *cptr_d=*cptr_s;
   *cptr_d='\0';
 
