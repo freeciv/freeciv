45b0431ca2731ebc6e24ed697ee02dc3890488aa
David Pfitzner <dwp@mso.anu.edu.au>
1999-07-14 11:08:11 +0000

    Removed some unncessary time includes.
    [[originally from CVS; cvs2svn created svn r1430]]

--- server/mapgen.c
+++ server/mapgen.c
@@ -14,7 +14,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-
 #include <time.h>
 
 #include "game.h"
--- server/meta.c
+++ server/meta.c
@@ -16,7 +16,6 @@
  */
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 
 #include <unistd.h>
 
