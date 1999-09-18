550efc90626c207a67f802b636cdd40d3d96ebe4
David Pfitzner <dwp@mso.anu.edu.au>
1999-09-18 02:49:36 +0000

    Put test in fcintl.h to ensure that files including it pre-include config.h.
    [[originally from CVS; cvs2svn created svn r1632]]

--- common/fcintl.h
+++ common/fcintl.h
@@ -13,6 +13,12 @@
 #ifndef FC__FCINTL_H
 #define FC__FCINTL_H
 
+#ifdef HAVE_CONFIG_H
+#ifndef FC_CONFIG_H		/* this should be defined in config.h */
+#error Files including fcintl.h should also include config.h directly
+#endif
+#endif
+
 #ifdef ENABLE_NLS
 #include <libintl.h>
 
