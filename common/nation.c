b5e877fbdde261f34d22002818248be682591429
David Pfitzner <dwp@mso.anu.edu.au>
1999-09-18 03:44:01 +0000

    Added bounds checking to nation accessor functions; in particular get_nation_name() is sometimes called by client before nations are setup or with bad nation id (now returns "" in such cases). Fixes bug reported by Tony Stuckey.
    [[originally from CVS; cvs2svn created svn r1636]]

--- common/nation.c
+++ common/nation.c
@@ -17,6 +17,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "game.h"
 #include "government.h"
@@ -28,6 +29,26 @@
 #include "nation.h"
 
 struct nation_type *nations = NULL;
+
+/***************************************************************
+  Returns 1 if nid is a valid nation id, else 0.
+  If returning 0, prints log message with given loglevel
+  quoting given func name, explaining problem.
+***************************************************************/
+static int bounds_check_nation_id(Nation_Type_id nid, int loglevel,
+				  const char *func_name)
+{
+  if (game.nation_count==0) {
+    freelog(loglevel, "%s before nations setup", func_name);
+    return 0;
+  }
+  if (nid < 0 || nid >= game.nation_count) {
+    freelog(loglevel, "Bad nation id %d (count %d) in %s",
+	    nid, game.nation_count, func_name);
+    return 0;
+  }
+  return 1;
+}
 
 /***************************************************************
 ...
@@ -48,6 +69,9 @@ Returns name of the nation
 ***************************************************************/
 char *get_nation_name(Nation_Type_id nation)
 {
+  if (!bounds_check_nation_id(nation, LOG_VERBOSE, "get_nation_name")) {
+    return "";
+  }
   return nations[nation].name;
 }
 
@@ -57,6 +81,9 @@ sets dim to number of leaders.
 ***************************************************************/
 char **get_nation_leader_names(Nation_Type_id nation, int *dim)
 {
+  if (!bounds_check_nation_id(nation, LOG_FATAL, "get_nation_leader_names")) {
+    exit(1);
+  }
   *dim = nations[nation].leader_count;
   return nations[nation].leader_name;
 }
@@ -68,6 +95,10 @@ return 0 (meaning male).
 int get_nation_leader_sex(Nation_Type_id nation, char *name)
 {
   int i;
+  
+  if (!bounds_check_nation_id(nation, LOG_NORMAL, "get_nation_leader_sex")) {
+    return 0;
+  }
   for( i=0; i < nations[nation].leader_count; i++ ) {
     if( !strcmp(nations[nation].leader_name[i],name) )
       break;
@@ -84,6 +115,10 @@ checks if given leader name exist for given nation.
 int check_nation_leader_name(Nation_Type_id nation, char *name)
 {
   int i, found=0;
+  
+  if (!bounds_check_nation_id(nation, LOG_NORMAL, "check_nation_leader_name")) {
+    return 1;			/* ? */
+  }
   for( i=0; i<nations[nation].leader_count; i++) {
     if( !strcmp(name, nations[nation].leader_name[i]) )
       found = 1;
@@ -96,6 +131,9 @@ Returns plural name of the nation.
 ***************************************************************/
 char *get_nation_name_plural(Nation_Type_id nation)
 {
+  if (!bounds_check_nation_id(nation, LOG_NORMAL, "get_nation_name_plural")) {
+    return "";
+  }
   return nations[nation].name_plural;
 }
 
@@ -104,11 +142,18 @@ Returns pointer to a nation
 ***************************************************************/
 struct nation_type *get_nation_by_plr(struct player *plr)
 {
+  assert(plr);
+  if (!bounds_check_nation_id(plr->nation, LOG_FATAL, "get_nation_by_plr")) {
+    exit(1);
+  }
   return &nations[plr->nation];
 }
 
 struct nation_type *get_nation_by_idx(Nation_Type_id nation)
 {
+  if (!bounds_check_nation_id(nation, LOG_FATAL, "get_nation_by_idx")) {
+    exit(1);
+  }
   return &nations[nation];
 }
 
