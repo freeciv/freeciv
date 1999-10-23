3b268e17a1def8b3753f8d4ff3b7f9773870156e
Jeff Mallatt <jjm@codewell.com>
1999-10-23 14:20:02 +0000

    Change version from 1.9.0 to 1.9.1-devel.
    [[originally from CVS; cvs2svn created svn r1773]]

--- client/gui-xaw/Freeciv.h
+++ client/gui-xaw/Freeciv.h
@@ -5,7 +5,7 @@
 "Freeciv*server:                              localhost",
 "Freeciv*metaserver:                          http://meta.freeciv.org/metaserver/",
 "Freeciv*port:                                5555",
-"Freeciv*version:                             1.9.0",
+"Freeciv*version:                             1.9.1-devel",
 "Freeciv*logLevel:                            1",
 "Freeciv*Command.foreground: White",
 "Freeciv*Command.background: Blue",
