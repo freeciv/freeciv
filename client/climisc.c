c02ba70bbf2d44740a0f0088caf2763aa29180c7
David Pfitzner <dwp@mso.anu.edu.au>
1999-11-05 10:48:33 +0000

    Initialize packet.name in client_change_all(); otherwise server sometimes reports, eg: "1: received bad string in packet (type 18, len 53)" when do "Change All" from City Report. Fixes bug reported by Mikolaj Konarski <mikon@mimuw.edu.pl>.
    [[originally from CVS; cvs2svn created svn r1789]]

--- client/climisc.c
+++ client/climisc.c
@@ -253,6 +253,7 @@ void client_change_all(int x, int y)
 	packet.city_id = pcity->id;
 	packet.build_id = to_id;
 	packet.is_build_id_unit_id = to_is_unit;
+	packet.name[0] = '\0';
 	send_packet_city_request (&aconnection, &packet,
 				  PACKET_CITY_CHANGE);
       }
