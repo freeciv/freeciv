ee6e120ba2cb222aaac0edf4be1f00b3289d15ac
Jeff Mallatt <jjm@codewell.com>
2000-02-21 21:43:19 +0000

    Final source update for Version 1.10.0 release.
    [[originally from CVS; cvs2svn created svn r2076]]

--- common/version.h
+++ common/version.h
@@ -25,19 +25,19 @@
 #define MAJOR_VERSION		1
 #endif
 #ifndef MINOR_VERSION
-#define MINOR_VERSION		9
+#define MINOR_VERSION		10
 #endif
 #ifndef PATCH_VERSION
-#define PATCH_VERSION		7
+#define PATCH_VERSION		0
 #endif
 #ifndef VERSION_LABEL
-#define VERSION_LABEL		"-devel"
+#define VERSION_LABEL		""
 #endif
 #ifndef IS_DEVEL_VERSION
-#define IS_DEVEL_VERSION	1
+#define IS_DEVEL_VERSION	0
 #endif
 #ifndef IS_BETA_VERSION
-#define IS_BETA_VERSION		1
+#define IS_BETA_VERSION		0
 #endif
 
 /* This is only used if IS_BETA_VERSION is true. */
