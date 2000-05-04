47e1c1e632f3aca892c75f27f61bda2ddd3e7931
Nicolas Brunel <brunel@ham.esil.univ-mrs.fr>
2000-05-04 22:55:25 +0000

    warmap could be initialized in two places : init_warmap and init_gotomap. Now, only init_warmap can init warmap. And, init_gotmap calls init_warmap to initialize the warmap.
    [[originally from CVS; cvs2svn created svn r2277]]

--- server/gotohand.c
+++ server/gotohand.c
@@ -470,21 +470,13 @@ static void init_gotomap(int orig_x, int orig_y)
 {
   int x;
 
-  if (!warmap.cost[0]) {
-    for (x = 0; x < map.xsize; x++) {
-      warmap.cost[x]=fc_malloc(map.ysize*sizeof(unsigned char));
-      warmap.seacost[x]=fc_malloc(map.ysize*sizeof(unsigned char));
-      warmap.vector[x]=fc_malloc(map.ysize*sizeof(unsigned char));
-    }
-  }
+  init_warmap(orig_x,orig_y,LAND_MOVING);
+  init_warmap(orig_x,orig_y,SEA_MOVING);
 
   for (x = 0; x < map.xsize; x++) {
-    memset(warmap.cost[x],255,map.ysize*sizeof(unsigned char));
-    memset(warmap.seacost[x],255,map.ysize*sizeof(unsigned char));
     memset(warmap.vector[x],0,map.ysize*sizeof(unsigned char));
   }
-  warmap.cost[orig_x][orig_y] = 0;
-  warmap.seacost[orig_x][orig_y] = 0;
+
   return;
 } 
 
