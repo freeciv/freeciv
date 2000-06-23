302d84a76a97b943036d9794d082617d75616d24
David Pfitzner <dwp@mso.anu.edu.au>
2000-06-23 02:22:37 +0000

    Fix erroneous assert for result==0 in m_pre_description().
    [[originally from CVS; cvs2svn created svn r2465]]

--- common/shared.c
+++ common/shared.c
@@ -641,7 +641,7 @@ const char *m_pre_description(enum m_pre_result result)
     N_("too long"),
     N_("non-match")
   };
-  assert(result > 0 && result < sizeof(descriptions)/sizeof(descriptions[0]));
+  assert(result >= 0 && result < sizeof(descriptions)/sizeof(descriptions[0]));
   return descriptions[result];
 }
 
