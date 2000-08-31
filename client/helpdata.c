6d1520efee4580ed15fce5db11c201731c07ce41
David Pfitzner <dwp@mso.anu.edu.au>
2000-08-31 06:59:17 +0000

    Remove some code which has been unused for a long time. (Includes some never-used gettext-marked strings.)
    [[originally from CVS; cvs2svn created svn r2882]]

--- client/helpdata.c
+++ client/helpdata.c
@@ -857,39 +857,3 @@ char *helptext_unit_upkeep_str(int i)
   }
   return buf;
 }
-
-#if 0
-/****************************************************************
-  Similar to above, with eg: "1s/1h"
-*****************************************************************/
-static char *helptext_unit_upkeep_str_short(int i)
-{
-  static char buf[32];
-  struct unit_type *utype = get_unit_type(i);
-
-  if (utype->shield_cost || utype->food_cost
-      || utype->gold_cost || utype->happy_cost) {
-    int any = 0;
-    buf[0] = '\0';
-    if (utype->shield_cost) {
-      sprintf(buf+strlen(buf), /*s=shields*/ _("%s%ds"),
-	      (any++ ? "/" : ""), utype->shield_cost);
-    }
-    if (utype->food_cost) {
-      sprintf(buf+strlen(buf), /*f=food*/ _("%s%df"),
-	      (any++ ? "/" : ""), utype->food_cost);
-    }
-    if (utype->happy_cost) {
-      sprintf(buf+strlen(buf), /*h=(un)happy*/ _("%s%dh"),
-	      (any++ ? "/" : ""), utype->happy_cost);
-    }
-    if (utype->gold_cost) {
-      sprintf(buf+strlen(buf), /*g=gold*/ _("%s%dg"),
-	      (any++ ? "/" : ""), utype->gold_cost);
-    }
-  } else {
-    sprintf(buf, "%d", 0);
-  }
-  return buf;
-}
-#endif
