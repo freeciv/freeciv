a96da39516e2ab1aca08d857ed5caad2874f98bc
Thue Janus Kristensen <thue@diku.dk>
2001-01-24 19:40:02 +0000

    Run create_lsend.pl to update the lsend functions to take constant packet arguments.
    [[originally from CVS; cvs2svn created svn r3114]]

--- common/packets_lsend.c
+++ common/packets_lsend.c
@@ -18,7 +18,7 @@
 #include "packets.h"
 
 void lsend_packet_diplomacy_info(struct conn_list *dest, enum packet_type pt,
-			       struct packet_diplomacy_info *packet)
+			       const struct packet_diplomacy_info *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_diplomacy_info(pconn, pt, packet);
@@ -26,7 +26,7 @@ void lsend_packet_diplomacy_info(struct conn_list *dest, enum packet_type pt,
 }
 
 void lsend_packet_diplomat_action(struct conn_list *dest, 
-				struct packet_diplomat_action *packet)
+				const struct packet_diplomat_action *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_diplomat_action(pconn, packet);
@@ -34,7 +34,7 @@ void lsend_packet_diplomat_action(struct conn_list *dest,
 }
 
 void lsend_packet_nuke_tile(struct conn_list *dest, 
-			  struct packet_nuke_tile *packet)
+			  const struct packet_nuke_tile *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_nuke_tile(pconn, packet);
@@ -42,7 +42,7 @@ void lsend_packet_nuke_tile(struct conn_list *dest,
 }
 
 void lsend_packet_unit_combat(struct conn_list *dest, 
-			    struct packet_unit_combat *packet)
+			    const struct packet_unit_combat *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_unit_combat(pconn, packet);
@@ -50,7 +50,7 @@ void lsend_packet_unit_combat(struct conn_list *dest,
 }
 
 void lsend_packet_unit_connect(struct conn_list *dest, 
-			    struct packet_unit_connect *packet)
+			     const struct packet_unit_connect *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_unit_connect(pconn, packet);
@@ -58,7 +58,7 @@ void lsend_packet_unit_connect(struct conn_list *dest,
 }
 
 void lsend_packet_tile_info(struct conn_list *dest, 
-			 struct packet_tile_info *pinfo)
+			  const struct packet_tile_info *pinfo)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_tile_info(pconn, pinfo);
@@ -66,7 +66,7 @@ void lsend_packet_tile_info(struct conn_list *dest,
 }
 
 void lsend_packet_map_info(struct conn_list *dest, 
-			  struct packet_map_info *pinfo)
+			 const struct packet_map_info *pinfo)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_map_info(pconn, pinfo);
@@ -74,7 +74,7 @@ void lsend_packet_map_info(struct conn_list *dest,
 }
 
 void lsend_packet_game_info(struct conn_list *dest, 
-			  struct packet_game_info *pinfo)
+			  const struct packet_game_info *pinfo)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_game_info(pconn, pinfo);
@@ -82,7 +82,7 @@ void lsend_packet_game_info(struct conn_list *dest,
 }
 
 void lsend_packet_player_info(struct conn_list *dest, 
-			    struct packet_player_info *pinfo)
+			    const struct packet_player_info *pinfo)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_player_info(pconn, pinfo);
@@ -90,7 +90,7 @@ void lsend_packet_player_info(struct conn_list *dest,
 }
 
 void lsend_packet_conn_info(struct conn_list *dest,
-			  struct packet_conn_info *pinfo)
+			  const struct packet_conn_info *pinfo)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_conn_info(pconn, pinfo);
@@ -98,7 +98,7 @@ void lsend_packet_conn_info(struct conn_list *dest,
 }
 
 void lsend_packet_new_year(struct conn_list *dest, 
-			 struct packet_new_year *request)
+			 const struct packet_new_year *request)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_new_year(pconn, request);
@@ -106,7 +106,7 @@ void lsend_packet_new_year(struct conn_list *dest,
 }
 
 void lsend_packet_move_unit(struct conn_list *dest, 
-			  struct packet_move_unit *request)
+			  const struct packet_move_unit *request)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_move_unit(pconn, request);
@@ -114,7 +114,7 @@ void lsend_packet_move_unit(struct conn_list *dest,
 }
 
 void lsend_packet_unit_info(struct conn_list *dest,
-			  struct packet_unit_info *req)
+			  const struct packet_unit_info *req)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_unit_info(pconn, req);
@@ -122,7 +122,7 @@ void lsend_packet_unit_info(struct conn_list *dest,
 }
 
 void lsend_packet_req_join_game(struct conn_list *dest, 
-			      struct packet_req_join_game *request)
+			      const struct packet_req_join_game *request)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_req_join_game(pconn, request);
@@ -130,7 +130,7 @@ void lsend_packet_req_join_game(struct conn_list *dest,
 }
 
 void lsend_packet_join_game_reply(struct conn_list *dest, 
-			       struct packet_join_game_reply *reply)
+			        const struct packet_join_game_reply *reply)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_join_game_reply(pconn, reply);
@@ -138,7 +138,7 @@ void lsend_packet_join_game_reply(struct conn_list *dest,
 }
 
 void lsend_packet_alloc_nation(struct conn_list *dest, 
-			   struct packet_alloc_nation *packet)
+			     const struct packet_alloc_nation *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_alloc_nation(pconn, packet);
@@ -146,7 +146,7 @@ void lsend_packet_alloc_nation(struct conn_list *dest,
 }
 
 void lsend_packet_generic_message(struct conn_list *dest, int type,
-				struct packet_generic_message *message)
+				const struct packet_generic_message *message)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_generic_message(pconn, type, message);
@@ -154,21 +154,23 @@ void lsend_packet_generic_message(struct conn_list *dest, int type,
 }
 
 void lsend_packet_generic_integer(struct conn_list *dest, int type,
-				struct packet_generic_integer *packet)
+				const struct packet_generic_integer *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_generic_integer(pconn, type, packet);
   conn_list_iterate_end;
 }
 
-void lsend_packet_city_info(struct conn_list *dest,struct packet_city_info *req)
+void lsend_packet_city_info(struct conn_list *dest,
+                          const struct packet_city_info *req)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_city_info(pconn, req);
   conn_list_iterate_end;
 }
 
-void lsend_packet_short_city(struct conn_list *dest,struct packet_short_city *req)
+void lsend_packet_short_city(struct conn_list *dest,
+                           const struct packet_short_city *req)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_short_city(pconn, req);
@@ -176,7 +178,7 @@ void lsend_packet_short_city(struct conn_list *dest,struct packet_short_city *re
 }
 
 void lsend_packet_city_request(struct conn_list *dest, 
-			     struct packet_city_request *packet,
+			     const struct packet_city_request *packet,
 			     enum packet_type req_type)
 {
   conn_list_iterate(*dest, pconn)
@@ -185,7 +187,7 @@ void lsend_packet_city_request(struct conn_list *dest,
 }
 
 void lsend_packet_player_request(struct conn_list *dest, 
-			       struct packet_player_request *packet,
+			       const struct packet_player_request *packet,
 			       enum packet_type req_type)
 {
   conn_list_iterate(*dest, pconn)
@@ -194,7 +196,7 @@ void lsend_packet_player_request(struct conn_list *dest,
 }
 
 void lsend_packet_unit_request(struct conn_list *dest, 
-			     struct packet_unit_request *packet,
+			     const struct packet_unit_request *packet,
 			     enum packet_type req_type)
 {
   conn_list_iterate(*dest, pconn)
@@ -217,7 +219,7 @@ void lsend_packet_unittype_info(struct conn_list *dest, int type, int action)
 }
 
 void lsend_packet_ruleset_control(struct conn_list *dest, 
-				struct packet_ruleset_control *pinfo)
+				const struct packet_ruleset_control *pinfo)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_control(pconn, pinfo);
@@ -225,7 +227,7 @@ void lsend_packet_ruleset_control(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_unit(struct conn_list *dest,
-			     struct packet_ruleset_unit *packet)
+			     const struct packet_ruleset_unit *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_unit(pconn, packet);
@@ -233,7 +235,7 @@ void lsend_packet_ruleset_unit(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_tech(struct conn_list *dest,
-			     struct packet_ruleset_tech *packet)
+			     const struct packet_ruleset_tech *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_tech(pconn, packet);
@@ -241,7 +243,7 @@ void lsend_packet_ruleset_tech(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_building(struct conn_list *dest,
-			     struct packet_ruleset_building *packet)
+			     const struct packet_ruleset_building *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_building(pconn, packet);
@@ -249,7 +251,7 @@ void lsend_packet_ruleset_building(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_terrain(struct conn_list *dest,
-			     struct packet_ruleset_terrain *packet)
+			     const struct packet_ruleset_terrain *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_terrain(pconn, packet);
@@ -257,7 +259,7 @@ void lsend_packet_ruleset_terrain(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_terrain_control(struct conn_list *dest,
-					struct terrain_misc *packet)
+					const struct terrain_misc *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_terrain_control(pconn, packet);
@@ -265,7 +267,7 @@ void lsend_packet_ruleset_terrain_control(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_government(struct conn_list *dest,
-				struct packet_ruleset_government *packet)
+			       const struct packet_ruleset_government *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_government(pconn, packet);
@@ -273,7 +275,7 @@ void lsend_packet_ruleset_government(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_government_ruler_title(struct conn_list *dest,
-				struct packet_ruleset_government_ruler_title *packet)
+		   const struct packet_ruleset_government_ruler_title *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_government_ruler_title(pconn, packet);
@@ -281,7 +283,7 @@ void lsend_packet_ruleset_government_ruler_title(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_nation(struct conn_list *dest,
-			       struct packet_ruleset_nation *packet)
+			       const struct packet_ruleset_nation *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_nation(pconn, packet);
@@ -289,7 +291,7 @@ void lsend_packet_ruleset_nation(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_city(struct conn_list *dest,
-			     struct packet_ruleset_city *packet)
+			     const struct packet_ruleset_city *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_city(pconn, packet);
@@ -297,7 +299,7 @@ void lsend_packet_ruleset_city(struct conn_list *dest,
 }
 
 void lsend_packet_ruleset_game(struct conn_list *dest,
-                             struct packet_ruleset_game *packet)
+                             const struct packet_ruleset_game *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_ruleset_game(pconn, packet);
@@ -305,7 +307,7 @@ void lsend_packet_ruleset_game(struct conn_list *dest,
 }
 
 void lsend_packet_generic_values(struct conn_list *dest, int type,
-			       struct packet_generic_values *req)
+			       const struct packet_generic_values *req)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_generic_values(pconn, type, req);
@@ -313,7 +315,7 @@ void lsend_packet_generic_values(struct conn_list *dest, int type,
 }
 
 void lsend_packet_spaceship_info(struct conn_list *dest,
-			       struct packet_spaceship_info *packet)
+			       const struct packet_spaceship_info *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_spaceship_info(pconn, packet);
@@ -321,7 +323,7 @@ void lsend_packet_spaceship_info(struct conn_list *dest,
 }
 
 void lsend_packet_spaceship_action(struct conn_list *dest,
-				 struct packet_spaceship_action *packet)
+				 const struct packet_spaceship_action *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_spaceship_action(pconn, packet);
@@ -329,7 +331,7 @@ void lsend_packet_spaceship_action(struct conn_list *dest,
 }
 
 void lsend_packet_city_name_suggestion(struct conn_list *dest,
-				     struct packet_city_name_suggestion *packet)
+				const struct packet_city_name_suggestion *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_city_name_suggestion(pconn, packet);
@@ -337,14 +339,15 @@ void lsend_packet_city_name_suggestion(struct conn_list *dest,
 }
 
 void lsend_packet_sabotage_list(struct conn_list *dest,
-			      struct packet_sabotage_list *packet)
+			      const struct packet_sabotage_list *packet)
 {
   conn_list_iterate(*dest, pconn)
     send_packet_sabotage_list(pconn, packet);
   conn_list_iterate_end;
 }
 
-void lsend_packet_goto_route(struct conn_list *dest, struct packet_goto_route *packet,
+void lsend_packet_goto_route(struct conn_list *dest,
+                           const struct packet_goto_route *packet,
 			   enum goto_route_type type)
 {
   conn_list_iterate(*dest, pconn)
--- common/packets_lsend.h
+++ common/packets_lsend.h
@@ -17,85 +17,88 @@
 **********************************************************************/
 
 void lsend_packet_diplomacy_info(struct conn_list *dest, enum packet_type pt,
-			       struct packet_diplomacy_info *packet);
+			       const struct packet_diplomacy_info *packet);
 void lsend_packet_diplomat_action(struct conn_list *dest, 
-				struct packet_diplomat_action *packet);
+				const struct packet_diplomat_action *packet);
 void lsend_packet_nuke_tile(struct conn_list *dest, 
-			  struct packet_nuke_tile *packet);
+			  const struct packet_nuke_tile *packet);
 void lsend_packet_unit_combat(struct conn_list *dest, 
-			    struct packet_unit_combat *packet);
+			    const struct packet_unit_combat *packet);
 void lsend_packet_unit_connect(struct conn_list *dest, 
-			    struct packet_unit_connect *packet);
+			     const struct packet_unit_connect *packet);
 void lsend_packet_tile_info(struct conn_list *dest, 
-			 struct packet_tile_info *pinfo);
+			  const struct packet_tile_info *pinfo);
 void lsend_packet_map_info(struct conn_list *dest, 
-			  struct packet_map_info *pinfo);
+			 const struct packet_map_info *pinfo);
 void lsend_packet_game_info(struct conn_list *dest, 
-			  struct packet_game_info *pinfo);
+			  const struct packet_game_info *pinfo);
 void lsend_packet_player_info(struct conn_list *dest, 
-			    struct packet_player_info *pinfo);
+			    const struct packet_player_info *pinfo);
 void lsend_packet_conn_info(struct conn_list *dest,
-			  struct packet_conn_info *pinfo);
+			  const struct packet_conn_info *pinfo);
 void lsend_packet_new_year(struct conn_list *dest, 
-			 struct packet_new_year *request);
+			 const struct packet_new_year *request);
 void lsend_packet_move_unit(struct conn_list *dest, 
-			  struct packet_move_unit *request);
+			  const struct packet_move_unit *request);
 void lsend_packet_unit_info(struct conn_list *dest,
-			  struct packet_unit_info *req);
+			  const struct packet_unit_info *req);
 void lsend_packet_req_join_game(struct conn_list *dest, 
-			      struct packet_req_join_game *request);
+			      const struct packet_req_join_game *request);
 void lsend_packet_join_game_reply(struct conn_list *dest, 
-			       struct packet_join_game_reply *reply);
+			        const struct packet_join_game_reply *reply);
 void lsend_packet_alloc_nation(struct conn_list *dest, 
-			   struct packet_alloc_nation *packet);
+			     const struct packet_alloc_nation *packet);
 void lsend_packet_generic_message(struct conn_list *dest, int type,
-				struct packet_generic_message *message);
+				const struct packet_generic_message *message);
 void lsend_packet_generic_integer(struct conn_list *dest, int type,
-				struct packet_generic_integer *packet);
-void lsend_packet_city_info(struct conn_list *dest,struct packet_city_info *req);
-void lsend_packet_short_city(struct conn_list *dest,struct packet_short_city *req);
+				const struct packet_generic_integer *packet);
+void lsend_packet_city_info(struct conn_list *dest,
+                          const struct packet_city_info *req);
+void lsend_packet_short_city(struct conn_list *dest,
+                           const struct packet_short_city *req);
 void lsend_packet_city_request(struct conn_list *dest, 
-			     struct packet_city_request *packet,
+			     const struct packet_city_request *packet,
 			     enum packet_type req_type);
 void lsend_packet_player_request(struct conn_list *dest, 
-			       struct packet_player_request *packet,
+			       const struct packet_player_request *packet,
 			       enum packet_type req_type);
 void lsend_packet_unit_request(struct conn_list *dest, 
-			     struct packet_unit_request *packet,
+			     const struct packet_unit_request *packet,
 			     enum packet_type req_type);
 void lsend_packet_before_new_year(struct conn_list *dest);
 void lsend_packet_unittype_info(struct conn_list *dest, int type, int action);
 void lsend_packet_ruleset_control(struct conn_list *dest, 
-				struct packet_ruleset_control *pinfo);
+				const struct packet_ruleset_control *pinfo);
 void lsend_packet_ruleset_unit(struct conn_list *dest,
-			     struct packet_ruleset_unit *packet);
+			     const struct packet_ruleset_unit *packet);
 void lsend_packet_ruleset_tech(struct conn_list *dest,
-			     struct packet_ruleset_tech *packet);
+			     const struct packet_ruleset_tech *packet);
 void lsend_packet_ruleset_building(struct conn_list *dest,
-			     struct packet_ruleset_building *packet);
+			     const struct packet_ruleset_building *packet);
 void lsend_packet_ruleset_terrain(struct conn_list *dest,
-			     struct packet_ruleset_terrain *packet);
+			     const struct packet_ruleset_terrain *packet);
 void lsend_packet_ruleset_terrain_control(struct conn_list *dest,
-					struct terrain_misc *packet);
+					const struct terrain_misc *packet);
 void lsend_packet_ruleset_government(struct conn_list *dest,
-				struct packet_ruleset_government *packet);
+			       const struct packet_ruleset_government *packet);
 void lsend_packet_ruleset_government_ruler_title(struct conn_list *dest,
-				struct packet_ruleset_government_ruler_title *packet);
+		   const struct packet_ruleset_government_ruler_title *packet);
 void lsend_packet_ruleset_nation(struct conn_list *dest,
-			       struct packet_ruleset_nation *packet);
+			       const struct packet_ruleset_nation *packet);
 void lsend_packet_ruleset_city(struct conn_list *dest,
-			     struct packet_ruleset_city *packet);
+			     const struct packet_ruleset_city *packet);
 void lsend_packet_ruleset_game(struct conn_list *dest,
-                             struct packet_ruleset_game *packet);
+                             const struct packet_ruleset_game *packet);
 void lsend_packet_generic_values(struct conn_list *dest, int type,
-			       struct packet_generic_values *req);
+			       const struct packet_generic_values *req);
 void lsend_packet_spaceship_info(struct conn_list *dest,
-			       struct packet_spaceship_info *packet);
+			       const struct packet_spaceship_info *packet);
 void lsend_packet_spaceship_action(struct conn_list *dest,
-				 struct packet_spaceship_action *packet);
+				 const struct packet_spaceship_action *packet);
 void lsend_packet_city_name_suggestion(struct conn_list *dest,
-				     struct packet_city_name_suggestion *packet);
+				const struct packet_city_name_suggestion *packet);
 void lsend_packet_sabotage_list(struct conn_list *dest,
-			      struct packet_sabotage_list *packet);
-void lsend_packet_goto_route(struct conn_list *dest, struct packet_goto_route *packet,
+			      const struct packet_sabotage_list *packet);
+void lsend_packet_goto_route(struct conn_list *dest,
+                           const struct packet_goto_route *packet,
 			   enum goto_route_type type);
