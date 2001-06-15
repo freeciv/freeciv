e8f6cd1cf85aecac98798cf44d0fa3bbf3795d48
Thue Janus Kristensen <thue@diku.dk>
2001-06-15 23:13:00 +0000

    Upgrade gettext to 0.10.38.
    [[originally from CVS; cvs2svn created svn r3395]]

--- intl/explodename.c
+++ intl/explodename.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1995, 1996, 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1995-1998, 2000, 2001 Free Software Foundation, Inc.
    Contributed by Ulrich Drepper <drepper@gnu.ai.mit.edu>, 1995.
 
    This program is free software; you can redistribute it and/or modify
@@ -19,15 +19,8 @@
 # include <config.h>
 #endif
 
-#if defined STDC_HEADERS || defined _LIBC
-# include <stdlib.h>
-#endif
-
-#if defined HAVE_STRING_H || defined _LIBC
-# include <string.h>
-#else
-# include <strings.h>
-#endif
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 
 #include "loadinfo.h"
@@ -44,7 +37,8 @@
 /* @@ end of prolog @@ */
 
 char *
-_nl_find_language (const char *name)
+_nl_find_language (name)
+     const char *name;
 {
   while (name[0] != '\0' && name[0] != '_' && name[0] != '@'
 	 && name[0] != '+' && name[0] != ',')
