0d34ee3e688d4ac27e521fd75c28d72352cd5876
Thue Janus Kristensen <thue@diku.dk>
2001-08-07 20:55:03 +0000

    Updates for the forth beta leading to version 1.12.0: - Changed version to 1.11.12 (beta). - Ran msgmerge on all .po files (normal	part of 'make dist'). - Updated Changelog
    [[originally from CVS; cvs2svn created svn r3625]]

--- common/version.h
+++ common/version.h
@@ -28,16 +28,16 @@
 #define MINOR_VERSION		11
 #endif
 #ifndef PATCH_VERSION
-#define PATCH_VERSION		11
+#define PATCH_VERSION		12
 #endif
 #ifndef VERSION_LABEL
-#define VERSION_LABEL		"-devel"
+#define VERSION_LABEL		""
 #endif
 #ifndef IS_DEVEL_VERSION
 #define IS_DEVEL_VERSION	1
 #endif
 #ifndef IS_BETA_VERSION
-#define IS_BETA_VERSION		0
+#define IS_BETA_VERSION		1
 #endif
 
 /* This is only used if IS_BETA_VERSION is true. */
