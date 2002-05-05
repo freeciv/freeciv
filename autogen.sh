4dd4448d6db3ca05e3cdc0e14bd4d520db483e3d
Raimar Falke <rf13@inf.tu-dresden.de>
2002-05-05 12:35:12 +0000

    Add support for autoconf 2.52+.
    Patch from Per I. Mathisen <Per.Inge.Mathisen@idi.ntnu.no>.
    
    [[originally from CVS; cvs2svn created svn r4494]]

--- autogen.sh
+++ autogen.sh
@@ -19,7 +19,6 @@ for NAME in $@ ; do
   fi
 done
 
-
 debug ()
 # print out a debug message if DEBUG is a defined variable
 {
@@ -31,11 +30,12 @@ debug ()
 
 version_check ()
 # check the version of a package
-# first argument : package name (executable)
-# second argument : source download url
+# first argument : complain ('1') or not ('0')
+# second argument : package name (executable)
+# third argument : source download url
 # rest of arguments : major, minor, micro version
 {
-  EXACT_VERSION=$1
+  COMPLAIN=$1
   PACKAGE=$2
   URL=$3
   MAJOR=$4
@@ -50,11 +50,7 @@ version_check ()
   if test ! -z "$MICRO"; then VERSION=$VERSION.$MICRO; else MICRO=0; fi
 
   debug "version $VERSION"
-  if [ "$EXACT_VERSION" -eq 0 ]; then
-    echo "+ checking for $PACKAGE >= $VERSION ... " | tr -d '\n'
-  else
-    echo "+ checking for $PACKAGE == $VERSION ... " | tr -d '\n'
-  fi
+  echo "+ checking for $PACKAGE >= $VERSION ... " | tr -d '\n'
 
   ($PACKAGE --version) < /dev/null > /dev/null 2>&1 || 
   {
@@ -68,56 +64,63 @@ version_check ()
   pkg_version=`$PACKAGE --version|head -n 1|sed 's/^[a-zA-z\.\ ()]*//;s/ .*$//'`
   debug "pkg_version $pkg_version"
   pkg_major=`echo $pkg_version | cut -d. -f1`
-  pkg_minor=`echo $pkg_version | sed s/-.*// | cut -d. -f2`
-  pkg_micro=`echo $pkg_version | sed s/-.*// | cut -d. -f3`
+  pkg_minor=`echo $pkg_version | sed s/[-,a-z,A-Z].*// | cut -d. -f2`
+  pkg_micro=`echo $pkg_version | sed s/[-,a-z,A-Z].*// | cut -d. -f3`
   test -z "$pkg_minor" && pkg_minor=0
   test -z "$pkg_micro" && pkg_micro=0
 
   debug "found major $pkg_major minor $pkg_minor micro $pkg_micro"
 
   #start checking the version
-  if [ "$EXACT_VERSION" -eq 0 ]; then
-    debug "version check >="
-    if [ "$pkg_major" -lt "$MAJOR" ]; then
+  if [ "$pkg_major" -lt "$MAJOR" ]; then
+    WRONG=1
+  elif [ "$pkg_major" -eq "$MAJOR" ]; then
+    if [ "$pkg_minor" -lt "$MINOR" ]; then
       WRONG=1
-    elif [ "$pkg_major" -eq "$MAJOR" ]; then
-      if [ "$pkg_minor" -lt "$MINOR" ]; then
-        WRONG=1
-      elif [ "$pkg_minor" -eq "$MINOR" -a "$pkg_minor" -lt "$MINOR" ]; then
-        WRONG=1
-      fi
-    fi
-  else
-    debug "version check =="
-    if [ "$MAJOR" -ne "$pkg_major" -o \
-         "$MINOR" -ne "$pkg_minor" -o \
-         "$MICRO" -ne "$pkg_micro" ]; then
+    elif [ "$pkg_minor" -eq "$MINOR" -a "$pkg_minor" -lt "$MINOR" ]; then
       WRONG=1
     fi
   fi
 
   if test ! -z "$WRONG"; then
-    echo "found $pkg_version, not ok !"
-    echo
-    if [ "$EXACT_VERSION" -eq 0 ]; then
-      echo "You must have $PACKAGE $VERSION or greater to compile $package."
-      echo "Get the latest version from <$URL>."
-    else
-      echo "You must have exactly $PACKAGE $VERSION to compile $package."
-      echo "Get this version from <$URL>."
-    fi
-    return 1
+   echo "found $pkg_version, not ok !"
+   if [ "$COMPLAIN" -eq "1" ] ; then
+     echo
+     echo "You must have $PACKAGE $VERSION or greater to compile $package."
+     echo "Get the latest version from <$URL>."
+     echo
+   fi
+   return 1
   else
     echo "found $pkg_version, ok."
   fi
 }
 
-version_check 1 "autoconf" "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 13 || DIE=1
-version_check 0 "automake" "ftp://ftp.gnu.org/pub/gnu/automake/" 1 4 || DIE=1
+test -f $srcfile || {
+	echo "You must run this script in the top-level $package directory"
+	exit 1
+}
+
+# We need to move configure.in out of the way in case we have autoconf > 2.13
+# since that would cause lots of spurious warnings
+test -f configure.in && {
+	mv configure.in configure.old
+}
+
+# x.252 is autoconf 2.52 compatible version of this macro, while x.213 is
+# the original autoconf 2.13 version; we must suppose 2.52 by default here
+cp m4/x.252 m4/x.m4
+
+version_check 0 "autoconf" "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || {
+  mv configure.old configure.in
+  cp m4/x.213 m4/x.m4
+  version_check 1 "autoconf" "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 13 || DIE=1
+}
+version_check 1 "automake" "ftp://ftp.gnu.org/pub/gnu/automake/" 1 4 || DIE=1
 if [ "$USE_NLS" = "yes" ] ; then
   DIE2=0
-  version_check 0 "xgettext" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 10 38 || DIE2=1
-  version_check 0 "msgfmt" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 10 38 || DIE2=1
+  version_check 1 "xgettext" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 10 38 || DIE2=1
+  version_check 1 "msgfmt" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 10 38 || DIE2=1
   if test "$DIE2" -eq 1; then
     echo 
     echo "You may want to use --disable-nls to disable NLS."
@@ -128,16 +131,6 @@ fi
 
 if test "$DIE" -eq 1; then
 	exit 1
-fi
-
-test -f $srcfile || {
-	echo "You must run this script in the top-level $package directory"
-	exit 1
-}
-
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
 fi
 
 echo "+ creating acinclude.m4"
@@ -174,7 +167,13 @@ echo "+ removing config.cache ... "
 rm -f config.cache
 
 echo "+ running configure ... "
-echo "using: $@"
+echo
+if test -z "$*"; then
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+else
+	echo "using: $@"
+fi
 echo
 
 ./configure "$@" || {
