3a49b5b71c0c6be8e955fb017c6c3b4efe4a9c41
Raimar Falke <rf13@inf.tu-dresden.de>
2002-05-17 14:11:45 +0000

    Cleanup of autogen.sh:  - replace == (bash extension) with =  - replace tab with two blanks  - replace test with [ and ]  - always define FC_USE_NEWAUTOCONF  - add FC_AUTOCONF to print the warning message only if it is  appropriate
    Reported by Andreas Kemnade <akemnade@informatik.uni-bremen.de>. Patch
    from me.
    
    [[originally from CVS; cvs2svn created svn r4529]]

--- autogen.sh
+++ autogen.sh
@@ -9,14 +9,15 @@ srcfile=client/civclient.h
 #DEBUG=defined
 
 FC_USE_NLS=yes
+FC_USE_NEWAUTOCONF=yes
 
 # Leave out NLS checks
 for NAME in $@ ; do
-  if test "x$NAME" = "x--disable-nls"; then 
+  if [ "x$NAME" = "x--disable-nls" ]; then 
     echo "+ nls checks disabled"
     FC_USE_NLS=no
   fi
-  if test "x$NAME" = "x--disable-autoconf2.52"; then 
+  if [ "x$NAME" = "x--disable-autoconf2.52" ]; then 
     echo "+ forcing old autoconf configuration"
     FC_USE_NEWAUTOCONF=no
   else
@@ -27,8 +28,7 @@ done
 debug ()
 # print out a debug message if DEBUG is a defined variable
 {
-  if test ! -z "$DEBUG"
-  then
+  if [ ! -z "$DEBUG" ]; then
     echo "DEBUG: $1"
   fi
 }
@@ -51,19 +51,19 @@ version_check ()
 
   debug "major $MAJOR minor $MINOR micro $MICRO"
   VERSION=$MAJOR
-  if test ! -z "$MINOR"; then VERSION=$VERSION.$MINOR; else MINOR=0; fi
-  if test ! -z "$MICRO"; then VERSION=$VERSION.$MICRO; else MICRO=0; fi
+  if [ ! -z "$MINOR" ]; then VERSION=$VERSION.$MINOR; else MINOR=0; fi
+  if [ ! -z "$MICRO" ]; then VERSION=$VERSION.$MICRO; else MICRO=0; fi
 
   debug "version $VERSION"
   echo "+ checking for $PACKAGE >= $VERSION ... " | tr -d '\n'
 
   ($PACKAGE --version) < /dev/null > /dev/null 2>&1 || 
   {
-	echo
-	echo "You must have $PACKAGE installed to compile $package."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at $URL"
-	return 1
+    echo
+    echo "You must have $PACKAGE installed to compile $package."
+    echo "Download the appropriate package for your distribution,"
+    echo "or get the source tarball at $URL"
+    return 1
   }
   # the following line is carefully crafted sed magic
   pkg_version=`$PACKAGE --version|head -n 1|sed 's/^[a-zA-z\.\ ()]*//;s/ .*$//'`
@@ -71,8 +71,8 @@ version_check ()
   pkg_major=`echo $pkg_version | cut -d. -f1`
   pkg_minor=`echo $pkg_version | sed s/[-,a-z,A-Z].*// | cut -d. -f2`
   pkg_micro=`echo $pkg_version | sed s/[-,a-z,A-Z].*// | cut -d. -f3`
-  test -z "$pkg_minor" && pkg_minor=0
-  test -z "$pkg_micro" && pkg_micro=0
+  [ -z "$pkg_minor" ] && pkg_minor=0
+  [ -z "$pkg_micro" ] && pkg_micro=0
 
   debug "found major $pkg_major minor $pkg_minor micro $pkg_micro"
 
@@ -87,9 +87,9 @@ version_check ()
     fi
   fi
 
-  if test ! -z "$WRONG"; then
+  if [ ! -z "$WRONG" ]; then
    echo "found $pkg_version, not ok !"
-   if [ "$COMPLAIN" -eq "1" ] ; then
+   if [ "$COMPLAIN" -eq "1" ]; then
      echo
      echo "You must have $PACKAGE $VERSION or greater to compile $package."
      echo "Get the latest version from <$URL>."
@@ -101,45 +101,51 @@ version_check ()
   fi
 }
 
-test -f $srcfile || {
-	echo "You must run this script in the top-level $package directory"
-	exit 1
+[ -f $srcfile ] || {
+  echo "You must run this script in the top-level $package directory"
+  exit 1
 }
 
 # We need to move configure.in out of the way in case we have autoconf
 # > 2.13 since that would cause lots of spurious warnings. Same goes
 # for acconfig.h
-test -f configure.in && {
-	mv configure.in configure.old
+[ -f configure.in ] && {
+  mv configure.in configure.old
 }
-test -f acconfig.h && {
-	mv acconfig.h acconfig.old
+
+[ -f acconfig.h ] && {
+  mv acconfig.h acconfig.old
 }
 
 # x.252 is autoconf 2.52 compatible version of this macro, while x.213 is
 # the original autoconf 2.13 version; we must suppose 2.52 by default here
 cp m4/x.252 m4/x.m4
 
-if [ "x$FC_USE_NEWAUTOCONF" != "xno" ] && test -f configure.old2; then
-	mv configure.old2 configure.ac
+if [ "$FC_USE_NEWAUTOCONF" = "yes" -a -f configure.old2 ]; then
+  mv configure.old2 configure.ac
 fi
-if [ "x$FC_USE_NEWAUTOCONF" == "xno" ] && test -f configure.ac; then
-	mv configure.ac configure.old2
+if [ "$FC_USE_NEWAUTOCONF" = "no" -a -f configure.ac ]; then
+  mv configure.ac configure.old2
 fi
 
-(test -z "$FC_USE_NEWAUTOCONF" \
- && version_check 0 "autoconf" "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52) || {
+if [ "$FC_USE_NEWAUTOCONF" = "yes" ] \
+ && version_check 0 "autoconf" "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52
+then
+  FC_AUTOCONF=new
+else
+  FC_AUTOCONF=old
   mv configure.old configure.in
   mv acconfig.old acconfig.h
   cp m4/x.213 m4/x.m4
   version_check 1 "autoconf" "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 13 || DIE=1
-}
+fi
+
 version_check 1 "automake" "ftp://ftp.gnu.org/pub/gnu/automake/" 1 4 || DIE=1
-if [ "$FC_USE_NLS" = "yes" ] ; then
+if [ "$FC_USE_NLS" = "yes" ]; then
   DIE2=0
   version_check 1 "xgettext" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 10 38 || DIE2=1
   version_check 1 "msgfmt" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 10 38 || DIE2=1
-  if test "$DIE2" -eq 1; then
+  if [ "$DIE2" -eq 1 ]; then
     echo 
     echo "You may want to use --disable-nls to disable NLS."
     echo "This will also remove the dependency for xgettext and msgfmt."
@@ -147,12 +153,12 @@ if [ "$FC_USE_NLS" = "yes" ] ; then
   fi    
 fi
 
-if test "$DIE" -eq 1; then
-	exit 1
+if [ "$DIE" -eq 1 ]; then
+  exit 1
 fi
 
-if [ "x$FC_USE_NEWAUTOCONF" != "xno" ] ; then
-	echo "+ using new autoconf configuration (use --disable-autoconf2.52 to use old)"
+if [ "$FC_AUTOCONF" = "new" ]; then
+  echo "+ using new autoconf configuration (use --disable-autoconf2.52 to use old)"
 fi
 
 echo "+ creating acinclude.m4"
@@ -160,28 +166,28 @@ cat m4/*.m4 > acinclude.m4
 
 echo "+ running aclocal ..."
 aclocal $ACLOCAL_FLAGS || {
-	echo
-	echo "aclocal failed - check that all needed development files are present on system"
-	exit 1
+  echo
+  echo "aclocal failed - check that all needed development files are present on system"
+  exit 1
 }
 
 echo "+ running autoheader ... "
 autoheader || {
-	echo
-	echo "autoheader failed"
-	exit 1
+  echo
+  echo "autoheader failed"
+  exit 1
 }
 echo "+ running autoconf ... "
 autoconf || {
-	echo
-	echo "autoconf failed"
-	exit 1
+  echo
+  echo "autoconf failed"
+  exit 1
 }
 echo "+ running automake ... "
 automake -a -c || {
-	echo
-	echo "automake failed"
-	exit 1
+  echo
+  echo "automake failed"
+  exit 1
 }
 
 # now remove the cache, because it can be considered dangerous in this case
@@ -190,30 +196,30 @@ rm -f config.cache
 
 echo "+ running configure ... "
 echo
-if test -z "$FC_NEWARGLINE"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
+if [ -z "$FC_NEWARGLINE" ]; then
+  echo "I am going to run ./configure with no arguments - if you wish "
+  echo "to pass any to it, please specify them on the $0 command line."
 else
-	echo "using: $FC_NEWARGLINE"
+  echo "using: $FC_NEWARGLINE"
 fi
 echo
 
 ./configure $FC_NEWARGLINE || {
-	echo
-	echo "configure failed"
-	exit 1
+  echo
+  echo "configure failed"
+  exit 1
 }
 
 echo 
 echo "Now type 'make' to compile $package."
 
 # Reverse changes to make tree sane
-test -f configure.old && { 
-	mv configure.old configure.in 
+[ -f configure.old ] && { 
+  mv configure.old configure.in 
 }
-test -f configure.old2 && { 
-	mv configure.old2 configure.ac 
+[ -f configure.old2 ] && { 
+  mv configure.old2 configure.ac 
 }
-test -f acconfig.old && { 
-	mv acconfig.old acconfig.h 
+[ -f acconfig.old ] && { 
+  mv acconfig.old acconfig.h 
 }
