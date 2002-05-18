4057838c8c8c0179e98d8da8539c5ccea3fb16c0
Vasco Alexandre da Silva Costa <vasc@rnl.ist.utl.pt>
2002-05-18 22:26:40 +0000

    Fixed two bugs:  - the strings returned by the metaserver must be converted to UTF-8.  - it was impossible to select items from the metaserver list.
    [[originally from CVS; cvs2svn created svn r4536]]

--- client/gui-gtk-2.0/connectdlg.c
+++ client/gui-gtk-2.0/connectdlg.c
@@ -36,15 +36,16 @@
 #include "connectdlg.h"
 
 static GtkWidget *iname, *ihost, *iport;
+static GtkListStore *store;
 
 static GtkWidget *dialog;
 
 /* meta Server */
-static bool update_meta_dialog(GtkWidget *meta_list);
-static void meta_list_callback(GtkWidget *w, gint row, gint column);
+static bool update_meta_dialog(void);
+static void meta_list_callback(GtkTreeSelection *select, GtkTreeModel *model);
 static void meta_update_callback(GtkWidget *w, gpointer data);
 
-static int get_meta_list(GtkWidget *list, char *errbuf, int n_errbuf);
+static int get_meta_list(char *errbuf, int n_errbuf);
 
 /**************************************************************************
 ...
@@ -68,11 +69,11 @@ static void connect_callback(GtkWidget *w, gpointer data)
 /**************************************************************************
 ...
 **************************************************************************/
-static bool update_meta_dialog(GtkWidget *meta_list)
+static bool update_meta_dialog(void)
 {
   char errbuf[128];
 
-  if(get_meta_list(meta_list, errbuf, sizeof(errbuf))!=-1)  {
+  if(get_meta_list(errbuf, sizeof(errbuf))!=-1)  {
     return TRUE;
   } else {
     append_output_window(errbuf);
@@ -85,28 +86,33 @@ static bool update_meta_dialog(GtkWidget *meta_list)
 **************************************************************************/
 static void meta_update_callback(GtkWidget *w, gpointer data)
 {
-  update_meta_dialog(GTK_WIDGET(data));
+  update_meta_dialog();
 }
 
 /**************************************************************************
 ...
 **************************************************************************/
-static void meta_list_callback(GtkWidget *w, gint row, gint column)
+static void meta_list_callback(GtkTreeSelection *select, GtkTreeModel *dummy)
 {
-  gchar *name, *port;
+  GtkTreeIter it;
+  char *name, *port;
 
-  gtk_clist_get_text(GTK_CLIST(w), row, 0, &name);
+  if (!gtk_tree_selection_get_selected(select, NULL, &it))
+    return;
+
+  gtk_tree_model_get(GTK_TREE_MODEL(store), &it, 0, &name, 1, &port, -1);
+
   gtk_entry_set_text(GTK_ENTRY(ihost), name);
-  gtk_clist_get_text(GTK_CLIST(w), row, 1, &port);
   gtk_entry_set_text(GTK_ENTRY(iport), port);
 }
 
 /**************************************************************************
 ...
 ***************************************************************************/
-static void meta_click_callback(GtkWidget *w, GdkEventButton *event, gpointer data)
+static gboolean meta_click_callback(GtkWidget *w, GdkEventButton *event, gpointer data)
 {
   if (event->type==GDK_2BUTTON_PRESS) connect_callback(w, data);
+  return FALSE;
 }
 
 /**************************************************************************
@@ -135,14 +141,9 @@ static void connect_command_callback(GtkWidget *w, gint response_id)
 void gui_server_connect(void)
 {
   GtkWidget *label, *table, *book, *scrolled, *list, *vbox, *update;
-  static char *titles_[6]= {N_("Server Name"), N_("Port"), N_("Version"),
-			    N_("Status"), N_("Players"), N_("Comment")};
-  static char **titles;
   char buf [256];
-  int i;
-
-  if (!titles)
-    titles = intl_slist(6, titles_);
+  GtkCellRenderer *renderer;
+  GtkTreeSelection *selection;
 
   dialog = gtk_dialog_new_with_buttons(_(" Connect to Freeciv Server"),
     GTK_WINDOW(toplevel),
@@ -215,11 +216,27 @@ void gui_server_connect(void)
   vbox=gtk_vbox_new(FALSE, 2);
   gtk_notebook_append_page (GTK_NOTEBOOK (book), vbox, label);
 
-  list=gtk_clist_new_with_titles(6, titles);
-  gtk_clist_column_titles_passive(GTK_CLIST(list));
+  store = gtk_list_store_new(6, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING,
+			     G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
 
-  for(i=0; i<6; i++)
-    gtk_clist_set_column_auto_resize(GTK_CLIST(list), i, TRUE);
+  list = gtk_tree_view_new_with_model(GTK_TREE_MODEL(store));
+  selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(list));
+  g_object_unref(store);
+  gtk_tree_view_columns_autosize(GTK_TREE_VIEW(list));
+
+  renderer = gtk_cell_renderer_text_new();
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(list),
+	-1, _("Server Name"), renderer, "text", 0, NULL);
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(list),
+	-1, _("Port"), renderer, "text", 1, NULL);
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(list),
+	-1, _("Version"), renderer, "text", 2, NULL);
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(list),
+	-1, _("Status"), renderer, "text", 3, NULL);
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(list),
+	-1, _("Players"), renderer, "text", 4, NULL);
+  gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(list),
+	-1, _("Comment"), renderer, "text", 5, NULL);
 
   scrolled=gtk_scrolled_window_new(NULL,NULL);
   gtk_container_add(GTK_CONTAINER(scrolled), list);
@@ -230,12 +247,12 @@ void gui_server_connect(void)
   update=gtk_button_new_with_label(_("Update"));
   gtk_box_pack_start(GTK_BOX(vbox), update, FALSE, FALSE, 2);
 
-  gtk_signal_connect(GTK_OBJECT(list), "select_row",
-			GTK_SIGNAL_FUNC(meta_list_callback), NULL);
-  gtk_signal_connect(GTK_OBJECT(list), "button_press_event",
-		     GTK_SIGNAL_FUNC(meta_click_callback), NULL);
-  gtk_signal_connect(GTK_OBJECT(update), "clicked",
-			GTK_SIGNAL_FUNC(meta_update_callback), (gpointer)list);
+  g_signal_connect(list, "button_press_event",
+		   G_CALLBACK(meta_click_callback), NULL);
+  g_signal_connect(selection, "changed",
+                   G_CALLBACK(meta_list_callback), NULL);
+  g_signal_connect(update, "clicked",
+		   G_CALLBACK(meta_update_callback), NULL);
 
   gtk_window_set_focus(GTK_WINDOW(dialog), iname);
 
@@ -256,35 +273,40 @@ void gui_server_connect(void)
 /**************************************************************************
   Get the list of servers from the metaserver
 **************************************************************************/
-static int get_meta_list(GtkWidget *list, char *errbuf, int n_errbuf)
+static int get_meta_list(char *errbuf, int n_errbuf)
 {
-  int i;
-  char *row[6];
-  char  buf[6][64];
   struct server_list *server_list = create_server_list(errbuf, n_errbuf);
-  if(!server_list) return -1;
+  gchar *row[6];
 
-  gtk_clist_freeze(GTK_CLIST(list));
-  gtk_clist_clear(GTK_CLIST(list));
+  if(!server_list) {
+    return -1;
+  }
 
-  for (i=0; i<6; i++)
-    row[i]=buf[i];
+  gtk_list_store_clear(store);
 
-  server_list_iterate(*server_list,pserver) {
-    sz_strlcpy(buf[0], pserver->name);
-    sz_strlcpy(buf[1], pserver->port);
-    sz_strlcpy(buf[2], pserver->version);
-    sz_strlcpy(buf[3], _(pserver->status));
-    sz_strlcpy(buf[4], pserver->players);
-    sz_strlcpy(buf[5], pserver->metastring);
+  server_list_iterate(*server_list, pserver) {
+    GtkTreeIter it;
+    int i;
 
-    gtk_clist_append(GTK_CLIST(list), row);
+    row[0] = g_locale_to_utf8(pserver->name, -1, NULL, NULL, NULL);
+    row[1] = g_locale_to_utf8(pserver->port, -1, NULL, NULL, NULL);
+    row[2] = g_locale_to_utf8(pserver->version,	-1, NULL, NULL, NULL);
+    row[3] = g_locale_to_utf8(_(pserver->status), -1, NULL, NULL, NULL);
+    row[4] = g_locale_to_utf8(pserver->players,	-1, NULL, NULL, NULL);
+    row[5] = g_locale_to_utf8(pserver->metastring, -1, NULL, NULL, NULL);
+
+    gtk_list_store_append(store, &it);
+    gtk_list_store_set(store, &it,
+		       0, row[0], 1, row[1], 2, row[2],
+		       3, row[3], 4, row[4], 5, row[5], -1);
+
+    for (i=0; i<6; i++) {
+      g_free(row[i]);
+    }
   }
   server_list_iterate_end;
 
   delete_server_list(server_list);
-  gtk_clist_thaw(GTK_CLIST(list));
-
   return 0;
 }
 
