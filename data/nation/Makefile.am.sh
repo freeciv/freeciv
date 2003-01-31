3cdf48f2ff948fa78c9a4aaa421fba2993355cb8
Eric S Raymond <esr@thyrsus.com>
2003-01-31 22:56:56 +0000

    A script to autogenerate Makefile.am from the directory contents
    [[originally from CVS; cvs2svn created svn r5855]]

---
+++ data/nation/Makefile.am.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Regenerate Makefile.am based on actual contents of directory.
+# This is all so 'make distcheck' will work.
+#
+cat <<EOF
+## Process this file with automake to produce Makefile.in
+# Note: After dding a new nation file, 'make Makefile.am'
+
+pkgdata_DATA = \\
+`find * -name "*.ruleset" -print | sed -e 's/.*ruleset$/		& \\\/' -e '$s/.$//'`
+
+EXTRA_DIST = \$(pkgdata_DATA) Makefile.am.sh
+
+Makefile.am: Makefile.am.sh \$(shell echo *.ruleset)
+	sh Makefile.am.sh >Makefile.am
+
+EOF
