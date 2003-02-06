3de5635b7796fc6736c39a9ae25887c8fc3cca2f
Jason Dorje Short <jdorje@users.sourceforge.net>
2003-02-06 07:19:23 +0000

    Make the science dialog non-modal when it pops up after discovering a new tech; there is no known reason why it should be modal and this causes several bugs.
    See PR#2905.
    
    [[originally from CVS; cvs2svn created svn r5904]]

--- client/packhand.c
+++ client/packhand.c
@@ -1203,7 +1203,7 @@ void handle_player_info(struct packet_player_info *pinfo)
   if(get_client_state()==CLIENT_GAME_RUNNING_STATE && pplayer==game.player_ptr) {
     if(poptechup) {
       if(!game.player_ptr->ai.control || ai_popup_windows)
-	popup_science_dialog(TRUE);
+	popup_science_dialog(FALSE);
       science_dialog_update();
 
       /* If we just learned bridge building and focus is on a settler
