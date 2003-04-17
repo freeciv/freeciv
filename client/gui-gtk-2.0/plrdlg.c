36032915688d5ebd4bb3cca432d6467846741ad8
Vasco Alexandre da Silva Costa <vasc@mega.ist.utl.pt>
2003-04-17 22:41:17 +0000

    * Change the window type to normal. * Add a thin border around the TreeView.
    [[originally from CVS; cvs2svn created svn r6201]]

--- client/gui-gtk-2.0/plrdlg.c
+++ client/gui-gtk-2.0/plrdlg.c
@@ -225,6 +225,8 @@ void create_players_dialog(void)
     0,
     GTK_STOCK_CLOSE, GTK_RESPONSE_CLOSE,
     NULL);
+  gtk_window_set_type_hint(GTK_WINDOW(players_dialog_shell),
+			   GDK_WINDOW_TYPE_HINT_NORMAL);
 
   g_signal_connect(players_dialog_shell, "destroy",
     G_CALLBACK(players_destroy_callback), NULL);
@@ -271,6 +273,8 @@ void create_players_dialog(void)
   gtk_tree_view_set_search_column(GTK_TREE_VIEW(players_list), DEF_SORT_COLUMN);
 
   sw = gtk_scrolled_window_new(NULL, NULL);
+  gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw),
+				      GTK_SHADOW_ETCHED_IN);
   gtk_container_add(GTK_CONTAINER(GTK_DIALOG(players_dialog_shell)->vbox), sw);
   gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw),
 		                 GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
