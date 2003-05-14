408d909fba7dcc5340c33befef1742d151421b1c
Vasco Alexandre da Silva Costa <vasc@mega.ist.utl.pt>
2003-05-14 01:00:31 +0000

    Replace 'sleep' calls for 'myusleep'. See PR#4043.
    [[originally from CVS; cvs2svn created svn r6341]]

--- client/gui-gtk-2.0/mapview.c
+++ client/gui-gtk-2.0/mapview.c
@@ -1529,7 +1529,7 @@ void put_nuke_mushroom_pixmaps(int x, int y)
 			    mysprite);
 
     gdk_flush();
-    sleep(1);
+    myusleep(1000000);
 
     update_map_canvas_visible();
   } else {
@@ -1552,7 +1552,7 @@ void put_nuke_mushroom_pixmaps(int x, int y)
     }
 
     gdk_flush();
-    sleep(1);
+    myusleep(1000000);
 
     update_map_canvas(x-1, y-1, 3, 3, TRUE);
   }
--- client/gui-gtk/mapview.c
+++ client/gui-gtk/mapview.c
@@ -1481,7 +1481,7 @@ void put_nuke_mushroom_pixmaps(int x, int y)
 			    mysprite);
 
     gdk_flush();
-    sleep(1);
+    myusleep(1000000);
 
     update_map_canvas_visible();
   } else {
@@ -1504,7 +1504,7 @@ void put_nuke_mushroom_pixmaps(int x, int y)
     }
 
     gdk_flush();
-    sleep(1);
+    myusleep(1000000);
 
     update_map_canvas(x-1, y-1, 3, 3, TRUE);
   }
