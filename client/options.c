025e9b877b819af490c0105d458561ce7e8a40a4
Raimar Falke <rf13@inf.tu-dresden.de>
2003-11-07 15:20:09 +0000

    Fix a case where the field allow_disorder was not initialized.
    Reported by Christian Knoke in PR#6732.
    
    [[originally from CVS; cvs2svn created svn r6978]]

--- client/options.c
+++ client/options.c
@@ -634,6 +634,7 @@ static void load_cma_preset(struct section_file *file, int inx)
       secfile_lookup_int_default(file, 0, "cma.preset%d.factortarget", inx);
   parameter.happy_factor =
       secfile_lookup_int_default(file, 0, "cma.preset%d.happyfactor", inx);
+  parameter.allow_disorder = FALSE;
 
   cmafec_preset_add(name, &parameter);
 }
