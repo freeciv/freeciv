02f9a2a3d5a4e35fd54a8260b65c8a00bbf56ef9
Raimar Falke <rf13@inf.tu-dresden.de>
2003-12-01 18:18:01 +0000

    Remove the trailing comma of enum authentication_type.
    [[originally from CVS; cvs2svn created svn r7118]]

--- common/packets.h
+++ common/packets.h
@@ -62,7 +62,7 @@ enum authentication_type {
   AUTH_LOGIN_FIRST,   /* request a password for a returning user */
   AUTH_NEWUSER_FIRST, /* request a password for a new user */
   AUTH_LOGIN_RETRY,   /* inform the client to try a different password */
-  AUTH_NEWUSER_RETRY, /* inform the client to try a different [new] password */
+  AUTH_NEWUSER_RETRY  /* inform the client to try a different [new] password */
 };
 
 #include "packets_gen.h"
