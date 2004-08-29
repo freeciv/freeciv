4dd08dc36e2de4e25d831cde07f87dbc06766bbb
Per I. Mathisen <per@leftist.net>
2004-08-29 12:36:12 +0000

    File missed in a previous commit.
    [[originally from CVS; cvs2svn created svn r8072]]

--- client/repodlgs_common.c
+++ client/repodlgs_common.c
@@ -81,7 +81,7 @@ void get_economy_report_data(struct improvement_entry *entries,
 
   city_list_iterate(game.player_ptr->cities, pcity) {
     *total_income += pcity->tax_total;
-    if (!pcity->is_building_unit && pcity->currently_building == B_CAPITAL) {
+    if (get_current_construction_bonus(pcity, EFT_PROD_TO_GOLD) > 0) {
       *total_income += MAX(0, pcity->shield_surplus);
     }
   } city_list_iterate_end;
