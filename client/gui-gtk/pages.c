758c5d1c54afeca042bafaa46f96d384bc4a6b73
Jason Dorje Short <jshort@andrew.cmu.edu>
2004-10-20 17:38:21 +0000

    Backport: allow all clients to compile again.
    See PR#10638.
    
    [[originally from CVS; cvs2svn created svn r8638]]

---
+++ client/gui-gtk/pages.c
@@ -0,0 +1,28 @@
+/********************************************************************** 
+ Freeciv - Copyright (C) 1996-2004 - The Freeciv Team
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+***********************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "pages.h"
+
+/**************************************************************************
+  Sets the "page" that the client should show.  See documentation in
+  pages_g.h.
+**************************************************************************/
+void set_client_page(enum client_pages page)
+{
+  /* PORTME */
+}
+
