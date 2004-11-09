2a9ea6f8b741258d837b4a01b7835e306ba95169
Vasco Alexandre da Silva Costa <vasc@rnl.ist.utl.pt>
2004-11-09 18:25:30 +0000

    PR#10757: new preset dialog opens below city window.
    Reported by Christian Knoke <chrisk@cknoke.de>.
    
    [[originally from CVS; cvs2svn created svn r8860]]

--- client/gui-gtk-2.0/inputdlg.c
+++ client/gui-gtk-2.0/inputdlg.c
@@ -67,7 +67,7 @@ GtkWidget *input_dialog_create(GtkWindow *parent, const char *dialogname,
         parent,
         GTK_DIALOG_DESTROY_WITH_PARENT,
         NULL);
-  setup_dialog(shell, toplevel);
+  setup_dialog(shell, GTK_WIDGET(parent));
   gtk_window_set_position(GTK_WINDOW(shell), GTK_WIN_POS_CENTER_ON_PARENT);
   label = gtk_frame_new(text);
   gtk_box_pack_start(GTK_BOX(GTK_DIALOG(shell)->vbox), label, TRUE, TRUE, 0);
