a7873be185756bba98de2ae2a295d3f5a95d685b
Jason Dorje Short <jdorje@cwazy.co.uk>
2004-11-13 08:41:42 +0000

    Avoid a potential crash if invalid coordinates are passed into overview_to_map_pos.
    Reported by Geert Nijpels <nijpels@euronet.nl> in PR#10927.
    
    [[originally from CVS; cvs2svn created svn r8906]]

--- client/mapview_common.c
+++ client/mapview_common.c
@@ -2318,6 +2318,7 @@ void overview_to_map_pos(int *map_x, int *map_y,
 {
   int ntl_x = overview_x / OVERVIEW_TILE_SIZE + overview.map_x0;
   int ntl_y = overview_y / OVERVIEW_TILE_SIZE + overview.map_y0;
+  struct tile *ptile;
 
   if (MAP_IS_ISOMETRIC && !topo_has_flag(TF_WRAPX)) {
     /* Clip half tile left and right.  See comment in map_to_overview_pos. */
@@ -2325,10 +2326,12 @@ void overview_to_map_pos(int *map_x, int *map_y,
   }
 
   NATURAL_TO_MAP_POS(map_x, map_y, ntl_x, ntl_y);
-  if (!normalize_map_pos(map_x, map_y)) {
-    /* All positions on the overview should be valid. */
-    assert(FALSE);
-  }
+
+  /* HACK: there are reports of normalize_map_pos failing here, so we use
+   * nearest_real_tile instead. */
+  ptile = nearest_real_tile(*map_x, *map_y);
+  *map_x = ptile->x;
+  *map_y = ptile->y;
 }
 
 /**************************************************************************
