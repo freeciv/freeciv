f21bdd611c348200ddbb7df7e61290447828e191
Vasco Alexandre da Silva Costa <vasc@rnl.ist.utl.pt>
2004-12-19 16:48:33 +0000

    Bump up capstr.
    [[originally from CVS; cvs2svn created svn r9380]]

--- common/capstr.c
+++ common/capstr.c
@@ -82,7 +82,7 @@ const char * const our_capability = our_capability_internal;
  *     as long as possible.  We want to maintain network compatibility with
  *     the stable branch for as long as possible.
  */
-#define CAPABILITY "+Freeciv.Devel.2004.Dec.17b"
+#define CAPABILITY "+Freeciv.Devel.2004.Dec.19"
 
 void init_our_capability(void)
 {
