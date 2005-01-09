4ce36954a15a0493a1984353c83ed21cf8e680a3
Mateusz Stefek <mstefek@poczta.fm>
2005-01-09 10:08:09 +0000

    Send updated player info before unit_goes_out_of_sight packets, which are generated in remove_allied_visibility(). See #11819
    [[originally from CVS; cvs2svn created svn r9515]]

--- server/plrhand.c
+++ server/plrhand.c
@@ -1167,6 +1167,10 @@ repeat_break_treaty:
   /* If the old state was alliance, the players' units can share tiles
      illegally, and we need to call resolve_unit_stacks() */
   if (old_type == DS_ALLIANCE) {
+    /* The client needs updated diplomatic state, because it is used
+     * during calculation of new states of occupied flags in cities */
+    send_player_info(pplayer, NULL);
+    send_player_info(pplayer2, NULL);
     remove_allied_visibility(pplayer, pplayer2);
     remove_allied_visibility(pplayer2, pplayer);    
     resolve_unit_stacks(pplayer, pplayer2, TRUE);
