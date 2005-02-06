050e81dde0cc63cfe2756863fa94d08869db2c35
Vasco Alexandre da Silva Costa <vasc@rnl.ist.utl.pt>
2005-02-06 21:39:09 +0000

    PR#12144: switching tilesets crashes freeciv.
    Fixes a crash due to premature local options window destruction.
    
    Patch by me.
    
    [[originally from CVS; cvs2svn created svn r9732]]

--- client/gui-gtk-2.0/dialogs.c
+++ client/gui-gtk-2.0/dialogs.c
@@ -2145,35 +2145,12 @@ gboolean taxrates_callback(GtkWidget * w, GdkEventButton * ev, gpointer data)
   return TRUE;
 }
 
-/**************************************************************************
-...
-**************************************************************************/
-static void nuke_children(gpointer data, gpointer user_data)
-{
-  if (data != user_data) {
-    if (GTK_IS_WINDOW(data) && GTK_WINDOW(data)->type == GTK_WINDOW_TOPLEVEL) {
-      gtk_widget_destroy(data);
-    }
-  }
-}
-
 /********************************************************************** 
   This function is called when the client disconnects or the game is
   over.  It should close all dialog windows for that game.
 ***********************************************************************/
 void popdown_all_game_dialogs(void)
 {
-  GList *res;
-
   gui_dialog_destroy_all();
-
-
-  res = gtk_window_list_toplevels();
-
-  g_list_foreach(res, (GFunc)g_object_ref, NULL);
-  g_list_foreach(res, nuke_children, toplevel);
-  g_list_foreach(res, (GFunc)g_object_unref, NULL);
-
-  g_list_free(res);
 }
 
