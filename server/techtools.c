42578085bb720371e670ef7500ea09a2d3cceb45
Mateusz Stefek <mstefek@poczta.fm>
2005-09-09 07:52:38 +0000

    A_UNSET was practically unused, because update_tech() called found_new_tech(). Reported by Jason in PR#13910
    [[originally from CVS; cvs2svn created svn r10974]]

--- server/techtools.c
+++ server/techtools.c
@@ -460,25 +460,20 @@ void update_tech(struct player *plr, int bulbs)
   int excessive_bulbs;
   struct player_research *research = get_player_research(plr);
 
-  /* At the end of the turn an unset tech target will lead to one being
-   * picked randomly.  You can't save bulbs over between turns. */
-  if (research->researching == A_UNSET) {
-    choose_random_tech(plr);
-  }
-  assert(research->researching != A_UNSET);
-
   /* count our research contribution this turn */
   plr->bulbs_last_turn += bulbs;
 
   research->bulbs_researched += bulbs;
-  
-  excessive_bulbs =
+
+  if (research->researching != A_UNSET) {  
+    excessive_bulbs =
       (research->bulbs_researched - total_bulbs_required(plr));
 
-  if (excessive_bulbs >= 0) {
-    tech_researched(plr);
-    if (research->researching != A_UNSET) {
-      update_tech(plr, 0);
+    if (excessive_bulbs >= 0) {
+      tech_researched(plr);
+      if (research->researching != A_UNSET) {
+        update_tech(plr, 0);
+      }
     }
   }
 }
