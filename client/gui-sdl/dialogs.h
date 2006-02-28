2fbb843e7be3132155b2ef6b15a38732fcb7325c
Christian Prochaska <cp.ml.freeciv.dev@googlemail.com>
2006-02-28 19:11:20 +0000

    Move diplomat/spy dialogs to diplomat_dialog.c.
    [[originally from svn r11676]]

--- client/gui-sdl/dialogs.h
+++ client/gui-sdl/dialogs.h
@@ -29,11 +29,24 @@
 struct GUI;
   
 void popup_advanced_terrain_dialog(struct tile *ptile, Uint16 pos_x, Uint16 pos_y);
+void popdown_advanced_terrain_dialog(void);
+
 const char *sdl_map_get_tile_info_text(struct tile *pTile);
 const char *sdl_get_tile_defense_info_text(struct tile *pTile);
 void put_window_near_map_tile(struct GUI *pWindow,
   		int window_width, int window_height, struct tile *ptile);
 void popup_unit_upgrade_dlg(struct unit *pUnit, bool city);
 void popup_revolution_dialog(void);
-  
+
+#define create_active_iconlabel(pBuf, pDest, pStr, pString, pCallback)   \
+do { 									 \
+  pStr = create_str16_from_char(pString, 10);				 \
+  pStr->style |= TTF_STYLE_BOLD;					 \
+  pBuf = create_iconlabel(NULL, pDest, pStr, 				 \
+    	     (WF_DRAW_THEME_TRANSPARENT|WF_DRAW_TEXT_LABEL_WITH_SPACE)); \
+  pBuf->string16->bgcol.unused = 128;					 \
+  pBuf->string16->render = 3;						 \
+  pBuf->action = pCallback;						 \
+} while(0)
+
 #endif	/* FC__DIALOGS_H */
