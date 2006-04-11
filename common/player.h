93e4f200e779417ad79fe248e14041edc5a6db35
Jason Dorje Short <jdorje@gmail.com>
2006-04-11 07:05:50 +0000

    Revert: PR#16096.
    [[originally from svn r11840]]

--- common/player.h
+++ common/player.h
@@ -27,8 +27,8 @@
 #define PLAYER_DEFAULT_SCIENCE_RATE 100
 #define PLAYER_DEFAULT_LUXURY_RATE 0
 
-#define ANON_PLAYER_NAME _("noname")
-#define ANON_USER_NAME _("Unassigned")
+#define ANON_PLAYER_NAME "noname"
+#define ANON_USER_NAME "Unassigned"
 
 /*
  * pplayer->ai.barbarian_type uses this enum. Note that the values
