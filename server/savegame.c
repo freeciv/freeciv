5fc8c1aca927962780e082af523422a67d882f4c
Jason Dorje Short <jdorje@gmail.com>
2006-04-24 18:30:22 +0000

    Save and reload the game.allow_take variable.
    Reported by Nanakaland <nanakaland@gmail.com> in PR#16540.
    
    [[originally from svn r11888]]

--- server/savegame.c
+++ server/savegame.c
@@ -3661,6 +3661,9 @@ void game_load(struct section_file *file)
     sz_strlcpy(game.demography,
 	       secfile_lookup_str_default(file, GAME_DEFAULT_DEMOGRAPHY,
 					  "game.demography"));
+    sz_strlcpy(game.allow_take,
+	       secfile_lookup_str_default(file, GAME_DEFAULT_ALLOW_TAKE,
+					  "game.allow_take"));
 
     game.info.spacerace = secfile_lookup_bool_default(file, game.info.spacerace,
 						"game.spacerace");
@@ -4129,6 +4132,7 @@ void game_save(struct section_file *file, const char *save_reason)
   secfile_insert_int(file, game.info.occupychance, "game.occupychance");
   secfile_insert_bool(file, game.info.autoattack, "game.autoattack");
   secfile_insert_str(file, game.demography, "game.demography");
+  secfile_insert_str(file, game.allow_take, "game.allow_take");
   secfile_insert_int(file, game.info.borders, "game.borders");
   secfile_insert_bool(file, game.info.happyborders, "game.happyborders");
   secfile_insert_int(file, game.info.diplomacy, "game.diplomacy");
