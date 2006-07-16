077537ac5c5c69224c8110589d8a9519ec45b21c
Per Inge Mathisen <per.mathisen@gmail.com>
2006-07-16 12:49:53 +0000

    Fix possible crashes in network code. Reported by Luigi Auriemma <aluigi@autistici.org>.
    [[originally from svn r12107]]

--- common/packets.c
+++ common/packets.c
@@ -562,6 +562,7 @@ void generic_handle_player_attribute_chunk(struct player *pplayer,
 	  chunk->total_length, chunk->chunk_length);
 
   if (chunk->total_length < 0
+      || chunk->chunk_length < 0
       || chunk->total_length >= MAX_ATTRIBUTE_BLOCK
       || chunk->offset < 0
       || chunk->offset + chunk->chunk_length > chunk->total_length
