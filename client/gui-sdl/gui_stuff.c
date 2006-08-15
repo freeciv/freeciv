ca3d40836aef612d8ea69424aa44b26d7d74dae8
Christian Prochaska <cp.ml.freeciv.dev@googlemail.com>
2006-08-15 15:40:43 +0000

    use user-defined colors for button texts (widget_*_text in colors.themespec)
    [[originally from svn r12260]]

--- client/gui-sdl/gui_stuff.c
+++ client/gui-sdl/gui_stuff.c
@@ -3071,8 +3071,15 @@ int real_redraw_ibutton(struct widget *pIButton)
     /* make copy of string16 */
     TMPString = *pIButton->string16;
 
-    if (get_wstate(pIButton) == FC_WS_SELLECTED) {
+    if (get_wstate(pIButton) == FC_WS_NORMAL) {
+      TMPString.fgcol = *get_game_colorRGB(COLOR_THEME_WIDGET_NORMAL_TEXT);
+    } else if (get_wstate(pIButton) == FC_WS_SELLECTED) {
+      TMPString.fgcol = *get_game_colorRGB(COLOR_THEME_WIDGET_SELECTED_TEXT);
       TMPString.style |= TTF_STYLE_BOLD;
+    } else if (get_wstate(pIButton) == FC_WS_PRESSED) {
+      TMPString.fgcol = *get_game_colorRGB(COLOR_THEME_WIDGET_PRESSED_TEXT);
+    } else if (get_wstate(pIButton) == FC_WS_DISABLED) {
+      TMPString.fgcol = *get_game_colorRGB(COLOR_THEME_WIDGET_DISABLED_TEXT);
     }
 
     pText = create_text_surf_from_str16(&TMPString);
