cd40f441c60fc028412a48878db646bdca55a274
Marko Lindqvist <cazfi74@gmail.com>
2007-01-25 15:40:24 +0000

    Do not mark current nation available for others in handle_nation_select_req() when requested nation is same as current nation. player_set_nation() does all the nation assignment work correctly for us.
    Problem reported by Karl-Ingo Friese <kif@gdv.uni-hannover.de>
    and Daniel Markstedt <markstedt@gmail.com> in #17436
    
    [[originally from svn r12549]]

--- server/srv_main.c
+++ server/srv_main.c
@@ -1304,7 +1304,7 @@ void handle_nation_select_req(struct player *requestor,
 			      Nation_type_id nation_no, bool is_male,
 			      char *name, int city_style)
 {
-  struct nation_type *old_nation, *new_nation;
+  struct nation_type *new_nation;
   struct player *pplayer = get_player(player_no);
 
   if (server_state != PRE_GAME_STATE || !pplayer) {
@@ -1315,7 +1315,6 @@ void handle_nation_select_req(struct player *requestor,
     return;
   }
 
-  old_nation = pplayer->nation;
   new_nation = get_nation_by_idx(nation_no);
 
   if (new_nation != NO_NATION_SELECTED) {
@@ -1362,10 +1361,6 @@ void handle_nation_select_req(struct player *requestor,
 
   (void) player_set_nation(pplayer, new_nation);
   send_player_info_c(pplayer, game.est_connections);
-
-  if (old_nation != NO_NATION_SELECTED) {
-    old_nation->player = NULL;
-  }
 }
 
 /****************************************************************************
