fb56121ae2c3ac9982faf6d29c56c9c9521ecd67
Marko Lindqvist <cazfi74@gmail.com>
2007-08-04 21:42:38 +0000

    Removed assert that fail initial root_req is simply processed later than tech depending on it.
    See #38323
    
    [[originally from svn r13163]]

--- server/techtools.c
+++ server/techtools.c
@@ -278,7 +278,9 @@ void found_new_tech(struct player *plr, Tech_type_id tech_found,
   assert((tech_exists(tech_found)
 	  && get_invention(plr, tech_found) != TECH_KNOWN)
 	 || tech_found == A_FUTURE);
-  assert(tech_is_available(plr, tech_found) || tech_found == A_FUTURE);
+  /* It is possible that !tech_is_available() simply because we are only
+   * setting up initial techs and root_req is one of them.
+   * assert(tech_is_available(plr, tech_found) || tech_found == A_FUTURE); */
 
   /* got_tech allows us to change research without applying techpenalty
    * (without loosing bulbs) */
