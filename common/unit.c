eb0f6cabe911b456b5371243342b1a2e38513810
Marko Lindqvist <cazfi74@gmail.com>
2007-08-06 13:21:40 +0000

    Removed concept of foul spies. Broken code was causing problems.
    Reported by Joona Kurki <joona.kurki@tkk.fi> in #38165
    
    [[originally from svn r13191]]

--- common/unit.c
+++ common/unit.c
@@ -1404,7 +1404,6 @@ struct unit *create_unit_virtual(struct player *pplayer, struct city *pcity,
   memset(punit->upkeep, 0, O_COUNT * sizeof(*punit->upkeep));
   punit->unhappiness = 0;
   /* A unit new and fresh ... */
-  punit->foul = FALSE;
   punit->debug = FALSE;
   punit->fuel = unit_type(punit)->fuel;
   punit->hp = unit_type(punit)->hp;
