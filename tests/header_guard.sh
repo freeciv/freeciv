3dc963d8789437e464564d46f5d32dc16b692543
Marko Lindqvist <cazfi74@gmail.com>
2007-08-31 17:12:11 +0000

    Added more 3rd party programs to lists of src-check scripts to ignore.
    See #39633
    
    [[originally from svn r13445]]

--- tests/header_guard.sh
+++ tests/header_guard.sh
@@ -4,9 +4,13 @@ files=`find $1 -name "*.h" \
        | sort \
        | grep -v intl \
        | grep -v common/spec \
-       | grep -v "Freeciv.h" \
-       | grep -v ./common/packets_gen.h \
-       | grep -v config.h`
+       | grep -v "Freeciv\.h" \
+       | grep -v \./common/packets_gen\.h \
+       | grep -v config\.h \
+       | grep -v dependencies \
+       | grep -v utility/md5\.h \
+       | grep -v client/gui-sdl/SDL_rotozoom\.h \
+       | grep -v client/gui-sdl/SDL_ttf\.h `
 
 echo "# Header files without any include guard:"
 for file in $files; do
