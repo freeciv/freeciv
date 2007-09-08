b113b2b438cfa99079c1d7bc70fea9177f9e5d94
Christian Prochaska <cp.ml.freeciv.dev@googlemail.com>
2007-09-08 17:50:31 +0000

    - Replace Vera.ttf by DejaVuSans.ttf (http://dejavu.sourceforge.net/), which contains more characters than Vera.ttf. - Fix a bug in SDL_ttf that caused wrong line spacings with the new font.
    Requested by Nikita Bukhvostov <dragon.djanic@gmail.com> in PR#39675, patch by me.
    
    [[originally from svn r13537]]

--- client/gui-sdl/SDL_ttf.c
+++ client/gui-sdl/SDL_ttf.c
@@ -309,8 +309,8 @@ TTF_Font* TTF_OpenFontIndexRW( SDL_RWops *src, int freesrc, int ptsize, long ind
 
 	  /* Get the scalable font metrics for this font */
 	  scale = face->size->metrics.y_scale;
-	  font->ascent  = FT_CEIL(FT_MulFix(face->bbox.yMax, scale));
-	  font->descent = FT_CEIL(FT_MulFix(face->bbox.yMin, scale));
+	  font->ascent  = FT_CEIL(FT_MulFix(face->ascender, scale));
+	  font->descent = FT_CEIL(FT_MulFix(face->descender, scale));
 	  font->height  = font->ascent - font->descent + /* baseline */ 1;
 	  font->lineskip = FT_CEIL(FT_MulFix(face->height, scale));
 	  font->underline_offset = FT_FLOOR(FT_MulFix(face->underline_position, scale));
