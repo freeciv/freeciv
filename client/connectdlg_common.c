2a534b6aedf73e1ed784eca45f8c66d5f8d6fa41
Christian Prochaska <cp.ml.freeciv.dev@googlemail.com>
2007-09-11 12:08:30 +0000

    Fix the problem that my_shutdown_network() was called too early. See PR#39692.
    [[originally from svn r13564]]

--- client/connectdlg_common.c
+++ client/connectdlg_common.c
@@ -178,14 +178,6 @@ void client_kill_server(bool force)
   client_has_hack = FALSE;
 }   
 
-/**************************************************************************
- This is called at program exit.
-**************************************************************************/
-static void server_shutdown(void)
-{
-  client_kill_server(TRUE);
-}
-                                                                               
 /**************************************************************** 
 forks a server if it can. returns FALSE is we find we couldn't start
 the server.
@@ -211,17 +203,11 @@ bool client_start_server(void)
   char logcmdline[512];
   char scriptcmdline[512];
 # endif
-  static bool initialized = FALSE;
 
   /* only one server (forked from this client) shall be running at a time */
   /* This also resets client_has_hack. */
   client_kill_server(TRUE);
   
-  if (!initialized) {
-    atexit(server_shutdown);
-    initialized = TRUE;
-  }
-
   append_output_window(_("Starting server..."));
 
   /* find a free port */ 
