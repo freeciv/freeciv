d4996de32933ccca88f75ca6fc3bd17d826a0340
William Allen Simpson <william.allen.simpson@gmail.com>
2008-01-27 12:21:14 +0000

    (PR#40028) gtk/pango invalid UTF-8 warning, fix documentation
    [[originally from svn r14330]]

--- utility/shared.c
+++ utility/shared.c
@@ -1223,6 +1223,9 @@ char *datafilename_required(const char *filename)
   }
 }
 
+/***************************************************************************
+  Language environmental variable.
+***************************************************************************/
 char *get_langname(void)
 {
   char *langname = NULL;      
