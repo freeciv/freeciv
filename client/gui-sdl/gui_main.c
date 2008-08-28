b98b8c363d5f2d5ce145afe9c1e693516fd80a61
Marko Lindqvist <cazfi74@gmail.com>
2008-08-28 21:02:10 +0000

    Fixed sdl-client compilation
    See #40468
    
    [[originally from svn r15182]]

--- client/gui-sdl/gui_main.c
+++ client/gui-sdl/gui_main.c
@@ -560,7 +560,7 @@ Uint16 gui_event_loop(void *pData,
       tv.tv_sec = 0;
       tv.tv_usec = 10000;/* 10ms*/
     
-      result = my_select(MAX(net_socket, ggz_socket) + 1, &civfdset, NULL, NULL, &tv);
+      result = fc_select(MAX(net_socket, ggz_socket) + 1, &civfdset, NULL, NULL, &tv);
       if (result < 0) {
         if (errno != EINTR) {
 	  break;
