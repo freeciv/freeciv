afa2508f3c51fb427dc7c76dbb9f93e4feae799c
Madeline Book <madeline.book@gmail.com>
2008-10-06 22:16:03 +0000

    Show public chat messages in server console.
    See #21794
    
    [[originally from svn r15243]]

--- server/handchat.c
+++ server/handchat.c
@@ -26,6 +26,7 @@
 #include "shared.h"
 #include "support.h"
 
+#include "console.h"
 #include "stdinhand.h"
 
 #include "handchat.h"
@@ -312,6 +313,7 @@ void handle_chat_msg_req(struct connection *pconn, char *message)
   form_chat_name(pconn, sender_name, sizeof(sender_name));
   my_snprintf(chat, sizeof(chat),
 	      "<%s> %s", sender_name, message);
+  con_puts(C_COMMENT, chat);
   dlsend_packet_chat_msg(game.est_connections, chat,
 			 -1, -1, E_CHAT_MSG, pconn->id);
 }
