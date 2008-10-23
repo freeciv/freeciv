792676ef39d88520c53aac5ccc347e201ae8f82b
Madeline Book <madeline.book@gmail.com>
2008-10-23 00:30:44 +0000

    Make shallow ocean areas near coast for the island generator.
    See #40503
    
    Reported by: Andreas Røsdal <andrearo@pvv.ntnu.no>
    
    [[originally from svn r15278]]

--- server/generator/utilities.h
+++ server/generator/utilities.h
@@ -16,12 +16,14 @@
 typedef void (*tile_knowledge_cb)(struct tile *ptile);
 
 void regenerate_lakes(tile_knowledge_cb knowledge_cb);
+void smooth_water_depth(void);
 void assign_continent_numbers(void);
 int get_lake_surrounders(Continent_id cont);
 int get_continent_size(Continent_id id);
 int get_ocean_size(Continent_id id);
 
 struct terrain *most_shallow_ocean(void);
+struct terrain *pick_ocean(int depth);
 
 /* Provide a block to convert from native to map coordinates.  For instance
  *   do_in_map_pos(mx, my, xn, yn) {
