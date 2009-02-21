91038735f9c33238dd008614b5c054ea5b2f854a
Madeline Book <madeline.book@gmail.com>
2009-02-21 23:08:30 +0000

    Option to have new events appear at the top of the message window.
    - Some cleanup and generalization of message
      window code.
    - The message window list store now keeps track
      of the "meswin" row of an event added to the
      store. This value is the index passed to such
      functions as get_message() and meswin_goto().
    
    See #40718
    
    [[originally from svn r15524]]

--- client/gui-gtk-2.0/gui_main.h
+++ client/gui-gtk-2.0/gui_main.h
@@ -28,6 +28,7 @@ extern bool enable_tabs;
 extern bool better_fog;
 extern bool show_chat_message_time;
 extern bool split_bottom_notebook;
+extern bool new_messages_go_to_top;
 extern bool metaserver_tab_first;
 
 extern GdkGC *          civ_gc;
