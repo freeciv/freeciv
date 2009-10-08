457dd450bcce4f71cbee74c577475820ceb05d8c
pepeto <pepeto69@gmail.com>
2009-10-08 22:28:58 +0000

    [Compilation fix] Remove gtk_action_set_label() because it was included in Gtk 2.16.
    Reported by Matthias Pfafferodt (syntron)
    
    See gna bug #14483
    
    [[originally from svn r16077]]

--- client/gui-gtk-2.0/menu.c
+++ client/gui-gtk-2.0/menu.c
@@ -1664,7 +1664,8 @@ static void menus_rename(GtkActionGroup *group,
     return;
   }
 
-  gtk_action_set_label(action, new_label);
+  /* gtk_action_set_label() was added in Gtk 2.16. */
+  g_object_set(G_OBJECT(action), "label", new_label, NULL);
 }
 
 /****************************************************************
