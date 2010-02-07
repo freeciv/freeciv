d8d236b812909207f7b71179b7d7294dfdecff93
pepeto <pepeto69@gmail.com>
2010-02-07 21:19:43 +0000

    After a global warming or a nuclear winter, never change the city center terrain to a terrain which doesn't support cities (e.g. ice or ocean).
    Reported by bernd.jendrissek@gmail.com
    Patch by Matthias Pfafferodt (syntron)
    
    See gna bug #15258
    
    [[originally from svn r16740]]

--- server/maphand.c
+++ server/maphand.c
@@ -100,6 +100,13 @@ void global_warming(int effect)
     } else {
       new = old->warmer_drier_result;
     }
+
+    if (tile_city(ptile) != NULL && terrain_has_flag(new, TER_NO_CITIES)) {
+      /* do not change to a terrain with the flag TER_NO_CITIES if the tile
+       * has a city */
+      continue;
+    }
+
     if (new != T_NONE && old != new) {
       effect--;
       tile_change_terrain(ptile, new);
@@ -144,6 +151,13 @@ void nuclear_winter(int effect)
     } else {
       new = old->cooler_drier_result;
     }
+
+    if (tile_city(ptile) != NULL && terrain_has_flag(new, TER_NO_CITIES)) {
+      /* do not change to a terrain with the flag TER_NO_CITIES if the tile
+       * has a city */
+      continue;
+    }
+
     if (new != T_NONE && old != new) {
       effect--;
       tile_change_terrain(ptile, new);
