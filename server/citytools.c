0ab2aa763630b5e13c136c32cd66e774245bad3c
Matthias Pfafferodt <matthias.pfafferodt@mapfa.de>
2010-02-26 21:12:01 +0000

    'migration_score' is used uninitialized in packets
    solution: initialize it and rename the variable to 'migration_score_unused'
    
    see gna bug #15332
    
    bug reported by Andreas <anbe>
    
    [[originally from svn r16934]]

--- server/citytools.c
+++ server/citytools.c
@@ -1868,6 +1868,8 @@ void package_city(struct city *pcity, struct packet_city_info *packet,
   packet->turn_last_built=pcity->turn_last_built;
   packet->turn_founded = pcity->turn_founded;
 
+  packet->migration_score_unused = 0.0;
+
   packet->changed_from_kind = pcity->changed_from.kind;
   packet->changed_from_value = universal_number(&pcity->changed_from);
 
