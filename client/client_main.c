42999aa92fbe696c8dbe76bc58ed178249522a2a
pepeto <pepeto69@gmail.com>
2010-03-03 21:25:00 +0000

    Revert partially patch #1326 and document why it's not a good idea to use client.conn as the connection info of our client.
    Reported by Matthias Pfafferodt (syntron)
    
    See gna bug #15500
    
    [[originally from svn r16987]]

--- client/client_main.c
+++ client/client_main.c
@@ -761,9 +761,8 @@ void client_remove_cli_conn(struct connection *pconn)
   }
   conn_list_unlink(game.all_connections, pconn);
   conn_list_unlink(game.est_connections, pconn);
-  if (pconn != &client.conn) {
-    free(pconn);
-  }
+  RETURN_IF_FAIL(pconn != &client.conn);
+  free(pconn);
 }
 
 /**************************************************************************
