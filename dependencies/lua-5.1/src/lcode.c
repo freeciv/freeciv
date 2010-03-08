37753197385c91914818e025ca48985ef4e5ddb9
Marko Lindqvist <cazfi74@gmail.com>
2010-03-08 15:47:53 +0000

    Applied official patches from lua.org
    See gna patch #1503
    
    [[originally from svn r17024]]

--- dependencies/lua-5.1/src/lcode.c
+++ dependencies/lua-5.1/src/lcode.c
@@ -544,15 +544,19 @@ void luaK_goiftrue (FuncState *fs, expdesc *e) {
       pc = NO_JUMP;  /* always true; do nothing */
       break;
     }
-    case VFALSE: {
-      pc = luaK_jump(fs);  /* always jump */
-      break;
-    }
     case VJMP: {
       invertjump(fs, e);
       pc = e->u.s.info;
       break;
     }
+    case VFALSE: {
+      if (!hasjumps(e)) {
+        pc = luaK_jump(fs);  /* always jump */
+        break;
+      }
+      /* else go through */
+    }
+
     default: {
       pc = jumponcond(fs, e, 0);
       break;
@@ -572,13 +576,16 @@ static void luaK_goiffalse (FuncState *fs, expdesc *e) {
       pc = NO_JUMP;  /* always false; do nothing */
       break;
     }
-    case VTRUE: {
-      pc = luaK_jump(fs);  /* always jump */
-      break;
-    }
     case VJMP: {
       pc = e->u.s.info;
       break;
+    }
+    case VTRUE: {
+      if (!hasjumps(e)) {
+        pc = luaK_jump(fs);  /* always jump */
+        break;
+      }
+      /* else go through */
     }
     default: {
       pc = jumponcond(fs, e, 1);
--- dependencies/lua-5.1/src/ldblib.c
+++ dependencies/lua-5.1/src/ldblib.c
@@ -45,6 +45,7 @@ static int db_setmetatable (lua_State *L) {
 
 
 static int db_getfenv (lua_State *L) {
+  luaL_checkany(L, 1);
   lua_getfenv(L, 1);
   return 1;
 }
