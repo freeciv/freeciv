4a1a4b0ac52a5c9db16384454e64364cb5efde33
Marko Lindqvist <cazfi74@gmail.com>
2010-03-08 15:48:06 +0000

    Applied official patches from lua.org
    See gna patch #1503
    
    [[originally from svn r17025]]

--- dependencies/lua-5.1/src/llex.c
+++ dependencies/lua-5.1/src/llex.c
@@ -118,8 +118,10 @@ TString *luaX_newstring (LexState *ls, const char *str, size_t l) {
   lua_State *L = ls->L;
   TString *ts = luaS_newlstr(L, str, l);
   TValue *o = luaH_setstr(L, ls->fs->h, ts);  /* entry for `str' */
-  if (ttisnil(o))
+  if (ttisnil(o)) {
     setbvalue(o, 1);  /* make sure `str' will not be collected */
+    luaC_checkGC(L);
+  }
   return ts;
 }
 
