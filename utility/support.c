b3833c36f26e9d264ef33779cc133341050171e0
pepeto <pepeto69@gmail.com>
2010-05-08 16:18:54 +0000

    Replace remaning my_nonblock()s with fc_nonblock().
    Patch by Jonathan Landis (jkl)
    
    See gna bug #15983
    
    [[originally from svn r17421]]

--- utility/support.c
+++ utility/support.c
@@ -773,7 +773,7 @@ void my_init_console(void)
   if (!initialized) {
     initialized = 1;
 #ifdef HAVE_FILENO
-    my_nonblock(fileno(stdin));
+    fc_nonblock(fileno(stdin));
 #endif
   }
 #endif
@@ -799,7 +799,7 @@ char *my_read_console(void)
   if (!feof(stdin)) {    /* input from server operator */
     static char *bufptr = mybuf;
     /* fetch chars until \n, or run out of space in buffer */
-    /* blocks if my_nonblock() in my_init_console() failed */
+    /* blocks if fc_nonblock() in my_init_console() failed */
     while ((*bufptr = fgetc(stdin)) != EOF) {
       if (*bufptr == '\n') *bufptr = '\0';
       if (*bufptr == '\0') {
