7aecc25f04b74cf3d2268ecc6b292b06c7c21432
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2010-05-16 12:55:25 +0000

    Fix typos in English version of settings help.
    Patch by Joan Creus (jcreus).
    
    See gna bug #15890
    
    [[originally from svn r17429]]

--- server/settings.c
+++ server/settings.c
@@ -1632,9 +1632,9 @@ static struct setting settings[] = {
                 "Example: 'civgame-T%04T-Y%+05Y-%R' => "
                 "'civgame-T0100-Y00001-manual'\n"
                 "\n"
-                "Be carefull to use at least one of %T and %Y, else newer "
-                "savegames will overwrite old ones. If non of the formats "
-                "are used '-T%04T-Y%05Y-%R' is appended to the value of "
+                "Be careful to use at least one of %T and %Y, else newer "
+                "savegames will overwrite old ones. If none of the formats "
+                "is used '-T%04T-Y%05Y-%R' is appended to the value of "
                 "'savename'."),
              NULL, NULL, GAME_DEFAULT_SAVE_NAME)
 
