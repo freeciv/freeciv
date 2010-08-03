37a170529f62978ffa1f65f30ae269c8eeedb762
Marko Lindqvist <cazfi74@gmail.com>
2010-08-03 08:09:34 +0000

    Moved ai function pointer setting to ai side in new function fc_ai_default_setup(). fc_ai_default_setup() is now the only entry point to ai code needed by ai_init().
    See gna patch #1792
    
    [[originally from svn r17639]]

---
+++ ai/defaultai.c
@@ -0,0 +1,64 @@
+/***********************************************************************
+ Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+***********************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+/* common */
+#include "ai.h"
+#include "player.h"
+
+/* server/advisors */
+#include "autosettlers.h"
+
+/* ai */
+#include "aicity.h"
+#include "aiexplorer.h"
+#include "aihand.h"
+#include "aisettler.h"
+#include "aitools.h"
+#include "aiunit.h"
+
+#include "defaultai.h"
+
+/**************************************************************************
+  Setup player ai_funcs function pointers.
+**************************************************************************/
+void fc_ai_default_setup(struct ai_type *ai)
+{
+  ai->funcs.data_init = ai_data_init;
+  ai->funcs.data_default = ai_data_default;
+  ai->funcs.data_close = ai_data_close;
+
+  ai->funcs.city_init = ai_city_init;
+  ai->funcs.city_update = ai_city_update;
+  ai->funcs.city_close = ai_city_close;
+
+  ai->funcs.unit_init = ai_unit_init;
+  ai->funcs.unit_turn_end = ai_unit_turn_end;
+  ai->funcs.unit_close = ai_unit_close;
+
+  ai->funcs.auto_settlers = auto_settlers_player;
+  ai->funcs.building_advisor_init = ai_manage_buildings;
+  ai->funcs.building_advisor = ai_advisor_choose_building;
+  ai->funcs.auto_explorer = ai_manage_explorer;
+  ai->funcs.first_activities = ai_do_first_activities;
+  ai->funcs.diplomacy_actions = ai_diplomacy_actions;
+  ai->funcs.last_activities = ai_do_last_activities;
+  ai->funcs.before_auto_settlers = ai_settler_init;
+  ai->funcs.treaty_evaluate = ai_treaty_evaluate;
+  ai->funcs.treaty_accepted = ai_treaty_accepted;
+  ai->funcs.first_contact = ai_diplomacy_first_contact;
+  ai->funcs.incident = ai_incident;
+}
--- server/aiiface.c
+++ server/aiiface.c
@@ -26,16 +26,10 @@
 #include "autosettlers.h"
 
 /* ai */
-#include "aicity.h"
-#include "aidata.h"
-#include "aiexplorer.h"
-#include "aihand.h"
-#include "aisettler.h"
-#include "aitools.h"
-#include "aiunit.h"
+#include "defaultai.h"
 
 /**************************************************************************
-  Initialize player ai_funcs function pointers.
+  Initialize ai_type.
 **************************************************************************/
 void ai_init(void)
 {
@@ -43,30 +37,7 @@ void ai_init(void)
 
   init_ai(ai);
 
-  ai->funcs.data_init = ai_data_init;
-  ai->funcs.data_default = ai_data_default;
-  ai->funcs.data_close = ai_data_close;
-
-  ai->funcs.city_init = ai_city_init;
-  ai->funcs.city_update = ai_city_update;
-  ai->funcs.city_close = ai_city_close;
-
-  ai->funcs.unit_init = ai_unit_init;
-  ai->funcs.unit_turn_end = ai_unit_turn_end;
-  ai->funcs.unit_close = ai_unit_close;
-
-  ai->funcs.auto_settlers = auto_settlers_player;
-  ai->funcs.building_advisor_init = ai_manage_buildings;
-  ai->funcs.building_advisor = ai_advisor_choose_building;
-  ai->funcs.auto_explorer = ai_manage_explorer;
-  ai->funcs.first_activities = ai_do_first_activities;
-  ai->funcs.diplomacy_actions = ai_diplomacy_actions;
-  ai->funcs.last_activities = ai_do_last_activities;
-  ai->funcs.before_auto_settlers = ai_settler_init;
-  ai->funcs.treaty_evaluate = ai_treaty_evaluate;
-  ai->funcs.treaty_accepted = ai_treaty_accepted;
-  ai->funcs.first_contact = ai_diplomacy_first_contact;
-  ai->funcs.incident = ai_incident;
+  fc_ai_default_setup(ai);
 }
 
 /**************************************************************************
