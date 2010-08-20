e40ef0d6777319fd9dfebee40e7ad4fcc53b9bf1
pepeto <pepeto69@gmail.com>
2010-08-20 10:44:15 +0000

    Adapt Book's rewonder ruleset in multiplayer ruleset.
    See gna patch #1838
    
    [[originally from svn r17816]]

--- data/multiplayer/script.lua
+++ data/multiplayer/script.lua
@@ -1,14 +1,3 @@
--- Freeciv - Copyright (C) 2007 - The Freeciv Project
---   This program is free software; you can redistribute it and/or modify
---   it under the terms of the GNU General Public License as published by
---   the Free Software Foundation; either version 2, or (at your option)
---   any later version.
---
---   This program is distributed in the hope that it will be useful,
---   but WITHOUT ANY WARRANTY; without even the implied warranty of
---   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---   GNU General Public License for more details.
-
 function city_destroyed_callback(city, loser, destroyer)
   create_base(city.tile, "Ruins", NIL)
   -- continue processing
@@ -16,3 +5,33 @@ function city_destroyed_callback(city, loser, destroyer)
 end
 
 signal.connect("city_destroyed", "city_destroyed_callback")
+
+
+tevo_tech = find.tech_type("Theory of Evolution")
+darw_btype = find.building_type("Darwin's Voyage")
+darw_id = darw_btype.id
+
+function building_built_handler(btype, city)
+  local player, id = city.owner, btype.id
+  if id == darw_id then
+    if give_technology(player, tevo_tech) then
+      give_technology(player, nil)
+      give_technology(player, nil)
+    end
+  end
+end
+
+signal.connect("building_built", "building_built_handler")
+
+
+phil_tech = find.tech_type("Philosophy")
+phil_id = phil_tech.id
+
+function tech_researched_handler(tech, player, how)
+  local id = tech.id
+  if id == phil_id and how == "researched" then
+    give_technology(player, nil)
+  end
+end 
+
+signal.connect("tech_researched", "tech_researched_handler")
