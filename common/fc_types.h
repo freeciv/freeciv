703ad53d3d9c64af9c530c1388be3d8b9c55acdd
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2010-09-15 00:31:04 +0000

    Increase MAX_LEN_NAME from 32 octets to 48.
    See gna patch #1924
    
    [[originally from svn r18026]]

--- common/fc_types.h
+++ common/fc_types.h
@@ -38,7 +38,7 @@
 #define MAX_NUM_USER_UNIT_FLAGS 4
 
 /* Changing these will probably break network compatability. */
-#define MAX_LEN_NAME     32
+#define MAX_LEN_NAME     48
 #define MAX_LEN_DEMOGRAPHY 16
 #define MAX_LEN_ALLOW_TAKE 16
 #define MAX_LEN_GAME_IDENTIFIER 33
