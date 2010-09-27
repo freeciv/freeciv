ec58ce9d6f62e24be760e4b2d2584b5fad86535d
pepeto <pepeto69@gmail.com>
2010-09-27 20:17:54 +0000

    Implements the command echo system like in Warserver to control what command lines should be displayed by the users.
    From a report by Marko Lindqvist (cazfi).
    
    See gna bug #15472.
    
    [[originally from svn r18097]]

--- server/commands.h
+++ server/commands.h
@@ -16,6 +16,12 @@
 
 #include "connection.h"         /* enum cmdlevel */
 
+enum cmd_echo {
+  CMD_ECHO_NONE = 0,
+  CMD_ECHO_ADMINS,      /* i.e. all with 'admin' access and above. */
+  CMD_ECHO_ALL,
+};
+
 /**************************************************************************
   Commands - can be recognised by unique prefix
 **************************************************************************/
@@ -98,6 +104,7 @@ const char *command_short_help(const struct command *pcommand);
 const char *command_extra_help(const struct command *pcommand);
 
 enum cmdlevel command_level(const struct command *pcommand);
+enum cmd_echo command_echo(const struct command *pcommand);
 int command_vote_flags(const struct command *pcommand);
 int command_vote_percent(const struct command *pcommand);
 
--- server/stdinhand.c
+++ server/stdinhand.c
@@ -287,7 +287,7 @@ static void cmd_reply_line(enum command_id cmd, struct connection *caller,
   if (rfc_status == C_OK) {
     struct packet_chat_msg packet;
 
-    package_event(&packet, NULL, E_SETTING, ftc_command, "%s", line);
+    package_event(&packet, NULL, E_SETTING, ftc_server, "%s", line);
     conn_list_iterate(game.est_connections, pconn) {
       /* Do not tell caller, since he was told above! */
       if (caller != pconn) {
@@ -295,6 +295,11 @@ static void cmd_reply_line(enum command_id cmd, struct connection *caller,
       }
     } conn_list_iterate_end;
     event_cache_add_for_all(&packet);
+
+    if (NULL != caller) {
+      /* Echo to the console. */
+      log_normal("%s", line);
+    }
   }
 }
 
@@ -428,9 +433,9 @@ static void open_metaserver_connection(struct connection *caller)
 {
   server_open_meta();
   if (send_server_info_to_metaserver(META_INFO)) {
-    notify_conn(NULL, NULL, E_CONNECTION, ftc_server,
-                _("Open metaserver connection to [%s]."),
-                meta_addr_port());
+    cmd_reply(CMD_METACONN, caller, C_OK,
+              _("Open metaserver connection to [%s]."),
+              meta_addr_port());
   }
 }
 
@@ -441,9 +446,9 @@ static void close_metaserver_connection(struct connection *caller)
 {
   if (send_server_info_to_metaserver(META_GOODBYE)) {
     server_close_meta();
-    notify_conn(NULL, NULL, E_CONNECTION, ftc_server,
-                _("Close metaserver connection to [%s]."),
-                meta_addr_port());
+    cmd_reply(CMD_METACONN, caller, C_OK,
+              _("Close metaserver connection to [%s]."),
+              meta_addr_port());
   }
 }
 
@@ -456,10 +461,10 @@ static bool metaconnection_command(struct connection *caller, char *arg,
   if ((*arg == '\0') ||
       (0 == strcmp (arg, "?"))) {
     if (is_metaserver_open()) {
-      cmd_reply(CMD_METACONN, caller, C_OK,
+      cmd_reply(CMD_METACONN, caller, C_COMMENT,
                 _("Metaserver connection is open."));
     } else {
-      cmd_reply(CMD_METACONN, caller, C_OK,
+      cmd_reply(CMD_METACONN, caller, C_COMMENT,
                 _("Metaserver connection is closed."));
     }
   } else if (0 == fc_strcasecmp(arg, "u")
@@ -506,12 +511,12 @@ static bool metapatches_command(struct connection *caller,
 
   if (is_metaserver_open()) {
     send_server_info_to_metaserver(META_INFO);
-    notify_conn(NULL, NULL, E_SETTING, ftc_server,
-                _("Metaserver patches string set to '%s'."), arg);
+    cmd_reply(CMD_METAPATCHES, caller, C_OK,
+              _("Metaserver patches string set to '%s'."), arg);
   } else {
-    notify_conn(NULL, NULL, E_SETTING, ftc_server,
-                _("Metaserver patches string set to '%s', "
-                  "not reporting to metaserver."), arg);
+    cmd_reply(CMD_METAPATCHES, caller, C_OK,
+              _("Metaserver patches string set to '%s', "
+                "not reporting to metaserver."), arg);
   }
 
   return TRUE;
@@ -530,12 +535,12 @@ static bool metamessage_command(struct connection *caller,
   set_user_meta_message_string(arg);
   if (is_metaserver_open()) {
     send_server_info_to_metaserver(META_INFO);
-    notify_conn(NULL, NULL, E_SETTING, ftc_server,
-                _("Metaserver message string set to '%s'."), arg);
+    cmd_reply(CMD_METAMESSAGE, caller, C_OK,
+              _("Metaserver message string set to '%s'."), arg);
   } else {
-    notify_conn(NULL, NULL, E_SETTING, ftc_server,
-                _("Metaserver message string set to '%s', "
-                  "not reporting to metaserver."), arg);
+    cmd_reply(CMD_METAMESSAGE, caller, C_OK,
+              _("Metaserver message string set to '%s', "
+                "not reporting to metaserver."), arg);
   }
 
   return TRUE;
@@ -554,9 +559,8 @@ static bool metaserver_command(struct connection *caller, char *arg,
 
   sz_strlcpy(srvarg.metaserver_addr, arg);
 
-  notify_conn(NULL, NULL, E_SETTING, ftc_server,
-              _("Metaserver is now [%s]."),
-	      meta_addr_port());
+  cmd_reply(CMD_METASERVER, caller, C_OK,
+            _("Metaserver is now [%s]."), meta_addr_port());
   return TRUE;
 }
 
@@ -785,7 +789,7 @@ static bool create_command(struct connection *caller, const char *arg,
 
   if (strlen(buf) > 0) {
     /* Send a notification. */
-    notify_conn(NULL, NULL, E_SETTING, ftc_server, "%s", buf);
+    cmd_reply(CMD_CREATE, caller, C_OK, "%s", buf);
   }
 
   return TRUE;
@@ -1927,15 +1931,14 @@ static bool set_away(struct connection *caller, char *name, bool check)
               _("Only players may use the away command."));
     return FALSE;
   } else if (!caller->playing->ai_controlled && !check) {
-    notify_conn(game.est_connections, NULL, E_SETTING, ftc_any,
-                _("%s set to away mode."), 
-                player_name(caller->playing));
+    cmd_reply(CMD_AWAY, caller, C_OK,
+              _("%s set to away mode."), player_name(caller->playing));
     set_ai_level_directer(caller->playing, AI_LEVEL_AWAY);
     caller->playing->ai_controlled = TRUE;
     cancel_all_meetings(caller->playing);
   } else if (!check) {
-    notify_conn(game.est_connections, NULL, E_SETTING, ftc_any,
-                _("%s returned to game."), player_name(caller->playing));
+    cmd_reply(CMD_AWAY, caller, C_OK,
+              _("%s returned to game."), player_name(caller->playing));
     caller->playing->ai_controlled = FALSE;
     /* We have to do it, because the client doesn't display 
      * dialogs for meetings in AI mode. */
@@ -2939,6 +2942,8 @@ static bool set_command(struct connection *caller, char *str, bool check)
         notify_conn(pconn->self, NULL, E_SETTING, ftc_server, "%s", buffer);
       }
     } conn_list_iterate_end;
+    /* Notify the console. */
+    con_write(C_OK, "%s", buffer);
   }
 
   if (!check && do_update) {
@@ -3343,12 +3348,11 @@ static bool take_command(struct connection *caller, char *str, bool check)
 
   player_was_created = (NULL != pplayer ? pplayer->was_created : FALSE);
 
-  /* if the player is controlled by another user,
-   * forcibly convert the user to an observer.
-   */
+  /* If the player is controlled by another user, forcibly detach
+   * the user. */
   if (pplayer && pplayer->is_connected) {
     if (NULL == caller) {
-      notify_conn(NULL, NULL, E_CONNECTION, ftc_server,
+       notify_conn(NULL, NULL, E_CONNECTION, ftc_server,
                   _("Reassigned nation to %s by server console."),
                   pconn->username);
     } else {
@@ -4070,19 +4074,40 @@ static bool handle_stdin_input_real(struct connection *caller,
     return FALSE;
   }
 
-  if (!check && level > ALLOW_INFO) {
-    /*
-     * this command will affect the game - inform all players
-     *
-     * use command,arg instead of str because of the trailing
-     * newline in str when it comes from the server command line
-     */
-    if (caller) {
-      notify_conn(NULL, NULL, E_SETTING, ftc_any,
-                  "%s: '%s %s'", caller->username, command, arg);
-    } else {
-      notify_conn(NULL, NULL, E_SETTING, ftc_server_prompt,
-                  "%s: '%s %s'", _("(server prompt)"), command, arg);
+  if (!check) {
+    struct conn_list *echo_list = NULL;
+    bool echo_list_allocated = FALSE;
+
+    switch (command_echo(command_by_number(cmd))) {
+    case CMD_ECHO_NONE:
+      break;
+    case CMD_ECHO_ADMINS:
+      conn_list_iterate(game.est_connections, pconn) {
+        if (ALLOW_ADMIN <= conn_get_access(pconn)) {
+          if (NULL == echo_list) {
+            echo_list = conn_list_new();
+            echo_list_allocated = TRUE;
+          }
+          conn_list_append(echo_list, pconn);
+        }
+      } conn_list_iterate_end;
+      break;
+    case CMD_ECHO_ALL:
+      echo_list = game.est_connections;
+      break;
+    }
+
+    if (NULL != echo_list) {
+      if (caller) {
+        notify_conn(echo_list, NULL, E_SETTING, ftc_any,
+                    "%s: '%s %s'", caller->username, command, arg);
+      } else {
+        notify_conn(echo_list, NULL, E_SETTING, ftc_server_prompt,
+                    "%s: '%s %s'", _("(server prompt)"), command, arg);
+      }
+      if (echo_list_allocated) {
+        conn_list_destroy(echo_list);
+      }
     }
   }
 
@@ -4204,7 +4229,7 @@ static bool end_command(struct connection *caller, char *str, bool check)
     force_end_of_sniff = TRUE;
     cmd_reply(CMD_END_GAME, caller, C_OK,
               _("Ending the game. The server will restart once all clients "
-              "have disconnected."));
+                "have disconnected."));
     return TRUE;
   } else {
     cmd_reply(CMD_END_GAME, caller, C_FAIL, 
@@ -4348,8 +4373,7 @@ static bool reset_command(struct connection *caller, char *arg, bool check,
   }
 
   send_server_settings(game.est_connections);
-  notify_conn(NULL, NULL, E_SETTING, ftc_server,
-              _("Settings re-initialized."));
+  cmd_reply(CMD_RESET, caller, C_OK, _("Settings re-initialized."));
 
   /* list changed values */
   show_changed(caller, check, read_recursion);
