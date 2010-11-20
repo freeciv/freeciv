0ae56cdb9a1346473d29cf1ceb8bf8e29d9d9198
Matthias Pfafferodt <matthias.pfafferodt@mapfa.de>
2010-11-20 16:11:31 +0000

    cleanup saving old savegame format
    * remove dead code
    
    see gna bug #17091
    
    [[originally from svn r18552]]

--- server/savegame2.c
+++ server/savegame2.c
@@ -548,23 +548,9 @@ void savegame2_save(struct section_file *file, const char *save_reason,
   struct timer *savetimer = new_timer_start(TIMER_CPU, TIMER_DEBUG);
 #endif
 
-  if (game.server.saveversion == -1) {
-    /* freeciv 2.2.x */
-    if (player_count() > 32) {
-      log_error("Error: freeciv 2.2.x can't handle more than 32 players.");
-    } else if (MAPSIZE_FULLSIZE != map.server.mapsize
-               || 30 < map.server.size) {
-      log_error("Error: freeciv 2.2.x can't handle bigger maps.");
-    } else {
-      log_verbose("saving game in old format ...");
-      game_save(file, save_reason, scenario);
-    }
-  } else {
-    /* freeciv 2.2.99 or newer */
-    log_verbose("saving game in new format ...");
-    savegame2_save_real(file, save_reason, scenario,
-                        game.server.saveversion);
-  }
+  /* freeciv 2.2.99 or newer */
+  log_verbose("saving game in new format ...");
+  savegame2_save_real(file, save_reason, scenario, game.server.saveversion);
 
 #ifdef DEBUG
   stop_timer(savetimer);
@@ -5304,14 +5290,6 @@ static const int compat_num = ARRAY_SIZE(compat);
 ****************************************************************************/
 const struct sset_val_name *saveversion_name(int saveversion)
 {
-#ifdef DEBUG
-  /* Debug builds can create a savefile using the old format. */
-  static struct sset_val_name old_savegame = {"2.2.x", N_("freeciv 2.2.x")};
-  if (saveversion == -1) {
-    return &old_savegame;
-  }
-#endif
-
   return (0 <= saveversion && saveversion < compat_num
           ? &compat[saveversion].name : NULL);
 }
