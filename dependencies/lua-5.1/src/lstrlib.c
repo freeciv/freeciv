c25a89f9c5efd9542c43dbc4add09431645c3b31
Vijay Kiran Kamuju <infyquest@gmail.com>
2010-12-10 23:31:11 +0000

    Applied official patches from lua.org See gna patch #2229
    [[originally from svn r18716]]

--- dependencies/lua-5.1/src/lstrlib.c
+++ dependencies/lua-5.1/src/lstrlib.c
@@ -754,6 +754,7 @@ static void addintlen (char *form) {
 
 
 static int str_format (lua_State *L) {
+  int top = lua_gettop(L);  
   int arg = 1;
   size_t sfl;
   const char *strfrmt = luaL_checklstring(L, arg, &sfl);
@@ -768,7 +769,8 @@ static int str_format (lua_State *L) {
     else { /* format item */
       char form[MAX_FORMAT];  /* to store the format (`%...') */
       char buff[MAX_ITEM];  /* to store the formatted item */
-      arg++;
+      if (++arg > top)
+        luaL_argerror(L, arg, "no value");
       strfrmt = scanformat(L, strfrmt, form);
       switch (*strfrmt++) {
         case 'c': {
