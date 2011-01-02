47ff8261973784da7f2cd25a7d0e6a40ff75ea5e
Matthias Pfafferodt <matthias.pfafferodt@mapfa.de>
2011-01-02 21:12:37 +0000

    fix load of scenarios
    * for scenarios server_state _must_ be S_S_INITIAL (= 0)
    * or S_S_RUNNING (= 2) with a definition of players
    
    see gna bug #17381
    
    [[originally from svn r18816]]

--- server/savegame.c
+++ server/savegame.c
@@ -4549,6 +4549,16 @@ static void game_load_internal(struct section_file *file)
     tmp_server_state = S_S_INITIAL;
   }
 
+  if (game.scenario.is_scenario == TRUE
+      && (tmp_server_state != S_S_INITIAL
+          || (tmp_server_state == S_S_RUNNING
+              && game.scenario.players == TRUE))) {
+    log_fatal("Invalid scenario definition (server state '%d' and "
+              "players are %s).", tmp_server_state,
+              game.scenario.players ? "saved" : "not saved");
+    exit(EXIT_FAILURE);
+  }
+
   {
     set_meta_patches_string(secfile_lookup_str_default(file, 
                                                 default_meta_patches_string(),
