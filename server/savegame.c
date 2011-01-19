512ea3be769a87694016ab0867925b19fe69f950
Matthias Pfafferodt <matthias.pfafferodt@mapfa.de>
2011-01-19 09:55:57 +0000

    fix scenarios
    * onsetbarbarian: was given as year (2.1.x?) but is now defined as turn
    * add a check for onsetbarbarian in savegame.c
    * convert japan and north_amerika to unix style line endings
    
    see gna bug #17513
    
    [[originally from svn r19021]]

--- server/savegame.c
+++ server/savegame.c
@@ -4821,6 +4821,9 @@ static void game_load_internal(struct section_file *file)
     game.server.onsetbarbarian =
       secfile_lookup_int_default(file, game.server.onsetbarbarian,
                                  "game.onsetbarbs");
+    /* old savegames use the year while newer savegames use the turn. */
+    game.server.onsetbarbarian = MIN(0, game.server.onsetbarbarian);
+
     game.server.revolution_length =
       secfile_lookup_int_default(file, game.server.revolution_length,
                                  "game.revolen");
