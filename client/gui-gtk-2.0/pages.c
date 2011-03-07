ace9561abf1785c7b36c72219629bee2af5ad252
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2011-03-07 13:39:48 +0000

    Translate "Unknown" in server list (added in bug #17708).
    See gna bug #17847.
    
    [[originally from svn r19487]]

--- client/gui-gtk-2.0/pages.c
+++ client/gui-gtk-2.0/pages.c
@@ -633,7 +633,7 @@ static void update_server_list(enum server_scan_type sstype,
     if (pserver->humans >= 0) {
       fc_snprintf(buf, sizeof(buf), "%d", pserver->humans);
     } else {
-      strncpy(buf, "Unknown", sizeof(buf));
+      strncpy(buf, _("Unknown"), sizeof(buf));
     }
     gtk_list_store_append(store, &it);
     gtk_list_store_set(store, &it,
