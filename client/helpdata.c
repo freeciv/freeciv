09cd8fe06cd9f90d684db45fef7e8640fb0dc338
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2011-03-25 19:44:43 +0000

    Fix spurious "chance of migration into your cities" lines in autogenerated Government help.
    Reported by Angelo Locritani (alocritani@gna).
    
    See gna bug #17919.
    
    [[originally from svn r19553]]

--- client/helpdata.c
+++ client/helpdata.c
@@ -2394,6 +2394,7 @@ void helptext_government(char *buf, size_t bufsz, struct player *pplayer,
                          " with distance from capital.\n"),
                        astr_str(&outputs_and));
         }
+        break;
       case EFT_MIGRATION_PCT:
         if (peffect->value > 0) {
           CATLSTR(buf, bufsz, _("* Increases the chance of migration"
