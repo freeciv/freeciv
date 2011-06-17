8a73fb58b36983b33d0f6d71a42fd99fb031f34e
Marko Lindqvist <cazfi74@gmail.com>
2011-06-17 18:34:15 +0000

    Made sure than try_to_connect() really returns failure on case of network failure. Previously it was returning errno value that was no longer error value.
    See gna bug #18211
    
    [[originally from svn r19777]]

--- client/clinet.c
+++ client/clinet.c
@@ -232,12 +232,14 @@ static int try_to_connect(const char *username, char *errbuf, int errbufsize)
 
   client.conn.sock = sock;
   if (client.conn.sock == -1) {
-    (void) fc_strlcpy(errbuf, fc_strerror(fc_get_errno()), errbufsize);
+    fc_errno err = fc_get_errno(); /* Save errno value before calling anything */
+
+    (void) fc_strlcpy(errbuf, fc_strerror(err), errbufsize);
 #ifdef HAVE_WINSOCK
     return -1;
 #else
-    return errno;
-#endif
+    return err;
+#endif /* HAVE_WINSOCK */
   }
 
   make_connection(client.conn.sock, username);
