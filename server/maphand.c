0edd030491ec888024944397d8dad95ce8e810a9
Marko Lindqvist <cazfi74@gmail.com>
2011-08-28 05:15:06 +0000

    Fixed claiming of border source tile on Ocean.
    Originally reported by heilkitty
    
    See gna bug #18557
    
    [[originally from svn r20188]]

--- server/maphand.c
+++ server/maphand.c
@@ -1567,7 +1567,12 @@ static bool is_claimable_ocean(struct tile *ptile, struct tile *source)
       && get_lake_surrounders(cont) == source_cont) {
     return TRUE;
   }
-  
+
+  if (ptile == source) {
+    /* Source itself is always claimable. */
+    return TRUE;
+  }
+
   ocean_tiles = 0;
   adjc_iterate(ptile, tile2) {
     cont2 = tile_continent(tile2);
@@ -1736,7 +1741,7 @@ void map_claim_border(struct tile *ptile, struct player *owner)
       strength_new = tile_border_strength(dtile, ptile);
 
       if (strength_new <= strength_old) {
-        /* Stronger shall prevail,cd 
+        /* Stronger shall prevail,
          * in case of equel strength older shall prevail */
         continue;
       }
