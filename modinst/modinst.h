29fcc0292ef3b99bb91120802017e278aebf84bc
Marko Lindqvist <cazfi74@gmail.com>
2011-10-27 21:39:23 +0000

    Added file modinst.h forgotten from previous commit.
    [[originally from svn r20338]]

---
+++ modinst/modinst.h
@@ -0,0 +1,22 @@
+/********************************************************************** 
+ Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+***********************************************************************/
+#ifndef FC__MODINST_H
+#define FC__MODINST_H
+
+struct fcmp_params
+{
+  const char *list_url;
+  const char *inst_prefix;
+};
+
+#endif /* FC__MODINST_H */
