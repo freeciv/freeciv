6200c566d8c9b97d2f0d2aeb1fc705d46b4e60bf
Matthias Pfafferodt <matthias.pfafferodt@mapfa.de>
2011-10-30 19:17:29 +0000

    add autogenerated tolua files to whitelist on header check
    see gna bug #18865
    
    [[originally from svn r20379]]

--- tests/header_guard.sh
+++ tests/header_guard.sh
@@ -6,6 +6,7 @@ files=`find $1 -name "*.h" \
        | grep -v common/spec \
        | grep -v "Freeciv\.h" \
        | grep -v \./common/packets_gen\.h \
+       | grep -v tolua_.*_gen\.h \
        | grep -v config\.h \
        | grep -v dependencies \
        | grep -v utility/md5\.h \
