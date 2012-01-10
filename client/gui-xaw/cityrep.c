54f500d01a67951e77aa0c08ea8365f391044ee0
Matthias Pfafferodt <matthias.pfafferodt@mapfa.de>
2012-01-10 20:26:57 +0000

    [unused var fix] xaw client
    remove variables which are set but not used
    
    see gna bug #19150
    
    [[originally from svn r20685]]

--- client/gui-xaw/cityrep.c
+++ client/gui-xaw/cityrep.c
@@ -818,8 +818,6 @@ static void popup_chgall_dialog (Widget parent)
   struct chgall_data *state;
   Widget shell;
   Widget main_form;
-  Widget fr_label;
-  Widget to_label;
   Widget fr_viewport;
   Widget to_viewport;
   Position x, y;
@@ -868,24 +866,6 @@ static void popup_chgall_dialog (Widget parent)
      state->w.shell,
      NULL
     );
-
-  fr_label =
-    I_LW(XtVaCreateManagedWidget
-    (
-     "chgallfrlabel",
-     labelWidgetClass, 
-     main_form,
-     NULL
-    ));
-
-  to_label =
-    I_LW(XtVaCreateManagedWidget
-    (
-     "chgalltolabel",
-     labelWidgetClass, 
-     main_form,
-     NULL
-    ));
 
   fr_viewport =
     XtVaCreateManagedWidget
