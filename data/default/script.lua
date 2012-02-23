7c1056b2d40d3b930c6570bacafe9835e893ac1c
Marko Lindqvist <cazfi74@gmail.com>
2012-02-23 23:05:59 +0000

    Added postprocessing of generated map to default ruleset script.lua. It adds some tile labels randomly.
    See gna patch #3147
    
    [[originally from svn r20908]]

--- data/default/script.lua
+++ data/default/script.lua
@@ -16,3 +16,88 @@ function city_destroyed_callback(city, loser, destroyer)
 end
 
 signal.connect("city_destroyed", "city_destroyed_callback")
+
+function place_map_labels()
+  local mountains = 0
+  local deep_oceans = 0
+  local deserts = 0
+  local glaciers = 0
+  local selected
+
+  for place in whole_map_iterate() do
+    local terr = place.terrain
+    local tname = terr:rule_name()
+    if tname == "Mountains" then
+      mountains = mountains + 1
+    end
+    if tname == "Deep Ocean" then
+      deep_oceans = deep_oceans + 1
+    end
+    if tname == "Desert" then
+      deserts = deserts + 1
+    end
+    if tname == "Glacier" then
+      glaciers = glaciers + 1
+    end
+  end
+
+  if random(1, 100) <= 75 then
+    selected = random(1, mountains)
+
+    for place in whole_map_iterate() do
+      local terr = place.terrain
+      if terr:rule_name() == "Mountains" then
+        selected = selected - 1
+        if selected == 0 then
+          place:set_label("Highest Peak")
+        end
+      end
+    end
+  end
+
+  if random(1, 100) <= 75 then
+    selected = random(1, deep_oceans)
+
+    for place in whole_map_iterate() do
+      local terr = place.terrain
+      if terr:rule_name() == "Deep Ocean" then
+        selected = selected - 1
+        if selected == 0 then
+          place:set_label("Deep Trench")
+        end
+      end
+    end
+  end
+
+  if random(1, 100) <= 75 then
+    selected = random(1, deserts)
+
+    for place in whole_map_iterate() do
+      local terr = place.terrain
+      if terr:rule_name() == "Desert" then
+        selected = selected - 1
+        if selected == 0 then
+          place:set_label("Scorched spot")
+        end
+      end
+    end
+  end
+
+  if random(1, 100) <= 75 then
+    selected = random(1, glaciers)
+
+    for place in whole_map_iterate() do
+      local terr = place.terrain
+      if terr:rule_name() == "Glacier" then
+        selected = selected - 1
+        if selected == 0 then
+          place:set_label("Frozen lake")
+        end
+      end
+    end
+  end
+
+  return false
+end
+
+signal.connect("map_generated", "place_map_labels")
