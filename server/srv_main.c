203e07d6c85ec285c53c35231f979315c9eeb513
Marko Lindqvist <cazfi74@gmail.com>
2012-04-13 00:33:20 +0000

    Fixed checking if game should end due to allied victory.
    Reported by Brandon J. Van Every <bvanevery>
    
    See gna bug #17986
    
    [[originally from svn r21158]]

--- server/srv_main.c
+++ server/srv_main.c
@@ -423,31 +423,6 @@ bool check_for_game_over(void)
       } players_iterate_alive_end;
 
       if (NULL != winner_list) {
-        /* Now ensure a non allied has conceded the game. */
-        bool found = FALSE;
-
-        players_iterate_alive(pplayer) {
-          if (is_barbarian(pplayer)
-              || !player_status_check((pplayer), PSTATUS_SURRENDER)) {
-            continue;
-          }
-
-          player_list_iterate(winner_list, aplayer) {
-            if (!pplayers_allied(aplayer, pplayer)) {
-              found = TRUE;
-              break;
-            }
-          } player_list_iterate_end;
-        } players_iterate_alive_end;
-
-        if (!found) {
-          /* Seems all players are allied. */
-          player_list_destroy(winner_list);
-          winner_list = NULL;
-        }
-      }
-
-      if (NULL != winner_list) {
         bool first = TRUE;
 
         fc_assert(candidates == player_list_size(winner_list));
