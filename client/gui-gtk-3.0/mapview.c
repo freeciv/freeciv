0dc50e2fc6fd7fe3f79e836580803bb8aaa707ba
Marko Lindqvist <cazfi74@gmail.com>
2012-07-20 22:37:00 +0000

    Make unit movement animation visible on gtk3-client.
    Reported by Jacob Nevins <jtn>, patch by Anonymous.
    
    See gna bug #19921
    
    [[originally from svn r21593]]

--- client/gui-gtk-3.0/mapview.c
+++ client/gui-gtk-3.0/mapview.c
@@ -482,6 +482,7 @@ void dirty_all(void)
 **************************************************************************/
 void flush_dirty(void)
 {
+  gdk_window_process_updates(gtk_widget_get_window(map_canvas), FALSE);
 }
 
 /****************************************************************************
