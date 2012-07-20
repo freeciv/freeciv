a32f29777963d2a573d47dc8faef73d0aba567fc
Marko Lindqvist <cazfi74@gmail.com>
2012-07-20 21:56:23 +0000

    Added functions to create or set gtkpixcomm from sprite.
    Patch by Anonymous with some function headers added by me
    
    See gna patch #3337
    
    [[originally from svn r21588]]

--- client/gui-gtk-3.0/gtkpixcomm.h
+++ client/gui-gtk-3.0/gtkpixcomm.h
@@ -70,6 +70,8 @@ cairo_surface_t *gtk_pixcomm_get_surface(GtkPixcomm *pixcomm);
 void gtk_pixcomm_copyto(GtkPixcomm *pixcomm, struct sprite *src,
 			gint x, gint y);
 void       gtk_pixcomm_clear	 (GtkPixcomm *pixcomm);
+GtkWidget *gtk_pixcomm_new_from_sprite(struct sprite *sprite);
+void gtk_pixcomm_set_from_sprite(GtkPixcomm *p, struct sprite *sprite);
 
 G_END_DECLS
 
