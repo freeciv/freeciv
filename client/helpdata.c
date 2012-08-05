88c648016be93a6caf601600d8f9d33f437bcbe9
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2012-08-05 12:28:09 +0000

    Remove hardcoded help message claiming diplomatic units are always built as veteran under communist governments. Add handwritten text to rulesets' unit help instead.
    See gna bug #20021.
    
    [[originally from svn r21721]]

--- client/helpdata.c
+++ client/helpdata.c
@@ -1610,8 +1610,6 @@ char *helptext_unit(char *buf, size_t bufsz, struct player *pplayer,
         break;
       case UMT_LAND:
         if (utype_has_flag(utype, F_DIPLOMAT)||utype_has_flag(utype, F_SPY)) {
-          CATLSTR(buf, bufsz,
-                  _("* Will be built as a veteran under communist governments.\n"));
           if (veteran_through_combat)
             CATLSTR(buf, bufsz,
                     _("* May be promoted after a successful mission.\n"));
