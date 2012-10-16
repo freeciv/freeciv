8315157d2f3bbca8d6725b542d71be14cae848bb
Marko Lindqvist <cazfi74@gmail.com>
2012-10-16 01:35:42 +0000

    Avoid division by zero in defense_bonus() when cityspot so far has value 0.
    See gna bug #20239
    
    [[originally from svn r21954]]

--- ai/aisettler.c
+++ ai/aisettler.c
@@ -330,7 +330,7 @@ static int defense_bonus(struct cityresult *result, struct ai_data *ai)
         (defense_bonus * terrain_control.river_defense_bonus) / 100;
   }
 
-  return 100 / result->total * (100 / defense_bonus * DEFENSE_EMPHASIS);
+  return 100 / (result->total + 1) * (100 / defense_bonus * DEFENSE_EMPHASIS);
 }
 
 /**************************************************************************
