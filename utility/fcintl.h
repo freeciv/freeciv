c1bc41f623ebe756260663e3da909436917c2ab3
Marko Lindqvist <cazfi74@gmail.com>
2013-02-10 23:47:29 +0000

    Replaced "config.h" in fcintl.h comment and error message with "fc_config.h"
    See gna patch #3676
    
    [[originally from svn r22321]]

--- utility/fcintl.h
+++ utility/fcintl.h
@@ -18,8 +18,8 @@ extern "C" {
 #endif /* __cplusplus */
 
 #ifdef HAVE_CONFIG_H
-#ifndef FC_CONFIG_H		/* this should be defined in config.h */
-#error Files including fcintl.h should also include config.h directly
+#ifndef FC_CONFIG_H             /* this should be defined in fc_config.h */
+#error Files including fcintl.h should also include fc_config.h directly
 #endif
 #endif
 
