b5f735d42077edf2abecfc69f0df7871e7dc0092
Marko Lindqvist <cazfi74@gmail.com>
2013-03-30 08:38:41 +0000

    Do not mention fortifying defense bonus in helptext when unit in question has no defense value.
    See patch #3801
    
    [[originally from svn r22627]]

--- client/helpdata.c
+++ client/helpdata.c
@@ -1463,13 +1463,19 @@ char *helptext_unit(char *buf, size_t bufsz, struct player *pplayer,
   }
   if (uclass_has_flag(utype_class(utype), UCF_CAN_FORTIFY)
       && !utype_has_flag(utype, UTYF_SETTLERS)) {
-    CATLSTR(buf, bufsz,
-            /* xgettext:no-c-format */
-            _("  * Gets a 50% defensive bonus while in cities.\n"));
-    CATLSTR(buf, bufsz,
-            /* xgettext:no-c-format */
-            _("  * May fortify, granting a 50% defensive bonus when not in "
-              "a city.\n"));
+    if (utype->defense_strength > 0) {
+      CATLSTR(buf, bufsz,
+              /* xgettext:no-c-format */
+              _("  * Gets a 50% defensive bonus while in cities.\n"));
+      CATLSTR(buf, bufsz,
+              /* xgettext:no-c-format */
+              _("  * May fortify, granting a 50% defensive bonus when not in "
+                "a city.\n"));
+    } else {
+      CATLSTR(buf, bufsz,
+              /* xgettext:no-c-format */
+              _("  * May fortify to stay put.\n"));
+    }
   }
   if (uclass_has_flag(utype_class(utype), UCF_UNREACHABLE)) {
     CATLSTR(buf, bufsz,
