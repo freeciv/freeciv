3045bdbf659aa87ba4be16791db01f740c2d2ab8
Marko Lindqvist <cazfi74@gmail.com>
2013-05-12 21:14:38 +0000

    Fixed some minor client-side memory leaks.
    Patch by Guillaume Melquiond <silene>
    
    See patch #3896
    
    [[originally from svn r22858]]

--- client/tilespec.c
+++ client/tilespec.c
@@ -944,7 +944,10 @@ void tilespec_try_read(const char *tileset_name, bool verbose)
       struct tileset *t = tileset_read_toplevel(file, FALSE);
 
       if (t) {
-        if (!tileset || t->priority > tileset->priority) {
+        if (!tileset) {
+          tileset = t;
+        } else if (t->priority > tileset->priority) {
+          tileset_free(tileset);
           tileset = t;
         } else {
           tileset_free(t);
