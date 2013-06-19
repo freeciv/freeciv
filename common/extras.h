9a3d64d4b2613ffaf6b45a9b89c29a5ac343c5c9
Marko Lindqvist <cazfi74@gmail.com>
2013-06-19 07:20:27 +0000

    Added extra_type_iterate()
    See patch #3942
    
    [[originally from svn r22966]]

--- common/extras.h
+++ common/extras.h
@@ -20,6 +20,7 @@ extern "C" {
 /* common */
 #include "base.h"
 #include "fc_types.h"
+#include "game.h"
 #include "road.h"
 
 struct extra_type
@@ -42,8 +43,23 @@ struct extra_type *extra_by_number(int id);
 
 struct extra_type *extra_type_get(enum extra_type_id type, int subid);
 
+static inline int extra_max(void)
+{
+  return S_LAST + game.control.num_base_types + game.control.num_road_types;
+}
+
+#define extra_type_iterate(_p)                    \
+{                                                 \
+  int _i_;                                        \
+  for (_i_ = 0; _i_ < extra_max(); _i_++) {       \
+    struct extra_type *_p = extra_by_number(_i_);
+
+#define extra_type_iterate_end                    \
+  }                                               \
+}
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
 
-#endif  /* FC__ROAD_H */
+#endif  /* FC__EXTRAS_H */
