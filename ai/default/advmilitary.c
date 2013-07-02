5c2a41b9d095e55a517c872844e1d3ff70be99e6
Marko Lindqvist <cazfi74@gmail.com>
2013-07-02 22:37:23 +0000

    Removed factor of 3 when estimating defense strength in relation to opposing attack strength. This means that AI both does not feel safe with so weak defense, and considers attack against enemy defenses more likely to success.
    See patch #3960
    
    [[originally from svn r23010]]

--- ai/default/advmilitary.c
+++ ai/default/advmilitary.c
@@ -216,14 +216,14 @@ static int base_assess_defense_unit(struct city *pcity, struct unit *punit,
   defense = get_fortified_defense_power(NULL, punit) * punit->hp;
   if (!is_sailing_unit(punit)) {
     defense *= unit_type(punit)->firepower;
-    if (is_ground_unit(punit)) {
-      if (pcity) {
-        /* FIXME: We check if city got defense effect against *some*
-         * unit type. Sea unit danger might cause us to build defenses
-         * against air units... */
-        do_wall = (!igwall && city_got_defense_effect(pcity, NULL));
-        defense *= 3;
-      }
+  }
+
+  if (is_ground_unit(punit)) {
+    if (pcity) {
+      /* FIXME: We check if city got defense effect against *some*
+       * unit type. Sea unit danger might cause us to build defenses
+       * against air units... */
+      do_wall = (!igwall && city_got_defense_effect(pcity, NULL));
     }
   }
   defense /= POWER_DIVIDER;
