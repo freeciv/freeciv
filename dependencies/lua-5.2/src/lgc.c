fff9c87792597162e4f4610faa697f219c6fb58c
Marko Lindqvist <cazfi74@gmail.com>
2013-09-02 23:11:05 +0000

    Applied lua.org patches 1-5 to lua-5.2.
    See patch #4067
    
    [[originally from svn r23273]]

--- dependencies/lua-5.2/src/lgc.c
+++ dependencies/lua-5.2/src/lgc.c
@@ -493,6 +493,7 @@ static lu_mem traverseLclosure (global_State *g, LClosure *cl) {
 
 
 static lu_mem traversestack (global_State *g, lua_State *th) {
+  int n = 0;
   StkId o = th->stack;
   if (o == NULL)
     return 1;  /* stack not completely built yet */
@@ -503,7 +504,13 @@ static lu_mem traversestack (global_State *g, lua_State *th) {
     for (; o < lim; o++)  /* clear not-marked stack slice */
       setnilvalue(o);
   }
-  return sizeof(lua_State) + sizeof(TValue) * th->stacksize;
+  else {  /* count call infos to compute size */
+    CallInfo *ci;
+    for (ci = &th->base_ci; ci != th->ci; ci = ci->next)
+      n++;
+  }
+  return sizeof(lua_State) + sizeof(TValue) * th->stacksize +
+         sizeof(CallInfo) * n;
 }
 
 
--- dependencies/lua-5.2/src/luac.c
+++ dependencies/lua-5.2/src/luac.c
@@ -251,7 +251,7 @@ static void PrintString(const TString* ts)
 static void PrintConstant(const Proto* f, int i)
 {
  const TValue* o=&f->k[i];
- switch (ttype(o))
+ switch (ttypenv(o))
  {
   case LUA_TNIL:
 	printf("nil");
