aee1699234b3f26c79671eccbe34f8082b963a78
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2013-09-14 22:54:43 +0000

    When the default AI considers if it should continue thinking about bribing a unit it checks things that makes it impossible to incite a city. Make it check things that makes bribing impossible in stead.
    See bug #21064
    
    [[originally from svn r23396]]

--- ai/default/aidiplomat.c
+++ ai/default/aidiplomat.c
@@ -535,9 +535,9 @@ static bool ai_diplomat_bribe_nearby(struct player *pplayer,
     if (!pvictim
         || !HOSTILE_PLAYER(pplayer, unit_owner(pvictim))
         || unit_list_size(ptile->units) > 1
-        || (tile_city(ptile)
-         && get_city_bonus(tile_city(ptile), EFT_NO_INCITE) > 0)
-        || get_player_bonus(unit_owner(pvictim), EFT_NO_INCITE) > 0) {
+        || tile_city(ptile)
+        || unit_has_type_flag(pvictim, UTYF_UNBRIBABLE)
+        || get_player_bonus(unit_owner(pvictim), EFT_UNBRIBABLE_UNITS) > 0) {
       continue;
     }
 
