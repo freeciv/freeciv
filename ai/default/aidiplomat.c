a5c7fed54bd95ca27eb6e9eef9da6f2f953006da
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2013-09-15 10:17:56 +0000

    AI: Check that the bribe action is enabled when looking for a bribe target.
    It already checks if EFT_UNBRIBABLE_UNITS makes bribing impossible. This
    makes it consistent.
    
    See patch #4180
    
    [[originally from svn r23413]]

--- ai/default/aidiplomat.c
+++ ai/default/aidiplomat.c
@@ -23,6 +23,7 @@
 #include "timing.h"
 
 /* common */
+#include "actions.h"
 #include "city.h"
 #include "combat.h"
 #include "game.h"
@@ -551,6 +552,8 @@ static bool dai_diplomat_bribe_nearby(struct ai_type *ait,
         || unit_list_size(ptile->units) > 1
         || tile_city(ptile)
         || unit_has_type_flag(pvictim, UTYF_UNBRIBABLE)
+        || is_action_enabled_unit_on_unit(ACTION_SPY_BRIBE_UNIT,
+                                          punit, pvictim)
         || get_player_bonus(unit_owner(pvictim), EFT_UNBRIBABLE_UNITS) > 0) {
       continue;
     }
