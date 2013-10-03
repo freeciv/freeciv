6346cda05c75c5d71ff90fd3d6285959c3d0e6ba
Marko Lindqvist <cazfi74@gmail.com>
2013-10-03 21:46:54 +0000

    Make it easier for user to notice ruleset loading failure leading to old ruleset still being used, so one does not assume that new ruleset is in use. - Send E_LOG_FATAL to client, which usually leads to popup error dialog - Rearranged server side output so that error notification is last thing   printed instead of being somewhere in the middle
    See patch #3796
    
    [[originally from svn r23531]]

--- server/ruleset.c
+++ server/ruleset.c
@@ -37,6 +37,7 @@
 #include "city.h"
 #include "effects.h"
 #include "fc_types.h"
+#include "featured_text.h"
 #include "game.h"
 #include "government.h"
 #include "map.h"
@@ -56,6 +57,7 @@
 
 /* server */
 #include "citytools.h"
+#include "notify.h"
 #include "plrhand.h"
 #include "rssanity.h"
 #include "settings.h"
@@ -5807,6 +5809,14 @@ static void send_ruleset_team_names(struct conn_list *dest)
 }
 
 /**************************************************************************
+  Make it clear to everyone that requested ruleset has not been loaded.
+**************************************************************************/
+static void notify_ruleset_fallback(const char *msg)
+{
+  notify_conn(NULL, NULL, E_LOG_FATAL, ftc_warning, "%s", msg);
+}
+
+/**************************************************************************
   Loads the rulesets.
 **************************************************************************/
 bool load_rulesets(const char *restore, bool act)
@@ -5819,6 +5829,8 @@ bool load_rulesets(const char *restore, bool act)
   if (restore != NULL) {
     if (load_rulesetdir(restore, act)) {
       sz_strlcpy(game.server.rulesetdir, restore);
+
+      notify_ruleset_fallback(_("Ruleset couldn't be loaded. Keeping previous one."));
 
       /* We're in sane state as restoring previous ruleset succeeded,
        * but return failure to indicate that this is not what caller
@@ -5835,6 +5847,8 @@ bool load_rulesets(const char *restore, bool act)
        * but return failure to indicate that this is not what caller
        * wanted. */
       sz_strlcpy(game.server.rulesetdir, GAME_DEFAULT_RULESETDIR);
+
+      notify_ruleset_fallback(_("Ruleset couldn't be loaded. Switching to default one."));
 
       return FALSE;
     }
