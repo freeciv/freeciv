ab7577749a3d7fb78792ad5660f4efb13c314039
Marko Lindqvist <cazfi74@gmail.com>
2014-02-14 01:47:57 +0000

    Moved concat_tile_activity_text() from client/text.[ch] to new common/clientutils.[ch] to be available for freeciv-web.
    See patch #4484
    
    [[originally from svn r24459]]

--- client/text.h
+++ client/text.h
@@ -28,7 +28,6 @@ struct player_spaceship;
 ****************************************************************************/
 const char *get_tile_output_text(const struct tile *ptile);
 const char *popup_info_text(struct tile *ptile);
-const char *concat_tile_activity_text(struct tile *ptile);
 const char *get_nearest_city_text(struct city *pcity, int sq_dist);
 const char *unit_description(struct unit *punit);
 const char *get_airlift_text(const struct unit_list *punits,
