a0c71c55cb1ad83431df4453746f687be4f4accb
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-03-04 16:36:56 +0000

    AI: Set bonus to 0 when starting to evaluate a new government.
    If this isn't done a governments inherits the bonuses of all previous
    governments.
    
    See bug #21742
    
    [[originally from svn r24607]]

--- server/advisors/advdata.c
+++ server/advisors/advdata.c
@@ -788,7 +788,6 @@ void adv_best_government(struct player *pplayer)
 {
   struct adv_data *adv = adv_data_get(pplayer, NULL);
   int best_val = 0;
-  int bonus = 0; /* in percentage */
   struct government *current_gov = government_of_player(pplayer);
 
   adv->goal.govt.gov = current_gov;
@@ -803,6 +802,7 @@ void adv_best_government(struct player *pplayer)
   if (adv->govt_reeval == 0) {
     governments_iterate(gov) {
       int val = 0;
+      int bonus = 0; /* in percentage */
       int dist;
       int revolution_turns;
 
