d2097a8add3111f89233e8d57680d89f302a4235
pepeto <pepeto69@gmail.com>
2014-05-15 14:47:57 +0000

    Fix that a unit which is tranporting other units can conquer a city.
    Reported by Marko Lindqvist (cazfi@gna)
    
    See gna bug #20789
    
    [[originally from svn r24887]]

--- server/unithand.c
+++ server/unithand.c
@@ -1500,6 +1500,7 @@ bool unit_move_handling(struct unit *punit, struct tile *pdesttile,
 {
   struct player *pplayer = unit_owner(punit);
   struct city *pcity = tile_city(pdesttile);
+  bool taking_over_city = FALSE;
 
   /*** Phase 1: Basic checks ***/
 
@@ -1720,6 +1721,7 @@ bool unit_move_handling(struct unit *punit, struct tile *pdesttile,
         return TRUE;
       }
 
+      taking_over_city = TRUE;
       /* Taking over a city is considered a move, so fall through */
     }
   }
@@ -1739,7 +1741,9 @@ bool unit_move_handling(struct unit *punit, struct tile *pdesttile,
       }
       if (ptrans == punit
           && (is_non_allied_unit_tile(pdesttile, unit_owner(pcargo))
-              || is_non_allied_city_tile(pdesttile, unit_owner(pcargo)))) {
+              || (!taking_over_city
+                  && is_non_allied_city_tile(pdesttile,
+                                             unit_owner(pcargo))))) {
          notify_player(pplayer, unit_tile(punit), E_BAD_COMMAND, ftc_server,
                        _("A transported unit is not allied to all "
                          "units or city on target tile."));
