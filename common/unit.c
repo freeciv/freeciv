26d8fa1ae5199e5a072f51e8e1f0d778a5db310c
pepeto <pepeto69@gmail.com>
2014-06-02 07:14:14 +0000

    Do not slow down unit_transport_get() with the useless usage of game_unit_by_number().
    See gna patch #4694
    
    [[originally from svn r25007]]

--- common/unit.c
+++ common/unit.c
@@ -2144,12 +2144,7 @@ struct unit *unit_transport_get(const struct unit *pcargo)
 {
   fc_assert_ret_val(pcargo != NULL, NULL);
 
-  if (pcargo->transporter == NULL) {
-    return NULL;
-  } else {
-    /* Need this as the return value is not 'const'! */
-    return game_unit_by_number(pcargo->transporter->id);
-  }
+  return pcargo->transporter;
 }
 
 /*****************************************************************************
