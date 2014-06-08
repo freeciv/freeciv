f955604c4c61c11d04851c7f0dc3a7a9a9b40935
Marko Lindqvist <cazfi74@gmail.com>
2014-06-08 20:17:14 +0000

    Added script to print translation percentages from all domains.
    See patch #4778
    
    [[originally from svn r25089]]

---
+++ translations/stats.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#/**********************************************************************
+# Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2, or (at your option)
+#   any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#***********************************************************************/
+
+SRCDIR="$(dirname "$0")"
+
+dir_stats() {
+declare -i TRANS
+declare -i FUZZY
+declare -i UNTRANS
+declare -i SUM
+declare -i PRCT
+
+for POFILE in $@
+do
+  if test "x$POFILE" != "x" ; then
+    CODE=$(echo "$POFILE" | sed 's/.po//')
+    FSTR="$(LANG=C msgfmt --stat "$POFILE" 2>&1)"
+    if echo $FSTR | grep translated >/dev/null ; then
+      TRANS=$(echo $FSTR | sed 's/ translated.*//')
+      FSTR="$(echo $FSTR | sed 's/.* translated messages*[.,]//')"
+    fi
+    if echo $FSTR | grep fuzzy >/dev/null ; then
+      FUZZY=$(echo $FSTR | sed 's/ fuzzy.*//')
+      FSTR="$(echo $FSTR | sed 's/.* fuzzy translations*[.,]//')"
+    fi
+    if echo $FSTR | grep untranslated >/dev/null ; then
+      UNTRANS=$(echo $FSTR | sed 's/ untranslated.*//')
+    fi
+
+    SUM=$TRANS+$FUZZY+$UNTRANS
+    PRCT=$TRANS*100/$SUM
+
+    echo "$CODE $PRCT%"
+  fi
+done
+}
+
+for domain in freeciv nations
+do
+  echo
+  echo "$domain"
+  echo "----------"
+  ( cd "$SRCDIR/$domain"
+    dir_stats *.po
+    rm messages.mo )
+done
