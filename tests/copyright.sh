42089fc093b571c3b22b03b1f8a3518d5be11e16
Marko Lindqvist <cazfi74@gmail.com>
2014-06-16 20:12:37 +0000

    Fixed copyright.sh test - Check also ".cpp" files - "config.h" -> "fc_config.h" - Removed no longer existing "config.mac.h" from list of files to skip - Removed SDL_ttf files no longer part of freeciv from list of files to skip - Removed SDL_rotozoom files no longer part of freeciv from list of files to skip
    See patch #4792
    
    [[originally from svn r25153]]

--- tests/copyright.sh
+++ tests/copyright.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
-files=`find $1 -name "*.c" -o -name "*.h" \
+files=`find $1 -name "*.c" -o -name "*.h" -o -name "*.cpp" \
        | sort \
        | grep -v intl \
        | grep -v "Freeciv.h" \
        | fgrep -v "_gen." \
-       | grep -v "config.h" \
-       | grep -v "config.mac.h" \
+       | grep -v "fc_config.h" \
        | grep -v gtkpixcomm \
        | grep -v mmx.h \
-       | grep -v SDL_ttf \
        | grep -v xaw/canvas \
        | grep -v pixcomm \
        | grep -v dependencies \
        | grep -v utility/md5\.. \
-       | grep -v client/gui-sdl/SDL_rotozoom\.. \
        | grep -v client/gui-sdl/alphablit.c `
 
 echo "# No Freeciv Copyright:"
