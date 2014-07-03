22ff997a68d0402272ce64c45d1ac8751c7c4cfb
Marko Lindqvist <cazfi74@gmail.com>
2014-07-03 19:10:18 +0000

    Optimized one function call away from players_iterate()
    See patch #4883
    
    [[originally from svn r25376]]

--- common/player.h
+++ common/player.h
@@ -423,11 +423,10 @@ const char *diplrel_name_translation(int value);
 /* iterate over all players, which are used at the moment */
 #define players_iterate(_pplayer)                                           \
   player_slots_iterate(_pslot) {                                            \
-    if (!player_slot_is_used(_pslot)) {                                     \
-      continue;                                                             \
-    }                                                                       \
-    struct player *_pplayer = player_slot_get_player(_pslot);
+    struct player *_pplayer = player_slot_get_player(_pslot);               \
+    if (_pplayer != NULL) {
 #define players_iterate_end                                                 \
+    }                                                                       \
   } player_slots_iterate_end;
 
 /* iterate over all players, which are used at the moment and are alive */
