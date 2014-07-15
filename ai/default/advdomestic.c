f7ea4b15a331415776fb612d9185b36fe52d8efc
Marko Lindqvist <cazfi74@gmail.com>
2014-07-15 01:48:51 +0000

    Corrected comment as settler_want is nowadays calculated in aicity.c
    [[originally from svn r25566]]

--- ai/default/advdomestic.c
+++ ai/default/advdomestic.c
@@ -390,8 +390,8 @@ void domestic_advisor_choose_build(struct ai_type *ait, struct player *pplayer,
   /* Find out desire for workers (terrain improvers) */
   settler_type = dai_role_utype_for_move_type(pcity, UTYF_SETTLERS, UMT_LAND);
 
-  /* The worker want is calculated in settlers.c called from
-   * ai_manage_cities.  The expand value is the % that the AI should
+  /* The worker want is calculated in aicity.c called from
+   * dai_manage_cities.  The expand value is the % that the AI should
    * value expansion (basically to handicap easier difficulty levels)
    * and is set when the difficulty level is changed (stdinhand.c). */
   settler_want = city_data->settler_want * pplayer->ai_common.expand / 100;
