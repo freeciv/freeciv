07ae4897ab4e236434becd464434cdb493f96046
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-07-23 14:26:28 +0000

    Check that ruleset supplied specialist names won't break savegames
    Savegames stores the number of each kind of specialists a city has using the
    specialist rule name in the entry name. A specialist rule name must therefore be
    a valid entry name in a section file.
    
    See patch #4972
    
    [[originally from svn r25700]]

--- server/rssanity.c
+++ server/rssanity.c
@@ -1,4 +1,4 @@
-/********************************************************************** 
+/**********************************************************************
  Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -590,6 +590,14 @@ bool sanity_check_ruleset_data(void)
                               specialist_rule_name(psp))) {
       ruleset_error(LOG_ERROR,
                     "Specialists have conflicting or invalid requirements!");
+      ok = FALSE;
+    }
+
+    /* Specialist rulenames are used as an entry name in savegames. */
+    if (!is_secfile_entry_name_valid(specialist_rule_name(psp))) {
+      ruleset_error(LOG_ERROR,
+                    "Specialist rule name \"%s\" unusable in savegames!",
+                    specialist_rule_name(psp));
       ok = FALSE;
     }
   } specialist_type_iterate_end;
