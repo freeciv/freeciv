cce74881ac5ef06f5e927bf416a3fb704e16db9c
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-08-20 09:16:56 +0000

    SDL clients: Fix segfault during industrial sabotage in a city with no buildings
    The dialog were the building to sabotage would only initialize the variable
    pEndActiveWidgetList when the city had at least one building to sabotage. It
    would the read it later. Always initialize it.
    
    See bug #22495
    
    [[originally from svn r25959]]

--- client/gui-sdl/diplomat_dialog.c
+++ client/gui-sdl/diplomat_dialog.c
@@ -911,6 +911,9 @@ void popup_sabotage_dialog(struct unit *actor, struct city *pCity)
     
   add_to_gui_list(ID_SEPARATOR, pBuf);
   area.h += pBuf->next->size.h;
+
+  pDiplomat_Dlg->pdialog->pEndActiveWidgetList = pBuf;
+
   /* ------------------ */
   n = 0;
   city_built_iterate(pCity, pImprove) {
@@ -926,11 +929,6 @@ void popup_sabotage_dialog(struct unit *actor, struct city *pCity)
     
       area.w = MAX(area.w , pBuf->size.w);
       imp_h += pBuf->size.h;
-      
-      if (!pDiplomat_Dlg->pdialog->pEndActiveWidgetList)
-      {
-	pDiplomat_Dlg->pdialog->pEndActiveWidgetList = pBuf;
-      }
     
       if (n > 9) {
         set_wflag(pBuf, WF_HIDDEN);
