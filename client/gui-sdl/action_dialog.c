922a0ac5d2d16e86664cd58bc85dc016e9175eb2
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-09-12 12:24:23 +0000

    SDL clients: Merge diplomat_dialog.c and caravan_dialog.c to action_dialog.c
    This prepares for the merger of the dialogs them self.
    
    See patch #5214
    
    [[originally from svn r26370]]

--- client/gui-sdl/action_dialog.c
+++ client/gui-sdl/action_dialog.c
@@ -25,13 +25,14 @@
 #include "game.h"
 #include "movement.h"
 #include "research.h"
+#include "traderoutes.h"
 #include "unitlist.h"
 
 /* client */
 #include "client_main.h"
 #include "control.h"
 
-/* client/gui-sdl2 */
+/* client/gui-sdl */
 #include "citydlg.h"
 #include "colors.h"
 #include "dialogs.h"
@@ -44,6 +45,13 @@
 #include "widget.h"
 
 #include "dialogs_g.h"
+
+extern bool is_unit_move_blocked;
+
+void popdown_caravan_dialog(void);
+
+static int caravan_city_id;
+static int caravan_unit_id;
 
 struct diplomat_dialog {
   int diplomat_id;
@@ -62,6 +70,230 @@ extern bool is_unit_move_blocked;
 void popdown_diplomat_dialog(void);
 void popdown_incite_dialog(void);
 void popdown_bribe_dialog(void);
+
+/* ====================================================================== */
+/* ============================ CARAVAN DIALOG ========================== */
+/* ====================================================================== */
+static struct SMALL_DLG *pCaravan_Dlg = NULL;
+
+static int caravan_dlg_window_callback(struct widget *pWindow)
+{
+  if (Main.event.button.button == SDL_BUTTON_LEFT) {
+    move_window_group(pCaravan_Dlg->pBeginWidgetList, pWindow);
+  }
+  return -1;
+}
+
+/****************************************************************
+  User selected that caravan should establish traderoute.
+*****************************************************************/
+static int caravan_establish_trade_callback(struct widget *pWidget)
+{
+  if (Main.event.button.button == SDL_BUTTON_LEFT) {
+    dsend_packet_unit_establish_trade(&client.conn, pWidget->data.cont->id0);
+
+    popdown_caravan_dialog();
+  }
+  return -1;
+}
+
+
+/****************************************************************
+  User selected that caravan should help in wonder building.
+*****************************************************************/
+static int caravan_help_build_wonder_callback(struct widget *pWidget)
+{
+  if (Main.event.button.button == SDL_BUTTON_LEFT) {
+    dsend_packet_unit_help_build_wonder(&client.conn, pWidget->data.cont->id0);
+
+    popdown_caravan_dialog();
+  }
+  return -1;
+}
+
+/****************************************************************
+  User selected that caravan should not do anything special
+*****************************************************************/
+static int exit_caravan_dlg_callback(struct widget *pWidget)
+{
+  if (Main.event.button.button == SDL_BUTTON_LEFT) {
+    popdown_caravan_dialog();
+    process_caravan_arrival(NULL);
+  }
+  return -1;
+}
+
+/****************************************************************
+  Close caravan dialog
+*****************************************************************/
+void popdown_caravan_dialog(void)
+{
+  if (pCaravan_Dlg) {
+    is_unit_move_blocked = FALSE;
+    popdown_window_group_dialog(pCaravan_Dlg->pBeginWidgetList,
+                                pCaravan_Dlg->pEndWidgetList);
+    FC_FREE(pCaravan_Dlg);
+    flush_dirty();
+  }
+}
+
+/**************************************************************************
+  Popup a dialog giving a player choices when their caravan arrives at
+  a city (other than its home city).  Example:
+    - Establish trade route.
+    - Help build wonder.
+    - Keep moving.
+**************************************************************************/
+void popup_caravan_dialog(struct unit *pUnit,
+                          struct city *pHomecity, struct city *pDestcity)
+{
+  struct widget *pWindow = NULL, *pBuf = NULL;
+  SDL_String16 *pStr;
+  struct CONTAINER *pCont;
+  char cBuf[128];
+  SDL_Rect area;
+
+  if (pCaravan_Dlg) {
+    return;
+  }
+
+  caravan_unit_id=pUnit->id;
+  caravan_city_id=pDestcity->id;
+
+  pCont = fc_calloc(1, sizeof(struct CONTAINER));
+  pCont->id0 = pUnit->id;
+  pCont->id1 = pDestcity->id;
+
+  pCaravan_Dlg = fc_calloc(1, sizeof(struct SMALL_DLG));
+  is_unit_move_blocked = TRUE;
+
+  fc_snprintf(cBuf, sizeof(cBuf), _("Your %s has arrived at %s"),
+              unit_name_translation(pUnit), city_name(pDestcity));
+
+  /* window */
+  pStr = create_str16_from_char(cBuf, adj_font(12));
+  pStr->style |= TTF_STYLE_BOLD;
+
+  pWindow = create_window_skeleton(NULL, pStr, 0);
+
+  pWindow->action = caravan_dlg_window_callback;
+  set_wstate(pWindow, FC_WS_NORMAL);
+
+  add_to_gui_list(ID_CARAVAN_DLG_WINDOW, pWindow);
+  pCaravan_Dlg->pEndWidgetList = pWindow;
+
+  area = pWindow->area;
+  area.h = MAX(area.h, adj_size(2));
+
+  /* ---------- */
+  if (unit_has_type_flag(pUnit, UTYF_TRADE_ROUTE)
+      && can_cities_trade(pHomecity, pDestcity))
+  {
+    int revenue = get_caravan_enter_city_trade_bonus(pHomecity, pDestcity);
+
+    if (can_establish_trade_route(pHomecity, pDestcity)) {
+      fc_snprintf(cBuf, sizeof(cBuf),
+                  _("Establish Trade route with %s ( %d R&G + %d trade )"),
+                  city_name(pHomecity),
+                  revenue,
+                  trade_between_cities(pHomecity, pDestcity));
+    } else {
+      revenue = (revenue + 2) / 3;
+      fc_snprintf(cBuf, sizeof(cBuf),
+                _("Enter Marketplace ( %d R&G bonus )"), revenue);
+    }
+
+    create_active_iconlabel(pBuf, pWindow->dst, pStr,
+            cBuf, caravan_establish_trade_callback);
+    pBuf->data.cont = pCont;
+    set_wstate(pBuf, FC_WS_NORMAL);
+
+    add_to_gui_list(ID_LABEL, pBuf);
+
+    area.w = MAX(area.w, pBuf->size.w);
+    area.h += pBuf->size.h;
+  }
+
+  /* ---------- */
+  if (unit_can_help_build_wonder(pUnit, pDestcity)) {
+
+    create_active_iconlabel(pBuf, pWindow->dst, pStr,
+        _("Help build Wonder"), caravan_help_build_wonder_callback);
+
+    pBuf->data.cont = pCont;
+    set_wstate(pBuf, FC_WS_NORMAL);
+
+    add_to_gui_list(ID_LABEL, pBuf);
+
+    area.w = MAX(area.w, pBuf->size.w);
+    area.h += pBuf->size.h;
+  }
+  /* ---------- */
+
+  create_active_iconlabel(pBuf, pWindow->dst, pStr,
+            _("Keep moving"), exit_caravan_dlg_callback);
+
+  pBuf->data.cont = pCont;
+  set_wstate(pBuf, FC_WS_NORMAL);
+  set_wflag(pBuf, WF_FREE_DATA);
+  pBuf->key = SDLK_ESCAPE;
+
+  add_to_gui_list(ID_LABEL, pBuf);
+
+  area.w = MAX(area.w, pBuf->size.w);
+  area.h += pBuf->size.h;
+  /* ---------- */
+  pCaravan_Dlg->pBeginWidgetList = pBuf;
+
+  /* setup window size and start position */
+
+  resize_window(pWindow, NULL, NULL,
+                (pWindow->size.w - pWindow->area.w) + area.w,
+                (pWindow->size.h - pWindow->area.h) + area.h);
+
+  area = pWindow->area;
+
+  auto_center_on_focus_unit();
+  put_window_near_map_tile(pWindow, pWindow->size.w, pWindow->size.h,
+                           unit_tile(pUnit));
+
+  /* setup widget size and start position */
+
+  pBuf = pWindow->prev;
+  setup_vertical_widgets_position(1,
+        area.x,
+        area.y + 1, area.w, 0,
+        pCaravan_Dlg->pBeginWidgetList, pBuf);
+  /* --------------------- */
+  /* redraw */
+  redraw_group(pCaravan_Dlg->pBeginWidgetList, pWindow, 0);
+
+  widget_flush(pWindow);
+}
+
+/**************************************************************************
+  Is there currently a caravan dialog open?  This is important if there
+  can be only one such dialog at a time; otherwise return FALSE.
+**************************************************************************/
+bool caravan_dialog_is_open(int *unit_id, int *city_id)
+{
+  if (unit_id) {
+    *unit_id = caravan_unit_id;
+  }
+  if (city_id) {
+    *city_id = caravan_city_id;
+  }
+
+  return pCaravan_Dlg != NULL;
+}
+
+/**************************************************************************
+  Updates caravan dialog
+**************************************************************************/
+void caravan_dialog_update(void)
+{
+  /* PORT ME */
+}
 
 /* ====================================================================== */
 /* ============================ DIPLOMAT DIALOG ========================= */
@@ -334,9 +566,9 @@ static int spy_steal_popup(struct widget *pWidget)
   
   count++; /* count + at Spy's Discretion */
   /* max col - 104 is steal tech widget width */
-  max_col = (main_window_width() - (pWindow->size.w - pWindow->area.w) - adj_size(2)) / adj_size(104);
+  max_col = (Main.screen->w - (pWindow->size.w - pWindow->area.w) - adj_size(2)) / adj_size(104);
   /* max row - 204 is steal tech widget height */
-  max_row = (main_window_height() - (pWindow->size.h - pWindow->area.h)) / adj_size(204);
+  max_row = (Main.screen->h - (pWindow->size.h - pWindow->area.h)) / adj_size(204);
   
   /* make space on screen for scrollbar */
   if (max_col * max_row < count) {
@@ -369,7 +601,7 @@ static int spy_steal_popup(struct widget *pWidget)
       count++;
 
       copy_chars_to_string16(pStr, advance_name_translation(advance_by_number(i)));
-      pSurf = create_select_tech_icon(pStr, i, FULL_MODE);
+      pSurf = create_sellect_tech_icon(pStr, i, FULL_MODE);
       pBuf = create_icon2(pSurf, pWindow->dst,
       		WF_FREE_THEME | WF_RESTORE_BACKGROUND);
 
@@ -396,8 +628,8 @@ static int spy_steal_popup(struct widget *pWidget)
     copy_chars_to_string16(pStr, astr_str(&str));
     astr_free(&str);
   }
-  pSurf = create_select_tech_icon(pStr, i, FULL_MODE);
-
+  pSurf = create_sellect_tech_icon(pStr, i, FULL_MODE);
+	
   pBuf = create_icon2(pSurf, pWindow->dst,
     	(WF_FREE_THEME | WF_RESTORE_BACKGROUND| WF_FREE_DATA));
   set_wstate(pBuf, FC_WS_NORMAL);
@@ -442,8 +674,8 @@ static int spy_steal_popup(struct widget *pWidget)
   area = pWindow->area;
   
   widget_set_position(pWindow,
-                      (main_window_width() - pWindow->size.w) / 2,
-                      (main_window_height() - pWindow->size.h) / 2);
+                      (Main.screen->w - pWindow->size.w) / 2,
+                      (Main.screen->h - pWindow->size.h) / 2);
   
     /* exit button */
   pBuf = pWindow->prev;
@@ -697,7 +929,7 @@ void popup_diplomat_dialog(struct unit *pUnit, struct city *pCity,
   pDiplomat_Dlg->diplomat_target_id[ATK_UNIT] = -1;
 
   /* ---------- */
-  if (pCity)
+  if((pCity))
   {
     /* Spy/Diplomat acting against a city */
 
