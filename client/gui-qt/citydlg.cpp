8382cc0d0c2332ae1e14c767e05713732f6c6196
mir3x <mlewczuk80@gmail.com>
2014-09-14 18:07:51 +0000

    If there is no item selected on worklist, then it will be assumed its first item.
    See patch #5219
    
    
    [[originally from svn r26413]]

--- client/gui-qt/citydlg.cpp
+++ client/gui-qt/citydlg.cpp
@@ -2854,7 +2854,7 @@ void production_widget::prod_selected(const QItemSelection &sl,
       break;
     case 1:                 /* Change current (selected on list)*/
       if (curr_selection < 0 || curr_selection > worklist_length(&queue)) {
-        return;
+        curr_selection = 0;
       }
       worklist_remove(&queue, curr_selection);
       worklist_insert(&queue, *target, curr_selection);
@@ -2862,7 +2862,7 @@ void production_widget::prod_selected(const QItemSelection &sl,
       break;
     case 2:                 /* Insert before */
       if (curr_selection < 0 || curr_selection > worklist_length(&queue)) {
-        return;
+        curr_selection = 0;
       }
       curr_selection--;
       curr_selection = qMax(0, curr_selection);
@@ -2871,7 +2871,7 @@ void production_widget::prod_selected(const QItemSelection &sl,
       break;
     case 3:                 /* Insert after */
       if (curr_selection < 0 || curr_selection > worklist_length(&queue)) {
-        return;
+        curr_selection = 0;
       }
       curr_selection++;
       worklist_insert(&queue, *target, curr_selection);
