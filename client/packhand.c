bc366296942ebfd6e3d8ed644dd54ec236867992
pepeto <pepeto69@gmail.com>
2014-09-16 10:55:49 +0000

    Delete virtual units created in packhand with unit_virtual_destroy() to avoid memory leaks.
    See gna bug #22626
    
    [[originally from svn r26456]]

--- client/packhand.c
+++ client/packhand.c
@@ -1305,7 +1305,7 @@ void handle_unit_info(const struct packet_unit_info *packet)
 
   punit = unpackage_unit(packet);
   if (handle_unit_packet_common(punit)) {
-    free(punit);
+    unit_virtual_destroy(punit);
   }
 }
 
@@ -1797,7 +1797,7 @@ void handle_unit_short_info(const struct packet_unit_short_info *packet)
 
   punit = unpackage_short_unit(packet);
   if (handle_unit_packet_common(punit)) {
-    free(punit);
+    unit_virtual_destroy(punit);
   }
 }
 
