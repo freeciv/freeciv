89de47a4582883cb4137e31e1f65b36779814d8a
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2014-10-15 11:54:54 +0000

    Give more details when two requirements in the same vector are opposites.
    It is hard to identify the problematic requirements based on requirement
    type alone. Add requirement value, range and survives to the error message.
    
    See patch #5371
    
    [[originally from svn r26784]]

--- server/rssanity.c
+++ server/rssanity.c
@@ -275,9 +275,13 @@ static bool sanity_check_req_vec(const struct requirement_vector *preqs,
     }
     requirement_vector_iterate(preqs, nreq) {
       if (are_requirements_opposites(preq, nreq)) {
-        log_error("%s: Identical %s requirement both present and absent.",
+        log_error("%s: %s ranged %s %s requirement \"%s\" "
+                  "required to be both present and absent.",
                   list_for,
-                  universal_type_rule_name(&preq->source));
+                  req_range_name(preq->range),
+                  preq->survives ? "surviving" : "non surviving",
+                  universal_type_rule_name(&preq->source),
+                  universal_rule_name(&preq->source));
         return FALSE;
       }
     } requirement_vector_iterate_end;
