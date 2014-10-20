c9b317fdedd4bcbb4fd86ef9251c5188d8e81b4d
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2014-10-20 23:27:24 +0000

    Stop precalculating state in multiplayer/script.lua, as it ends up in savefiles and bypasses ruleset upgrade mechanisms, leading to potentially wrong results if loading old savefiles with new versions of the ruleset.
    See gna bug #22824.
    
    [[originally from svn r26809]]

--- data/multiplayer/script.lua
+++ data/multiplayer/script.lua
@@ -19,13 +19,10 @@ end
 signal.connect("city_destroyed", "city_destroyed_callback")
 
 
--- Look this up once, not each time building_built_handler() is called.
-tevo_tech = find.tech_type("Theory of Evolution")
-darw_btype = find.building_type("Darwin's Voyage")
-darw_id = darw_btype.id
-
 -- Grant two techs when the wonder Darwin`s Voyage is built.
 function building_built_handler(btype, city)
+  local darw_btype = find.building_type("Darwin's Voyage")
+  local darw_id = darw_btype.id
   local player, id = city.owner, btype.id
   local gained = {}
 
@@ -34,7 +31,8 @@ function building_built_handler(btype, city)
     -- getting two free advances again.
     -- This also prevents those they share research with from getting two
     -- free advances from building Darwin`s Voyage themselves.
-    if player:give_technology(tevo_tech, "researched") then
+    if player:give_technology(find.tech_type("Theory of Evolution"),
+                              "researched") then
       -- Give the player two free advances.
       gained[0] = player:give_technology(nil, "researched")
       gained[1] = player:give_technology(nil, "researched")
@@ -62,10 +60,6 @@ end
 signal.connect("building_built", "building_built_handler")
 
 
--- Look this up once, not each time tech_researched_handler() is called.
-phil_tech = find.tech_type("Philosophy")
-phil_id = phil_tech.id
-
 -- Grant one tech when the tech Philosophy is researched.
 function tech_researched_handler(tech, player, how)
   local id
@@ -77,7 +71,7 @@ function tech_researched_handler(tech, player, how)
   end
 
   id = tech.id
-  if id == phil_id and how == "researched" then
+  if id == find.tech_type("Philosophy").id and how == "researched" then
     -- Give the player a free advance.
     -- This will give a free advance for each player that shares research.
     gained = player:give_technology(nil, "researched")
