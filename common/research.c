d9ec75da0ca9dca4e49de307af3db0d93bae2bd0
pepeto <pepeto69@gmail.com>
2014-10-23 20:40:00 +0000

    Make research_invention_reachable() call cost not exponential with deep technology trees using root advances (e.g. experimental ruleset).
    Remove the hack when loading the ruleset consisting to propagate the root of
    an advance to its children, because one technology may have many parents-root.
    The test in research_invention_reachable() should now handle this case
    correctly.
    
    Report and original patch by aloril _PLUS_ freeciv _AT_ iki _DOT_ fi
    
    See gna patch #5357
    
    [[originally from svn r26838]]

--- common/research.c
+++ common/research.c
@@ -408,30 +408,50 @@ enum tech_state research_invention_set(struct research *presearch,
 bool research_invention_reachable(const struct research *presearch,
                                   const Tech_type_id tech)
 {
-  Tech_type_id root;
+  bv_techs done;
+  Tech_type_id techs[game.control.num_tech_types];
+  Tech_type_id t;
+  enum tech_req req;
+  int techs_num;
+  int i;
 
   if (!valid_advance_by_number(tech)) {
     return FALSE;
   }
 
-  root = advance_required(tech, AR_ROOT);
-  if (A_NONE != root) {
-    if (root == tech) {
+  techs[0] = tech;
+  BV_CLR_ALL(done);
+  BV_SET(done, A_NONE);
+  BV_SET(done, tech);
+  techs_num = 1;
+
+  for (i = 0; i < techs_num; i++) {
+    t = advance_required(techs[i], AR_ROOT);
+    if (t == techs[i]) {
       /* This tech requires itself; it can only be reached by special means
        * (init_techs, lua script, ...).
        * If you already know it, you can "reach" it; if not, not. (This case
        * is needed for descendants of this tech.) */
-      return TECH_KNOWN == research_invention_state(presearch, tech);
-    } else {
-      /* Recursive check if the player can ever reach this tech (root tech
-       * and both requirements). */
-      return (research_invention_reachable(presearch, root)
-              && research_invention_reachable(presearch,
-                                              advance_required(tech,
-                                                               AR_ONE))
-              && research_invention_reachable(presearch,
-                                              advance_required(tech,
-                                                               AR_TWO)));
+      if (TECH_KNOWN == research_invention_state(presearch, t)) {
+        continue;
+      } else {
+        return FALSE;
+      }
+    }
+
+    /* Check if requirements are reachable. */
+    for (req = 0; req < AR_SIZE; req++) {
+      t = advance_required(techs[i], req);
+      if (!valid_advance_by_number(t)) {
+        return FALSE;
+      } else if (!BV_ISSET(done, t)) {
+        if (TECH_KNOWN != research_invention_state(presearch, t)) {
+          fc_assert(techs_num < ARRAY_SIZE(techs));
+          techs[techs_num] = t;
+          techs_num++;
+        }
+        BV_SET(done, t);
+      }
     }
   }
 
