b49d1eb4e134671a255b1398091c966dcb3919ec
Marko Lindqvist <cazfi74@gmail.com>
2014-11-05 23:05:48 +0000

    Removed empty server structure, causing incompatibility between C and C++ compilations, from research.
    See bug #22895
    
    [[originally from svn r26983]]

--- common/research.h
+++ common/research.h
@@ -78,9 +78,7 @@ struct player_research {
   int num_known_tech_with_flag[TF_COUNT];
 
   union {
-    struct {
-      /* Only used in the server (./ai/ and ./server/). */
-    } server;
+    /* Add server side when needed */
 
     struct {
       /* Only used at the client (the server is omniscient; ./client/). */
