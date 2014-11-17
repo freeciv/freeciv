c49af2bc458c6a491fdfe889174a2d7e2b13e8e1
Marko Lindqvist <cazfi74@gmail.com>
2014-11-17 22:39:37 +0000

    Comment typofix "Marko" -> "Mark"
    [[originally from svn r27076]]

--- server/srv_main.c
+++ server/srv_main.c
@@ -1454,7 +1454,7 @@ void identity_number_release(int id)
 }
 
 /**************************************************************************
-  Marko identity number allocated.
+  Mark identity number allocated.
 **************************************************************************/
 void identity_number_reserve(int id)
 {
