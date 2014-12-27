ece1f634f5b6e8b14103fc1ddf61d3ceec2e7dd2
Marko Lindqvist <cazfi74@gmail.com>
2014-12-27 07:01:21 +0000

    Added gui-specific commandline parameter handling to modpack installer as was already claimed by "--help", notably added '-- --help" for all guis.
    See bug #23102
    
    [[originally from svn r27418]]

--- tools/mpgui_gtk3.c
+++ tools/mpgui_gtk3.c
@@ -20,6 +20,7 @@
 #include <gtk/gtk.h>
 
 /* utility */
+#include "fciconv.h"
 #include "fcintl.h"
 #include "log.h"
 #include "mem.h"
@@ -572,6 +573,23 @@ int main(int argc, char *argv[])
 
   /* This modifies argv! */
   ui_options = fcmp_parse_cmdline(argc, argv);
+
+  if (ui_options != -1) {
+    int i;
+
+    for (i = 1; i <= ui_options; i++) {
+      if (is_option("--help", argv[i])) {
+        fc_fprintf(stderr,
+             _("This modpack installer accepts the standard Gtk command-line options\n"
+               "after '--'. See the Gtk documentation.\n\n"));
+
+        /* TRANS: No full stop after the URL, could cause confusion. */
+        fc_fprintf(stderr, _("Report bugs at %s\n"), BUG_URL);
+
+        ui_options = -1;
+      }
+    }
+  }
 
   if (ui_options != -1) {
 
