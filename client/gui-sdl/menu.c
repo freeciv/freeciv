17e159e5a2cd71ff0230bd7e0a1be7fd00393a0d
Marko Lindqvist <cazfi74@gmail.com>
2015-01-12 20:38:57 +0000

    Corrected display of Transform command key to "O" in sdl-clients
    Reported anonymously
    
    See bug #23162
    
    [[originally from svn r27647]]

--- client/gui-sdl/menu.c
+++ client/gui-sdl/menu.c
@@ -1216,12 +1216,12 @@ void real_menus_update(void)
       }
 
       if (can_unit_do_activity(pUnit, ACTIVITY_TRANSFORM)) {
-	time = tile_activity_time(ACTIVITY_TRANSFORM, unit_tile(pUnit));
-	fc_snprintf(cBuf, sizeof(cBuf),"%s %s (%s) %d %s",
-	  _("Transform to"),
-	  terrain_name_translation(pTerrain->transform_result),
-			"M", time , 
-			PL_("turn", "turns", time));
+        time = tile_activity_time(ACTIVITY_TRANSFORM, unit_tile(pUnit));
+        fc_snprintf(cBuf, sizeof(cBuf),"%s %s (%s) %d %s",
+                    _("Transform to"),
+                    terrain_name_translation(pTerrain->transform_result),
+                    "O", time,
+                    PL_("turn", "turns", time));
         copy_chars_to_string16(pOrder_Transform_Button->info_label, cBuf);
 	clear_wflag(pOrder_Transform_Button, WF_HIDDEN);
       } else {
