4a2ccf3e5369a017e985f9a8c6354d01290fcfce
Marko Lindqvist <cazfi74@gmail.com>
2015-01-12 21:29:47 +0000

    Backported luasql commit: Bug correction in the implementation of luaL_setfuncs
    See patch #5675
    
    [[originally from svn r27650]]

--- dependencies/luasql/src/luasql.c
+++ dependencies/luasql/src/luasql.c
@@ -67,12 +67,12 @@ static int luasql_tostring (lua_State *L) {
 ** Adapted from Lua 5.2.0
 */
 void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
-	luaL_checkstack(L, nup, "too many upvalues");
+	luaL_checkstack(L, nup+1, "too many upvalues");
 	for (; l->name != NULL; l++) {	/* fill the table with given functions */
 		int i;
+                lua_pushstring(L, l->name);
 		for (i = 0; i < nup; i++)	/* copy upvalues to the top */
-			lua_pushvalue(L, -nup);
-		lua_pushstring(L, l->name);
+                        lua_pushvalue(L, -(nup + 1));
 		lua_pushcclosure(L, l->func, nup);	/* closure with those upvalues */
 		lua_settable(L, -(nup + 3));
 	}
