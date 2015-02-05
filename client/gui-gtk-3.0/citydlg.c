e735e200477272c6f29c5c357f20ae38e66332ca
Marko Lindqvist <cazfi74@gmail.com>
2015-02-05 02:05:16 +0000

    Initialize the variable telling if the citydlg system has been initialized itself.
    See patch #5780
    
    [[originally from svn r27990]]

--- client/gui-gtk-3.0/citydlg.c
+++ client/gui-gtk-3.0/citydlg.c
@@ -192,7 +192,7 @@ struct city_dialog {
 };
 
 static struct dialog_list *dialog_list;
-static bool city_dialogs_have_been_initialised;
+static bool city_dialogs_have_been_initialised = FALSE;
 static int canvas_width, canvas_height;
 static int new_dialog_def_page = OVERVIEW_PAGE;
 static int last_page = OVERVIEW_PAGE;
@@ -340,8 +340,9 @@ void reset_city_dialogs(void)
 *****************************************************************/
 static struct city_dialog *get_city_dialog(struct city *pcity)
 {
-  if (!city_dialogs_have_been_initialised)
+  if (!city_dialogs_have_been_initialised) {
     initialize_city_dialogs();
+  }
 
   dialog_list_iterate(dialog_list, pdialog) {
     if (pdialog->pcity == pcity)
@@ -553,10 +554,12 @@ void popdown_all_city_dialogs(void)
   if (!city_dialogs_have_been_initialised) {
     return;
   }
+
   while (dialog_list_size(dialog_list)) {
     close_city_dialog(dialog_list_get(dialog_list, 0));
   }
   dialog_list_destroy(dialog_list);
+
   city_dialogs_have_been_initialised = FALSE;
 }
 
