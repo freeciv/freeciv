17db8a2bf622b095e8b616e7b1961bbddb1dfc8f
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-03-13 13:00:49 +0000

    Give the requirement system the ability the check if a tile is claimed.
    (I think this was) requested by Marko Lindqvist <cazfi>
    
    See patch #5929
    
    [[originally from svn r28513]]

--- common/requirements.c
+++ common/requirements.c
@@ -2334,6 +2334,46 @@ static enum fc_tristate is_citytile_in_range(const struct tile *target_tile,
       fc_assert_msg(FALSE, "Invalid range %d for citytile.", range);
 
       return TRI_MAYBE;
+    } else if (citytile == CITYT_CLAIMED) {
+      switch (range) {
+      case REQ_RANGE_LOCAL:
+        return BOOL_TO_TRISTATE(target_tile->owner != NULL);
+      case REQ_RANGE_CADJACENT:
+        if (target_tile->owner != NULL) {
+          return TRI_YES;
+        }
+        cardinal_adjc_iterate(target_tile, adjc_tile) {
+          if (adjc_tile->owner != NULL) {
+            return TRI_YES;
+          }
+        } cardinal_adjc_iterate_end;
+
+        return TRI_NO;
+      case REQ_RANGE_ADJACENT:
+        if (target_tile->owner != NULL) {
+          return TRI_YES;
+        }
+        adjc_iterate(target_tile, adjc_tile) {
+          if (adjc_tile->owner != NULL) {
+            return TRI_YES;
+          }
+        } adjc_iterate_end;
+
+        return TRI_NO;
+      case REQ_RANGE_CITY:
+      case REQ_RANGE_TRADEROUTE:
+      case REQ_RANGE_CONTINENT:
+      case REQ_RANGE_PLAYER:
+      case REQ_RANGE_TEAM:
+      case REQ_RANGE_ALLIANCE:
+      case REQ_RANGE_WORLD:
+      case REQ_RANGE_COUNT:
+        break;
+      }
+
+      fc_assert_msg(FALSE, "Invalid range %d for citytile.", range);
+
+      return TRI_MAYBE;
     } else {
       /* Not implemented */
       log_error("is_req_active(): citytile %d not supported.",
