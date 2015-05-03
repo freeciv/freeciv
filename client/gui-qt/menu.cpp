7a19a50a99e384ad70ac17419aadd252ed5bf003
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-05-03 16:38:17 +0000

    Qt client: Use ruleset defined action ui names in menu items.
    See patch #6061
    
    [[originally from svn r28980]]

--- client/gui-qt/menu.cpp
+++ client/gui-qt/menu.cpp
@@ -1271,12 +1271,14 @@ void mr_menu::menus_sensitive()
         break;
 
       case BUILD_WONDER:
+        i.value()->setText(action_get_ui_name(ACTION_HELP_WONDER));
         if (can_units_do(punits, unit_can_help_build_wonder_here)) {
           i.value()->setEnabled(true);
         }
         break;
 
       case ORDER_TRADEROUTE:
+        i.value()->setText(action_get_ui_name(ACTION_TRADE_ROUTE));
         if (can_units_do(punits, unit_can_est_trade_route_here)) {
           i.value()->setEnabled(true);
         }
