948e1c8da34e755cd785ab3a7d3db5c6523ac253
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-07-03 11:33:35 +0000

    Let SuperSpy units fight back against spy actions.
    Let SuperSpy units like Leader defend them self when they are the intended
    victim of a spy action. SuperSpy units weren't considered when unit victims
    stopped defending them self.
    
    See bug #23672
    
    [[originally from svn r29193]]

--- server/diplomats.c
+++ server/diplomats.c
@@ -1336,8 +1336,12 @@ static bool diplomat_infiltrate_tile(struct player *pplayer,
       continue;
     }
 
-    /* This victim is, by ability or by will, defenseless. */
-    if (punit == pvictim) {
+    if (punit == pvictim
+        && !unit_has_type_flag(punit, UTYF_SUPERSPY)) {
+      /* The victim unit is defenseless unless it's a SuperSpy.
+       * Rationalization: A regular diplomat don't mind being bribed. A
+       * SuperSpy is high enough up the chain that accepting a bribe is
+       * against his own interests. */
       continue;
     }
 
