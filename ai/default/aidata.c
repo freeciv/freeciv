d23267d24a254217d7464b8c9ee21fc3b6826724
Marko Lindqvist <cazfi74@gmail.com>
2015-08-01 22:41:03 +0000

    Clear advisor and default AI task of a unit when goto_tile gets cleared as part of rescuing planes. Avoid crash if diplomat unit has attack-task, but no target selected.
    See bug #23728
    
    [[originally from svn r29289]]

--- ai/default/aidata.c
+++ ai/default/aidata.c
@@ -216,14 +216,16 @@ void dai_data_phase_begin(struct ai_type *ait, struct player *pplayer,
          || unit_can_do_action(punit, ACTION_SPY_INVESTIGATE_CITY)
          || unit_can_do_action(punit, ACTION_SPY_STEAL_GOLD))
         && def_ai_unit_data(punit, ait)->task == AIUNIT_ATTACK) {
-      /* Heading somewhere on a mission, reserve target. */
 
-      fc_assert_msg(punit->goto_tile, "No target city for spy action");
+      fc_assert_msg(punit->goto_tile != NULL, "No target city for spy action");
 
-      struct city *pcity = tile_city(punit->goto_tile);
+      if (punit->goto_tile != NULL) {
+        struct city *pcity = tile_city(punit->goto_tile);
 
-      if (pcity) {
-        BV_SET(ai->stats.diplomat_reservations, pcity->id);
+        if (pcity != NULL) {
+          /* Heading somewhere on a mission, reserve target. */
+          BV_SET(ai->stats.diplomat_reservations, pcity->id);
+        }
       }
     }
   } unit_list_iterate_end;
