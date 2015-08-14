1416e5bdf705637e7549b0c7ab01d9e575e3fba5
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-08-14 13:04:57 +0000

    orders system: support the "Enter Marketplace" action
    See patch #6225
    
    [[originally from svn r29496]]

--- server/unithand.c
+++ server/unithand.c
@@ -3644,6 +3644,7 @@ void handle_unit_orders(struct player *pplayer,
         break;
       case ACTION_ESTABLISH_EMBASSY:
       case ACTION_TRADE_ROUTE:
+      case ACTION_MARKETPLACE:
       case ACTION_HELP_WONDER:
       case ACTION_JOIN_CITY:
         /* Don't validate that the target tile really contains a city or
