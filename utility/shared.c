127f03b7a21c04d6c50a31b4d03de66f20667a8f
Marko Lindqvist <cazfi74@gmail.com>
2015-08-28 18:24:03 +0000

    Fixed user_home_dir() to correctly build up the home dir information again if it has been cleared since previous call. That never happens with current codebase.
    See bug #23823
    
    [[originally from svn r29708]]

--- utility/shared.c
+++ utility/shared.c
@@ -732,10 +732,10 @@ char *user_home_dir(void)
 #ifdef AMIGA
   return "PROGDIR:";
 #else  /* AMIGA */
-  static bool init = FALSE;
 
-  if (!init) {
+  if (home_dir == NULL) {
     char *env = getenv("HOME");
+
     if (env) {
       home_dir = fc_strdup(env);
       log_verbose("HOME is %s", home_dir);
@@ -788,7 +788,6 @@ char *user_home_dir(void)
       home_dir = NULL;
 #endif /* WIN32_NATIVE */
     }
-    init = TRUE;
   }
 
   return home_dir;
