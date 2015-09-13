eb15d4525e22d7196b8287b1a381d8cfbccd3392
Marko Lindqvist <cazfi74@gmail.com>
2015-09-13 06:49:55 +0000

    Turned genlist_head() to an inline function.
    See patch #6342
    
    [[originally from svn r29870]]

--- utility/genlist.h
+++ utility/genlist.h
@@ -40,13 +40,14 @@ extern "C" {
   See also the speclist module.
 ****************************************************************************/
 
+/* utility */
+#include "fcthread.h"
 #include "support.h"    /* bool, fc__warn_unused_result */
 
 /* A single element of a genlist, opaque type. */
 struct genlist_link;
 
-/* A genlist, opaque type. */
-struct genlist;
+struct genlist_link;
 
 /* Function type definitions. */
 typedef void (*genlist_free_fn_t) (void *);
@@ -54,6 +55,17 @@ typedef void * (*genlist_copy_fn_t) (const void *);
 typedef bool (*genlist_cond_fn_t) (const void *);
 typedef bool (*genlist_comp_fn_t) (const void *, const void *);
 
+/* A genlist, storing the number of elements (for quick retrieval and
+ * testing for empty lists), and pointers to the first and last elements
+ * of the list. */
+struct genlist {
+  int nelements;
+  fc_mutex mutex;
+  struct genlist_link *head_link;
+  struct genlist_link *tail_link;
+  genlist_free_fn_t free_data_func;
+};
+  
 struct genlist *genlist_new(void) fc__warn_unused_result;
 struct genlist *genlist_new_full(genlist_free_fn_t free_data_func)
                 fc__warn_unused_result;
@@ -95,7 +107,10 @@ void *genlist_get(const struct genlist *pgenlist, int idx);
 void *genlist_front(const struct genlist *pgenlist);
 void *genlist_back(const struct genlist *pgenlist);
 struct genlist_link *genlist_link(const struct genlist *pgenlist, int idx);
-struct genlist_link *genlist_head(const struct genlist *pgenlist);
+inline struct genlist_link *genlist_head(const struct genlist *pgenlist)
+{
+  return (NULL != pgenlist ? pgenlist->head_link : NULL);
+}
 struct genlist_link *genlist_tail(const struct genlist *pgenlist);
 
 struct genlist_link *genlist_search(const struct genlist *pgenlist,
