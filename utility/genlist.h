7b7eec0b3e83616d433bc35b720e46b813aa9f1a
Marko Lindqvist <cazfi74@gmail.com>
2015-09-15 03:39:56 +0000

    Made genlist_head() static.
    Reported by Sveinung Kvilhaugsvik <sveinung>
    
    See bug #23870
    
    [[originally from svn r29890]]

--- utility/genlist.h
+++ utility/genlist.h
@@ -107,7 +107,7 @@ void *genlist_get(const struct genlist *pgenlist, int idx);
 void *genlist_front(const struct genlist *pgenlist);
 void *genlist_back(const struct genlist *pgenlist);
 struct genlist_link *genlist_link(const struct genlist *pgenlist, int idx);
-inline struct genlist_link *genlist_head(const struct genlist *pgenlist)
+inline static struct genlist_link *genlist_head(const struct genlist *pgenlist)
 {
   return (NULL != pgenlist ? pgenlist->head_link : NULL);
 }
