b4f0841da07dc4a35f3610fcb4c2b0c826c8bb4e
Marko Lindqvist <cazfi74@gmail.com>
2015-09-16 03:02:05 +0000

    Avoid garbage characters in the beginning of the ruleset summary at client side.
    See bug #23869
    
    [[originally from svn r29895]]

--- client/packhand.c
+++ client/packhand.c
@@ -3028,7 +3028,7 @@ void handle_ruleset_summary(const struct packet_ruleset_summary *packet)
 
   game.ruleset_summary = fc_malloc(len + 1);
 
-  fc_strlcat(game.ruleset_summary, packet->text, len);
+  fc_strlcpy(game.ruleset_summary, packet->text, len);
 }
 
 /****************************************************************************
