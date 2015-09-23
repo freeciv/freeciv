d8cb1b357623ac156e68567ace4261835baa2174
Marko Lindqvist <cazfi74@gmail.com>
2015-09-23 13:30:51 +0000

    Added 'return' to activity_requires_target() for compilations where fc_assert_ret_val() is not defined.
    See bug #23889
    
    [[originally from svn r29956]]

--- common/unit.c
+++ common/unit.c
@@ -554,6 +554,8 @@ bool activity_requires_target(enum unit_activity activity)
   default:
     fc_assert_ret_val(FALSE, FALSE);
   }
+
+  return FALSE;
 }
 
 /**************************************************************************
