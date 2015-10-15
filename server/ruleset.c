86563e664d084eb3ecd7889ab26a53bbdd50539e
Marko Lindqvist <cazfi74@gmail.com>
2015-10-15 16:58:02 +0000

    Check unused entries from game.ruleset only after also settings have been loaded.
    See patch #6407
    
    [[originally from svn r30096]]

--- server/ruleset.c
+++ server/ruleset.c
@@ -5977,9 +5977,8 @@ static bool load_ruleset_game(struct section_file *file, bool act,
     }
   }
 
-  if (ok) {
-    secfile_check_unused(file);
-  }
+  /* secfile_check_unused() is not here, but only after also settings section
+   * has been loaded. */
 
   return ok;
 }
@@ -7134,6 +7133,10 @@ static bool load_rulesetdir(const char *rsdir, bool compat_mode,
   if (ok) {
     /* Only load settings for a sane ruleset */
     ok = settings_ruleset(gamefile, "settings", act);
+
+    if (ok) {
+      secfile_check_unused(gamefile);
+    }
   }
 
   nullcheck_secfile_destroy(techfile);
