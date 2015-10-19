ab97f7fa93191fa79ea97cc98acacd3d0958ddd3
Marko Lindqvist <cazfi74@gmail.com>
2015-10-19 18:45:56 +0000

    Added a couple of AI callbacks that classic AI uses to save its information to the same table as default AI part does.
    See bug #23874
    
    [[originally from svn r30143]]

--- ai/default/aiplayer.h
+++ ai/default/aiplayer.h
@@ -22,11 +22,17 @@ struct ai_plr;
 void dai_player_alloc(struct ai_type *ait, struct player *pplayer);
 void dai_player_free(struct ai_type *ait, struct player *pplayer);
 void dai_player_save(struct ai_type *ait, const char *aitstr,
-                     struct player *pplayer,
-                     struct section_file *file, int plrno);
+                     struct player *pplayer, struct section_file *file,
+                     int plrno);
+void dai_player_save_relations(struct ai_type *ait, const char *aitstr,
+                               struct player *pplayer, struct player *other,
+                               struct section_file *file, int plrno);
 void dai_player_load(struct ai_type *ait, const char *aitstr,
-                     struct player *pplayer,
-                     const struct section_file *file, int plrno);
+                     struct player *pplayer, const struct section_file *file,
+                     int plrno);
+void dai_player_load_relations(struct ai_type *ait, const char *aitstr,
+                               struct player *pplayer, struct player *other,
+                               const struct section_file *file, int plrno);
 
 void dai_player_copy(struct ai_type *ait,
                      struct player *original, struct player *created);
