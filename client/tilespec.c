667324c47a953a26ed628c7196d56b688207a7cb
Marko Lindqvist <cazfi74@gmail.com>
2015-10-20 15:52:37 +0000

    Use sdl2-client's own theme setting instead of that of sdl-client.
    See bug #23942
    
    [[originally from svn r30151]]

--- client/tilespec.c
+++ client/tilespec.c
@@ -6067,9 +6067,12 @@ void tileset_use_prefered_theme(const struct tileset *t)
     default_theme_name_sz = sizeof(options.gui_gtk3_default_theme_name);
     break;
   case GUI_SDL:
-  case GUI_SDL2:
     default_theme_name = options.gui_sdl_default_theme_name;
     default_theme_name_sz = sizeof(options.gui_sdl_default_theme_name);
+    break;
+  case GUI_SDL2:
+    default_theme_name = options.gui_sdl2_default_theme_name;
+    default_theme_name_sz = sizeof(options.gui_sdl2_default_theme_name);
     break;
   case GUI_STUB:
   case GUI_XAW:
