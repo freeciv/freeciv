528b0d8aa05f0e0916a1e142988e9732ca6a8aec
Marko Lindqvist <cazfi74@gmail.com>
2015-10-25 09:06:08 +0000

    Do not refresh cities during savegame loading when tradepartner has not necessarily beed loaded yet.
    See bug #23614
    
    [[originally from svn r30209]]

--- server/savegame2.c
+++ server/savegame2.c
@@ -3594,9 +3594,12 @@ static void sg_load_players(struct loaddata *loading)
   /* Update all city information.  This must come after all cities are
    * loaded (in player_load) but before player (dumb) cities are loaded
    * in player_load_vision(). */
-  cities_iterate(pcity) {
-    city_refresh_from_main_map(pcity, NULL);
-  } cities_iterate_end;
+  players_iterate(plr) {
+    city_list_iterate(plr->cities, pcity) {
+      city_refresh(pcity);
+      sanity_check_city(pcity);
+    } city_list_iterate_end;
+  } players_iterate_end;
 
   /* Since the cities must be placed on the map to put them on the
      player map we do this afterwards */
@@ -4321,20 +4324,8 @@ static void sg_load_player_cities(struct loaddata *loading,
     vision_reveal_tiles(pcity->server.vision, game.server.vision_reveal_tiles);
     city_refresh_vision(pcity);
 
-    /* Refresh the city. This also checks the squared city radius. Thus, it
-     * must be after improvements, as the effect City_Radius_SQ could be
-     * influenced by improvements; and after the vision is defined, as the
-     * function calls city_refresh_vision(). */
-    city_refresh(pcity);
-
     city_list_append(plr->cities, pcity);
   }
-
-  /* Check the sanity of the cities. */
-  city_list_iterate(plr->cities, pcity) {
-    city_refresh(pcity);
-    sanity_check_city(pcity);
-  } city_list_iterate_end;
 }
 
 /****************************************************************************
