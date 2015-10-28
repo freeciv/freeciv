b6135104bda8a433bb98673bbdce9c89b1e50935
mir3x <mlewczuk80@gmail.com>
2015-10-28 09:34:19 +0000

    Client could crash in pregame page, when there is ongoing game and turn was changed (client was told to update information label which wasn't created yet). Fix attatched.
    See bug #23963
    
    [[originally from svn r30271]]

--- client/gui-qt/mapview.cpp
+++ client/gui-qt/mapview.cpp
@@ -1146,8 +1146,12 @@ void info_label::info_update()
 ****************************************************************************/
 void update_info_label(void)
 {
-  QString eco_info;
-  QString s = QString(_("%1 (Turn:%2)")).arg(textyear(game.info.year),
+  QString s, eco_info;
+
+  if (gui()->current_page() != PAGE_GAME) {
+    return;
+  }
+  s = QString(_("%1 (Turn:%2)")).arg(textyear(game.info.year),
                                              QString::number(game.info.turn));
   gui()->game_info_label->set_turn_info(s);
   set_indicator_icons(client_research_sprite(),
