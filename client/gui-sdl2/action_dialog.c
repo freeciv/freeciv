d0a6a2724cd7d64f8e0a31d04a4fbf4d1373e8c5
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-11-06 12:34:54 +0000

    SDL 2 client: just show targeted in act sel dlg
    It is possible to do the untargeted variant of Steal Tech and Sabotage City
    from the target selection dialog. Don't show the untargeted variant when
    the targeted variant is possible. Let the player pick it as a target.
    
    This restores the 2.5 behavior.
    
    See patch #6540
    
    [[originally from svn r30440]]

--- client/gui-sdl2/action_dialog.c
+++ client/gui-sdl2/action_dialog.c
@@ -978,6 +978,22 @@ static void action_entry(const enum gen_action act,
   utf8_str *pstr;
   const char *ui_name;
 
+  if (act == ACTION_SPY_SABOTAGE_CITY
+      && action_prob_possible(
+        action_probabilities[ACTION_SPY_TARGETED_SABOTAGE_CITY])) {
+    /* The player can select Sabotage City from the target selection dialog
+     * of Targeted Sabotage City. */
+    return;
+  }
+
+  if (act == ACTION_SPY_STEAL_TECH
+      && action_prob_possible(
+        action_probabilities[ACTION_SPY_TARGETED_STEAL_TECH])) {
+    /* The player can select Steal Tech from the target selection dialog of
+     * Targeted Steal Tech. */
+    return;
+  }
+
   /* Don't show disabled actions */
   if (!action_prob_possible(action_probabilities[act])) {
     return;
