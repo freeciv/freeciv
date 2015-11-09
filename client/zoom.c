708ac4fb2f51069f33bf951f52139d96e558eab7
Marko Lindqvist <cazfi74@gmail.com>
2015-11-09 18:44:34 +0000

    Added zoom steps API
    See patch #6543
    
    [[originally from svn r30498]]

--- client/zoom.c
+++ client/zoom.c
@@ -24,6 +24,10 @@
 float map_zoom = 1.0;
 bool zoom_enabled = FALSE;
 
+static float zoom_steps[] = {
+  -1.0, 0.10, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 2.0, 2.5, 3.0, 4.0, -1.0
+};
+
 /**************************************************************************
   Set map zoom level.
 **************************************************************************/
@@ -44,4 +48,52 @@ void zoom_1_0(void)
   map_zoom = 1.0;
 
   map_canvas_resized(mapview.width, mapview.height);
+}
+
+/**************************************************************************
+  Zoom level one step up
+**************************************************************************/
+void zoom_step_up(void)
+{
+  int i;
+
+  /* Even if below previous step, close enough is considered to be in
+   * previous step so that change is not miniscule */
+  for (i = 1 ;
+       zoom_steps[i] < map_zoom * 1.05 && zoom_steps[i] > 0 ;
+       i++ ) {
+    /* empty */
+  }
+
+  if (zoom_steps[i] > 0) {
+    if (zoom_steps[i] > 0.99 && zoom_steps[i] < 1.01) {
+      zoom_1_0();
+    } else {
+      zoom_set(zoom_steps[i]);
+    }
+  }
+}
+
+/**************************************************************************
+  Zoom level one step down
+**************************************************************************/
+void zoom_step_down(void)
+{
+  int i;
+
+  /* Even if above previous step, close enough is considered to be in
+   * previous step so that change is not miniscule */
+  for (i = ARRAY_SIZE(zoom_steps) - 2 ;
+       zoom_steps[i] * 1.05 > map_zoom && zoom_steps[i] > 0 ;
+       i-- ) {
+    /* empty */
+  }
+
+  if (zoom_steps[i] > 0) {
+    if (zoom_steps[i] > 0.99 && zoom_steps[i] < 1.01) {
+      zoom_1_0();
+    } else {
+      zoom_set(zoom_steps[i]);
+    }
+  }
 }
