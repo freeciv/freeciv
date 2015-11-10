4ff3e76696d44b7ceabede6d4810b303c2e86c5d
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2015-11-10 22:55:51 +0000

    Lua functions for finding player and nation trait properties.
    Also, existing trait_mod() function now throws error if given invalid
    trait string.
    
    New file api_server_game_methods.c added for server-only methods on
    modules defined in tolua_game.pkg.
    
    See gna patch #6557.
    
    [[originally from svn r30526]]

--- server/scripting/api_server_edit.c
+++ server/scripting/api_server_edit.c
@@ -325,14 +325,18 @@ Tech_Type *api_edit_give_technology(lua_State *L, Player *pplayer,
 /*****************************************************************************
   Modify player's trait value.
 *****************************************************************************/
-bool api_edit_trait_mod(lua_State *L, Player *pplayer, const char *trait_name,
-                        const int mod)
+bool api_edit_trait_mod_set(lua_State *L, Player *pplayer,
+                            const char *trait_name, const int mod)
 {
-  enum trait tr = trait_by_name(trait_name, fc_strcasecmp);
+  enum trait tr;
 
-  if (!trait_is_valid(tr)) {
-    return FALSE;
-  }
+  LUASCRIPT_CHECK_STATE(L, -1);
+  LUASCRIPT_CHECK_ARG_NIL(L, pplayer, 2, Player, FALSE);
+  LUASCRIPT_CHECK_ARG_NIL(L, trait_name, 3, string, FALSE);
+
+  tr = trait_by_name(trait_name, fc_strcasecmp);
+
+  LUASCRIPT_CHECK_ARG(L, trait_is_valid(tr), 3, "no such trait", 0);
 
   pplayer->ai_common.traits[tr].mod += mod;
 
