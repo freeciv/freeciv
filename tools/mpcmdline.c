16d74308f47830dddef563db57fa795db5bed040
Marko Lindqvist <cazfi74@gmail.com>
2015-11-19 00:25:13 +0000

    Use freeciv_storage_dir() instead of user_home_dir() as the default storage location for modpack installer.
    See bug #24092
    
    [[originally from svn r30679]]

--- tools/mpcmdline.c
+++ tools/mpcmdline.c
@@ -98,15 +98,10 @@ int fcmp_parse_cmdline(int argc, char *argv[])
   }
 
   if (fcmp.inst_prefix == NULL) {
-    const char *home = user_home_dir();
+    fcmp.inst_prefix = freeciv_storage_dir();
 
-    if (home == NULL) {
-      log_error("Cannot determine user home directory");
-    } else {
-      static char pfx_buf[500];
-
-      snprintf(pfx_buf, sizeof(pfx_buf), "%s/.freeciv", home);
-      fcmp.inst_prefix = pfx_buf;
+    if (fcmp.inst_prefix == NULL) {
+      log_error("Cannot determine freeciv storage directory");
     }
   }
 
