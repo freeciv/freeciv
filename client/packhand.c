ee419b419cb755f96ad5162e518cd14b512168c2
Marko Lindqvist <cazfi74@gmail.com>
2015-11-19 00:17:27 +0000

    Consider server busy until beginning of first phase. That means that client keeps the busy cursor, and Turn Done button does not get sensitive while server is still processing beginning of the first phase.
    See patch #6584
    
    [[originally from svn r30678]]

--- client/packhand.c
+++ client/packhand.c
@@ -1184,6 +1184,13 @@ void handle_start_phase(int phase)
 
   game.info.phase = phase;
 
+  /* Possibly replace wait cursor with something else */
+  if (phase == 0) {
+    /* TODO: Have server set as busy also if switching phase
+     * is taking long in a alternating phases mode. */
+    set_server_busy(FALSE);
+  }
+
   if (NULL != client.conn.playing
       && is_player_phase(client.conn.playing, phase)) {
     agents_start_turn();
@@ -1219,8 +1226,8 @@ void handle_begin_turn(void)
 {
   log_debug("handle_begin_turn()");
 
-  /* Possibly replace wait cursor with something else */
-  set_server_busy(FALSE);
+  /* Server is still considered busy until it handles also the beginning
+   * of the first phase. */
 }
 
 /**************************************************************************
