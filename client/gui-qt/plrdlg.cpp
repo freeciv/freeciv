f293e421302329b998cecf9d7b8f2414284c8b24
mir3x <mlewczuk80@gmail.com>
2015-11-23 17:10:54 +0000

    Player dialog headers are now resized to fit actual contents in qt-client.
    See bug #24102
    
    [[originally from svn r30748]]

--- client/gui-qt/plrdlg.cpp
+++ client/gui-qt/plrdlg.cpp
@@ -649,6 +649,7 @@ void plr_report::update_report(bool update_selection)
     }
   }
 
+  plr_wdg->header()->resizeSections(QHeaderView::ResizeToContents);
   meet_but->setDisabled(true);
   cancel_but->setDisabled(true);
   withdraw_but->setDisabled(true);
