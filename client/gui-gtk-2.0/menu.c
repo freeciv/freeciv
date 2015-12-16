068c56568f355df542f2bdef6c45d52beffc1abd
Marko Lindqvist <cazfi74@gmail.com>
2015-12-16 14:18:52 +0000

    Renamed transform_terrain_callack() as transform_terrain_callback()
    See patch #6667
    
    [[originally from svn r31017]]

--- client/gui-gtk-2.0/menu.c
+++ client/gui-gtk-2.0/menu.c
@@ -1194,7 +1194,7 @@ static void connect_irrigation_callback(GtkAction *action, gpointer data)
 /****************************************************************
   Action "TRANSFORM_TERRAIN" callback.
 *****************************************************************/
-static void transform_terrain_callack(GtkAction *action, gpointer data)
+static void transform_terrain_callback(GtkAction *action, gpointer data)
 {
   key_unit_transform();
 }
@@ -1732,7 +1732,7 @@ static GtkActionGroup *get_unit_group(void)
        "<Shift>i", NULL, G_CALLBACK(connect_irrigation_callback)},
 
       {"TRANSFORM_TERRAIN", NULL, _("Transf_orm Terrain"),
-       "o", NULL, G_CALLBACK(transform_terrain_callack)},
+       "o", NULL, G_CALLBACK(transform_terrain_callback)},
       {"CLEAN_POLLUTION", NULL, _("Clean _Pollution"),
        "p", NULL, G_CALLBACK(clean_pollution_callback)},
       {"CLEAN_FALLOUT", NULL, _("Clean _Nuclear Fallout"),
