65e650673875d70f821411e934f1479400828f9d
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2015-12-17 02:33:41 +0000

    If a player's name corresponds to a leader in more than one nation at game start, pick a random nation rather than the first one encountered.
    Spotted by Marko Lindqvist (cazfi@gna).
    
    See gna bug #24181.
    
    [[originally from svn r31033]]

--- server/srv_main.c
+++ server/srv_main.c
@@ -2410,19 +2410,29 @@ static void generate_players(void)
       continue;
     }
 
-    /* See if the player name matches a known leader name. */
-    allowed_nations_iterate(pnation) {
-      struct nation_leader *pleader;
-      const char *name = player_name(pplayer);
+    /* See if the player name matches a known leader name.
+     * If more than one nation has this leader name, pick one at random.
+     * No attempt is made to avoid clashes to maximise the number of
+     * nations that can be assigned in this way. */
+    {
+      struct nation_list *candidates = nation_list_new();
+      int n = 0;
 
-      if (is_nation_playable(pnation)
-          && client_can_pick_nation(pnation)
-          && NULL == pnation->player
-          && (pleader = nation_leader_by_name(pnation, name))) {
-        player_set_nation_full(pplayer, pnation);
-        break;
+      allowed_nations_iterate(pnation) {
+        if (is_nation_playable(pnation)
+            && client_can_pick_nation(pnation)
+            && NULL == pnation->player
+            && (nation_leader_by_name(pnation, player_name(pplayer)))) {
+          nation_list_append(candidates, pnation);
+          n++;
+        }
+      } allowed_nations_iterate_end;
+      if (n > 0) {
+        player_set_nation_full(pplayer,
+                               nation_list_get(candidates, fc_rand(n)));
       }
-    } allowed_nations_iterate_end;
+      nation_list_destroy(candidates);
+    }
     if (pplayer->nation != NO_NATION_SELECTED) {
       announce_player(pplayer);
     } else {
