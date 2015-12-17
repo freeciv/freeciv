f72047e9858b80b0a3333976cbaa99c6e37da446
Marko Lindqvist <cazfi74@gmail.com>
2015-12-17 21:56:09 +0000

    Do not log error about floating point network transfer errors smaller than 1.1 units.
    Reported by Jacob Nevins <jtn>
    
    See bug #22960
    
    [[originally from svn r31058]]

--- common/dataio.c
+++ common/dataio.c
@@ -375,7 +375,7 @@ void dio_put_ufloat(struct data_out *dout, float value, int float_factor)
 {
   uint32_t v = value * float_factor;
 
-  if (fabsf((float) v / float_factor - value) > 1.0 / float_factor) {
+  if (fabsf((float) v / float_factor - value) > 1.1 / float_factor) {
     log_error("Trying to put %f with factor %d in 32 bits; "
               "it will result %f at receiving side, having error of %f units.",
               value, float_factor, (float) v / float_factor,
@@ -393,7 +393,7 @@ void dio_put_sfloat(struct data_out *dout, float value, int float_factor)
 {
   int32_t v = value * float_factor;
 
-  if (fabsf((float) v / float_factor - value) > 1.0 / float_factor) {
+  if (fabsf((float) v / float_factor - value) > 1.1 / float_factor) {
     log_error("Trying to put %f with factor %d in 32 bits; "
               "it will result %f at receiving side, having error of %f units.",
               value, float_factor, (float) v / float_factor,
