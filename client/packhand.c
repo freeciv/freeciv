eba2ce2edabce428a55d857a211611a59ea2a0cc
Marko Lindqvist <cazfi74@gmail.com>
2015-12-23 21:15:08 +0000

    Fix illegal memory read when client has previously accepted page messages but should now ignore them.
    See bug #24220
    
    [[originally from svn r31168]]

--- client/packhand.c
+++ client/packhand.c
@@ -1418,6 +1418,7 @@ void handle_page_msg_part(const char *lines)
       free(page_msg_report.caption);
       free(page_msg_report.headline);
       free(page_msg_report.lines);
+      page_msg_report.lines = NULL;
     }
   }
 }
