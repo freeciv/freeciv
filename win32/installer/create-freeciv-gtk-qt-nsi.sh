1412faf81ba96a107bdc5e2504e2c50bbf3a83f6
Marko Lindqvist <cazfi74@gmail.com>
2016-01-02 17:00:20 +0000

    Show translation percentages on client Windows Installer language lists. Ruledit Installer does not have these percentages.
    Requested by Jacob Nevins <jtn>
    
    See patch #5931
    
    [[originally from svn r31306]]

--- win32/installer/create-freeciv-gtk-qt-nsi.sh
+++ win32/installer/create-freeciv-gtk-qt-nsi.sh
@@ -163,16 +163,16 @@ EOF
 ### additional languages ###
 
 cat <<EOF
-SectionGroup "Additional languages"
+SectionGroup "Additional languages (translation %)"
 
 EOF
 
-cat ../../bootstrap/langnames.txt |
+cat ../../bootstrap/langstat.txt |
 sort -k 2 |
-while read -r code name
+while read -r code prct name
 do
 if test -e $1/share/locale/$code/LC_MESSAGES/freeciv.mo; then
-echo "  Section \"$name ($code)\""
+echo "  Section \"$name ($code) $prct\""
 echo "  SetOutPath \$INSTDIR\\share\\locale\\$code"
 echo "  File /r $1\\share\\locale\\$code\*.*"
 echo "  SetOutPath \$INSTDIR"
@@ -227,12 +227,12 @@ Start Menu shortcut properties."
   \${NSD_CB_AddString} \$DefaultLanguageDropList "US English (en_US)"
 EOF
 
-  cat ../../bootstrap/langnames.txt |
+  cat ../../bootstrap/langstat.txt |
   sort -k 2 |
-  while read -r code name
+  while read -r code prct name
   do
   if test -e $1/share/locale/$code/LC_MESSAGES/freeciv.mo; then
-  echo "  \${NSD_CB_AddString} \$DefaultLanguageDropList \"$name ($code)\""
+  echo "  \${NSD_CB_AddString} \$DefaultLanguageDropList \"$name ($code) $prct\""
   fi
   done
 
@@ -251,10 +251,10 @@ EOF
   echo "    StrCpy \$DefaultLanguageCode \"en_US\""
   echo "  \${EndIf}"
 
-  cat ../../bootstrap/langnames.txt |
-  while read -r code name
+  cat ../../bootstrap/langstat.txt |
+  while read -r code prct name
   do
-    echo "  \${If} \$LangName == \"$name ($code)\""
+    echo "  \${If} \$LangName == \"$name ($code) $prct\""
     echo "    StrCpy \$DefaultLanguageCode \"$code\""
     echo "  \${EndIf}"
   done
