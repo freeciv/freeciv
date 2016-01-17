c3f3006d55c353fe29c23babd01ea83cc0d18176
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-01-17 13:06:20 +0000

    spacerace.h: remove double function prototypes.
    Function prototypes for the packet handling functions are generated
    from packets.def. Use those.
    
    See patch #6817
    
    [[originally from svn r31475]]

--- server/spacerace.h
+++ server/spacerace.h
@@ -13,8 +13,12 @@
 #ifndef FC__SPACERACE_H
 #define FC__SPACERACE_H
 
+/* common */
 #include "fc_types.h"
 #include "packets.h"
+
+/* server */
+#include "hand_gen.h"
 
 struct player_spaceship;
 struct conn_list;
@@ -23,9 +27,5 @@ void spaceship_calc_derived(struct player_spaceship *ship);
 void send_spaceship_info(struct player *src, struct conn_list *dest);
 void spaceship_lost(struct player *pplayer);
 struct player *check_spaceship_arrival(void);
-
-void handle_spaceship_launch(struct player *pplayer);
-void handle_spaceship_place(struct player *pplayer,
-			    enum spaceship_place_type type, int num);
 
 #endif /* FC__SPACERACE_H */
