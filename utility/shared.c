49ce8e36e22d41d9300c1d9c0f0c8590d912c370
Marko Lindqvist <cazfi74@gmail.com>
2016-01-28 01:02:22 +0000

    Added configure check to make sure direct.h is available before trying to include it.
    See bug #24292
    
    [[originally from svn r31645]]

--- utility/shared.c
+++ utility/shared.c
@@ -54,8 +54,10 @@
 #include <windows.h>
 #include <lmcons.h>	/* UNLEN */
 #include <shlobj.h>
+#ifdef HAVE_DIRECT_H
 #include <direct.h>
-#endif
+#endif /* HAVE_DIRECT_H */
+#endif /* WIN32_NATIVE */
 
 /* utility */
 #include "astring.h"
