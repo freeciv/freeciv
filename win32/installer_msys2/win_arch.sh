e249988cb50de7c8a83d2848baca6ef521a7a5f5
Marko Lindqvist <cazfi74@gmail.com>
2016-02-24 06:09:05 +0000

    Do not run config.guess when already noticed that it's missing.
    See patch #6997
    
    [[originally from svn r32051]]

--- win32/installer_msys2/win_arch.sh
+++ win32/installer_msys2/win_arch.sh
@@ -2,10 +2,10 @@
 
 if ! test -f ../../bootstrap/config.guess ; then
     echo "win"
+else
+  case $(../../bootstrap/config.guess 2>/dev/null) in
+      *mingw32*) echo "win32" ;;
+      *mingw64*) echo "win64" ;;
+      *) echo "win" ;;
+  esac
 fi
-
-case $(../../bootstrap/config.guess 2>/dev/null) in
-    *mingw32*) echo "win32" ;;
-    *mingw64*) echo "win64" ;;
-    *) echo "win" ;;
-esac
