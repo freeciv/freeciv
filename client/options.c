5609a0badfccb554457afb21423ba364f69dd57d
Marko Lindqvist <cazfi74@gmail.com>
2016-03-05 12:00:04 +0000

    Set color option to NULL in addition to freeing it when it's being cleared.
    See bug #24351
    
    [[originally from svn r32181]]

--- client/options.c
+++ client/options.c
@@ -1,4 +1,4 @@
-/********************************************************************** 
+/**********************************************************************
  Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -3599,6 +3599,7 @@ static bool client_option_color_set(struct option *poption,
   } else {                                                                  \
     if (NULL == color) {                                                    \
       free((void *) color_tgt);                                             \
+      color_tgt = NULL;                                                     \
       changed = TRUE;                                                       \
     } else if (0 != strcmp(color_tgt, color)) {                             \
       free((void *) color_tgt);                                             \
