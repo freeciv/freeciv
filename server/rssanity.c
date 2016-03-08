bf2f5f8411520c32a1841d9c95945054cb0f4902
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-03-08 14:09:06 +0000

    Drop special text for exact req contradictions.
    A requirement vector self contradiction is a self contradiction no matter if
    it is an exact opposite (present and !present) or not. Simplify so the user
    only sees one message.
    
    See patch #7044
    
    [[originally from svn r32199]]

--- server/rssanity.c
+++ server/rssanity.c
@@ -307,16 +307,6 @@ static bool sanity_check_req_vec(const struct requirement_vector *preqs,
       return FALSE;
     }
     requirement_vector_iterate(preqs, nreq) {
-      if (are_requirements_opposites(preq, nreq)) {
-        log_error("%s: %s ranged %s %s requirement \"%s\" "
-                  "required to be both present and absent.",
-                  list_for,
-                  req_range_name(preq->range),
-                  preq->survives ? "surviving" : "non surviving",
-                  universal_type_rule_name(&preq->source),
-                  universal_rule_name(&preq->source));
-        return FALSE;
-      }
       if (are_requirements_contradictions(preq, nreq)) {
         log_error("%s: Requirements {%s} and {%s} contradict each other.",
                   list_for,
