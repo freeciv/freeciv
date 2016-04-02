4a4546faa3f572c64ad8321b168252e962c12568
Marko Lindqvist <cazfi74@gmail.com>
2016-04-02 08:07:24 +0000

    Fixed erronous deprecation warning about 'prefered' instead of 'preferred' in game.ruleset entry names when neither entry is present.
    See bug #24542
    
    [[originally from svn r32291]]

--- server/ruleset.c
+++ server/ruleset.c
@@ -5331,7 +5331,7 @@ static bool load_ruleset_game(struct section_file *file, bool act,
 
   /* section: tileset */
   pref_text = secfile_lookup_str_default(file, "", "tileset.prefered");
-  if (pref_text != NULL) {
+  if (pref_text[0] != '\0') {
     log_deprecation("Entry tileset.prefered in game.ruleset."
                     " Use correct spelling tileset.preferred instead");
   }
@@ -5346,7 +5346,7 @@ static bool load_ruleset_game(struct section_file *file, bool act,
 
   /* section: soundset */
   pref_text = secfile_lookup_str_default(file, "", "soundset.prefered");
-  if (pref_text != NULL) {
+  if (pref_text[0] != '\0') {
     log_deprecation("Entry soundset.prefered in game.ruleset."
                     " Use correct spelling soundset.preferred instead");
   }
@@ -5361,7 +5361,7 @@ static bool load_ruleset_game(struct section_file *file, bool act,
 
   /* section: musicset */
   pref_text = secfile_lookup_str_default(file, "", "musicset.prefered");
-  if (pref_text != NULL) {
+  if (pref_text[0] != '\0') {
     log_deprecation("Entry musicset.prefered in game.ruleset."
                     " Use correct spelling musicset.preferred instead");
   }
