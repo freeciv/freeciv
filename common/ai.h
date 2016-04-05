76bd4c9845d9582082a77a191447b6091d1c7820
Marko Lindqvist <cazfi74@gmail.com>
2016-04-05 16:08:51 +0000

    Set AI module interface capstring to "+Freeciv-2.6-ai-module"
    See patch #6422
    
    [[originally from svn r32306]]

--- common/ai.h
+++ common/ai.h
@@ -24,7 +24,7 @@ extern "C" {
  * structure below. When changing mandatory capability part, check that
  * there's enough reserved_xx pointers in the end of the structure for
  * taking to use without need to bump mandatory capability again. */
-#define FC_AI_MOD_CAPSTR "+Freeciv-2.6-ai-module-2016.Feb.09"
+#define FC_AI_MOD_CAPSTR "+Freeciv-2.6-ai-module"
 
 /* Timers for all AI activities. Define it to get statistics about the AI. */
 #ifdef FREECIV_DEBUG
