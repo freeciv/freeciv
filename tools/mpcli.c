3e82a088c88ac4e32b4e04d205e3b25536d33609
Marko Lindqvist <cazfi74@gmail.com>
2016-04-18 04:43:21 +0000

    Print empty line between version information and other output in cli modpack installer.
    See patch #7113
    
    [[originally from svn r32434]]

--- tools/mpcli.c
+++ tools/mpcli.c
@@ -1,4 +1,4 @@
-/**********************************************************************
+/***********************************************************************
  Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -139,6 +139,8 @@ int main(int argc, char *argv[])
     } else {
       log_normal("%s%s (%s)", word_version(), VERSION_STRING, rev_ver);
     }
+
+    log_normal("%s", "");
 
     if (fcmp.autoinstall == NULL) {
       download_modpack_list(&fcmp, setup_modpack_list, msg_callback);
