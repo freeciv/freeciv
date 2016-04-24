0447d55b5deeea84833d22800a4ec4845eddf2c9
Marko Lindqvist <cazfi74@gmail.com>
2016-04-24 07:08:30 +0000

    Free individual elements of the ruleset choices vector.
    See bug #24600
    
    [[originally from svn r32466]]

--- server/gamehand.c
+++ server/gamehand.c
@@ -1097,7 +1097,7 @@ static void send_ruleset_choices(struct connection *pc)
 void ruleset_choices_free(void)
 {
   if (ruleset_choices != NULL) {
-    free(ruleset_choices);
+    strvec_destroy(ruleset_choices);
     ruleset_choices = NULL;
   }
 }
