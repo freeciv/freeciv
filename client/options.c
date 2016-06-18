f8e4e7d016af097c2708f6764a690b6087ff9b62
Marko Lindqvist <cazfi74@gmail.com>
2016-06-18 09:35:09 +0000

    Try to load freeciv-2.7 generated client options file if no freeciv-3.0 generated one exist. It's not yet certain that there will ever be freeciv-2.7, but this change' prepares TRUNK for the possibility.
    See patch #7268
    
    [[originally from svn r32899]]

--- client/options.c
+++ client/options.c
@@ -1,4 +1,4 @@
-/**********************************************************************
+/***********************************************************************
  Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -5140,7 +5140,7 @@ static const char *get_last_option_file_name(bool *allow_digital_boolean)
   static int last_minors[] = {
     0,  /* There was no 0.x releases */
     14, /* 1.14 */
-    6   /* 2.6 */
+    7   /* 2.7 */
   };
 
 #if MINOR_VERSION >= 90
