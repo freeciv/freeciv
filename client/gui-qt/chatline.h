cb22c16eec9f882475e4c34a9ef661f2fe0b308f
mir3x <mlewczuk80@gmail.com>
2016-07-11 05:13:22 +0000

    Use toHtmlEscaped() to convert html tags.
    Reported by Jacob Nevins <jtn>
    
    See bug #24829
    
    [[originally from svn r33213]]

--- client/gui-qt/chatline.h
+++ client/gui-qt/chatline.h
@@ -31,7 +31,6 @@ class QPushButton;
 
 QString apply_tags(QString str, const struct text_tag_list *tags,
                    bool colors_change);
-int increase_tags(QString str, int i);
 /***************************************************************************
   Class for chat widget
 ***************************************************************************/
