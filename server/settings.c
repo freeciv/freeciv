60a89cebd373d104d9f88bd7921cd910aa32dfea
Marko Lindqvist <cazfi74@gmail.com>
2016-08-03 12:09:53 +0000

    Use INIT_BRACE_BEGIN and INIT_BRACE_END in settings.c as needed.
    See patch #7519
    
    [[originally from svn r33442]]

--- server/settings.c
+++ server/settings.c
@@ -1216,8 +1216,9 @@ static bool plrcol_validate(int value, struct connection *caller,
                  _default)                                                  \
   {name, sclass, to_client, short_help, extra_help, NULL, SSET_BOOL,        \
       scateg, slevel,                                                       \
+      INIT_BRACE_BEGIN                                                      \
       .boolean = {&value, _default, func_validate, bool_name,               \
-                   FALSE}, func_action, FALSE},
+                  FALSE} INIT_BRACE_END , func_action, FALSE},
 
 #define GEN_INT(name, value, sclass, scateg, slevel, to_client,         \
                 short_help, extra_help, func_help,                      \
@@ -1225,8 +1226,9 @@ static bool plrcol_validate(int value, struct connection *caller,
                 _min, _max, _default)                                   \
   {name, sclass, to_client, short_help, extra_help, func_help, SSET_INT, \
       scateg, slevel,                                                   \
+      INIT_BRACE_BEGIN                                                  \
       .integer = {(int *) &value, _default, _min, _max, func_validate,  \
-                   0},                                                  \
+                  0} INIT_BRACE_END,                                    \
       func_action, FALSE},
 
 #define GEN_STRING(name, value, sclass, scateg, slevel, to_client,      \
@@ -1234,7 +1236,9 @@ static bool plrcol_validate(int value, struct connection *caller,
                    _default)                                            \
   {name, sclass, to_client, short_help, extra_help, NULL, SSET_STRING,  \
       scateg, slevel,                                                   \
-      .string = {value, _default, sizeof(value), func_validate, ""},    \
+      INIT_BRACE_BEGIN                                                  \
+      .string = {value, _default, sizeof(value), func_validate, ""}     \
+      INIT_BRACE_END,                                                   \
       func_action, FALSE},
 
 #define GEN_ENUM(name, value, sclass, scateg, slevel, to_client,            \
@@ -1242,17 +1246,20 @@ static bool plrcol_validate(int value, struct connection *caller,
                  func_action, func_name, _default)                          \
   { name, sclass, to_client, short_help, extra_help, func_help, SSET_ENUM,  \
       scateg, slevel,                                                       \
+      INIT_BRACE_BEGIN                                                      \
        .enumerator = {  &value, sizeof(value), _default,                    \
                         func_validate,                                      \
-       (val_name_func_t) func_name, 0 }, func_action, FALSE},
+       (val_name_func_t) func_name, 0 } INIT_BRACE_END,                     \
+      func_action, FALSE},
 
 #define GEN_BITWISE(name, value, sclass, scateg, slevel, to_client,         \
                    short_help, extra_help, func_validate, func_action,      \
                    func_name, _default)                                     \
   { name, sclass, to_client, short_help, extra_help, NULL, SSET_BITWISE,    \
     scateg, slevel,                                                         \
+      INIT_BRACE_BEGIN                                                      \
       .bitwise = { (unsigned *) (void *) &value, _default, func_validate,   \
-       func_name, 0 }, func_action, FALSE},
+       func_name, 0 } INIT_BRACE_END, func_action, FALSE},
 
 /* game settings */
 static struct setting settings[] = {
