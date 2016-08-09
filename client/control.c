cb6ba8ea31061ee9bbb1b8f577fe0a2171a38b15
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-08-09 08:57:46 +0000

    Comment fix: update menu entry name.
    [[originally from svn r33522]]

--- client/control.c
+++ client/control.c
@@ -2911,7 +2911,7 @@ void key_unit_connect(enum unit_activity activity,
 }
 
 /**************************************************************************
-  Handle user 'diplomatic actions' input
+  Handle user 'Do...' input
 **************************************************************************/
 void key_unit_action_select(void)
 {
