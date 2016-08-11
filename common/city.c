edb26149405df52535983ea35aa5c65b05edee3e
Marko Lindqvist <cazfi74@gmail.com>
2016-08-11 02:51:59 +0000

    Fixed compiler warnings about comparisons of unsigned type 'citizen' against negative values.
    See bug #24948
    
    [[originally from svn r33561]]

--- common/city.c
+++ common/city.c
@@ -2336,7 +2336,6 @@ static void citizen_base_mood(struct city *pcity)
    * specialists or angry are unhappy. This is changed by luxuries and 
    * buildings later. */
   *unhappy = (size - spes - *content - *angry);
-  fc_assert_action(*unhappy >= 0, *unhappy = 0);
 
   /* No one is born happy. */
   *happy = 0;
