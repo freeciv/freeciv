d98d3d226fb3cce1cbf4ee1df0a8d687d267279d
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-09-26 10:14:03 +0000

    Comment spelling fix: probabilty -> probability
    See bug #25113
    
    [[originally from svn r33905]]

--- common/actions.c
+++ common/actions.c
@@ -359,7 +359,7 @@ const char *action_prepare_ui_name(int action_id, const char* mnemonic,
 
   /* Format the info part of the action's UI name. */
   if (probtxt != NULL && custom != NULL) {
-    /* TRANS: action UI name's info part with custom info and probabilty.
+    /* TRANS: action UI name's info part with custom info and probability.
      * Hint: you can move the paren handling from this sting to the action
      * names if you need to add extra information (like a mnemonic letter
      * that doesn't appear in the action UI name) to it. In that case you
@@ -369,7 +369,7 @@ const char *action_prepare_ui_name(int action_id, const char* mnemonic,
      * surrounding parens. */
     astr_set(&chance, _(" (%s; %s)"), custom, probtxt);
   } else if (probtxt != NULL) {
-    /* TRANS: action UI name's info part with probabilty.
+    /* TRANS: action UI name's info part with probability.
      * Hint: you can move the paren handling from this sting to the action
      * names if you need to add extra information (like a mnemonic letter
      * that doesn't appear in the action UI name) to it. In that case you
