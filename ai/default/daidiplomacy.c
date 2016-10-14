3982e54f8d340abca072d81d435771cff1e120ac
Marko Lindqvist <cazfi74@gmail.com>
2016-10-14 04:53:26 +0000

    Randomize AI love on first contact.
    Reported by Jacob Nevins <jtn>
    
    See bug #25045
    
    [[originally from svn r34119]]

--- ai/default/daidiplomacy.c
+++ ai/default/daidiplomacy.c
@@ -923,6 +923,9 @@ void dai_diplomacy_first_contact(struct ai_type *ait, struct player *pplayer,
 {
   bool wants_ceasefire = FALSE;
 
+  /* Randomize initial love */
+  pplayer->ai_common.love[player_index(aplayer)] += 2 - fc_rand(5);
+
   if (pplayer->ai_controlled
       && player_diplstate_get(pplayer, aplayer)->type == DS_WAR
       && could_meet_with_player(pplayer, aplayer)) {
