353f9100217bada0d8d6cc9fbf31c9b4bcc0be8c
Marko Lindqvist <cazfi74@gmail.com>
2016-10-20 13:52:18 +0000

    Adjusted msys2 based Windows Installer makefile to recent gtk3.22-client binary name change.
    See patch #7851
    
    [[originally from svn r34195]]

--- windows/installer_msys2/create-freeciv-gtk3x-nsi.sh
+++ windows/installer_msys2/create-freeciv-gtk3x-nsi.sh
@@ -2,4 +2,4 @@
 
 # ./create-freeciv-gtk3x-nsi.sh <Freeciv files directory> <version>
 
-./create-freeciv-gtk-qt-nsi.sh $1 $2 "gtk3x" "GTK+3x" $3
+./create-freeciv-gtk-qt-nsi.sh $1 $2 "gtk3.22" "GTK+3x" $3
