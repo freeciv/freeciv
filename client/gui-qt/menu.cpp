28f10c70cf9e6d0141a005f0a838aa0ce8e841d2
mir3x <mlewczuk80@gmail.com>
2016-11-17 09:03:54 +0000

    Qt client - added 5 actions to unit label
    See patch #7988
    
    [[originally from svn r34534]]

--- client/gui-qt/menu.cpp
+++ client/gui-qt/menu.cpp
@@ -1072,7 +1072,8 @@ void mr_menu::setup_menus()
   act->setShortcut(QKeySequence(tr("shift+b")));
   connect(act, SIGNAL(triggered()), this, SLOT(slot_go_build_city()));
   act = menu->addAction(_("Auto Settler"));
-  act->setShortcut(QKeySequence(tr("a")));
+  act->setShortcut(QKeySequence(shortcut_to_string(
+                   fc_shortcuts::sc()->get_shortcut(SC_AUTOMATE))));
   menu_list.insertMulti(AUTOSETTLER, act);
   connect(act, SIGNAL(triggered()), this, SLOT(slot_auto_settler()));
   menu->addSeparator();
@@ -1112,7 +1113,8 @@ void mr_menu::setup_menus()
   connect(act, SIGNAL(triggered()), this, SLOT(slot_transform()));
   act = menu->addAction(_("Clean Pollution"));
   menu_list.insertMulti(POLLUTION, act);
-  act->setShortcut(QKeySequence(tr("p")));
+  act->setShortcut(QKeySequence(shortcut_to_string(
+                   fc_shortcuts::sc()->get_shortcut(SC_PARADROP))));
   connect(act, SIGNAL(triggered()), this, SLOT(slot_clean_pollution()));
   act = menu->addAction(_("Clean Nuclear Fallout"));
   menu_list.insertMulti(FALLOUT, act);
@@ -1444,8 +1446,9 @@ void mr_menu::execute_shortcut(int sid)
   menu_list = findChildren<QMenu*>();
     foreach (m, menu_list) {
         foreach (a, m->actions()) {
-          if (a->shortcut() == seq) {
+          if (a->shortcut() == seq && a->isEnabled()) {
             a->activate(QAction::Trigger);
+            return;
           }
         }
     }
