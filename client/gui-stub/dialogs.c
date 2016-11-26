e7415ac648b29a829708e679d58833649cf8ea48
Marko Lindqvist <cazfi74@gmail.com>
2016-11-26 12:54:46 +0000

    Corrected the way gui-stub popup_combat_info() gets called through interface.
    See bug #25328
    
    [[originally from svn r34633]]

--- client/gui-stub/dialogs.c
+++ client/gui-stub/dialogs.c
@@ -307,8 +307,8 @@ bool gui_request_transport(struct unit *pcargo, struct tile *ptile)
   Popup detailed information about battle or save information for
   some kind of statistics
 ***************************************************************************/
-void popup_combat_info(int attacker_unit_id, int defender_unit_id,
-                       int attacker_hp, int defender_hp,
-                       bool make_winner_veteran)
+void gui_popup_combat_info(int attacker_unit_id, int defender_unit_id,
+                           int attacker_hp, int defender_hp,
+                           bool make_winner_veteran)
 {
 }
