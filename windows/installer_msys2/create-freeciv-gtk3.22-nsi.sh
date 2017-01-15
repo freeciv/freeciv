2e836e0920a3f38fcbba8f3225ee0cd39f9d76d8
Marko Lindqvist <cazfi74@gmail.com>
2017-01-15 10:07:18 +0000

    Gtk3.22-client Windows installer uses correct name of the client.
    See bug #25441
    
    [[originally from svn r34832]]

--- windows/installer_msys2/create-freeciv-gtk3.22-nsi.sh
+++ windows/installer_msys2/create-freeciv-gtk3.22-nsi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# ./create-freeciv-gtk3x-nsi.sh <Freeciv files directory> <version>
+# ./create-freeciv-gtk3.22-nsi.sh <Freeciv files directory> <version>
 
-./create-freeciv-gtk-qt-nsi.sh $1 $2 "gtk3.22" "GTK+3x" $3
+./create-freeciv-gtk-qt-nsi.sh $1 $2 "gtk3.22" "GTK+3.22" $3
