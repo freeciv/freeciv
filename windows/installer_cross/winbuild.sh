773789b476c56450062101429e75512fced19e3b
Marko Lindqvist <cazfi74@gmail.com>
2017-01-29 20:55:36 +0000

    Added separate 'ruledit' gui type to winbuild.sh
    See patch #8090
    
    [[originally from svn r34908]]

--- windows/installer_cross/winbuild.sh
+++ windows/installer_cross/winbuild.sh
@@ -7,9 +7,9 @@
 # This script is licensed under Gnu General Public License version 2 or later.
 # See COPYING available from the same location you got this script.
 
-# Version 2.2 (24-Jan-17)
+# Version 2.3 (26-Jan-17)
 
-WINBUILD_VERSION="2.2"
+WINBUILD_VERSION="2.3"
 MIN_WINVER=0x0600 # Vista
 CROSSER_FEATURE_LEVEL=1.2
 
@@ -50,8 +50,20 @@ fi
 
 SETUP=$(grep "Setup=" $DLLSPATH/crosser.txt | sed -e 's/Setup="//' -e 's/"//')
 
-if test "x$2" != "x" ; then
+if test "x$2" = "xruledit" ; then
   SINGLE_GUI=true
+  RULEDIT="yes"
+  CLIENTS="no"
+  FCMP="no"
+  SERVER="no"
+elif test "x$2" != "x" ; then
+  SINGLE_GUI=true
+  SERVER="yes"
+  if test "x$2" = "xqt" ; then
+    RULEDIT="yes"
+  else
+    RULEDIT="no"
+  fi
   CLIENTS="$2"
   case $2 in
     gtk3) FCMP="gtk3" ;;
@@ -61,6 +73,9 @@ if test "x$2" != "x" ; then
     *) echo "Unknown gui \"$2\"!" >&2
        exit 1 ;;
   esac
+else
+  SERVER="yes"
+  RULEDIT="yes"
 fi
 
 if ! mkdir -p build-$SETUP ; then
@@ -111,7 +126,7 @@ INSTALL_DIR="$(pwd)/freeciv-${VERREV}"
 if ! (
 cd build-$SETUP
 
-if ! ../../../configure CPPFLAGS="-I${DLLSPATH}/include -D_WIN32_WINNT=${MIN_WINVER}" CFLAGS="-Wno-error" PKG_CONFIG_LIBDIR="${DLLSPATH}/lib/pkgconfig" --enable-sys-tolua-cmd --with-magickwand="${DLLSPATH}/bin" --prefix="/" --enable-client=$CLIENTS --enable-fcmp=$FCMP --enable-svnrev --enable-debug --host=$TARGET --build=$(../../../bootstrap/config.guess) --with-libiconv-prefix=${DLLSPATH} --with-sqlite3-prefix=${DLLSPATH} --with-followtag="crosser" --enable-crosser --enable-ai-static=classic --disable-freeciv-manual --enable-sdl-mixer=sdl2 --with-qt5-includes=${DLLSPATH}/include --with-qt5-libs=${DLLSPATH}/lib --with-tinycthread
+if ! ../../../configure CPPFLAGS="-I${DLLSPATH}/include -D_WIN32_WINNT=${MIN_WINVER}" CFLAGS="-Wno-error" PKG_CONFIG_LIBDIR="${DLLSPATH}/lib/pkgconfig" --enable-sys-tolua-cmd --with-magickwand="${DLLSPATH}/bin" --prefix="/" --enable-client=$CLIENTS --enable-fcmp=$FCMP --enable-svnrev --enable-debug --host=$TARGET --build=$(../../../bootstrap/config.guess) --with-libiconv-prefix=${DLLSPATH} --with-sqlite3-prefix=${DLLSPATH} --with-followtag="crosser" --enable-crosser --enable-ai-static=classic --disable-freeciv-manual --enable-sdl-mixer=sdl2 --with-qt5-includes=${DLLSPATH}/include --with-qt5-libs=${DLLSPATH}/lib --with-tinycthread --enable-server=$SERVER --enable-ruledit=$RULEDIT
 then
   echo "Configure failed" >&2
   exit 1
