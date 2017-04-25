b3396db33b348b39344298c25abe7c4a72dc67e9
Marko Lindqvist <cazfi74@gmail.com>
2017-04-25 16:40:28 +0000

    Support VERSION_REVTYPE git builds on cleanup_checkout.sh
    See hrm Feature #654688
    
    [[originally from svn r35292]]

--- win32/installer/cleanup_checkout.sh
+++ win32/installer/cleanup_checkout.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-if test "x$VERSION_REVTYPE" = "xsvn" ; then
+if test "x$VERSION_REVTYPE" = "xgit" ; then
+    git checkout $1/translations
+elif test "x$VERSION_REVTYPE" = "xsvn" ; then
     cd $1/translations
     svn revert -R .
 fi
