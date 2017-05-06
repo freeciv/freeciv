4a78fae983820ab90b783f189f14dc5f55cafe88
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2017-05-06 21:02:15 +0000

    action_has_result() doesn't modify its action.
    Declare action_has_result()'s paction argument const.
    
    See Feature #657600
    
    [[originally from svn r35429]]

--- common/actions.h
+++ common/actions.h
@@ -353,7 +353,8 @@ enum action_target_kind action_get_target_kind(
 
 int action_number(const struct action *action);
 
-bool action_has_result(struct action *paction, enum gen_action result);
+bool action_has_result(const struct action *paction,
+                       enum gen_action result);
 #define action_id_has_result(action_id, result)                            \
   action_has_result(action_by_number(action_id), result)
 
