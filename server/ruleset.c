de44b695a673a779e74be0a7f910ff6c55cc12ca
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2017-05-12 12:57:21 +0000

    Change "Establish Embassy Stay"'s UI name.
    Change the default UI name of the action "Establish Embassy Stay" from
    "Establish embassy (and stay)" to "Become Ambassador".
    
    See hrm Feature #659189
    
    [[originally from svn r35523]]

--- server/ruleset.c
+++ server/ruleset.c
@@ -6052,8 +6052,8 @@ static bool load_ruleset_game(struct section_file *file, bool act,
                  text);
 
       text = secfile_lookup_str_default(file,
-          /* TRANS: Establish _Embassy (and stay) (100% chance of success). */
-          N_("Establish %sEmbassy (and stay)%s"),
+          /* TRANS: Becom_e Ambassador (100% chance of success). */
+          N_("Becom%se Ambassador%s"),
           "actions.ui_name_establish_embassy_stay");
       sz_strlcpy(action_by_number(ACTION_ESTABLISH_EMBASSY_STAY)->ui_name,
                  text);
