ae902e1e4165eea78b4ba35493cbdb03484a0e0d
Marko Lindqvist <cazfi74@gmail.com>
2017-05-13 18:32:05 +0000

    Increase AI desire to conquer cities
    See hrm Feature #659537
    
    [[originally from svn r35557]]

--- ai/default/daimilitary.c
+++ ai/default/daimilitary.c
@@ -1235,7 +1235,7 @@ static struct adv_choice *kill_something_with(struct ai_type *ait, struct player
     }
     if (unit_can_take_over(myunit) || acity_data->invasion.occupy > 0) {
       /* bonus for getting the city */
-      benefit += acity_data->worth / 5;
+      benefit += acity_data->worth / 3;
     }
 
     /* end dealing with cities */
