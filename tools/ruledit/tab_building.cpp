401b65383183d9bc3f13aa85820c066d9de07ba6
Marko Lindqvist <cazfi74@gmail.com>
2017-05-19 23:35:39 +0000

    Prevent opening multiple req_edit dialogs for the same requirement vector
    See hrm Feature #660194
    
    [[originally from svn r35669]]

--- tools/ruledit/tab_building.cpp
+++ tools/ruledit/tab_building.cpp
@@ -33,7 +33,6 @@
 #include "improvement.h"
 
 // ruledit
-#include "req_edit.h"
 #include "ruledit.h"
 #include "ruledit_qt.h"
 #include "validity.h"
@@ -271,9 +270,7 @@ void tab_building::same_name_toggle(bool checked)
 void tab_building::edit_reqs()
 {
   if (selected != nullptr) {
-    req_edit *redit = new req_edit(ui, QString::fromUtf8(improvement_rule_name(selected)),
-                                   &selected->reqs);
-
-    redit->show();
+    ui->open_req_edit(QString::fromUtf8(improvement_rule_name(selected)),
+                      &selected->reqs);
   }
 }
