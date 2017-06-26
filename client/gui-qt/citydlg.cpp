2fd079d75db393f9bdc5ec4d4249573d9fb134d0
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2017-06-26 09:10:02 +0200

    Comment spell fix: untis -> units.

--- client/gui-qt/citydlg.cpp
+++ client/gui-qt/citydlg.cpp
@@ -787,7 +787,7 @@ void unit_item::unload_unit()
 }
 
 /****************************************************************************
-  Unloads all untis from transporter
+  Unloads all units from transporter
 ****************************************************************************/
 void unit_item::unload_all()
 {
