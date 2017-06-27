4b357b1143e35a78b3494b43047ab62fdde61dcc
Marko Lindqvist <cazfi74@gmail.com>
2017-06-27 18:32:47 +0300

    Cache is_server() in is_my_zoc()
    See hrm Feature #675731
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/unit.c
+++ common/unit.c
@@ -1551,6 +1551,7 @@ struct unit *unit_occupies_tile(const struct tile *ptile,
 bool is_my_zoc(const struct player *pplayer, const struct tile *ptile0)
 {
   struct terrain *pterrain;
+  bool srv = is_server();
 
   square_iterate(ptile0, 1, ptile) {
     pterrain = tile_terrain(ptile);
@@ -1568,7 +1569,7 @@ bool is_my_zoc(const struct player *pplayer, const struct tile *ptile0)
       }
     } unit_list_iterate_end;
 
-    if (!is_server()) {
+    if (!srv) {
       struct city *pcity = is_non_allied_city_tile(ptile, pplayer);
 
       if (pcity 
