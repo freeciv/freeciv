3cc69b3dac64a047e981e27771fa5164e6b3d410
Andreas Røsdal <andreas.rosdal@gmail.com>
2017-08-04 18:43:10 +0200

    Add initial .travis.yml for Travis-CI testing.

---
+++ scripts/travis-build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Freeciv Travis CI Bootstrap Script 
+#
+# https://travis-ci.org/freeciv/freeciv
+echo "Building Freeciv on Travis CI."
+basedir=$(pwd)
+logfile="${basedir}/freeciv-travis.log"
+
+
+# Redirect copy of output to a log file.
+exec > >(tee ${logfile})
+exec 2>&1
+set -e
+
+uname -a
+echo basedir  $basedir
+echo logfile $logfile
+
+# Based on fresh install of Ubuntu 14.04
+dependencies="libcurl4-openssl-dev libtool automake autoconf autotools-dev language-pack-en python3.4 python3.4-dev liblzma-dev libicu-dev libsdl1.2-dev libsqlite3-dev"
+
+## Dependencies
+echo "==== Installing Updates and Dependencies ===="
+echo "apt-get update"
+apt-get -y update
+echo "apt-get install dependencies"
+apt-get -y install ${dependencies}
+
+cd freeciv && sudo -u travis make install
+
+./autogen.sh CFLAGS="-O3" --disable-nls --disable-fcmp --enable-freeciv-manual=html --enable-ai-static=classic,threaded --prefix=${HOME}/freeciv/ && make -s -j$(nproc)
+
+sudo -u travis make install
+
+#echo "=============================="
+echo "Freeciv built, tested and started correctly: Build successful!"
