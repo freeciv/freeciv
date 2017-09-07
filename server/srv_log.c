5c82585f17929d6f2d88966469e709b17f45c4ec
Marko Lindqvist <cazfi74@gmail.com>
2017-09-08 01:03:09 +0300

    Avoid featured text control sequences in CITY_LOG() and UNIT_LOG()
    See hrm Bug #661108
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/srv_log.c
+++ server/srv_log.c
@@ -60,7 +60,7 @@ void real_city_log(const char *file, const char *function, int line,
 
   CALL_PLR_AI_FUNC(log_fragment_city, city_owner(pcity), aibuf, sizeof(aibuf), pcity);
 
-  fc_snprintf(buffer, sizeof(buffer), "%s %s(%d,%d) [s%d] {%s} ",
+  fc_snprintf(buffer, sizeof(buffer), "%s %s(%d,%d) (s%d) {%s} ",
               nation_rule_name(nation_of_city(pcity)),
               city_name_get(pcity),
               TILE_XY(pcity->tile), city_size_get(pcity),
@@ -102,13 +102,13 @@ void real_unit_log(const char *file, const char *function, int line,
   }
 
   fc_snprintf(buffer, sizeof(buffer),
-	      "%s %s[%d] %s (%d,%d)->(%d,%d){%s} ",
+              "%s %s(%d) %s (%d,%d)->(%d,%d){%s} ",
               nation_rule_name(nation_of_unit(punit)),
               unit_rule_name(punit),
               punit->id,
-	      get_activity_text(punit->activity),
-	      TILE_XY(unit_tile(punit)),
-	      gx, gy, aibuf);
+              get_activity_text(punit->activity),
+              TILE_XY(unit_tile(punit)),
+              gx, gy, aibuf);
 
   va_start(ap, msg);
   fc_vsnprintf(buffer2, sizeof(buffer2), msg, ap);
