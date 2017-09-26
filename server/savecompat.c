c660c2605ab312ac63bec1965aa574ec638957ef
Marko Lindqvist <cazfi74@gmail.com>
2017-09-26 04:13:23 +0300

    savecompat: Fix converting of separate victory settings to victories array
    See hrm Bug #698566
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/savecompat.c
+++ server/savecompat.c
@@ -691,7 +691,10 @@ static void compat_load_020600(struct loaddata *loading,
           if (secfile_lookup_bool(loading->file, &value,
                                   "settings.set%d.value", i)) {
             if (value) {
-              sz_strlcat(value_buffer, "|SPACERACE");
+              if (value_buffer[0] != '\0') {
+                sz_strlcat(value_buffer, "|");
+              }
+              sz_strlcat(value_buffer, "SPACERACE");
             }
           } else {
             log_sg("Setting '%s': %s", name, secfile_error());
@@ -699,7 +702,10 @@ static void compat_load_020600(struct loaddata *loading,
           if (secfile_lookup_bool(loading->file, &value,
                                   "settings.set%d.gamestart", i)) {
             if (value) {
-              sz_strlcat(gamestart_buffer, "|SPACERACE");
+              if (gamestart_buffer[0] != '\0') {
+                sz_strlcat(gamestart_buffer, "|");
+              }
+              sz_strlcat(gamestart_buffer, "SPACERACE");
             }
           } else {
             log_sg("Setting '%s': %s", name, secfile_error());
@@ -715,7 +721,10 @@ static void compat_load_020600(struct loaddata *loading,
           if (secfile_lookup_bool(loading->file, &value,
                                   "settings.set%d.value", i)) {
             if (value) {
-              sz_strlcat(value_buffer, "|ALLIED");
+              if (value_buffer[0] != '\0') {
+                sz_strlcat(value_buffer, "|");
+              }
+              sz_strlcat(value_buffer, "ALLIED");
             }
           } else {
             log_sg("Setting '%s': %s", name, secfile_error());
@@ -723,7 +732,10 @@ static void compat_load_020600(struct loaddata *loading,
           if (secfile_lookup_bool(loading->file, &value,
                                   "settings.set%d.gamestart", i)) {
             if (value) {
-              sz_strlcat(gamestart_buffer, "|ALLIED");
+              if (gamestart_buffer[0] != '\0') {
+                sz_strlcat(gamestart_buffer, "|");
+              }
+              sz_strlcat(gamestart_buffer, "ALLIED");
             }
           } else {
             log_sg("Setting '%s': %s", name, secfile_error());
