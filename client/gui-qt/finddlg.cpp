85cf68b6b5687f838c02052414d166e782311b49
Marko Lindqvist <cazfi74@gmail.com>
2018-02-10 13:24:39 +0200

    client/gui-qt/f*: Mark function headers as doxygen function descriptions
    See hrm Feature #699770
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-qt/finddlg.cpp
+++ client/gui-qt/finddlg.cpp
@@ -1,4 +1,4 @@
-/********************************************************************** 
+/***********************************************************************
  Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -18,8 +18,9 @@
 // gui-qt
 #include "finddlg.h"
 
-/**************************************************************************
+/**********************************************************************//**
   Popup a dialog to ask for a city to find.
+  Not yet implemented.
 **************************************************************************/
 void popup_find_dialog(void)
 {
