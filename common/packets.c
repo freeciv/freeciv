94fcef4773b2edd4d30ee0cdb05e2f90bc9d7406
Marko Lindqvist <cazfi74@gmail.com>
2018-04-29 15:50:22 +0300

    Refactor network stream uncompressing
    Retry uncompressing with bigger buffer if it first fails
    due to to small buffer. Made initial buffer size smaller than
    old one that was meant to be big enough for all cases without
    retrying.
    
    Patch by simsulla, with minor changes by me
    
    See hrm #747319
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/packets.c
+++ common/packets.c
@@ -88,6 +88,8 @@ static int stat_size_uncompressed = 0;
 static int stat_size_compressed = 0;
 static int stat_size_no_compression = 0;
 
+#define MAX_DECOMPRESSION 400
+
 /****************************************************************************
   Returns the compression level. Initilialize it if needed.
 ****************************************************************************/
@@ -433,26 +435,34 @@ void *get_packet_from_connection(struct connection *pc,
 
   if (compressed_packet) {
     uLong compressed_size = whole_packet_len - header_size;
-    /* 
-     * We don't know the decompressed size. We assume a bad case
-     * here: an expansion by an factor of 150.
-     * We've had a case where factor of 100 was not big enough.
-     */
-    unsigned long int decompressed_size = 150 * compressed_size;
-    void *decompressed = fc_malloc(decompressed_size);
-    int error;
+    int decompress_factor = 80;
+    unsigned long int decompressed_size = decompress_factor * compressed_size;
+    int error = Z_DATA_ERROR;
     struct socket_packet_buffer *buffer = pc->buffer;
-    
-    error =
-	uncompress(decompressed, &decompressed_size,
-		   ADD_TO_POINTER(buffer->data, header_size), 
-		   compressed_size);
-    if (error != Z_OK) {
-      log_verbose("Uncompressing of the packet stream failed. "
-                  "The connection will be closed now.");
-      connection_close(pc, _("decoding error"));
-      return NULL;
-    }
+    void *decompressed = fc_malloc(decompressed_size);
+
+    do {
+      error =
+        uncompress(decompressed, &decompressed_size,
+                   ADD_TO_POINTER(buffer->data, header_size),
+                   compressed_size);
+
+      if (error == Z_DATA_ERROR) {
+        decompress_factor += 50;
+        decompressed_size = decompress_factor * compressed_size;
+        decompressed = fc_realloc(decompressed, decompressed_size);
+      }
+
+      if (error != Z_OK) {
+        if (error != Z_DATA_ERROR || decompress_factor > MAX_DECOMPRESSION ) {
+          log_verbose("Uncompressing of the packet stream failed. "
+                      "The connection will be closed now.");
+          connection_close(pc, _("decoding error"));
+          return NULL;
+        }
+      }
+
+    } while (error != Z_OK);
 
     buffer->ndata -= whole_packet_len;
     /* 
