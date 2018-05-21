8c68efb07c789de1d8438e941590f8942bfd6a29
simsulla <simplicissimus.sulla@gmail.com>
2018-05-21 20:33:20 +0200

    marked for translation using _() macro, see hrm #735948

--- utility/log.c
+++ utility/log.c
@@ -122,8 +122,8 @@ bool log_parse_level_str(const char *level_str, enum log_level *ret_level)
         return FALSE;
       }
     } else {
-        log_deprecation("Do not provide log level with a numerical value."
-            " Use one of the levels Fatal, Error, Warning, Normal, Verbose, Debug");
+        log_deprecation( _("Do not provide log level with a numerical value."
+            " Use one of the levels Fatal, Error, Warning, Normal, Verbose, Debug") );
     }
     if (level >= LOG_FATAL && level <= max_level) {
       if (NULL != ret_level) {
