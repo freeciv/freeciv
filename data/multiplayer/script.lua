4928f03d87af4bbe0234eb883982bd2b2c55bcf2
Zeko <zeko@freecivweb.org>
2018-08-07 07:57:52 -0700

    Make philosophy and Darwin global, add traderoutes, set new traderoute formulas as default, and change corruption rates to their 2.0 values.

--- data/multiplayer/script.lua
+++ data/multiplayer/script.lua
@@ -32,14 +32,8 @@ function building_built_handler(btype, city)
   local player, id = city.owner, btype.id
   local gained
 
-  if id == darw_id then
-    -- Block the player from destroying the wonder, rebuilding it and
-    -- getting free advance again.
-    -- This also prevents those they share research with from getting
-    -- free advance from building Darwin`s Voyage themselves.
-    if player:give_tech(find.tech_type("Theory of Evolution"),
-                        0, false, "researched") then
-      -- Give the player free advance.
+  if id == darw_id and not darwin_built then
+      darwin_built = true
       gained = player:give_tech(nil, 0, false, "researched")
 
       -- Notify the player. Include the tech name in a way that makes it
@@ -63,32 +57,13 @@ function building_built_handler(btype, city)
                                 player:research_name_translation(),
                                 gained:name_translation(),
                                 darw_btype:name_translation())
-    end
   end
 end
 
 signal.connect("building_built", "building_built_handler")
 
 
--- Hack: record which players already got Philosophy, to avoid
--- teams getting it multiple times with team_pooled_research.
--- Stored as a string as this is a type simple enough to be included
--- in savefiles.
--- (It`s probably not necessary to test for existence as savefile
--- data is loaded after this script is executed.)
-if philo_players == nil then
-  philo_players = ""
-end
-
--- Record that a player got Philosophy in our hacky string.
-function record_philo(player)
-  local pos = player.id + 1
-  philo_players = string.sub(philo_players, 1, pos-1) ..
-                  string.rep(" ", math.max(0, pos - 1 - #philo_players)) ..
-                  "." .. string.sub(philo_players, pos+1)
-end
-
--- Grant one tech when the tech Philosophy is researched.
+-- Grant one tech the first time Philosophy is researched.
 function tech_researched_handler(tech, player, how)
   local id
   local gained
@@ -99,43 +74,17 @@ function tech_researched_handler(tech, player, how)
   end
 
   id = tech.id
-  if id == find.tech_type("Philosophy").id and how == "researched" then
+  if id == find.tech_type("Philosophy").id and how == "researched" and not philo_researched then
 
-    -- Check potential teammates.
-    for p in players_iterate() do
-      if player:shares_research(p)
-         and string.sub(philo_players, p.id+1, p.id+1) == "." then
-        -- Another player in the same team already got Philosophy.
-        record_philo(player)
-        return
-      end
-    end
-
-    record_philo(player)
+    philo_researched = true
 
     -- Give the player a free advance.
     -- This will give a free advance for each player that shares research.
     gained = player:give_tech(nil, -1, false, "researched")
 
-      -- Notify the player. Include the tech names in a way that makes it
-      -- look natural no matter if each tech is announced or not.
-    notify.event(player, NIL, E.TECH_GAIN,
-                 _("Great philosophers from all the world join your civilization: you get the immediate advance %s."),
-                 gained:name_translation())
-
-    -- Notify research partners
-    notify.research(player, false, E.TECH_GAIN,
-                    _("Great philosophers from all the world join the %s: you get the immediate advance %s."),
-                    player.nation:plural_translation(),
-                    gained:name_translation())
-
-    -- default.lua informs the embassies when the tech source is a hut.
-    -- They should therefore be informed about the source here too.
-    notify.research_embassies(player, E.TECH_EMBASSY,
-            -- /* TRANS: first %s is leader or team name */
-            _("Great philosophers from all the world join %s: they get %s as an immediate advance."),
-            player:research_name_translation(),
-            gained:name_translation())
+    -- Notify all.
+    notify.all(_("Great philosophers from all the world join the %s: you get an immediate."),
+               player.nation:plural_translation())
   end
 end 
 
