bcd501016ad3e32f9fc7b03008821474dbade150
Marko Lindqvist <cazfi74@gmail.com>
2018-09-17 11:52:02 +0300

    installer_cross: Support win32 crosser setup
    See hrm Feature #774284
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- windows/installer_cross/installer_build.sh
+++ windows/installer_cross/installer_build.sh
@@ -70,9 +70,13 @@ add_common_env() {
   cp $1/bin/libintl-8.dll $2/ &&
   cp $1/bin/libsqlite3-0.dll $2/ &&
   cp $1/bin/libiconv-2.dll $2/ &&
-  cp $1/bin/libz.dll.1.2.11 $2/ &&
-  cp $1/lib/icuuc62.dll $2/ &&
-  cp $1/lib/icudt62.dll $2/ &&
+  cp $1/bin/libz.dll.1.2.11 $2 &&
+  ( test "x$SETUP" != "xwin32" ||
+    ( cp $1/lib/icuuc58.dll $2/ &&
+      cp $1/lib/icudt58.dll $2/ )) &&
+  ( test "x$SETUP" = "xwin32" ||
+    ( cp $1/lib/icuuc62.dll $2/ &&
+      cp $1/lib/icudt62.dll $2/ )) &&
   cp $1/bin/libpng16-16.dll $2/
 }
 
