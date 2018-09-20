d62428da94364102f6acb187f2d59661b2023d69
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2018-09-20 11:07:31 +0200

    ruledit: can't enable invalid action.
    Assert that there is at least one action to enable when adding a new action
    enabler. Also assert that the default action for the new enabler exists. Both
    assumptions are true as long as actions them self - unlike action enablers -
    remain hard coded.
    
    See hrm Feature #774865

--- tools/ruledit/tab_enablers.cpp
+++ tools/ruledit/tab_enablers.cpp
@@ -207,7 +207,9 @@ void tab_enabler::add_now()
   // Try to add completely new enabler
   new_enabler = action_enabler_new();
 
-  new_enabler->action = (enum gen_action)(NUM_ACTIONS - 1);
+  fc_assert_ret(NUM_ACTIONS > 0);
+  fc_assert_ret(action_id_exists(NUM_ACTIONS - 1));
+  new_enabler->action = (NUM_ACTIONS - 1);
 
   action_enabler_add(new_enabler);
 
