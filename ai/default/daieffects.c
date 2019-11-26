d874447a669e2a7d04c6656deeffe65f982a912b
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2019-11-26 05:55:52 +0100

    AI: A unit can't pay more than MAX_MOVE_FRAGS.
    MAX_MOVE_FRAGS represents all the movement a unit has. Cap the move fragment
    cost from Action_Success_Actor_Move_Cost and Action_Success_Target_Move_Cost
    when calculating the AI utility they provide.
    
    See hrm Feature #849446

--- ai/default/daieffects.c
+++ ai/default/daieffects.c
@@ -634,9 +634,13 @@ adv_want dai_effect_value(struct player *pplayer, struct government *gov,
     break;
   case EFT_ACTION_SUCCESS_MOVE_COST:
   case EFT_ACTION_SUCCESS_TARGET_MOVE_COST:
-    /* Someone may put MAX_MOVE_FRAGS in this effect to make sure that all
-     * movement is gone. */
-    v -= amount / (adv_want)MAX_MOVE_FRAGS;
+    {
+      /* Taking MAX_MOVE_FRAGS takes all the move fragments. */
+      adv_want move_fragment_cost = MIN(MAX_MOVE_FRAGS, amount);
+
+      /* Lose all movement => 1. */
+      v -= move_fragment_cost / (adv_want)MAX_MOVE_FRAGS;
+    }
     break;
   case EFT_COUNT:
     log_error("Bad effect type.");
