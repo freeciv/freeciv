de62f51c88a648df96af4c944470da69f9df1e7c
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2019-12-01 03:25:42 +0000

    Gtk3: restore mnemonics to action dialogs
    See hrm feature #850296.

--- client/gui-gtk-3.0/action_dialog.c
+++ client/gui-gtk-3.0/action_dialog.c
@@ -2156,7 +2156,7 @@ static void action_entry(GtkWidget *shl,
     return;
   }
 
-  label = action_prepare_ui_name(act_id, "",
+  label = action_prepare_ui_name(act_id, "_",
                                  act_probs[act_id],
                                  custom);
 
@@ -2187,7 +2187,7 @@ static void action_entry_update(GtkWidget *shl,
       action_prob_possible(act_probs[act_id]));
 
   /* The probability may have changed. */
-  label = action_prepare_ui_name(act_id, "",
+  label = action_prepare_ui_name(act_id, "_",
                                  act_probs[act_id], custom);
 
   tooltip = action_get_tool_tip(act_id,
