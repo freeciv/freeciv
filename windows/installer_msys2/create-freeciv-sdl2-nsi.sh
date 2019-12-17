4f2376491efeb8efd0a93860e0c9ea84b053211a
Marko Lindqvist <cazfi74@gmail.com>
2019-12-17 04:22:38 +0200

    msys2: Convert langstat files to ISO-8859-1 when building installer
    This fixes display of name of the language "Norwegian Bokmål"
    
    Reported by Jacob Nevins
    
    See hrm Bug #681314
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- windows/installer_msys2/create-freeciv-sdl2-nsi.sh
+++ windows/installer_msys2/create-freeciv-sdl2-nsi.sh
@@ -171,6 +171,7 @@ EOF
 
 cat ../../bootstrap/langstat_core.txt |
 sort -k 1 |
+iconv -f UTF-8 -t ISO-8859-1 |
 while read -r code prct name
 do
 if test -e $1/share/locale/$code/LC_MESSAGES/freeciv-core.mo; then
@@ -249,6 +250,7 @@ EOF
 
   cat ../../bootstrap/langstat_core.txt |
   sort -k 1 |
+  iconv -f UTF-8 -t ISO-8859-1 |
   while read -r code prct name
   do
   if test -e $1/share/locale/$code/LC_MESSAGES/freeciv-core.mo; then
@@ -272,6 +274,7 @@ EOF
   echo "  \${EndIf}"
 
   cat ../../bootstrap/langstat_core.txt |
+  iconv -f UTF-8 -t ISO-8859-1 |
   while read -r code prct name
   do
     echo "  \${If} \$LangName == \"$name ($code) $prct\""
