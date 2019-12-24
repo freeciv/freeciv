b72cace8f5065f36785c8d5bb3d3f411b70dbccd
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2019-12-24 14:27:02 +0100

    Travis: build the Qt-programs again.
    The ppa can be dropped once we move to the next LTS.
    
    Requested by Marko Lindqvist
    
    See hrm Feature #853722

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -41,7 +41,7 @@ ninja install
 # Configure and build Freeciv
 mkdir build
 cd build
-../autogen.sh CFLAGS="-O3" CXXFLAGS="-O3" --enable-client=gtk3.22,gtk3,sdl2,stub --enable-fcmp=cli,gtk3 --enable-freeciv-manual --enable-ai-static=classic,threaded,tex,stub --enable-fcdb=sqlite3,mysql --disable-ruledit --prefix=${HOME}/freeciv/ && make -s -j$(nproc)
+../autogen.sh CFLAGS="-O3" CXXFLAGS="-O3" --enable-client=gtk3.22,gtk3,qt,sdl2,stub --enable-fcmp=cli,gtk3,qt --enable-freeciv-manual --enable-ai-static=classic,threaded,tex,stub --enable-fcdb=sqlite3,mysql --prefix=${HOME}/freeciv/ && make -s -j$(nproc)
 sudo -u travis make install
 echo "Freeciv build successful!"
 
