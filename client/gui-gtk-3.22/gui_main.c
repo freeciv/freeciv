43d8d8e224884982ef42574b4935460a2ff36aeb
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2019-12-26 13:08:25 +0000

    Gtk3/4: set more GTK_ORIENTATION_VERTICAL
    For some GtkGrids that were GtkVBoxes in the Gtk2 client.
    This has no known functional effect; in practice, I think these VBoxes
    never have more than one child.
    
    See hrm bug #853921.

--- client/gui-gtk-3.22/gui_main.c
+++ client/gui-gtk-3.22/gui_main.c
@@ -1148,6 +1148,8 @@ static void setup_widgets(void)
   gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page, NULL);
 
   top_vbox = gtk_grid_new();
+  gtk_orientable_set_orientation(GTK_ORIENTABLE(top_vbox),
+                                 GTK_ORIENTATION_VERTICAL);
   gtk_grid_set_row_spacing(GTK_GRID(top_vbox), 5);
   hgrid = gtk_grid_new();
 
@@ -1241,6 +1243,8 @@ static void setup_widgets(void)
   main_frame_civ_name = frame;
 
   vgrid = gtk_grid_new();
+  gtk_orientable_set_orientation(GTK_ORIENTABLE(vgrid),
+                                 GTK_ORIENTATION_VERTICAL);
   gtk_container_add(GTK_CONTAINER(frame), vgrid);
   gtk_widget_set_hexpand(vgrid, TRUE);
 
