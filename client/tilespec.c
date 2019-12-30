498871a7795ff3b40298a7e1d51a9b06dc3986f0
Marko Lindqvist <cazfi74@gmail.com>
2019-12-30 22:51:34 +0200

    fill_sprite_array(): Call city_size_get() just once in LAYER_CITY2 handling
    See hrm Feature #854366
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/tilespec.c
+++ client/tilespec.c
@@ -5875,18 +5875,19 @@ int fill_sprite_array(struct tileset *t,
     /* City size.  Drawing this under fog makes it hard to read. */
     if (pcity && gui_options.draw_cities && !gui_options.draw_full_citybar) {
       bool warn = FALSE;
+      unsigned int size = city_size_get(pcity);
 
-      ADD_SPRITE(t->sprites.city.size[city_size_get(pcity) % 10], FALSE,
+      ADD_SPRITE(t->sprites.city.size[size % 10], FALSE,
                  FULL_TILE_X_OFFSET + t->city_size_offset_x,
                  FULL_TILE_Y_OFFSET + t->city_size_offset_y);
-      if (10 <= city_size_get(pcity)) {
-        ADD_SPRITE(t->sprites.city.size_tens[(city_size_get(pcity) / 10) 
+      if (10 <= size) {
+        ADD_SPRITE(t->sprites.city.size_tens[(size / 10)
                    % 10], FALSE,
                    FULL_TILE_X_OFFSET + t->city_size_offset_x,
                    FULL_TILE_Y_OFFSET + t->city_size_offset_y);
-        if (100 <= city_size_get(pcity)) {
+        if (100 <= size) {
           struct sprite *sprite =
-              t->sprites.city.size_hundreds[(city_size_get(pcity) / 100) % 10];
+            t->sprites.city.size_hundreds[(size / 100) % 10];
 
           if (NULL != sprite) {
             ADD_SPRITE(sprite, FALSE,
@@ -5895,7 +5896,7 @@ int fill_sprite_array(struct tileset *t,
           } else {
             warn = TRUE;
           }
-          if (1000 <= city_size_get(pcity)) {
+          if (1000 <= size) {
             warn = TRUE;
           }
         }
@@ -5908,7 +5909,7 @@ int fill_sprite_array(struct tileset *t,
         if (0 != strcmp(last_reported, t->name)) {
           log_normal(_("Tileset \"%s\" doesn't support big cities size, "
                        "such as %d. Size not displayed as expected."),
-                     t->name, city_size_get(pcity));
+                     t->name, size);
           sz_strlcpy(last_reported, t->name);
         }
       }
