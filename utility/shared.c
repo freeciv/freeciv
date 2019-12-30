f3a0ba80094eace578bf2c306c7befcc4635f174
Marko Lindqvist <cazfi74@gmail.com>
2019-12-30 16:08:26 +0200

    make_dir(): Set null character as '\0', not 0
    See hrm Feature #854295
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- utility/shared.c
+++ utility/shared.c
@@ -1815,7 +1815,7 @@ bool make_dir(const char *pathname)
     dir = strchr(dir, DIR_SEPARATOR_CHAR);
     /* We set the current / with 0, and restore it afterwards */
     if (dir) {
-      *dir = 0;
+      *dir = '\0';
     }
 
 #ifdef WIN32_NATIVE
