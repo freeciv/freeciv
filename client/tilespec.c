8e14bfed7a740b1512cef30249a893162a1030bd
Marko Lindqvist <cazfi74@gmail.com>
2020-01-01 13:29:51 +0200

    tileset_use_preferred_theme(): Fix theme name setting for gtk3x-client
    It was accidentally falling to use sdl2-client theme.
    
    See hrm Bug #854470
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/tilespec.c
+++ client/tilespec.c
@@ -6615,6 +6615,7 @@ void tileset_use_preferred_theme(const struct tileset *t)
   case GUI_GTK3x:
     default_theme_name = gui_options.gui_gtk4_default_theme_name;
     default_theme_name_sz = sizeof(gui_options.gui_gtk4_default_theme_name);
+    break;
   case GUI_SDL2:
     default_theme_name = gui_options.gui_sdl2_default_theme_name;
     default_theme_name_sz = sizeof(gui_options.gui_sdl2_default_theme_name);
