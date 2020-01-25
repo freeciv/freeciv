87e446673092a9816ee542c585cd990f53b10512
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2020-01-25 00:59:09 +0000

    Gtk3.22: shortcuts for musicset selection dialog
    Missed in hrm bug #857082.
    
    See hrm bug #857239.

--- client/gui-gtk-3.22/soundset_dlg.c
+++ client/gui-gtk-3.22/soundset_dlg.c
@@ -114,9 +114,9 @@ void popup_musicset_suggestion_dialog(void)
   dialog = gtk_dialog_new_with_buttons(_("Preferred musicset"),
                                        NULL,
                                        0,
-                                       _("Load musicset"),
+                                       _("_Load musicset"),
                                        GTK_RESPONSE_YES,
-                                       _("Keep current musicset"),
+                                       _("_Keep current musicset"),
                                        GTK_RESPONSE_NO,
                                        NULL);
   gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_YES);
