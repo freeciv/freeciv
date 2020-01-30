0463e938019b8f5b1ea8f57f55579202d50cdfde
Marko Lindqvist <cazfi74@gmail.com>
2020-01-30 17:45:23 +0200

    qt: Properly NULL-terminate gfx file extension list
    Reported by Chippo Elder
    
    See hrm Bug #857808
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-qt/sprite.cpp
+++ client/gui-qt/sprite.cpp
@@ -1,4 +1,4 @@
-/**********************************************************************
+/***********************************************************************
  Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -44,7 +44,7 @@ const char **gfx_fileextensions(void)
 
   gfx_ext = QImageReader::supportedImageFormats();
 
-  gfx_array_extensions = new const char *[gfx_ext.count()];
+  gfx_array_extensions = new const char *[gfx_ext.count() + 1];
   while (!gfx_ext.isEmpty()) {
     char *ext;
 
@@ -54,6 +54,7 @@ const char **gfx_fileextensions(void)
     gfx_array_extensions[j] = ext;
     j++;
   }
+  gfx_array_extensions[j] = NULL;
 
   return gfx_array_extensions;
 }
