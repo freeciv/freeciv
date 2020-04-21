d266c3ee7b34d4a9cb815d37b90560f8ab562f42
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2020-04-21 10:34:33 +0200

    Auto document action failed dice roll.
    Requested by Lexxie L and Marko Lindqvist
    
    See hrm Feature #869739

--- client/helpdata.c
+++ client/helpdata.c
@@ -2519,6 +2519,17 @@ char *helptext_unit(char *buf, size_t bufsz, struct player *pplayer,
       }
 
       {
+        int odds = action_dice_roll_initial_odds(paction);
+
+        if (odds != ACTION_ODDS_PCT_DICE_ROLL_NA) {
+          /* TODO: try to detect that the odds always will be 100% because
+           * of the Action_Odds_Pct effect. */
+          cat_snprintf(buf, bufsz,
+                       _("  * may fail because of a dice throw.\n"));
+        }
+      }
+
+      {
         struct universal req_pattern[] = {
           { .kind = VUT_ACTION, .value.action = paction },
           { .kind = VUT_UTYPE,  .value.utype = utype },
