c7db7b9e17c28f727b3ccfc1f4f169fb1c9d023c
Marko Lindqvist <cazfi74@gmail.com>
2020-04-27 04:17:56 +0300

    Update to crosser-1.10
    See hrm Feature #870964
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- windows/installer_cross/installer_build.sh
+++ windows/installer_cross/installer_build.sh
@@ -29,7 +29,6 @@ add_gtk3_env() {
   cp $1/bin/libgmodule-2.0-0.dll $2/ &&
   cp $1/bin/libpangowin32-1.0-0.dll $2/ &&
   cp $1/bin/libfontconfig-1.dll $2/ &&
-  cp $1/bin/libfreetype-6.dll $2/ &&
   cp $1/bin/libpangoft2-1.0-0.dll $2/ &&
   cp $1/bin/libxml2-2.dll $2/ &&
   cp $1/bin/libharfbuzz-0.dll $2/ &&
@@ -72,14 +71,11 @@ add_common_env() {
   cp $1/bin/libsqlite3-0.dll $2/ &&
   cp $1/bin/libiconv-2.dll $2/ &&
   cp $1/bin/libz.dll.1.2.11 $2/ &&
-  ( test "x$SETUP" != "xwin32" ||
-    ( cp $1/lib/icuuc58.dll $2/ &&
-      cp $1/lib/icudt58.dll $2/ )) &&
-  ( test "x$SETUP" = "xwin32" ||
-    ( cp $1/lib/icuuc62.dll $2/ &&
-      cp $1/lib/icudt62.dll $2/ )) &&
+  cp $1/lib/icuuc63.dll     $2/ &&
+  cp $1/lib/icudt63.dll     $2/ &&
   cp $1/bin/libpng16-16.dll $2/ &&
-  cp $1/bin/libintl.dll     $2/
+  cp $1/bin/libintl.dll     $2/ &&
+  cp $1/bin/libfreetype-6.dll $2/
 }
 
 if test "x$1" = x || test "x$1" = "x-h" || test "x$1" = "x--help" || test "x$2" = "x" ; then
