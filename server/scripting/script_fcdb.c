17cd50c3ae779b34bcca950cda3ac1effcae670d
Louis Moureaux <m_louis30@yahoo.com>
2020-05-04 07:53:10 +0300

    Add tolua_game to the fcdb lua context
    tolua_game is required to pass the 'player' argument of user_take().
    
    See hrm Bug #866850
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/scripting/script_fcdb.c
+++ server/scripting/script_fcdb.c
@@ -48,6 +48,7 @@
 #include "luascript_types.h"
 #include "tolua_common_a_gen.h"
 #include "tolua_common_z_gen.h"
+#include "tolua_game_gen.h"
 
 /* server */
 #include "console.h"
@@ -235,6 +236,7 @@ bool script_fcdb_init(const char *fcdb_luafile)
   }
 
   tolua_common_a_open(fcl->state);
+  tolua_game_open(fcl->state);
   tolua_fcdb_open(fcl->state);
   lua_register(fcl->state, "md5sum", md5sum);
 #ifdef HAVE_FCDB_MYSQL
