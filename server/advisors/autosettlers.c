b3a041b1856773805fa87a0b22ef8e8f870eda32
Marko Lindqvist <cazfi74@gmail.com>
2020-05-12 22:28:02 +0300

    Autosettlers: Drop code related to terrain changing ACTIVITY_IRRIGATE/_MINE
    See hrm Feature #873056
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/advisors/autosettlers.c
+++ server/advisors/autosettlers.c
@@ -1247,14 +1247,7 @@ bool auto_settlers_speculate_can_act_at(const struct unit *punit,
 
   switch (activity) {
   case ACTIVITY_MINE:
-    if (pterrain->mining_result != pterrain
-        && pterrain->mining_result != T_NONE) {
-      return action_prob_possible(action_speculate_unit_on_tile(
-                                    ACTION_PLANT,
-                                    punit, unit_home(punit), ptile,
-                                    omniscient_cheat,
-                                    ptile, target));
-    } else if (pterrain->mining_result == pterrain) {
+    if (pterrain->mining_result == pterrain) {
       return action_prob_possible(action_speculate_unit_on_tile(
                                     ACTION_MINE,
                                     punit, unit_home(punit), ptile,
@@ -1277,14 +1270,7 @@ bool auto_settlers_speculate_can_act_at(const struct unit *punit,
     }
 
   case ACTIVITY_IRRIGATE:
-    if (pterrain->irrigation_result != pterrain
-        && pterrain->irrigation_result != T_NONE) {
-      return action_prob_possible(action_speculate_unit_on_tile(
-                                    ACTION_CULTIVATE,
-                                    punit, unit_home(punit), ptile,
-                                    omniscient_cheat,
-                                    ptile, target));
-    } else if (pterrain->irrigation_result == pterrain) {
+    if (pterrain->irrigation_result == pterrain) {
       return action_prob_possible(action_speculate_unit_on_tile(
                                     ACTION_IRRIGATE,
                                     punit, unit_home(punit), ptile,
