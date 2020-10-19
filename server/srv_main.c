af3c4469bcadd093e35ddd43fd04404c12b84728
Marko Lindqvist <cazfi74@gmail.com>
2020-10-19 05:27:05 +0300

    Avoid infinite loop when phase from savegame == num phases
    Reported by George Koehler
    
    See hrm Bug #815196
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/srv_main.c
+++ server/srv_main.c
@@ -2856,6 +2856,14 @@ static void srv_running(void)
       }
       game.server.additional_phase_seconds = 0;
     }
+
+    /* Make sure is_new_turn is reseted before next turn even if
+     * we did zero rounds in the loop (i.e. if current phase from
+     * the savegame was >= num phases). Without this begin_turn()
+     * would not reset phase, so there would be infinite loop
+     * where phase is too high for is_new_turn to get set. */
+    is_new_turn = TRUE;
+
     end_turn();
     log_debug("Sendinfotometaserver");
     (void) send_server_info_to_metaserver(META_REFRESH);
