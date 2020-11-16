18fc63d82ce226e5a49f3c08d0c43b2844ada541
Marko Lindqvist <cazfi74@gmail.com>
2020-11-16 20:30:14 +0200

    Sandbox: Fix msgstr of Hermit's nest messages
    See hrm Bug #897725
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- data/sandbox/script.lua
+++ data/sandbox/script.lua
@@ -35,8 +35,7 @@ function hermit_nest(unit, extra)
     if chance <= 3 then
       local tech = unit.owner:give_tech(nil, 20, false, "hut")
       notify.event(unit.owner, unit.tile, E.HUT_TECH,
-                 _("Secluded studies have led the Hermit to "
-                   .. "the discovery of %s!"),
+                 _("Secluded studies have led the Hermit to the discovery of %s!"),
                  tech:name_translation())
     else
       notify.event(unit.owner, unit.tile, E.HUT_BARB_CITY_NEAR,
@@ -52,8 +51,8 @@ signal.connect("hut_enter", "hermit_nest")
 function hermit_nest_blown(unit, extra)
   if extra == "Hermit" then
     notify.event(unit.owner, unit.tile, E.HUT_BARB,
-                 _("Your %s has overflied a Hermit's Place" 
-                   .. " and destroyed it!"), unit.utype:name_translation())
+                 _("Your %s has overflied a Hermit's Place and destroyed it!"),
+                 unit.utype:name_translation())
     -- do not process default.lua
     return true
   end
