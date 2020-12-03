794655d8c7bc27017615d2c6cb970c75be83e1b5
Marko Lindqvist <cazfi74@gmail.com>
2020-12-03 07:26:25 +0200

    sdl2: Correct create_utf8_str() function header
    It talked about parameter called pInTextString, when in reality
    it's called in_text.
    
    See hrm Feature #902490
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-sdl2/gui_string.c
+++ client/gui-sdl2/gui_string.c
@@ -158,7 +158,7 @@ void utf8_str_size(utf8_str *pstr, SDL_Rect *fill)
 /**************************************************************************
   Create utf8_str struct with ptsize font.
   Font will be loaded or aliased with existing font of that size.
-  pInTextString must be allocated in memory (MALLOC/fc_calloc)
+  in_text must be allocated in memory (malloc/fc_calloc)
 **************************************************************************/
 utf8_str *create_utf8_str(char *in_text, size_t n_alloc, Uint16 ptsize)
 {
@@ -182,7 +182,7 @@ utf8_str *create_utf8_str(char *in_text, size_t n_alloc, Uint16 ptsize)
   str->fgcol = *get_theme_color(COLOR_THEME_TEXT);
   str->render = 2;
 
-  /* pInTextString must be allocated in memory (MALLOC/fc_calloc) */
+  /* in_text must be allocated in memory (malloc/fc_calloc) */
   str->text = in_text;
   str->n_alloc = n_alloc;
 
