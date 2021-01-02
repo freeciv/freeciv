183b871c32a77435cca478567aa161d4204b8760
Marko Lindqvist <cazfi74@gmail.com>
2021-01-02 16:45:36 +0200

    Qt: Popdown gotodlg when leaving the game
    Also fixes a crash when quitting the client, and gotodlg has been
    opened.
    
    Crash reported by John Robertson
    
    See hrm Bug #911885
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-qt/dialogs.cpp
+++ client/gui-qt/dialogs.cpp
@@ -3697,6 +3697,7 @@ void popdown_all_game_dialogs(void)
   int i;
   QList <choice_dialog *> cd_list;
   QList <notify_dialog *> nd_list;
+  goto_dialog *gtd;
 
   QApplication::alert(gui()->central_wdg);
   cd_list = gui()->game_tab_widget->findChildren <choice_dialog *>();
@@ -3717,6 +3718,12 @@ void popdown_all_game_dialogs(void)
   popdown_city_report();
   popdown_endgame_report();
   gui()->popdown_unit_sel();
+
+  gtd = gui()->gtd;
+
+  if (gtd != nullptr) {
+    gtd->close_dlg();
+  }
 }
 
 /***********************************************************************//**
