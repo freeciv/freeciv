a528ffc80c69595d88371092ee248d39e5347af6
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2021-01-31 15:22:52 +0100

    Comment pronuon fix.

--- common/metaknowledge.c
+++ common/metaknowledge.c
@@ -339,7 +339,7 @@ static bool is_req_knowable(const struct player *pow_player,
         return TRUE;
       }
 
-      /* The player knows its city even if he can't see it */
+      /* The player knows his city even if he can't see it */
       pcity = tile_city(target_tile);
       return pcity && city_owner(pcity) == pow_player;
     case REQ_RANGE_CADJACENT:
@@ -348,7 +348,7 @@ static bool is_req_knowable(const struct player *pow_player,
         return TRUE;
       }
 
-      /* The player knows its city even if he can't see it */
+      /* The player knows his city even if he can't see it */
       cardinal_adjc_iterate(target_tile, ptile) {
         pcity = tile_city(ptile);
         if (pcity && city_owner(pcity) == pow_player) {
@@ -364,7 +364,7 @@ static bool is_req_knowable(const struct player *pow_player,
         return TRUE;
       }
 
-      /* The player knows its city even if he can't see it */
+      /* The player knows his city even if he can't see it */
       adjc_iterate(target_tile, ptile) {
         pcity = tile_city(ptile);
         if (pcity && city_owner(pcity) == pow_player) {
