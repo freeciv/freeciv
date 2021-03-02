6570959b3b01cb7721a3123151551fde1bc2dd0f
Marko Lindqvist <cazfi74@gmail.com>
2021-03-02 07:53:07 +0200

    Comment why we don't check bodyguard ferry boarding legality
    Requested by Sveinung Kvilhaugsvik
    
    See hrm Bug #849990
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- ai/default/aiferry.c
+++ ai/default/aiferry.c
@@ -872,8 +872,12 @@ bool aiferry_gobyboat(struct ai_type *ait, struct player *pplayer,
         }
       }
       if (bodyguard) {
-        /* FIXME: check if action is enabled. */
         fc_assert(same_pos(unit_tile(punit), unit_tile(bodyguard)));
+
+        /* We don't need to check if this is legal beforehand.
+         * It fails if it fails, and there's no other things we would
+         * want to do in that case - bodyguard either uses the same boat
+         * or none at all. */
         handle_unit_do_action(pplayer, bodyguard->id, ferryboat->id, 0, "",
                               /* FIXME: don't hard code action id. */
                               ACTION_TRANSPORT_BOARD);
