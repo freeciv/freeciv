2ead4127a51ff3ba03c3144d58345f6fc208f39c
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2021-03-14 13:29:17 +0100

    New effect Heal_Unit_Pct.
    Make how much of the unit's full health the heal unit action will restore
    ruleset controlled.
    
    See osdn #41789

--- ai/default/daieffects.c
+++ ai/default/daieffects.c
@@ -561,6 +561,7 @@ adv_want dai_effect_value(struct player *pplayer, struct government *gov,
   case EFT_MAPS_STOLEN_PCT:
   case EFT_UNIT_SHIELD_VALUE_PCT:
   case EFT_NUKE_BLAST_RADIUS_1_SQ:
+  case EFT_HEAL_UNIT_PCT:
     break;
     /* This has no effect for AI */
   case EFT_VISIBLE_WALLS:
