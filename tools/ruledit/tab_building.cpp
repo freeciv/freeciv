ccd544b553c419c26ee2747bf40512c90745a131
Marko Lindqvist <cazfi74@gmail.com>
2021-04-09 00:55:20 +0300

    Ruledit: Delete "Edit Values" dialog when ruleset item is deleted
    See osdn #41957
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tools/ruledit/tab_building.cpp
+++ tools/ruledit/tab_building.cpp
@@ -209,7 +209,7 @@ void tab_building::name_given()
 **************************************************************************/
 void tab_building::delete_now()
 {
-  if (selected != 0) {
+  if (selected != nullptr) {
     requirers_dlg *requirers;
 
     requirers = ui->create_requirers(improvement_rule_name(selected));
@@ -218,6 +218,10 @@ void tab_building::delete_now()
     }
 
     selected->ruledit_disabled = true;
+
+    if (selected->ruledit_dlg != nullptr) {
+      ((edit_impr *)selected->ruledit_dlg)->done(0);
+    }
 
     refresh();
     update_bldg_info(nullptr);
