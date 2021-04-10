efb46e8f1ef1c117dbc85e40e1d39493578ee974
Marko Lindqvist <cazfi74@gmail.com>
2021-04-10 18:54:01 +0300

    Tell in help if nukes can destroy cities completely or not
    See osdn #41967
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/helpdata.c
+++ client/helpdata.c
@@ -2709,6 +2709,17 @@ char *helptext_unit(char *buf, size_t bufsz, struct player *pplayer,
                        _("  * %d%% of the population of each city inside"
                          " the nuclear blast dies.\n"),
                          game.info.nuke_pop_loss_pct);
+          if (game.info.nuke_pop_loss_pct < 50) {
+            cat_snprintf(buf, bufsz,
+                         _("  * can never destroy city completely "
+                           "(%d%% of size 1 rounds down to 0).\n"),
+                         game.info.nuke_pop_loss_pct);
+          } else {
+            cat_snprintf(buf, bufsz,
+                         _("  * can even destroy city completely "
+                           "(%d%% of size 1 rounds up to 1).\n"),
+                         game.info.nuke_pop_loss_pct);
+          }
         }
         if (game.info.nuke_defender_survival_chance_pct > 0) {
           cat_snprintf(buf, bufsz,
