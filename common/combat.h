4d5067629e5ff7fd82dedd3c3a0acce5df495060
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2021-04-15 11:41:17 +0200

    Fix unit_attack_*_at_tile_result() header length.
    Fix the line length of the function headers of
    unit_attack_unit_at_tile_result(), unit_attack_all_at_tile_result(),
    unit_attack_any_at_tile_result() and unit_attack_units_at_tile_result().
    
    See osdn #42006

--- common/combat.h
+++ common/combat.h
@@ -39,11 +39,13 @@ enum unit_attack_result {
 bool is_unit_reachable_at(const struct unit *defender,
                           const struct unit *attacker,
                           const struct tile *location);
-enum unit_attack_result unit_attack_unit_at_tile_result(const struct unit *punit,
-                                                        const struct unit *pdefender,
-                                                        const struct tile *dest_tile);
-enum unit_attack_result unit_attack_units_at_tile_result(const struct unit *punit,
-                                                         const struct tile *ptile);
+enum unit_attack_result
+unit_attack_unit_at_tile_result(const struct unit *punit,
+                                const struct unit *pdefender,
+                                const struct tile *dest_tile);
+enum unit_attack_result
+unit_attack_units_at_tile_result(const struct unit *punit,
+                                 const struct tile *ptile);
 bool can_unit_attack_tile(const struct unit *punit,
 			  const struct tile *ptile);
 
