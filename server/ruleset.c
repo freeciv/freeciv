740df94a9975f2ef8652f3341df1d5516b5b1b39
Marko Lindqvist <cazfi74@gmail.com>
2021-04-16 06:22:39 +0300

    Drop rscompat code for effects nreqs requirements
    See osdn #41977
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/ruleset.c
+++ server/ruleset.c
@@ -6058,17 +6058,6 @@ static bool load_ruleset_effects(struct section_file *file,
       effect_req_append(peffect, *preq);
     } requirement_vector_iterate_end;
 
-    if (compat->compat_mode) {
-      reqs = lookup_req_list(file, compat, sec_name, "nreqs", type);
-      if (reqs == NULL) {
-        ok = FALSE;
-        break;
-      }
-      requirement_vector_iterate(reqs, preq) {
-        preq->present = !preq->present;
-        effect_req_append(peffect, *preq);
-      } requirement_vector_iterate_end;
-    }
   } section_list_iterate_end;
   section_list_destroy(sec);
 
