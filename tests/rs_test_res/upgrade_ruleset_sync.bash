6bdb12448b7a83632072de3178843cfc1fb68dfa
Marko Lindqvist <cazfi74@gmail.com>
2021-05-05 14:02:55 +0300

    upgrade_ruleset_sync.bash: Update S3_1 revision to fetch
    Get a S3_1 revision where ruleset capstr has been bumped
    to one we support in compatibility mode.
    
    See osdn #42159
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tests/rs_test_res/upgrade_ruleset_sync.bash
+++ tests/rs_test_res/upgrade_ruleset_sync.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # the revision from the old branch to pick if no revision is specified
-DEFAULT_REVISION="3c62d61956"
+DEFAULT_REVISION="abf808eb60"
 
 # the branch of the previous version
 PREVIOUS_BRANCH="S3_1"
