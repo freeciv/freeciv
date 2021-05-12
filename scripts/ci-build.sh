9af718279ce7031ee7580e955288a703ba9ee37c
Marko Lindqvist <cazfi74@gmail.com>
2021-05-12 07:44:31 +0300

    CI: Temporarily switch OSX build to use Qt5
    OSX Qt6 build currently fails. Use Qt5 until that gets fixed.
    
    See osdn #42250
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -39,16 +39,16 @@ ninja install
 "os_x")
 # gcc is an alias for clang on OS X
 
-export PATH="/usr/local/opt/gettext/bin:/usr/local/opt/icu4c/bin:$(brew --prefix qt@6)/bin:$PATH"
-export CPPFLAGS="-I/usr/local/opt/gettext/include -I/usr/local/opt/icu4c/include -I$(brew --prefix qt@6)/include $CPPFLAGS"
-export LDFLAGS="-L/usr/local/opt/gettext/lib -L/usr/local/opt/icu4c/lib -L$(brew --prefix qt@6)/lib"
+export PATH="/usr/local/opt/gettext/bin:/usr/local/opt/icu4c/bin:$(brew --prefix qt@5)/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/gettext/include -I/usr/local/opt/icu4c/include -I$(brew --prefix qt@5)/include $CPPFLAGS"
+export LDFLAGS="-L/usr/local/opt/gettext/lib -L/usr/local/opt/icu4c/lib -L$(brew --prefix qt@5)/lib"
 export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 mkdir build
 cd build
 ../autogen.sh --no-configure-run
 ../configure \
- --enable-sys-lua --with-qt6 \
+ --enable-sys-lua \
  --enable-client=gtk3.22,sdl2,qt \
  --enable-freeciv-manual \
  || (let config_exit_status=$? \
