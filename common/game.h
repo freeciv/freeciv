13d7fcb691d6da457aaa38514ae090f3cebbb560
Marko Lindqvist <cazfi74@gmail.com>
2021-05-21 01:41:12 +0300

    Inline is_server()
    See osdn #42331
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/game.h
+++ common/game.h
@@ -293,9 +293,19 @@ struct civ_game {
   } callbacks;
 };
 
-bool is_server(void);
+extern bool am_i_server;
+
+/**********************************************************************//**
+  Is the program type server?
+**************************************************************************/
+static inline bool is_server(void)
+{
+  return am_i_server;
+}
+
 void i_am_server(void);
 void i_am_client(void);
+
 static inline void i_am_tool(void)
 {
   i_am_server(); /* No difference between a tool and server at the moment */
