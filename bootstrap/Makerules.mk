bec6fb57f6d139b01fd64b6293162dd7ecf8e194
Marko Lindqvist <cazfi74@gmail.com>
2021-06-15 09:47:16 +0300

    Makerules.mk: Make moc rule to respect automake's silent mode
    See osdn #42458
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- bootstrap/Makerules.mk
+++ bootstrap/Makerules.mk
@@ -1,4 +1,4 @@
-#/********************************************************************** 
+#/***********************************************************************
 #   Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
 #
 #   This program is free software; you can redistribute it and/or modify
@@ -15,4 +15,5 @@
 
 # Generate Qt meta object source from header
 $(MOC_FILES) : meta_%.cpp: %.h
-	$(MOCCMD) -o $@ $<
+	@if ! $(AM_V_P) ; then echo "  MOC      $@" ; fi
+	$(AM_V_at) $(MOCCMD) -o $@ $<
