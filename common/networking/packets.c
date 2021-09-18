185ac3940e978e18b008eec1c221e927d6d50dc7
Marko Lindqvist <cazfi74@gmail.com>
2021-09-18 05:22:22 +0300

    Accept mandatory capabilities for packet variant
    Patch by Máximo Castañeda
    
    See hrm Bug #829304
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/networking/packets.c
+++ common/networking/packets.c
@@ -839,22 +839,34 @@ const struct packet_handlers *packet_handlers_get(const char *capability)
   struct packet_handlers *phandlers;
   char functional_capability[MAX_LEN_CAPSTR] = "";
   char *tokens[MAX_LEN_CAPSTR / 2];
+  char *functional_tokens[MAX_LEN_CAPSTR / 2];
   int tokens_num;
+  int functional_tokens_num = 0;
   int i;
 
   fc_assert(strlen(capability) < sizeof(functional_capability));
 
   /* Get functional network capability string. */
   tokens_num = get_tokens(capability, tokens, ARRAY_SIZE(tokens), " \t\n,");
-  qsort(tokens, tokens_num, sizeof(*tokens), compare_strings_ptrs);
   for (i = 0; i < tokens_num; i++) {
-    if (!has_capability(tokens[i], packet_functional_capability)) {
-      continue;
+    char *token;
+
+    token = tokens[i];
+    if (token[0] == '+') {
+      token++;
     }
+
+    if (has_capability(token, packet_functional_capability)) {
+      functional_tokens[functional_tokens_num++] = token;
+    }
+  }
+  qsort(functional_tokens, functional_tokens_num,
+        sizeof(*functional_tokens), compare_strings_ptrs);
+  for (i = 0; i < functional_tokens_num; i++) {
     if (functional_capability[0] != '\0') {
       sz_strlcat(functional_capability, " ");
     }
-    sz_strlcat(functional_capability, tokens[i]);
+    sz_strlcat(functional_capability, functional_tokens[i]);
   }
   free_tokens(tokens, tokens_num);
 
