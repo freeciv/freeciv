00163179bd4dda85393bd6a83cf90e32d9c828c9
Marko Lindqvist <cazfi74@gmail.com>
2021-09-19 08:23:23 +0300

    AI: Drop unused military_advisor_choose_tech()
    It was never really implemented, and would not suit current design.
    
    See osdn #42227
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- ai/default/daimilitary.h
+++ ai/default/daimilitary.h
@@ -33,8 +33,6 @@ typedef struct unit_list *(player_unit_list_getter)(struct player *pplayer);
 
 struct unit_type *dai_choose_defender_versus(struct city *pcity,
                                              struct unit *attacker);
-void military_advisor_choose_tech(struct player *pplayer,
-                                  struct adv_choice *choice);
 struct adv_choice *military_advisor_choose_build(struct ai_type *ait,
                                                  struct player *pplayer,
                                                  struct city *pcity,
