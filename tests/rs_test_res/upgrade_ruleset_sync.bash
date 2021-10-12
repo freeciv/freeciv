b194d15be8aaa553faa5c547e1486b8a0149652c
Marko Lindqvist <cazfi74@gmail.com>
2021-10-12 17:47:25 +0300

    CI: Update commit id for rulesets to update from S3_1
    Use S3_1 commit that has the alien ruleset wonder upkeep fix
    as the base for ruleset update tests.
    
    See osdn #43033
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tests/rs_test_res/upgrade_ruleset_sync.bash
+++ tests/rs_test_res/upgrade_ruleset_sync.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # the revision from the old branch to pick if no revision is specified
-DEFAULT_REVISION="9eb95e212b"
+DEFAULT_REVISION="03c4c787cd"
 
 # the branch of the previous version
 PREVIOUS_BRANCH="S3_1"
