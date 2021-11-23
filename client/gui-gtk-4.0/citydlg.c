62d3eb80718641a76192be0bb20fab96c8b5f3a9
Marko Lindqvist <cazfi74@gmail.com>
2021-11-23 05:16:05 +0200

    gtk3x: Avoid gtk_widget_destroy() on removal of cma shell
    See osdn #43263
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-gtk-4.0/citydlg.c
+++ client/gui-gtk-4.0/citydlg.c
@@ -1367,17 +1367,15 @@ static void create_and_append_cma_page(struct city_dialog *pdialog)
 {
   GtkWidget *page, *label;
   const char *tab_title = _("_Governor");
-  int page_col = 0;
 
-  page = gtk_grid_new();
+  page = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 
   label = gtk_label_new_with_mnemonic(tab_title);
 
   gtk_notebook_append_page(GTK_NOTEBOOK(pdialog->notebook), page, label);
 
   pdialog->cma_editor = create_cma_dialog(pdialog->pcity, low_citydlg);
-  gtk_grid_attach(GTK_GRID(page), pdialog->cma_editor->shell,
-                  page_col++, 0, 1, 1);
+  gtk_box_append(GTK_BOX(page), pdialog->cma_editor->shell);
 
   gtk_widget_show(page);
 }
