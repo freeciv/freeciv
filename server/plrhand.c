c88fc62a949145426817c18f7ea823758c0de9bf
Marko Lindqvist <cazfi74@gmail.com>
2021-12-12 04:31:35 +0200

    AI: Allow changing government when revolentype is "Quickening"
    See osdn #43392
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/plrhand.c
+++ server/plrhand.c
@@ -466,8 +466,9 @@ void handle_player_change_government(struct player *pplayer,
      * a government). */
     turns = pplayer->revolution_finishes - game.info.turn;
   } else if ((is_ai(pplayer) && !has_handicap(pplayer, H_REVOLUTION))
-	     || !anarchy) {
+             || !anarchy) {
     /* AI players without the H_REVOLUTION handicap can skip anarchy */
+    anarchy = FALSE;
     turns = 0;
   } else {
     turns = revolution_length(gov, pplayer);
