ede2f83b2b9993f2f6e9f4e15372721bc971cc02
Marko Lindqvist <cazfi74@gmail.com>
2022-01-11 20:56:25 +0200

    installer_cross: Complete gtk4 environment
    Install all gtk4 environment files needed by gtk4 modpack installer
    
    See osdn #43602
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

---
+++ windows/installer_cross/helpers/installer-helper-gtk4.cmd
@@ -0,0 +1,5 @@
+cd %~dp0\..
+
+bin\glib-compile-schemas.exe share\glib-2.0\schemas
+bin\gdk-pixbuf-query-loaders.exe > lib\gdk-pixbuf-2.0\2.10.0\loaders.cache
+bin\gtk4-update-icon-cache.exe share\icons\Adwaita
