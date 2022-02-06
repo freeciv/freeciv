9316d2e2fda32736766fa4bea4bd5cd7ba0f0e50
Marko Lindqvist <cazfi74@gmail.com>
2022-02-07 00:30:41 +0200

    ls_odbc.c: Fix "unused variable" -compiler warnings
    See osdn #43797
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dependencies/luasql/src/ls_odbc.c
+++ dependencies/luasql/src/ls_odbc.c
@@ -774,7 +774,6 @@ static int set_param(lua_State *L, stmt_data *stmt, int i, param_data *data)
 */
 static int raw_readparams_table(lua_State *L, stmt_data *stmt, int iparams)
 {
-	static SQLINTEGER cbNull = SQL_NULL_DATA;
 	SQLSMALLINT i;
 	param_data *data;
 	int res = 0;
@@ -802,7 +801,6 @@ static int raw_readparams_table(lua_State *L, stmt_data *stmt, int iparams)
 */
 static int raw_readparams_args(lua_State *L, stmt_data *stmt, int arg, int ltop)
 {
-	static SQLINTEGER cbNull = SQL_NULL_DATA;
 	SQLSMALLINT i;
 	param_data *data;
 	int res = 0;
