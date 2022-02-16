e46162864ec0633716b171db1eda60f092bcc663
Marko Lindqvist <cazfi74@gmail.com>
2022-02-16 16:39:30 +0200

    generate_gitrev.sh: Handle relative output file path correctly
    See osdn #43879
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- bootstrap/generate_gitrev.sh
+++ bootstrap/generate_gitrev.sh
@@ -22,7 +22,9 @@ REV2="dist"
  # Check that all commands required by this script are available
  # If not, we will not claim to know which git revision this is
  # (REVSTATE will be OFF)
- if command -v git && command -v tail && command -v wc ; then
+ if command -v git >/dev/null &&
+    command -v tail >/dev/null &&
+    command -v wc >/dev/null ; then
    REVTMP="$(git rev-parse --short HEAD 2>/dev/null)"
    if test "x$REVTMP" != "x" ; then
      # This is git repository. Check for local modifications
@@ -37,11 +39,11 @@ REV2="dist"
    fi
  fi
 
- sed -e "s,<GITREV1>,$REV1," -e "s,<GITREV2>,$REV2," -e "s,<GITREVSTATE>,$REVSTATE," fc_gitrev_gen.h.tmpl > "$OUTPUTDIR/fc_gitrev_gen.h.tmp"
- if ! test -f "$OUTPUTDIR/fc_gitrev_gen.h" ||
-    ! cmp "$OUTPUTDIR/fc_gitrev_gen.h" "$OUTPUTDIR/fc_gitrev_gen.h.tmp"
- then
-   mv "$OUTPUTDIR/fc_gitrev_gen.h.tmp" "$OUTPUTDIR/fc_gitrev_gen.h"
- fi
- rm -f "$OUTPUTDIR/fc_gitrev_gen.h.tmp"
-) > /dev/null
+ sed -e "s,<GITREV1>,$REV1," -e "s,<GITREV2>,$REV2," -e "s,<GITREVSTATE>,$REVSTATE," fc_gitrev_gen.h.tmpl ) > "$OUTPUTDIR/fc_gitrev_gen.h.tmp"
+
+if ! test -f "$OUTPUTDIR/fc_gitrev_gen.h" ||
+   ! cmp "$OUTPUTDIR/fc_gitrev_gen.h" "$OUTPUTDIR/fc_gitrev_gen.h.tmp" >/dev/null
+then
+  mv "$OUTPUTDIR/fc_gitrev_gen.h.tmp" "$OUTPUTDIR/fc_gitrev_gen.h"
+fi
+rm -f "$OUTPUTDIR/fc_gitrev_gen.h.tmp"
