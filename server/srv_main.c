bb3f8168f3774bceb18a0f0d44eaac2cdba1cac6
Marko Lindqvist <cazfi74@gmail.com>
2022-02-17 20:44:00 +0200

    Lua: Pass correct parameter types to "turn_begin" signal
    As parameters were not passed as 'lua_Integer' type, 'turn' was
    garbage in 32bit systems, 'year' in all systems.
    This affects many existing ruleset scripts in 32bit systems, where
    welcome message is supposed to be shown on turn 1.
    
    See osdn #41662
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/srv_main.c
+++ server/srv_main.c
@@ -42,6 +42,9 @@
 #include <unistd.h>
 #endif
 
+/* dependencies/lua */
+#include "lua.h" /* lua_Integer */
+
 /* utility */
 #include "astring.h"
 #include "bitvector.h"
@@ -1133,7 +1136,9 @@ static void begin_turn(bool is_new_turn)
   send_game_info(NULL);
 
   if (is_new_turn) {
-    script_server_signal_emit("turn_begin", game.info.turn, game.info.year32);
+    script_server_signal_emit("turn_begin",
+                              (lua_Integer)game.info.turn,
+                              (lua_Integer)game.info.year32);
     script_server_signal_emit("turn_started",
                               game.info.turn > 0 ? game.info.turn - 1
                               : game.info.turn, game.info.year32);
