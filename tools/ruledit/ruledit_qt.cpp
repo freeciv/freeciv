ca223ced44ddd0c72868c4e41c9d2c81ebc885b7
Marko Lindqvist <cazfi74@gmail.com>
2022-02-20 05:57:55 +0200

    Ruledit: Set window icon
    See osdn #43553
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tools/ruledit/ruledit_qt.cpp
+++ tools/ruledit/ruledit_qt.cpp
@@ -428,7 +428,13 @@ void ruledit_gui::refresh_effect_edits()
 **************************************************************************/
 ruledit_main::ruledit_main(QApplication *qapp_in, QWidget *central_in) : QMainWindow()
 {
+  QPixmap *pm = new QPixmap;
+  const char *full_icon_path = fileinfoname(get_data_dirs(), "freeciv-ruledit.png");
   const QString title = QString::fromUtf8(R__("Freeciv Ruleset Editor"));
+  const QString fip = QString(full_icon_path);
+
+  pm->load(fip);
+  qapp_in->setWindowIcon(QIcon(*pm));
 
   qapp = qapp_in;
   central = central_in;
