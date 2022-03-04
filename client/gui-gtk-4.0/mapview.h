858bb7ad7cea895685d5235b8bbe0155ac53ef1e
Marko Lindqvist <cazfi74@gmail.com>
2022-03-04 19:05:45 +0200

    gtk4: Detect map canvas size changes
    Listen to "resize" signal instead of "size-allocate" that does not
    exist in gtk4.
    
    See osdn #44024
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-gtk-4.0/mapview.h
+++ client/gui-gtk-4.0/mapview.h
@@ -35,8 +35,8 @@ void overview_canvas_draw(GtkDrawingArea *w, cairo_t *cr,
                           int width, int height, gpointer data);
 void map_canvas_draw(GtkDrawingArea *w, cairo_t *cr,
                      int width, int height, gpointer data);
-void map_canvas_configure(GtkWidget *w, GdkRectangle *allocation,
-                          gpointer data);
+void map_canvas_resize(GtkWidget *w, int width, int height,
+                       gpointer data);
 
 void put_unit_image(struct unit *punit, GtkImage *p, int height);
 
