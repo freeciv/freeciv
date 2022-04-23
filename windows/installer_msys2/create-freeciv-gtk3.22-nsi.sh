f2e1da4639e9fdd9539eb82fa28914fbf8144850
Marko Lindqvist <cazfi74@gmail.com>
2022-04-23 11:07:34 +0300

    Msys2: Document arch parameter of create-freeciv-<gui>-nsi.sh
    See osdn #43620
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- windows/installer_msys2/create-freeciv-gtk3.22-nsi.sh
+++ windows/installer_msys2/create-freeciv-gtk3.22-nsi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# ./create-freeciv-gtk3.22-nsi.sh <Freeciv files directory> <version>
+# ./create-freeciv-gtk3.22-nsi.sh <Freeciv files directory> <version> <win32|win64|win>
 
 ./create-freeciv-gtk-qt-nsi.sh $1 $2 "gtk3.22" "GTK+3.22" $3 "gtk3"
--- windows/installer_msys2/create-freeciv-qt-nsi.sh
+++ windows/installer_msys2/create-freeciv-qt-nsi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# ./create-freeciv-qt-nsi.sh <Freeciv files directory> <version>
+# ./create-freeciv-qt-nsi.sh <Freeciv files directory> <version> <win32|win64|win>
 
 ./create-freeciv-gtk-qt-nsi.sh $1 $2 "qt" "Qt" $3
