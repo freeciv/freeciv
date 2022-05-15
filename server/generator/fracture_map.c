f22d066ad9afc79c0ddea5f664cffcd0ed0bad94
Marko Lindqvist <cazfi74@gmail.com>
2022-05-15 07:32:27 +0300

    local_ave_elevation(): Fix clang analyzer div by zero warning
    See osdn #44569
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/generator/fracture_map.c
+++ server/generator/fracture_map.c
@@ -260,8 +260,8 @@ static void fmfill(int x, int y, int c, int r)
 }
 
 /**************************************************************************
-    Determine the local average elevation. Used to determine where hills
-    and mountains are. 
+  Determine the local average elevation. Used to determine where hills
+  and mountains are.
 **************************************************************************/
 static int local_ave_elevation(struct tile *ptile)
 {
@@ -273,7 +273,10 @@ static int local_ave_elevation(struct tile *ptile)
     ele = ele + hmap(tile2);
     n++;
   } square_iterate_end;
-  ele /= n;
+
+  if (ele > 0) { /* Avoids div by zero, as means also that n > 0 */
+    ele /= n;
+  }
 
   return ele;
 }
