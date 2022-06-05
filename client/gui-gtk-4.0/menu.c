899c15c774baa2e43d6ed930d9de5c13c9c84b32
Marko Lindqvist <cazfi74@gmail.com>
2022-06-05 07:10:55 +0300

    Comment typofix: "constrution" -> "construction"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-gtk-4.0/menu.c
+++ client/gui-gtk-4.0/menu.c
@@ -2010,7 +2010,7 @@ GtkWidget *setup_menus(GtkWidget *window)
 
 #ifdef MENUS_GTK3
 /************************************************************************//**
-  Find menu entry constrution data
+  Find menu entry construction data
 ****************************************************************************/
 static struct menu_entry_info *menu_entry_info_find(const char *key)
 {
