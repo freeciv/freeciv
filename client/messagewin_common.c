86b367b470ebf84c170475e229eb3e175f81a825
Marko Lindqvist <cazfi74@gmail.com>
2022-07-02 15:32:01 +0300

    Replace strncat() call with a fc_strlcat() one
    See osdn #44919
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/messagewin_common.c
+++ client/messagewin_common.c
@@ -96,7 +96,7 @@ void meswin_add(const char *message, const struct text_tag_list *tags,
 
   nspc = min_msg_len - strlen(s);
   if (nspc > 0) {
-    strncat(s, "                                                  ", nspc);
+    fc_strlcat(s, "                                                  ", min_msg_len);
   }
 
   messages[messages_total].tile = ptile;
