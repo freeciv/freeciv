85abc999ea7ddff5d1bec59f97c8290feaedd801
Marko Lindqvist <cazfi74@gmail.com>
2022-07-30 10:15:41 +0300

    meson-winbuild.sh: Explicitly disable readline
    See osdn #45137
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- windows/installer_cross/meson-winbuild.sh
+++ windows/installer_cross/meson-winbuild.sh
@@ -124,9 +124,18 @@ export PKG_CONFIG_PATH=${DLLSPATH}/lib/pkgconfig
 
 export PATH="$DLLSPATH/linux/libexec:$PATH"
 
-if ! meson --cross-file=cross.txt -Dprefix=$MESON_INSTALL_DIR -Dclients=$CLIENT -Dfcmp=$FCMP \
-           ${NLS} -Dsyslua=false -Dmwand=false -Druledit=$RULEDIT $QTPARAMS \
-           ../../.. $EXTRA_CONFIG ; then
+if ! meson \
+     --cross-file=cross.txt \
+     -Dprefix="$MESON_INSTALL_DIR" \
+     -Dclients="$CLIENT" -Dfcmp="$FCMP" \
+     ${NLS} \
+     -Dsyslua=false \
+     -Dmwand=false \
+     -Dreadline=false \
+     -Druledit="$RULEDIT" \
+     $QTPARAMS \
+     $EXTRA_CONFIG \
+     ../../.. ; then
   echo "Meson run failed!" >&2
   exit 1
 fi
