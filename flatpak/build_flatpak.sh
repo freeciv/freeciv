d25cf7ca2be92cfae6873f02443c77003746537c
Marko Lindqvist <cazfi74@gmail.com>
2022-08-05 05:11:40 +0300

    flatpak: Build modpack installer flatpak
    See osdn #45104
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- flatpak/build_flatpak.sh
+++ flatpak/build_flatpak.sh
@@ -3,8 +3,10 @@
 FCVER=$(../fc_version)
 
 if ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.gtk322.yml ||
+   ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.mp.gtk3.yml ||
    ! flatpak build-update-repo repo ||
-   ! flatpak build-bundle repo "freeciv-gtk3.22-${FCVER}.flatpak" org.freeciv.gtk322
+   ! flatpak build-bundle repo "freeciv-gtk3.22-${FCVER}.flatpak" org.freeciv.gtk322 ||
+   ! flatpak build-bundle repo "freeciv-mp-gtk3-${FCVER}.flatpak" org.freeciv.mp.gtk3
 then
   echo "FAILURE" >&2
   exit 1
