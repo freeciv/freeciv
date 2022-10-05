f0d4e14e73ccd29d3ea2ebb92d0f9fa45edd2e39
Marko Lindqvist <cazfi74@gmail.com>
2022-10-06 02:30:04 +0300

    gtk3.22: popup_theme_suggestion_dialog() fallback to correct default
    gtk3.22-client was using gtk3-client's default theme
    instead of its own.
    
    See osdn #45780
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-gtk-3.22/theme_dlg.c
+++ client/gui-gtk-3.22/theme_dlg.c
@@ -53,7 +53,7 @@ bool popup_theme_suggestion_dialog(const char *theme_name)
     /* gui option default_theme_name is not yet set.
      * This can happen when we load tileset requested at command line and
      * user has not saved theme information to .freeciv-client-rc.A.B. */
-    current_name = FC_GTK3_DEFAULT_THEME_NAME;
+    current_name = FC_GTK3_22_DEFAULT_THEME_NAME;
   }
 
   dialog = gtk_dialog_new_with_buttons(_("Theme suggested"),
