57c650c2cd34eae7cceb08be685c055b02aae3bb
Marko Lindqvist <cazfi74@gmail.com>
2022-10-08 02:19:31 +0300

    gtk: Add tooltip to nationdlg columns
    See osdn #45512
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-gtk-2.0/plrdlg.c
+++ client/gui-gtk-2.0/plrdlg.c
@@ -64,6 +64,9 @@ static GtkListStore *players_dialog_store;
 #define PLR_DLG_COL_STYLE       (0 + num_player_dlg_columns)
 #define PLR_DLG_COL_WEIGHT      (1 + num_player_dlg_columns)
 #define PLR_DLG_COL_ID          (2 + num_player_dlg_columns)
+#define PLR_DLG_COL_TOOLTIP     (3 + num_player_dlg_columns)
+
+#define PLR_DLG_COL_TOTAL       (PLR_DLG_COL_TOOLTIP + 1)
 
 static void create_players_dialog(void);
 static void players_meet_callback(GtkMenuItem *item, gpointer data);
@@ -253,7 +256,7 @@ static gint plrdlg_sort_func(GtkTreeModel *model,
 static GtkListStore *players_dialog_store_new(void)
 {
   GtkListStore *store;
-  GType model_types[num_player_dlg_columns + 3];
+  GType model_types[num_player_dlg_columns + PLR_DLG_COL_TOTAL];
   int i;
 
   for (i = 0; i < num_player_dlg_columns; i++) {
@@ -273,10 +276,12 @@ static GtkListStore *players_dialog_store_new(void)
       break;
     }
   }
-  /* special (invisible rows) - Text style, weight and player id */
+
+  /* Special (invisible rows) - Text style, weight and player id */
   model_types[i++] = G_TYPE_INT;        /* PLR_DLG_COL_STYLE. */
   model_types[i++] = G_TYPE_INT;        /* PLR_DLG_COL_WEIGHT. */
   model_types[i++] = G_TYPE_INT;        /* PLR_DLG_COL_ID. */
+  model_types[i++] = G_TYPE_STRING;     /* PLR_DLG_COL_TOOLTIP */
 
   store = gtk_list_store_newv(i, model_types);
 
@@ -376,22 +381,22 @@ static GtkWidget* create_show_menu(void)
 {
   int i;
   GtkWidget *menu = gtk_menu_new();
-  GtkWidget *item;    
-  
-  /* index starting at one (1) here to force playername to always be shown */
+  GtkWidget *item;
+
+  /* Index starting at one (1) here to force playername to always be shown */
   for (i = 1; i < num_player_dlg_columns; i++) {
     struct player_dlg_column *pcol;
-    
+
     pcol = &player_dlg_columns[i];
     item = gtk_check_menu_item_new_with_label(pcol->title);
     gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(item), pcol->show);
     gtk_menu_shell_append(GTK_MENU_SHELL(menu), item);
     g_signal_connect(item, "toggled", G_CALLBACK(toggle_view), pcol);
   }
-  
+
   item = gtk_separator_menu_item_new();
   gtk_menu_shell_append(GTK_MENU_SHELL(menu), item);
-  
+
   item = gtk_check_menu_item_new_with_label(Q_("?show:Dead Players"));
   gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(item),
                                  gui_options.player_dlg_show_dead_players);
@@ -426,13 +431,15 @@ void create_players_dialog(void)
   players_list = gtk_tree_view_new_with_model(GTK_TREE_MODEL
                                               (players_dialog_store));
   g_object_unref(players_dialog_store);
+  gtk_tree_view_set_tooltip_column(GTK_TREE_VIEW(players_list),
+                                   PLR_DLG_COL_TOOLTIP);
   gtk_widget_set_name(players_list, "small_font");
 
   players_selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(players_list));
   g_signal_connect(players_selection, "changed",
-      G_CALLBACK(selection_callback), NULL);
+                   G_CALLBACK(selection_callback), NULL);
   g_signal_connect(players_list, "button_press_event",
-      G_CALLBACK(button_press_callback), NULL);
+                   G_CALLBACK(button_press_callback), NULL);
 
   for (i = 0; i < num_player_dlg_columns; i++) {
     struct player_dlg_column *pcol;
@@ -486,7 +493,7 @@ void create_players_dialog(void)
       gtk_tree_view_column_set_alignment(col, 1.0);
       break;
     }
-    
+
     if (col) {
       gtk_tree_view_append_column(GTK_TREE_VIEW(players_list), col);
     }
@@ -497,16 +504,16 @@ void create_players_dialog(void)
 
   sw = gtk_scrolled_window_new(NULL, NULL);
   gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw),
-				      GTK_SHADOW_ETCHED_IN);
+                                      GTK_SHADOW_ETCHED_IN);
   gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw),
-		                 GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
+                                 GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
   gtk_container_add(GTK_CONTAINER(sw), players_list);
 
   gtk_box_pack_start(GTK_BOX(players_dialog_shell->vbox), sw,
-		     TRUE, TRUE, 0);
+                     TRUE, TRUE, 0);
 
   vbox = gtk_vbox_new(FALSE, 0);
-  
+
   sep = gtk_hseparator_new();
   gtk_box_pack_start(GTK_BOX(vbox), sep, FALSE, FALSE, 0);
 
@@ -654,7 +661,10 @@ static void fill_row(GtkListStore *store, GtkTreeIter *it,
     }
   }
 
-   /* now add some eye candy ... */
+  gtk_list_store_set(store, it, PLR_DLG_COL_TOOLTIP,
+                     score_tooltip(pplayer, pplayer->score.game), -1);
+
+   /* Now add some eye candy ... */
   if (client_has_player()) {
     switch (player_diplstate_get(client_player(), pplayer)->type) {
     case DS_WAR:
