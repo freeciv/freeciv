4cdb468a9a84a8707b8e6d455b6105660df04923
Marko Lindqvist <cazfi74@gmail.com>
2022-10-09 12:31:15 +0300

    flatpak: Add Qt-client flatpak
    See osdn #45814
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- flatpak/build_flatpak.sh
+++ flatpak/build_flatpak.sh
@@ -4,9 +4,11 @@ FCVER=$(../fc_version)
 
 if ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.gtk322.yml ||
    ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.mp.gtk3.yml ||
+   ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.qt.yml ||
    ! flatpak build-update-repo repo ||
    ! flatpak build-bundle repo "freeciv-gtk3.22-${FCVER}.flatpak" org.freeciv.gtk322 ||
-   ! flatpak build-bundle repo "freeciv-mp-gtk3-${FCVER}.flatpak" org.freeciv.mp.gtk3
+   ! flatpak build-bundle repo "freeciv-mp-gtk3-${FCVER}.flatpak" org.freeciv.mp.gtk3 ||
+   ! flatpak build-bundle repo "freeciv-qt-${FCVER}.flatpak" org.freeciv.qt
 then
   echo "FAILURE" >&2
   exit 1
