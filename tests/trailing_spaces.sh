ba9b9cabab8cd1d49300cda4cd804b8e3080d283
Marko Lindqvist <cazfi74@gmail.com>
2022-10-24 01:58:59 +0300

    trailing_spaces.sh: Check more files
    Added "*.lua", "*.modpack", "*.serv", and "*.txt"
    There already was "README*" but it didn't catch all .txt cases
    
    See osdn #45804
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tests/trailing_spaces.sh
+++ tests/trailing_spaces.sh
@@ -3,6 +3,10 @@
 files=$(find $1 -name "*.ruleset" \
              -o -name "*.tileset" \
              -o -name "*.spec" \
+             -o -name "*.lua" \
+             -o -name "*.modpack" \
+             -o -name "*.serv" \
+             -o -name "*.txt" \
              -o -name "README*" | sort)
 
 echo "# Check for trailing spaces:"
