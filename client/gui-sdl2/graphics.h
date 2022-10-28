a3cc9ade101eb2d8c746c419a2f418e272f5edae
Marko Lindqvist <cazfi74@gmail.com>
2022-10-28 21:20:40 +0300

    sdl2: Do error checking in set_video_mode()
    See osdn #46000
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-sdl2/graphics.h
+++ client/gui-sdl2/graphics.h
@@ -278,7 +278,7 @@ void create_line(SDL_Surface *dest, Sint16 x0, Sint16 y0, Sint16 x1, Sint16 y1,
 /* SDL */
 void init_sdl(int f);
 void quit_sdl(void);
-int set_video_mode(int width, int height, int flags);
+bool set_video_mode(unsigned width, unsigned height, unsigned flags);
 
 void update_main_screen(void);
 
