82b6d9978f7c7310382b604685855305a5424383
Marko Lindqvist <cazfi74@gmail.com>
2022-10-29 04:00:42 +0300

    Qt: Replace magic '0' with A_NONE
    See osdn #45530
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-qt/citydlg.cpp
+++ client/gui-qt/citydlg.cpp
@@ -3915,7 +3915,7 @@ QString get_tooltip_unit(const struct unit_type *utype, bool ext)
   if (obsolete) {
     tech = obsolete->require_advance;
     obsolete_str = QString("</td></tr><tr><td colspan=\"3\">");
-    if (tech && tech != advance_by_number(0)) {
+    if (tech && tech != advance_by_number(A_NONE)) {
       /* TRANS: this and nearby literal strings are interpreted
        * as (Qt) HTML */
       obsolete_str = obsolete_str + QString(_("Obsoleted by %1 (%2)."))
