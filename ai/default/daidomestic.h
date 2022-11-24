8f66bb93fe7a099d7669b6714a0450e1e380d1ba
Marko Lindqvist <cazfi74@gmail.com>
2022-11-24 02:50:24 +0200

    Correct name of daicity.c in comments
    Several comments referred to it as 'aicity.c'
    
    See osdn #46105
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- ai/default/daidomestic.h
+++ ai/default/daidomestic.h
@@ -16,10 +16,11 @@
 /* common */
 #include "fc_types.h"
 
-struct adv_choice *domestic_advisor_choose_build(struct ai_type *ait, struct player *pplayer,
+struct adv_choice *domestic_advisor_choose_build(struct ai_type *ait,
+                                                 struct player *pplayer,
                                                  struct city *pcity);
 
 void dai_wonder_city_distance(struct ai_type *ait, struct player *pplayer, 
                               struct adv_data *adv);
 
-#endif  /* FC__DAIDOMESTIC_H */
+#endif /* FC__DAIDOMESTIC_H */
