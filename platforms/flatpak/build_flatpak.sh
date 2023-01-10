624a21aace3ef90fa8360ede0ad63e8e11dcdbba
Marko Lindqvist <cazfi74@gmail.com>
2023-01-10 23:22:30 +0200

    flatpak: Add gtk4-client flatpak
    See osdn #46509
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- platforms/flatpak/build_flatpak.sh
+++ platforms/flatpak/build_flatpak.sh
@@ -13,11 +13,13 @@
 
 FCVER=$(../../fc_version)
 
-if ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.gtk322.yml ||
+if ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.gtk4.yml ||
+   ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.gtk322.yml ||
    ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.mp.gtk3.yml ||
    ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.qt.yml ||
    ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.sdl2.yml ||
    ! flatpak build-update-repo repo ||
+   ! flatpak build-bundle repo "freeciv-gtk4-${FCVER}.flatpak" org.freeciv.gtk4 ||
    ! flatpak build-bundle repo "freeciv-gtk3.22-${FCVER}.flatpak" org.freeciv.gtk322 ||
    ! flatpak build-bundle repo "freeciv-mp-gtk3-${FCVER}.flatpak" org.freeciv.mp.gtk3 ||
    ! flatpak build-bundle repo "freeciv-qt-${FCVER}.flatpak" org.freeciv.qt ||
