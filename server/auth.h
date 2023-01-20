28d13f56dedeb76c481a1f4daccf8ae5307415bc
Marko Lindqvist <cazfi74@gmail.com>
2023-01-21 01:41:29 +0200

    auth.h: Remove declarations of non-existent functions
    See osdn #46410
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/auth.h
+++ server/auth.h
@@ -1,4 +1,4 @@
-/**********************************************************************
+/***********************************************************************
  Freeciv - Copyright (C) 2005 - M.C. Kaufman
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -13,6 +13,7 @@
 #ifndef FC__AUTH_H
 #define FC__AUTH_H
 
+/* utility */
 #include "shared.h"
 
 struct connection;
@@ -23,9 +24,5 @@ bool auth_handle_reply(struct connection *pconn, char *password);
 
 const char *auth_get_username(struct connection *pconn);
 const char *auth_get_ipaddr(struct connection *pconn);
-bool auth_set_password(struct connection *pconn, const char *password);
-const char *auth_get_password(struct connection *pconn);
-bool auth_set_salt(struct connection *pconn, int salt);
-int auth_get_salt(struct connection *pconn);
 
 #endif /* FC__AUTH_H */
