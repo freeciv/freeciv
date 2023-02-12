546fa9dfe2fe61a44800caea2b93fb9c0e0277a3
Marko Lindqvist <cazfi74@gmail.com>
2023-02-13 00:15:11 +0200

    flatpak: Add gtk4 modpack installer flatpak
    See osdn #47286
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- flatpak/build_flatpak.sh
+++ flatpak/build_flatpak.sh
@@ -15,12 +15,14 @@ FCVER=$(../fc_version)
 
 if ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.gtk4.yml ||
    ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.gtk322.yml ||
+   ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.mp.gtk4.yml ||
    ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.mp.gtk3.yml ||
    ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.qt.yml ||
    ! flatpak-builder --user --repo=repo --force-clean build org.freeciv.sdl2.yml ||
    ! flatpak build-update-repo repo ||
    ! flatpak build-bundle repo "freeciv-gtk4-${FCVER}.flatpak" org.freeciv.gtk4 ||
    ! flatpak build-bundle repo "freeciv-gtk3.22-${FCVER}.flatpak" org.freeciv.gtk322 ||
+   ! flatpak build-bundle repo "freeciv-mp-gtk4-${FCVER}.flatpak" org.freeciv.mp.gtk4 ||
    ! flatpak build-bundle repo "freeciv-mp-gtk3-${FCVER}.flatpak" org.freeciv.mp.gtk3 ||
    ! flatpak build-bundle repo "freeciv-qt-${FCVER}.flatpak" org.freeciv.qt ||
    ! flatpak build-bundle repo "freeciv-sdl2-${FCVER}.flatpak" org.freeciv.sdl2
