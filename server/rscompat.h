a877c4b405bfacc9162c3c662ce7760cf12dcf8a
Marko Lindqvist <cazfi74@gmail.com>
2023-02-26 14:26:48 +0200

    Correct RULESET_COMPAT_CAP
    See osdn #47490
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/rscompat.h
+++ server/rscompat.h
@@ -24,7 +24,7 @@ extern "C" {
 #include "ruleset.h"
 #include "settings.h"
 
-#define RULESET_COMPAT_CAP "+Freeciv-3.2-Devel-2022.Feb.02"
+#define RULESET_COMPAT_CAP "+Freeciv-ruleset-3.2-Devel-2022.Feb.02"
 
 struct rscompat_info
 {
