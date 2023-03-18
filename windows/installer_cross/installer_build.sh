619fa63afc6bfaf4f51e0cbc7d35a51c7e2617fd
Marko Lindqvist <cazfi74@gmail.com>
2023-03-18 02:33:48 +0200

    installer_build.sh: Add error handling for failed mkdir
    See osdn #46611
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- windows/installer_cross/installer_build.sh
+++ windows/installer_cross/installer_build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#/***********************************************************************
+# Freeciv - Copyright (C) 2017-2023
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2, or (at your option)
+#   any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#***********************************************************************/
 
 add_glib_env() {
   mkdir -p $2/share/glib-2.0/schemas &&
@@ -319,7 +332,10 @@ else
     fi
   fi
 
-  mkdir -p autotools/Output
+  if ! mkdir -p autotools/Output ; then
+    echo "Cannot create autotools/Output directory" >&2
+    exit 1
+  fi
   if ! makensis Freeciv-$SETUP-$VERREV-$GUI.nsi
   then
     echo "Creating installer failed!" >&2
