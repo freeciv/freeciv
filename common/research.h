403c292f8851d7a88df9679ae5bfea82538f1e5b
Marko Lindqvist <cazfi74@gmail.com>
2023-04-01 05:35:14 +0300

    gtk3(.22) research dialog: Set Future Tech to menu  immediately
    When last regular tech got researched, the old code listed only
    current research, which at that point was "None" in the
    research target menus. Make it Future Tech instead.
    
    See osdn #45434
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/research.h
+++ common/research.h
@@ -173,8 +173,10 @@ struct iterator *research_player_iter_init(struct research_player_iter *it,
 
 int recalculate_techs_researched(const struct research *presearch);
 
+bool research_future_next(const struct research *presearch);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
 
-#endif  /* FC__RESEARCH_H */
+#endif /* FC__RESEARCH_H */
