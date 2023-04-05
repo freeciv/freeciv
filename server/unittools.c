096a55ee7d04a571d4334ef7da93b465e40c1ebf
Marko Lindqvist <cazfi74@gmail.com>
2023-04-05 06:43:35 +0300

    Fix dead assignment of 'pcity' from unit_move()
    See osdn #47765
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/unittools.c
+++ server/unittools.c
@@ -3862,7 +3862,8 @@ bool unit_move(struct unit *punit, struct tile *pdesttile, int move_cost,
 {
   struct player *pplayer;
   struct tile *psrctile;
-  struct city *pcity;
+  struct city *psrccity;
+  struct city *pdestcity;
   struct unit *ptransporter;
   struct packet_unit_info src_info, dest_info;
   struct packet_unit_short_info src_sinfo, dest_sinfo;
@@ -4158,7 +4159,8 @@ bool unit_move(struct unit *punit, struct tile *pdesttile, int move_cost,
   /* Inform the owner's client about actor unit arrival. Can, depending on
    * the client settings, cause the client to start the process that makes
    * the action selection dialog pop up. */
-  if ((pcity = tile_city(pdesttile))) {
+  pdestcity = tile_city(pdesttile);
+  if (pdestcity != NULL) {
     /* Arrival in a city counts. */
 
     unit_move_data_list_iterate(plist, pmove_data) {
@@ -4231,13 +4233,13 @@ bool unit_move(struct unit *punit, struct tile *pdesttile, int move_cost,
   }
 
   unit_move_data_list_destroy(plist);
-
   /* Check cities at source and destination. */
-  if ((pcity = tile_city(psrctile))) {
-    refresh_dumb_city(pcity);
+  psrccity = tile_city(psrctile);
+  if (psrccity != NULL) {
+    refresh_dumb_city(psrccity);
   }
-  if ((pcity = tile_city(pdesttile))) {
-    refresh_dumb_city(pcity);
+  if (pdestcity != NULL) {
+    refresh_dumb_city(pdestcity);
   }
 
   if (unit_lives) {
