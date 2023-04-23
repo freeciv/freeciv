3730492746d5d3d247522700031e8a27e87f595c
Marko Lindqvist <cazfi74@gmail.com>
2023-04-24 00:03:34 +0300

    installer_cross: Use 64bit side of the Registry
    See osdn #47724
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- windows/installer_cross/create-freeciv-ruledit-nsi.sh
+++ windows/installer_cross/create-freeciv-ruledit-nsi.sh
@@ -190,10 +190,11 @@ EOF
 
 cat <<EOF
 ;--------------------------------
-;Installer Functions
+; Installer Functions
 
 Function .onInit
 
+  SetRegView 64
   !insertmacro MULTIUSER_INIT
 
 FunctionEnd
@@ -327,10 +328,11 @@ EOF
 
 cat <<EOF
 ;--------------------------------
-;Uninstaller Functions
+; Uninstaller Functions
 
 Function un.onInit
 
+  SetRegView 64
   !insertmacro MULTIUSER_UNINIT
 
 FunctionEnd
