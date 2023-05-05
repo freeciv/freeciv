9bec326a3321bccaeebc69615ddb065c4058b28f
Alain BKR <alain.baeckeroot@laposte.net>
2023-05-05 09:05:19 +0200

    Fix out of bounds on cargo iter
    See osdn #47982

--- common/unit.c
+++ common/unit.c
@@ -2448,11 +2448,11 @@ static void *cargo_iter_get(const struct iterator *it)
 static void cargo_iter_next(struct iterator *it)
 {
   struct cargo_iter *iter = CARGO_ITER(it);
-  const struct unit_list_link *piter = iter->links[iter->depth - 1];
+  const struct unit_list_link *piter;
   const struct unit_list_link *pnext;
 
   /* Variant 1: unit has cargo. */
-  pnext = unit_list_head(unit_transport_cargo(unit_list_link_data(piter)));
+  pnext = unit_list_head(unit_transport_cargo(cargo_iter_get(it)));
   if (NULL != pnext) {
     fc_assert(iter->depth < ARRAY_SIZE(iter->links));
     iter->links[iter->depth++] = pnext;
@@ -2460,6 +2460,8 @@ static void cargo_iter_next(struct iterator *it)
   }
 
   while (iter->depth > 0) {
+    piter = iter->links[iter->depth - 1];
+
     /* Variant 2: there are other cargo units at same level. */
     pnext = unit_list_link_next(piter);
     if (NULL != pnext) {
@@ -2468,7 +2470,7 @@ static void cargo_iter_next(struct iterator *it)
     }
 
     /* Variant 3: return to previous level, and do same tests. */
-    piter = iter->links[iter->depth-- - 2];
+    iter->depth--;
   }
 }
 
