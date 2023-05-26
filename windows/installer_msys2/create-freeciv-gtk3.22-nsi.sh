1118e83adba80571beb01bd0a76f89ae3f922cc0
Marko Lindqvist <cazfi74@gmail.com>
2023-05-27 01:10:04 +0300

    msys2: Rename "Output" as "output"
    See osdn #48041
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- windows/installer_msys2/create-freeciv-gtk3.22-nsi.sh
+++ windows/installer_msys2/create-freeciv-gtk3.22-nsi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# ./create-freeciv-gtk3.22-nsi.sh <Freeciv files dir> <Output dir> <version> <win32|win64|win>
+# ./create-freeciv-gtk3.22-nsi.sh <freeciv files dir> <output dir> <version> <win32|win64|win>
 
 ./create-freeciv-gtk-qt-nsi.sh "$1" "$2" "$3" "gtk3.22" "GTK+3.22" "$4" "gtk3"
