1c28e7239060b88248a7aa4e11dbad397299c6fe
Marko Lindqvist <cazfi74@gmail.com>
2023-06-08 17:35:10 +0300

    gtk: Make Small Font setting to take effect
    See osdn #48186
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/options.c
+++ client/options.c
@@ -710,7 +710,7 @@ bool option_reset(struct option *poption)
 }
 
 /************************************************************************//**
-  Set the function to call every time this option changes.  Can be NULL.
+  Set the function to call every time this option changes. Can be NULL.
 ****************************************************************************/
 void option_set_changed_callback(struct option *poption,
                                  void (*callback) (struct option *))
@@ -2843,12 +2843,12 @@ static struct client_option client_options[] = {
                   "Sans Italic 10", font_changed_callback),
   GEN_FONT_OPTION(gui_gtk3_22_font_small, "small_font",
                   N_("Small Font"),
-                  N_("This font is used for any small font request.  For "
+                  N_("This font is used for any small font request. For "
                      "example, it is used for display the building lists "
                      "in the city dialog, the Economy report or the Units "
                      "report."),
                   COC_FONT, GUI_GTK3_22,
-                  "Sans 9", NULL),
+                  "Sans 9", font_changed_callback),
   GEN_FONT_OPTION(gui_gtk3_22_font_comment_label, "comment_label",
                   N_("Comment Label"),
                   N_("This font is used to display comment labels, such as "
@@ -3064,12 +3064,12 @@ static struct client_option client_options[] = {
                   "Sans Italic 10", font_changed_callback),
   GEN_FONT_OPTION(gui_gtk4_font_small, "small_font",
                   N_("Small Font"),
-                  N_("This font is used for any small font request.  For "
+                  N_("This font is used for any small font request. For "
                      "example, it is used for display the building lists "
                      "in the city dialog, the Economy report or the Units "
                      "report."),
                   COC_FONT, GUI_GTK4,
-                  "Sans 9", NULL),
+                  "Sans 9", font_changed_callback),
   GEN_FONT_OPTION(gui_gtk4_font_comment_label, "comment_label",
                   N_("Comment Label"),
                   N_("This font is used to display comment labels, such as "
