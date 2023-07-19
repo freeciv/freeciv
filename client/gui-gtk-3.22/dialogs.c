b5aba4d455436f4c4b8fe9053651fa2f5f613631
Marko Lindqvist <cazfi74@gmail.com>
2023-07-19 21:16:55 +0300

    Qt: Fix non-fatal tileset error messages on startup
    All tileset errors, both fatal and non-fatal, were
    just crashing the client on startup.
    
    Fixed, so that:
    - Fatal errors show an error dialog before
      client closes.
    - Non-fatal won't crash, and error dialogs
      will be shown when client runs
    
    See osdn #47915
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-gtk-3.22/dialogs.c
+++ client/gui-gtk-3.22/dialogs.c
@@ -1533,22 +1533,24 @@ void show_tech_gained_dialog(Tech_type_id tech)
   Show tileset error dialog. It's blocking as client will
   shutdown as soon as this function returns.
 **************************************************************************/
-void show_tileset_error(const char *tset_name, const char *msg)
+void show_tileset_error(bool fatal, const char *tset_name, const char *msg)
 {
   if (is_gui_up()) {
     GtkWidget *dialog;
 
     if (tset_name != NULL) {
-      dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR,
-                                      GTK_BUTTONS_CLOSE,
+      dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_DESTROY_WITH_PARENT,
+                                      GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE,
                                       _("Tileset \"%s\" problem, "
-                                        "it's probably incompatible with the ruleset:\n%s"),
+                                        "it's probably incompatible with "
+                                        "the ruleset:\n%s"),
                                       tset_name, msg);
     } else {
-      dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR,
-                                      GTK_BUTTONS_CLOSE,
+      dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_DESTROY_WITH_PARENT,
+                                      GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE,
                                       _("Tileset problem, "
-                                        "it's probably incompatible with the ruleset:\n%s"),
+                                        "it's probably incompatible with "
+                                        "the ruleset:\n%s"),
                                       msg);
     }
 
