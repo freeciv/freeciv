322d2478059b2b29317347218cd29c9c7d876ae0
Marko Lindqvist <cazfi74@gmail.com>
2023-09-02 15:13:16 +0300

    Update to crosser-2.7
    See osdn #48552
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- windows/installer_cross/meson-winbuild.sh
+++ windows/installer_cross/meson-winbuild.sh
@@ -9,7 +9,7 @@
 
 MESON_WINBUILD_VERSION="3.1.0-beta"
 MIN_WINVER=0x0603 # Windows 8.1. Qt6-client and Qt6-ruledit builds override this
-CROSSER_FEATURE_LEVEL=2.6
+CROSSER_FEATURE_LEVEL=2.7
 
 if test "$1" = "" || test "$1" = "-h" || test "$1" = "--help" ; then
   echo "Usage: $0 <crosser dir> <gui>"
