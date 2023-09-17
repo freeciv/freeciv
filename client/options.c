c86be7ad992f605ec34680dc856b1257cff06e2f
Marko Lindqvist <cazfi74@gmail.com>
2023-09-17 06:50:12 +0300

    Comment typofix: "determin" -> "determine"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/options.c
+++ client/options.c
@@ -1475,7 +1475,7 @@ struct client_option {
  * ohelp: The help text for the client option.  Should be used with the N_()
  *        macro.
  * ocat:  The client_option_class of this client option.
- * ospec: A gui_type enumerator which determin for what particular client
+ * ospec: A gui_type enumerator which determine for what particular client
  *        gui this option is for. Sets to GUI_STUB for common options.
  * odef:  The default value of this client option (FALSE or TRUE).
  * ocb:   A callback function of type void (*)(struct option *) called when
@@ -1509,7 +1509,7 @@ struct client_option {
  * ohelp: The help text for the client option.  Should be used with the N_()
  *        macro.
  * ocat:  The client_option_class of this client option.
- * ospec: A gui_type enumerator which determin for what particular client
+ * ospec: A gui_type enumerator which determine for what particular client
  *        gui this option is for. Sets to GUI_STUB for common options.
  * odef:  The default value of this client option.
  * omin:  The minimal value of this client option.
@@ -1588,7 +1588,7 @@ struct client_option {
  * ohelp: The help text for the client option.  Should be used with the N_()
  *        macro.
  * ocat:  The client_option_class of this client option.
- * ospec: A gui_type enumerator which determin for what particular client
+ * ospec: A gui_type enumerator which determine for what particular client
  *        gui this option is for. Sets to GUI_STUB for common options.
  * odef:  The default string for this client option.
  * oacc:  The string accessor where to find the allowed values of type
@@ -1626,7 +1626,7 @@ struct client_option {
  * ohelp: The help text for the client option.  Should be used with the N_()
  *        macro.
  * ocat:  The client_option_class of this client option.
- * ospec: A gui_type enumerator which determin for what particular client
+ * ospec: A gui_type enumerator which determine for what particular client
  *        gui this option is for. Sets to GUI_STUB for common options.
  * odef:  The default value for this client option.
  * oacc:  The name accessor of type 'const struct copt_val_name * (*) (int)'.
@@ -1664,7 +1664,7 @@ struct client_option {
  * ohelp: The help text for the client option.  Should be used with the N_()
  *        macro.
  * ocat:  The client_option_class of this client option.
- * ospec: A gui_type enumerator which determin for what particular client
+ * ospec: A gui_type enumerator which determine for what particular client
  *        gui this option is for. Sets to GUI_STUB for common options.
  * odef:  The default value for this client option.
  * oacc:  The name accessor of type 'const struct copt_val_name * (*) (int)'.
@@ -1706,7 +1706,7 @@ struct client_option {
  * ohelp: The help text for the client option. Should be used with the N_()
  *        macro.
  * ocat:  The client_option_class of this client option.
- * ospec: A gui_type enumerator which determin for what particular client
+ * ospec: A gui_type enumerator which determine for what particular client
  *        gui this option is for. Sets to GUI_STUB for common options.
  * odef:  The default string for this client option.
  * ocb:   A callback function of type void (*)(struct option *) called when
@@ -1742,7 +1742,7 @@ struct client_option {
  * ohelp: The help text for the client option. Should be used with the N_()
  *        macro.
  * ocat:  The client_option_class of this client option.
- * ospec: A gui_type enumerator which determin for what particular client
+ * ospec: A gui_type enumerator which determine for what particular client
  *        gui this option is for. Sets to GUI_STUB for common options.
  * odef_fg, odef_bg:  The default values for this client option.
  * ocb:   A callback function of type void (*)(struct option *) called when
@@ -1777,7 +1777,7 @@ struct client_option {
  * ohelp: The help text for the client option.  Should be used with the N_()
  *        macro.
  * ocat:  The client_option_class of this client option.
- * ospec: A gui_type enumerator which determin for what particular client
+ * ospec: A gui_type enumerator which determine for what particular client
  *        gui this option is for. Sets to GUI_STUB for common options.
  * odef_width, odef_height:  The default values for this client option.
  * ocb:   A callback function of type void (*)(struct option *) called when
