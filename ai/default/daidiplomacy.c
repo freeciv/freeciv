c8365432bff59677ee9b43b6be3ede23c8a2c28e
Marko Lindqvist <cazfi74@gmail.com>
2023-09-21 09:47:43 +0300

    Comment typofix: "meaningfull" -> "meaningful"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- ai/default/daidiplomacy.c
+++ ai/default/daidiplomacy.c
@@ -2077,7 +2077,7 @@ void dai_incident(struct ai_type *ait, enum incident_type type,
     }
     break;
   case INCIDENT_LAST:
-    /* Assert that always fails, but with meaningfull message */
+    /* Assert that always fails, but with meaningful message */
     fc_assert(type != INCIDENT_LAST);
     break;
   }
