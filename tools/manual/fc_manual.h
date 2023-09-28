393f573588351738767f03969a0f0701f61ff281
Marko Lindqvist <cazfi74@gmail.com>
2023-09-28 23:09:46 +0300

    Split Settings manual creation to separate source file
    See osdn #48742
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

---
+++ tools/manual/fc_manual.h
@@ -0,0 +1,68 @@
+/***********************************************************************
+ Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+***********************************************************************/
+#ifndef FC__FC_MANUAL_H
+#define FC__FC_MANUAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include <stdio.h> /* FILE */
+
+/* utility */
+#include "support.h"
+
+
+enum manuals {
+  MANUAL_SETTINGS,
+  MANUAL_COMMANDS,
+  MANUAL_TERRAIN,
+  MANUAL_BUILDINGS,
+  MANUAL_WONDERS,
+  MANUAL_GOVS,
+  MANUAL_UNITS,
+  MANUAL_TECHS,
+  MANUAL_COUNT
+};
+
+struct tag_types {
+  const char *file_ext;
+  const char *header;
+  const char *title_begin;
+  const char *title_end;
+  const char *sect_title_begin;
+  const char *sect_title_end;
+  const char *image_begin;
+  const char *image_end;
+  const char *item_begin;
+  const char *item_end;
+  const char *subitem_begin;
+  const char *subitem_end;
+  const char *tail;
+  const char *hline;
+};
+
+/* Utility functions */
+FILE *manual_start(struct tag_types *tag_info, int manual_number);
+void manual_finalize(struct tag_types *tag_info, FILE *doc,
+                     const char *manual_name);
+char *html_special_chars(char *str, size_t *len);
+
+/* Individual manual pages */
+bool manual_settings(struct tag_types *tag_info);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* FC__FC_MANUAL_H */
