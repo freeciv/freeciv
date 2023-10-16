81b15776439b9b8b50321d35a3f27a862d605753
Marko Lindqvist <cazfi74@gmail.com>
2023-10-17 00:28:32 +0300

    Qt: Rename "Append to Battle Group x" menu entries
    Unify menu entry names with those of gtk clients.
    
    See osdn #48860
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-qt/menu.cpp
+++ client/gui-qt/menu.cpp
@@ -1590,7 +1590,7 @@ void mr_menu::setup_menus()
   act->setShortcut(QKeySequence(tr("Ctrl+F1")));
   connect(act, &QAction::triggered, this, &mr_menu::slot_bg1assign);
 
-  act = main_menu->addAction(_("Append Battle Group 1"));
+  act = main_menu->addAction(_("Append to Battle Group 1"));
   act->setShortcut(QKeySequence(tr("Ctrl+Shift+F1")));
   connect(act, &QAction::triggered, this, &mr_menu::slot_bg1append);
 
@@ -1602,7 +1602,7 @@ void mr_menu::setup_menus()
   act->setShortcut(QKeySequence(tr("Ctrl+F2")));
   connect(act, &QAction::triggered, this, &mr_menu::slot_bg2assign);
 
-  act = main_menu->addAction(_("Append Battle Group 2"));
+  act = main_menu->addAction(_("Append to Battle Group 2"));
   act->setShortcut(QKeySequence(tr("Ctrl+Shift+F2")));
   connect(act, &QAction::triggered, this, &mr_menu::slot_bg2append);
 
@@ -1614,7 +1614,7 @@ void mr_menu::setup_menus()
   act->setShortcut(QKeySequence(tr("Ctrl+F3")));
   connect(act, &QAction::triggered, this, &mr_menu::slot_bg3assign);
 
-  act = main_menu->addAction(_("Append Battle Group 3"));
+  act = main_menu->addAction(_("Append to Battle Group 3"));
   act->setShortcut(QKeySequence(tr("Ctrl+Shift+F3")));
   connect(act, &QAction::triggered, this, &mr_menu::slot_bg3append);
 
@@ -1626,7 +1626,7 @@ void mr_menu::setup_menus()
   act->setShortcut(QKeySequence(tr("Ctrl+F4")));
   connect(act, &QAction::triggered, this, &mr_menu::slot_bg4assign);
 
-  act = main_menu->addAction(_("Append Battle Group 4"));
+  act = main_menu->addAction(_("Append to Battle Group 4"));
   act->setShortcut(QKeySequence(tr("Ctrl+Shift+F4")));
   connect(act, &QAction::triggered, this, &mr_menu::slot_bg4append);
 
