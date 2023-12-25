4e084d45f8686fda157c57634a62719667ed5524
Marko Lindqvist <cazfi74@gmail.com>
2023-12-25 16:27:55 +0200

    Replace #!/bin/bash with posix shebang
    See RM #44
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- bootstrap/generate_metainfo.sh
+++ bootstrap/generate_metainfo.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 #/***********************************************************************
-# Freeciv - Copyright (C) 2022 The Freeciv project
+# Freeciv - Copyright (C) 2022-2023 The Freeciv project
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation; either version 2, or (at your option)
--- translations/stats.sh
+++ translations/stats.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #/**********************************************************************
 # Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
 #   This program is free software; you can redistribute it and/or modify
