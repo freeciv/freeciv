0e8b7fb8fee65ea06ca508822bdef199c9832bbc
Marko Lindqvist <cazfi74@gmail.com>
2024-01-05 12:58:34 +0200

    Comment typofix: "progres" -> "progress"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/control.c
+++ client/control.c
@@ -1025,7 +1025,7 @@ void set_units_in_combat(struct unit *pattacker, struct unit *pdefender)
 }
 
 /**********************************************************************//**
-  The action selection process is no longer in progres for the specified
+  The action selection process is no longer in progress for the specified
   unit. It is safe to let another unit enter action selection.
 **************************************************************************/
 void action_selection_no_longer_in_progress(const int old_actor_id)
