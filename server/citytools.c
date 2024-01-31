407b9ebe0a5ef45dcdfed49ab31eb46a096b7443
Marko Lindqvist <cazfi74@gmail.com>
2024-01-31 03:10:35 +0200

    Comment typofix: "as" -> "was"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/citytools.c
+++ server/citytools.c
@@ -1462,7 +1462,7 @@ void city_build_free_buildings(struct city *pcity)
         || (game.server.savepalace
             && improvement_has_flag(pimprove, IF_SAVE_SMALL_WONDER))) {
       city_add_improvement_with_gov_notice(pcity, pimprove,
-         /* TRANS: Reason that a building as added to the city.
+         /* TRANS: Reason that a building was added to the city.
           *        Building is given for free to player's first city. */
                                            Q_("?initbldg:Free %s"));
       if (is_small_wonder(pimprove)) {
@@ -1485,7 +1485,7 @@ void city_build_free_buildings(struct city *pcity)
         || (game.server.savepalace
             && improvement_has_flag(pimprove, IF_SAVE_SMALL_WONDER))) {
       city_add_improvement_with_gov_notice(pcity, pimprove,
-         /* TRANS: Reason that a building as added to the city.
+         /* TRANS: Reason that a building was added to the city.
           *        Building is given for free to player's first city. */
                                            Q_("?initbldg:Free %s"));
       if (is_small_wonder(pimprove)) {
