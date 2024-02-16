4e0ab165c48169196afc265c7b8ffa6c2df0e6c5
Marko Lindqvist <cazfi74@gmail.com>
2024-02-16 23:18:17 +0200

    Improve connecthand.[ch] coding style
    See RM #264
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/connecthand.h
+++ server/connecthand.h
@@ -1,4 +1,4 @@
-/**********************************************************************
+/***********************************************************************
  Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -13,8 +13,10 @@
 #ifndef FC__CONNECTHAND_H
 #define FC__CONNECTHAND_H
 
+/* utility */
 #include "support.h"            /* bool type */
 
+/* common */
 #include "fc_types.h"
 
 struct connection;
