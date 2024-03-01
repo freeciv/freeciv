ddb0d71fbf4eb703c0a9c077a7cc3d9b18e3a846
Marko Lindqvist <cazfi74@gmail.com>
2024-03-01 12:56:58 +0200

    Comment typofix: "classess" -> "classes'"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tools/ruleutil/comments.c
+++ tools/ruleutil/comments.c
@@ -175,7 +175,7 @@ void comment_buildings(struct section_file *sfile)
 }
 
 /**********************************************************************//**
-  Write tech classess header.
+  Write tech classes' header.
 **************************************************************************/
 void comment_tech_classes(struct section_file *sfile)
 {
