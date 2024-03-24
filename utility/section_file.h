f5c93c4bbdb352f4df18e87b5c075163a82653b5
Marko Lindqvist <cazfi74@gmail.com>
2024-03-24 22:06:53 +0200

    Comment typofix: "soruce" -> "source"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- utility/section_file.h
+++ utility/section_file.h
@@ -1,4 +1,4 @@
-/********************************************************************** 
+/***********************************************************************
  Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 #define FC__SECTION_FILE_H
 
 /* This header contains internals of section_file that its users should
- * not care about. This header should be included by soruce files implementing
+ * not care about. This header should be included by source files implementing
  * registry itself. */
 
 #ifdef __cplusplus
