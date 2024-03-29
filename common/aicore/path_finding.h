c6dbb03fa92338dea84b452412545a55e264d265
Marko Lindqvist <cazfi74@gmail.com>
2024-03-29 11:34:52 +0200

    Comment typofix: "prefering" -> "preferring"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/aicore/path_finding.h
+++ common/aicore/path_finding.h
@@ -311,7 +311,7 @@ enum tile_behavior {
   TB_DONT_LEAVE         /* Paths can lead _to_ such tile, but are not
                          * allowed to go _through_. This move cost is
                          * always evaluated to a constant single move cost,
-                         * prefering straight paths because we don't need
+                         * preferring straight paths because we don't need
                          * moves left to leave this tile. */
 };
 
