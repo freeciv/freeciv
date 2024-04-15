aceb37776ec3a3e3b09587f98683f1d6730e0061
Marko Lindqvist <cazfi74@gmail.com>
2024-04-16 00:15:14 +0300

    Trigger action system when client requests activity change
    Lacking action handling for some activities reported by bard
    
    See osdn #57670
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/unithand.c
+++ server/unithand.c
@@ -6540,8 +6540,18 @@ bool unit_activity_handling_targeted(struct unit *punit,
     unit_activity_handling(punit, new_activity);
   } else if (can_unit_do_activity_targeted(&(wld.map), punit,
                                            new_activity, *new_target)) {
+    struct action_list *list = action_list_by_activity(new_activity);
+
     free_unit_orders(punit);
-    unit_activity_targeted_internal(punit, new_activity, new_target);
+
+    if (list != NULL && action_list_size(list) > 0) {
+      /* Trigger action system */
+      unit_do_action(unit_owner(punit), punit->id, punit->tile->index,
+                     (*new_target) != NULL ? (*new_target)->id : NO_TARGET,
+                     "", action_number(action_list_get(list, 0)));
+    } else {
+      unit_activity_targeted_internal(punit, new_activity, new_target);
+    }
   }
 
   return TRUE;
