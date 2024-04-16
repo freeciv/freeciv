6acc400c60745b629871daca86c1dad5508c9d85
Marko Lindqvist <cazfi74@gmail.com>
2024-04-17 00:38:32 +0300

    Initialize extra before calling unit_assign_specific_activity_target()
    If the extra going in is not NULL, it's assumed to be ok to begin with,
    and no real extra gets assigned.
    
    See RM #460
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/actiontools.c
+++ server/actiontools.c
@@ -904,7 +904,7 @@ int action_sub_target_id_for_action(const struct action *paction,
   case ASTK_EXTRA_NOT_THERE:
     if (action_has_result(paction, ACTRES_PILLAGE)) {
       /* Special treatment for "Pillage" */
-      struct extra_type *pextra;
+      struct extra_type *pextra = NULL;
       enum unit_activity activity = actres_get_activity(paction->result);
 
       unit_assign_specific_activity_target(actor_unit, &activity, &pextra);
