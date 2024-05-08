353f3bd36b9bdb74380fa0ce4fd92b5771a0bc4b
Marko Lindqvist <cazfi74@gmail.com>
2024-05-09 02:55:55 +0300

    AI: Cache 'worker' by unit's possible actions
    Previously unit's workerness was determined by Settlers
    flag alone. Now consider also actions that it can do.
    Notably units that can't do other worker actions than
    Build Base are not considered (auto)workers.
    
    Requested by bard
    
    See osdn #57666
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/advisors/advruleset.c
+++ server/advisors/advruleset.c
@@ -104,7 +104,13 @@ void adv_units_ruleset_init(void)
       } unit_class_iterate_end;
     }
 
-    ptype->adv.worker = utype_has_flag(ptype, UTYF_SETTLERS);
+    ptype->adv.worker
+      = (utype_has_flag(ptype, UTYF_SETTLERS)
+         && (utype_can_do_action_result(ptype, ACTRES_CULTIVATE)
+             || utype_can_do_action_result(ptype, ACTRES_PLANT)
+             || utype_can_do_action_result(ptype, ACTRES_IRRIGATE)
+             || utype_can_do_action_result(ptype, ACTRES_MINE)
+             || utype_can_do_action_result(ptype, ACTRES_TRANSFORM_TERRAIN)));
   } unit_type_iterate_end;
 
   /* Initialize autosettlers actions */
