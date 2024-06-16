83cf1426c818eede4121e664a286170cf96cef0f
Marko Lindqvist <cazfi74@gmail.com>
2024-06-16 22:16:21 +0300

    Comment typofix: "sting" -> "string"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/actions.c
+++ common/actions.c
@@ -1320,7 +1320,7 @@ const char *action_prepare_ui_name(action_id act_id, const char *mnemonic,
   /* Format the info part of the action's UI name. */
   if (probtxt != NULL && custom != NULL) {
     /* TRANS: action UI name's info part with custom info and probability.
-     * Hint: you can move the paren handling from this sting to the action
+     * Hint: you can move the paren handling from this string to the action
      * names if you need to add extra information (like a mnemonic letter
      * that doesn't appear in the action UI name) to it. In that case you
      * must do so for all strings with this comment and for every action
@@ -1330,7 +1330,7 @@ const char *action_prepare_ui_name(action_id act_id, const char *mnemonic,
     astr_set(&chance, _(" (%s; %s)"), custom, probtxt);
   } else if (probtxt != NULL) {
     /* TRANS: action UI name's info part with probability.
-     * Hint: you can move the paren handling from this sting to the action
+     * Hint: you can move the paren handling from this string to the action
      * names if you need to add extra information (like a mnemonic letter
      * that doesn't appear in the action UI name) to it. In that case you
      * must do so for all strings with this comment and for every action
@@ -1340,7 +1340,7 @@ const char *action_prepare_ui_name(action_id act_id, const char *mnemonic,
     astr_set(&chance, _(" (%s)"), probtxt);
   } else if (custom != NULL) {
     /* TRANS: action UI name's info part with custom info.
-     * Hint: you can move the paren handling from this sting to the action
+     * Hint: you can move the paren handling from this string to the action
      * names if you need to add extra information (like a mnemonic letter
      * that doesn't appear in the action UI name) to it. In that case you
      * must do so for all strings with this comment and for every action
