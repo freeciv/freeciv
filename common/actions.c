ea8a7a2acb3567f86a3b5e049b81199baf85cd51
Marko Lindqvist <cazfi74@gmail.com>
2024-09-08 12:58:44 +0300

    Unhardcode wld.map from action_speculate_unit_on_city()
    See RM #839
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/actions.c
+++ common/actions.c
@@ -6452,7 +6452,8 @@ struct act_prob action_prob_unit_vs_tgt(const struct civ_map *nmap,
   performing the chosen action on the target city given the specified
   game state changes.
 **************************************************************************/
-struct act_prob action_speculate_unit_on_city(const action_id act_id,
+struct act_prob action_speculate_unit_on_city(const struct civ_map *nmap,
+                                              const action_id act_id,
                                               const struct unit *actor,
                                               const struct city *actor_home,
                                               const struct tile *actor_tile,
@@ -6463,7 +6464,6 @@ struct act_prob action_speculate_unit_on_city(const action_id act_id,
    * current position rather than on actor_tile. Maybe this function should
    * return ACTPROB_NOT_IMPLEMENTED when one of those is detected and no
    * other requirement makes the action ACTPROB_IMPOSSIBLE? */
-  const struct civ_map *nmap = &(wld.map);
 
   if (omniscient_cheat) {
     if (is_action_enabled_unit_on_city_full(nmap, act_id,
