41567aa96a80ffcca632ee4db407a4f060d7a79b
Marko Lindqvist <cazfi74@gmail.com>
2024-09-08 13:20:42 +0300

    Comment typofix: "Processess" -> "Processes"
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-qt/chatline.cpp
+++ client/gui-qt/chatline.cpp
@@ -461,7 +461,7 @@ void chatwdg::paintEvent(QPaintEvent *event)
 }
 
 /***********************************************************************//**
-  Processess history for chat
+  Processes history for chat
 ***************************************************************************/
 bool chatwdg::eventFilter(QObject *obj, QEvent *event)
 {
