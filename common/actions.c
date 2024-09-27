a245d5ccc0ae08e3d720d3b6a963b432812219b9
Marko Lindqvist <cazfi74@gmail.com>
2024-09-27 05:43:01 +0300

    Unhardcode wld.map from action_speculate_unit_on_extras()
    See RM #916
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/actions.c
+++ common/actions.c
@@ -4928,7 +4928,8 @@ action_speculate_unit_on_tile(const struct civ_map *nmap,
   specified, specific extra) given the specified game state changes.
 **************************************************************************/
 struct act_prob
-action_speculate_unit_on_extras(action_id act_id,
+action_speculate_unit_on_extras(const struct civ_map *nmap,
+                                action_id act_id,
                                 const struct unit *actor,
                                 const struct city *actor_home,
                                 const struct tile *actor_tile,
@@ -4940,7 +4941,6 @@ action_speculate_unit_on_extras(action_id act_id,
    * current position rather than on actor_tile. Maybe this function should
    * return ACTPROB_NOT_IMPLEMENTED when one of those is detected and no
    * other requirement makes the action ACTPROB_IMPOSSIBLE? */
-  const struct civ_map *nmap = &(wld.map);
 
   if (omniscient_cheat) {
     if (is_action_enabled_unit_on_extras_full(nmap, act_id,
