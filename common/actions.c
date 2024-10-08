d465436f9597a9a29ae2962d5cfe9c1e1b5523e0
Marko Lindqvist <cazfi74@gmail.com>
2024-10-08 19:29:35 +0300

    Unhardcode wld.map from action_speculate_unit_on_self()
    See RM #940
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/actions.c
+++ common/actions.c
@@ -5739,7 +5739,8 @@ action_speculate_unit_on_extras(const struct civ_map *nmap,
   changes.
 **************************************************************************/
 struct act_prob
-action_speculate_unit_on_self(action_id act_id,
+action_speculate_unit_on_self(const struct civ_map *nmap,
+                              action_id act_id,
                               const struct unit *actor,
                               const struct city *actor_home,
                               const struct tile *actor_tile,
@@ -5749,8 +5750,6 @@ action_speculate_unit_on_self(action_id act_id,
    * current position rather than on actor_tile. Maybe this function should
    * return ACTPROB_NOT_IMPLEMENTED when one of those is detected and no
    * other requirement makes the action ACTPROB_IMPOSSIBLE? */
-  const struct civ_map *nmap = &(wld.map);
-
   if (omniscient_cheat) {
     if (is_action_enabled_unit_on_self_full(nmap, act_id,
                                             actor, actor_home, actor_tile)) {
