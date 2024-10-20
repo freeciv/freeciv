c465866c8436240881d570b50996c05e936ec9a5
Marko Lindqvist <cazfi74@gmail.com>
2024-10-20 22:04:20 +0300

    sdl3: Replace SDL_TRUE with true
    See RM #987
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-sdl3/gui_string.c
+++ client/gui-sdl3/gui_string.c
@@ -378,7 +378,7 @@ static SDL_Surface *create_utf8_multi_surf(utf8_str *pstr)
   case 1:
     text = create_surf(w, count * tmp[0]->h);
     SDL_FillSurfaceRect(text, NULL, color);
-    SDL_SetSurfaceColorKey(text, SDL_TRUE, color);
+    SDL_SetSurfaceColorKey(text, true, color);
     break;
   case 2:
     text = create_surf_with_format(tmp[0]->format,
