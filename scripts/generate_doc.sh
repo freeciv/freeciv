e4f3189ecd596bf4e0bfa44733b6cdeeb4f37e92
Marko Lindqvist <cazfi74@gmail.com>
2024-10-25 01:52:49 +0300

    generate_doc.sh: Sanity check parameter
    See RM #1017
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- scripts/generate_doc.sh
+++ scripts/generate_doc.sh
@@ -11,6 +11,21 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 
-. $1/fc_version
+if test "$1" = "" ; then
+  echo "Needs freeciv source directory as a parameter!" >&2
+  exit 1
+fi
+
+if test "$1" = "-h" || test "$1" = "--help" ; then
+  echo "Usage: $0 <freeciv source directory>"
+  exit
+fi
+
+if ! test -x "$1/fc_version" ; then
+  echo "\"$1\" doesn't look like a freeciv source directory (lacking fc_version)" >&2
+  exit 1
+fi
+
+. "$1/fc_version"
 
 doxy_srcdir="$1/" doxy_version="-${MAIN_VERSION}" doxygen "$1/doc/freeciv.doxygen"
