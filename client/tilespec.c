a3c453ea6a6014dbdbd1dc3fa7d9b83f7a051187
Marko Lindqvist <cazfi74@gmail.com>
2024-11-16 04:08:45 +0200

    Bump tilespec capstr
    See RM #1126
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/tilespec.c
+++ client/tilespec.c
@@ -79,7 +79,7 @@
 
 #include "tilespec.h"
 
-#define TILESPEC_CAPSTR "+Freeciv-tilespec-3.2-Devel-2022-Jun-21 duplicates_ok"
+#define TILESPEC_CAPSTR "+Freeciv-tilespec-3.3-Devel-2024-Nov-16 duplicates_ok"
 /*
  * Tilespec capabilities acceptable to this program:
  *
