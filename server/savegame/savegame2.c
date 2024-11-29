796c60dd429eb8870e41b295a6c2a3516b5c7017
Marko Lindqvist <cazfi74@gmail.com>
2024-11-29 22:44:35 +0200

    savegame2.c: Load map.random_seed
    Reported by Molo_Parko
    
    See RM #1166
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/savegame/savegame2.c
+++ server/savegame/savegame2.c
@@ -2182,6 +2182,11 @@ static void sg_load_map(struct loaddata *loading)
   wld.map.server.have_huts
     = secfile_lookup_bool_default(loading->file, TRUE, "map.have_huts");
 
+  /* Savegame may have stored random_seed for documentation purposes only,
+   * but we want to keep it for resaving. */
+  wld.map.server.seed
+    = secfile_lookup_int_default(loading->file, 0, "map.random_seed");
+
   if (S_S_INITIAL == loading->server_state
       && MAPGEN_SCENARIO == wld.map.server.generator) {
     /* Generator MAPGEN_SCENARIO is used;
