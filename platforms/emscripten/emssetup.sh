d25f689a9168ba652f8044bc5bb2d87c0d47fc79
Marko Lindqvist <cazfi74@gmail.com>
2024-12-18 17:20:17 +0200

    emssetup.sh: Add
    Move emscripten environment setup functionality from
    ci-build.sh to a script of its own. This new script
    is included in the release tarball, for making
    emscipten builds easier from tarball.
    
    See RM #1193
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

---
+++ platforms/emscripten/emssetup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# emssetup.sh: Setup emscripten environment for freeciv build
+#
+# (c) 2024 Freeciv team
+#
+# This script is licensed under Gnu General Public License version 2 or later.
+# See COPYING available from the same location you got this script.
+
+# https://github.com/emscripten-core/emscripten/blob/main/ChangeLog.md
+EMSDK_VER=3.1.67
+
+if test "$1" = "" || test "$1" = "-h" || test "$1" = "--help" ; then
+  echo "Usage: $0 <target directory>"
+  exit 1
+fi
+
+if test -e "$1" ; then
+  echo "$1 exist already!" >&2
+  exit 1
+fi
+
+if ! git clone https://github.com/emscripten-core/emsdk "$1" ; then
+  echo "Failed to clone https://github.com/emscripten-core/emsdk" >&2
+  exit 1
+fi
+
+if ! cd "$1" ||
+   ! ./emsdk install "${EMSDK_VER}" ||
+   ! ./emsdk activate "${EMSDK_VER}"
+then
+  echo "emsdk setup failed!" >&2
+  exit 1
+fi
