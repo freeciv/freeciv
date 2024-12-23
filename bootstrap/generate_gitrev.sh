80004308fac009641b2749fc8d944270d9fbadfe
Marko Lindqvist <cazfi74@gmail.com>
2024-12-23 23:13:47 +0200

    generate_gitrev.sh: Do not require tail nor wc
    See RM #1199
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- bootstrap/generate_gitrev.sh
+++ bootstrap/generate_gitrev.sh
@@ -22,9 +22,7 @@ REV2="dist"
  # Check that all commands required by this script are available
  # If not, we will not claim to know which git revision this is
  # (REVSTATE will be OFF)
- if command -v git >/dev/null &&
-    command -v tail >/dev/null &&
-    command -v wc >/dev/null ; then
+ if command -v git >/dev/null ; then
    REVTMP="$(git rev-parse --short HEAD 2>/dev/null)"
    if test "x$REVTMP" != "x" ; then
      # This is git repository. Check for local modifications
