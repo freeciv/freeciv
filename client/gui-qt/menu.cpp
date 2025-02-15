e019e93dedded5064d7726f3403a7749b229228e
Dino <macwiz23@comcast.net>
2025-02-14 23:53:11 -0500

    Qt: Clear message window between games
    See RM #1239

--- client/gui-qt/menu.cpp
+++ client/gui-qt/menu.cpp
@@ -3913,6 +3913,7 @@ void mr_menu::back_to_menu()
 
     connect(ask, &hud_message_box::accepted, [=]() {
       if (client.conn.used) {
+        gui()->infotab->msgwdg->clr();
         disconnect_from_server(TRUE);
       }
     });
