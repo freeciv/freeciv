558d7f218a9979170cbae6fc9e88552e54758ebb
Marko Lindqvist <cazfi74@gmail.com>
2025-03-21 04:01:51 +0200

    tests: Remove x-prefixing
    See RM #1259
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tests/check_macros.sh
+++ tests/check_macros.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
+#/***********************************************************************
+# Freeciv - Copyright (C) 2011-2025
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2, or (at your option)
+#   any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#***********************************************************************/
 
 files=`find $1 -name "*.c" -o -name "*.cpp" -o -name "*.h" | sort`
 
 echo "# check for __LINE__ (should be replaced by __FC_LINE__):"
 for file in $files; do
   COUNT=`cat $file | grep _LINE__ | grep -v __FC_LINE__ | wc -l`
-  if [ "x$COUNT" != "x0" ]; then
+  if [ "${COUNT}" != "0" ]; then
     echo $file
   fi
 done
--- tests/fcintl.sh
+++ tests/fcintl.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+#/***********************************************************************
+# Freeciv - Copyright (C) 2011-2025
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2, or (at your option)
+#   any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#***********************************************************************/
 
 files=`find $1 -name "*.c" -o -name "*.h" -o -name "*.cpp" | sort`
 
@@ -29,7 +42,7 @@ done
 
 # print results
 echo "# Check for header files including fc_config.h"
-if [ "x$RES_ERR3" = "x" ]; then
+if [ "${RES_ERR3}" = "" ]; then
   echo "(no match)"
 else
   for II in $RES_ERR3; do
@@ -39,7 +52,7 @@ fi
 echo
 
 echo "# Check for header files including fcintl.h"
-if [ "x$RES_ERR1" = "x" ]; then
+if [ "${RES_ERR1}" = "" ]; then
   echo "(no match)"
 else
   for II in $RES_ERR1; do
@@ -49,7 +62,7 @@ fi
 echo
 
 echo "# Check if fc_config.h is include when fcintl.h is used:"
-if [ "x$RES_ERR2" = "x" ]; then
+if [ "${RES_ERR2}" = "" ]; then
   echo "(no match)"
 else
   for II in $RES_ERR2; do
