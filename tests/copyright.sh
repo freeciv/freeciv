099de8c84e80a79875b663f8288e9d2c8cfebbe6
Marko Lindqvist <cazfi74@gmail.com>
2025-04-19 05:47:50 +0300

    Filter out SDL3_rotozoom.[ch] from files to src-check
    See RM #1291
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tests/copyright.sh
+++ tests/copyright.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+#/***********************************************************************
+# Freeciv - Copyright (C) 2004-2025
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2, or (at your option)
+#   any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#***********************************************************************/
 
 files=`find $1 -name "*.c" -o -name "*.h" -o -name "*.cpp" \
        | sort \
@@ -6,12 +19,12 @@ files=`find $1 -name "*.c" -o -name "*.h" -o -name "*.cpp" \
        | fgrep -v "_gen." \
        | grep -v "fc_config.h" \
        | grep -v dependencies \
+       | grep -v SDL3_rotozoom \
        | grep -v utility/md5\.. `
 
 echo "# No Freeciv Copyright:"
 echo "# Excludes: generated files, various 3rd party sources"
 for file in $files; do
-#    echo "testing $file..."
     grep "Freeciv - Copyright" $file >/dev/null || echo $file
 done
 echo
