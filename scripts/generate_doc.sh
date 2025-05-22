892f82a0bb780fb0ec8fe1f0df4a5c349bfd0a58
Marko Lindqvist <cazfi74@gmail.com>
2025-05-22 19:12:32 +0300

    Doxygen: Include generated sources from builddir
    See RM #1423
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- scripts/generate_doc.sh
+++ scripts/generate_doc.sh
@@ -11,18 +11,23 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 
-if test "$1" = "" ; then
-  echo "Needs freeciv source directory as a parameter!" >&2
+if test "$1" = "" || test "$2" = "" ; then
+  echo "Needs freeciv source and build directories as parameters!" >&2
   exit 1
 fi
 
 if test "$1" = "-h" || test "$1" = "--help" ; then
-  echo "Usage: $0 <freeciv source directory>"
+  echo "Usage: $0 <freeciv source directory> <freeciv build directory>"
   exit
 fi
 
 if ! test -x "$1/fc_version" ; then
   echo "\"$1\" doesn't look like a freeciv source directory (lacking fc_version)" >&2
+  exit 1
+fi
+
+if ! test -f "$2/version_gen.h" ; then
+  echo "\"$2\" is not a build directory with generated files (lacking version_gen.h)" >&2
   exit 1
 fi
 
@@ -33,4 +38,5 @@ fi
 
 VERSION_SCRIPT_SILENT=yes . "$1/fc_version"
 
-doxy_srcdir="$1/" doxy_version="-${MAIN_VERSION}" doxygen "$1/doc/freeciv.doxygen"
+doxy_srcdir="$1/" doxy_builddir="$2" doxy_version="-${MAIN_VERSION}" \
+           doxygen "$1/doc/freeciv.doxygen"
