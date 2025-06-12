5f633af08e4078642253f625915a138f6c659163
Marko Lindqvist <cazfi74@gmail.com>
2025-06-12 03:16:03 +0300

    meson-winbuild.sh: Set audio driver explicitly
    See RM #1513
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- platforms/windows/installer_cross/meson-winbuild.sh
+++ platforms/windows/installer_cross/meson-winbuild.sh
@@ -84,6 +84,7 @@ fi
 
 QTPARAMS=""
 
+AUDIO=sdl2
 TOOLS=manual,ruleup
 case "${GUI}" in
   gtk3.22) FCMP="gtk3" ;;
@@ -164,6 +165,7 @@ if ! meson setup \
      -Dsyslua=false \
      -Dmwand="${MWAND}" \
      -Dreadline=false \
+     -Daudio="${AUDIO}" \
      -Dserver="${SERVER}" \
      -Dtools="${TOOLS}" \
      -Ddefault_library=static \
