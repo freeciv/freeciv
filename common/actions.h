b3fcc76fbad861178bb6675a4347ad09b470b8c5
Marko Lindqvist <cazfi74@gmail.com>
2025-07-07 02:32:46 +0300

    actions.h: Add comment that enum action_actor_kind used in network protocol
    See RM #1582
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/actions.h
+++ common/actions.h
@@ -24,6 +24,8 @@
 extern "C" {
 #endif /* __cplusplus */
 
+
+/* Used in the network protocol. */
 #define SPECENUM_NAME action_actor_kind
 #define SPECENUM_VALUE0 AAK_UNIT
 #define SPECENUM_VALUE0NAME N_("a unit")
