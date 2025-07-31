9330477a15e0b4f177bb91c1c85a2fa9d6597496
Marko Lindqvist <cazfi74@gmail.com>
2025-07-31 07:20:44 +0300

    Msys2: Parametrize create-freeciv-sdl-nsi.sh with the gui
    See RM #1640
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- platforms/windows/installer_msys2/create-freeciv-sdl2-nsi.sh
+++ platforms/windows/installer_msys2/create-freeciv-sdl2-nsi.sh
@@ -2,4 +2,4 @@
 
 # ./create-freeciv-sdl2-nsi.sh <freeciv files dir> <output dir> <version> <win32|win64|win>
 
-./create-freeciv-sdl-nsi.sh "$1" "$2" "$3" "$4"
+./create-freeciv-sdl-nsi.sh "$1" "$2" "$3" "$4" "sdl2" "SDL2"
