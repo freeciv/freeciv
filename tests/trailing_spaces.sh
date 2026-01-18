4138c76556395d114182b95efa51b5b2ae916473
Marko Lindqvist <cazfi74@gmail.com>
2026-01-18 09:03:10 +0200

    trailing_spaces.sh: Check python files (.py)
    See RM #1891
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tests/trailing_spaces.sh
+++ tests/trailing_spaces.sh
@@ -4,6 +4,7 @@ files=$(find $1 -name "*.ruleset" \
              -o -name "*.tilespec" \
              -o -name "*.spec" \
              -o -name "*.lua" \
+             -o -name "*.py" \
              -o -name "*.modpack" \
              -o -name "*.serv" \
              -o -name "*.txt" \
