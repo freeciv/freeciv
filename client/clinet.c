a9aff295e34fc34a3f3bf1c7fbdf7b6c1d76e42f
Marko Lindqvist <cazfi74@gmail.com>
2026-03-03 06:54:31 +0200

    Protect against DOS attack with recursive jumbo packets
    Receiving side of the old network code allowed jumbo
    packets inside jumbo packets recursively, though such
    recursion is never needed. Handling such recursion
    could easily lead to a crash of the receiver.
    Malicious sender could craft such recursive jumbo packets
    to crash servers or clients.
    The fix simply disallows recursive jumbo packets.
    
    Reported by Louis Moureaux
    
    See RM #1955
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/clinet.c
+++ client/clinet.c
@@ -419,7 +419,7 @@ void input_from_server(int fd)
     agents_freeze_hint();
     while (client.conn.used) {
       enum packet_type type;
-      void *packet = get_packet_from_connection(&client.conn, &type);
+      void *packet = get_packet_from_connection(&client.conn, &type, FALSE);
 
       if (NULL != packet) {
         client_packet_input(packet, type);
@@ -460,7 +460,7 @@ void input_from_server_till_request_got_processed(int fd,
       enum packet_type type;
 
       while (TRUE) {
-        void *packet = get_packet_from_connection(&client.conn, &type);
+        void *packet = get_packet_from_connection(&client.conn, &type, FALSE);
 
         if (NULL == packet) {
           break;
