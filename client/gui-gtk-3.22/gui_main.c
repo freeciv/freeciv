0a106935d7b73fa0dae56e25b867e5467a1e8bda
Marko Lindqvist <cazfi74@gmail.com>
2026-08-22 03:57:27 +0300

    gtk: Drop unused bottom_hpaned
    See RM #2157
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- client/gui-gtk-3.22/gui_main.c
+++ client/gui-gtk-3.22/gui_main.c
@@ -129,7 +129,6 @@ GtkWidget *toplevel_tabs;
 GtkWidget *top_vbox;
 GtkWidget *top_notebook, *bottom_notebook, *right_notebook;
 GtkWidget *map_widget;
-static GtkWidget *bottom_hpaned;
 
 PangoFontDescription *city_names_style = NULL;
 PangoFontDescription *city_productions_style = NULL;
@@ -1534,7 +1533,6 @@ static void setup_widgets(void)
   /* *** The message window -- this is a detachable widget *** */
 
   if (GUI_GTK_OPTION(message_chat_location) == GUI_GTK_MSGCHAT_MERGED) {
-    bottom_hpaned = paned;
     right_notebook = bottom_notebook = top_notebook;
   } else {
     GtkWidget *hpaned;
@@ -1558,7 +1556,6 @@ static void setup_widgets(void)
     }
     gtk_container_add(GTK_CONTAINER(vgrid), hpaned);
     g_object_set(hpaned, "margin", 4, NULL);
-    bottom_hpaned = hpaned;
 
     bottom_notebook = gtk_notebook_new();
     gtk_notebook_set_tab_pos(GTK_NOTEBOOK(bottom_notebook), GTK_POS_TOP);
