de58e4a9002fe650dca1bae3d170c1217c6a362f
Marko Lindqvist <cazfi74@gmail.com>
2026-08-25 03:12:34 +0300

    savegame: Fix Heap Buffer Overflow in worklist_load()
    Reported by Tristan
    
    See RM #2161
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/savegame2.c
+++ server/savegame2.c
@@ -752,6 +752,14 @@ static void worklist_load(struct section_file *file, int wlist_max_length,
   worklist_init(pwl);
   pwl->length = secfile_lookup_int_default(file, 0,
                                            "%s.wl_length", path_str);
+  if (pwl->length > MAX_LEN_WORKLIST) {
+    log_sg("worklist length %d, while MAX_LEN_WORKLIST %d.",
+           pwl->length, MAX_LEN_WORKLIST);
+    pwl->length = MAX_LEN_WORKLIST;
+  } else if (pwl->length > wlist_max_length) {
+    log_sg("worklist length %d, while player's max worklist length %d.",
+           pwl->length, wlist_max_length);
+  }
 
   for (i = 0; i < pwl->length; i++) {
     kind = secfile_lookup_str(file, "%s.wl_kind%d", path_str, i);
